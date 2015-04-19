26220b6ff6d85425d2ed588e6e61bad41316465b
Eric Soderberg <eric.soderberg@hp.com>
2015-04-19 07:40:48 -0700

    Added DocumentDoc

--- docs/src/documentation/Documentation.js
+++ docs/src/documentation/Documentation.js
@@ -15,6 +15,7 @@ var Tutorial = require('./Tutorial');
 var GetStarted = require('./GetStarted');
 var Architecture = require('./Architecture');
 var DialogDoc = require('./components/DialogDoc');
+var DocumentDoc = require('./components/DocumentDoc');
 var DonutDoc = require('./components/DonutDoc');
 var FooterDoc = require('./components/FooterDoc');
 var FormDoc = require('./components/FormDoc');
@@ -52,7 +53,7 @@ var CONTENTS = [
     contents: [
       {route: 'doc_chart', label: 'Chart', component: TBD},
       {route: 'doc_dialog', label: 'Dialog', component: DialogDoc},
-      {route: 'doc_document', label: 'Document', component: TBD},
+      {route: 'doc_document', label: 'Document', component: DocumentDoc},
       {route: 'doc_donut', label: 'Donut', component: DonutDoc},
       {route: 'doc_footer', label: 'Footer', component: FooterDoc},
       {route: 'doc_form', label: 'Form', component: FormDoc},
