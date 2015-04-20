01e412193de07a021e0d6ab5451807aa897d6153
Eric Soderberg <eric.soderberg@hp.com>
2015-04-20 13:38:57 -0700

    Convert docs to use HTML5 history

--- docs/src/index.js
+++ docs/src/index.js
@@ -11,10 +11,107 @@ if (! Modernizr.flexbox ||
     'If you are using the latest Internet Explorer, you will need to turn off Compatibility Mode.');
 }
 
+var rootPath = '/docs/';
+var theme = 'generic';
+
 if (__THEME__.hpe) {
   require("!style!css!sass!index-hpe.scss");
+  rootPath = '/docs/hpe/';
+  theme = 'hpe';
 } else {
   require("!style!css!sass!index.scss");
 }
 
-require('./docs');
+if ("/" === window.location.pathname) {
+  rootPath = "/"; // webpack-dev-server
+}
+
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+var Link = Router.Link;
+var App = require('grommet/components/App');
+var Header = require('grommet/components/Header');
+var Title = require('grommet/components/Title');
+var Menu = require('grommet/components/Menu');
+var Label = require('grommet/components/Label');
+var Footer = require('grommet/components/Footer');
+var Home = require('./Home');
+var DesignIcon = require('./img/Design');
+var DevelopIcon = require('./img/Develop');
+var StyleGuide = require('./style_guide/StyleGuide');
+var Documentation = require('./documentation/Documentation');
+var Downloads = require('./downloads/Downloads');
+var RequestAccess = require('./RequestAccess');
+
+var Docs = React.createClass({
+
+  render: function() {
+
+    return (
+      <App className="docs">
+        <Header primary={true} className="docs__header">
+          <Title>
+            <Link to="docs">
+              <img src="img/grommet.svg" title="Grommet" />
+              Grommet
+            </Link>
+          </Title>
+          <Menu direction="left">
+            <Link key="style-guide" to="style guide">
+              <Label text="Style Guide" icon={<DesignIcon />} />
+            </Link>
+            <Link key="documentation" to="documentation">
+              <Label text="Documentation" icon={<DevelopIcon />} />
+            </Link>
+            <Link key="downloads" to="downloads">
+              <Label text="Downloads" />
+            </Link>
+            <Link key="request-access" to="request_access"
+              className="button primary call-to-action docs__header-request-access">
+              <Label text="Request access" />
+            </Link>
+          </Menu>
+        </Header>
+        <RouteHandler />
+        <Footer className="docs__footer" primary={true} centered={true}>
+          <img src="img/hpesm_sec_grn_pos_rgb.svg" alt="Hewlett Packard Enterprise logo" />
+          <h3>Build your ideas!</h3>
+          <div>
+            This work is licensed under the <a href="http://creativecommons.org/licenses/by/4.0/legalcode">Creative Commons Attribution 4.0 International License</a>.
+            <div>
+              <Menu label="Theme" direction="up" small={true}>
+                <a href="/docs/" className={theme === 'generic' ? 'active' : ''}>Grommet</a>
+                <a href="/docs/hpe/" className={theme === 'hpe' ? 'active' : ''}>HPE</a>
+              </Menu>
+            </div>
+          </div>
+        </Footer>
+      </App>
+    );
+  }
+
+});
+
+var routes = (
+  <Route name="docs" path={rootPath} handler={Docs}>
+    <Route name="home" path={rootPath} handler={Home}>
+      <Route name="request_access" handler={RequestAccess} />
+    </Route>
+    {StyleGuide.routes()}
+    {Documentation.routes()}
+    <Route name="downloads" handler={Downloads} />
+  </Route>
+);
+
+var router = Router.create({routes: routes, location: Router.HistoryLocation});
+
+router.run(function (Handler) {
+  var factory = React.createFactory(Handler);
+  var element = document.getElementById('content');
+  React.render(factory(), element);
+  document.querySelectorAll('.docs')[0].scrollTop = 0;
+});
+
+document.body.classList.remove('loading');
--- examples/server/docs.js
+++ examples/server/docs.js
@@ -2,13 +2,32 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+// Convert static resources defined by relative URLs when using HTML5 pushState
+function translateStatics(req, res, next) {
+  if (req.url.match(/.+\/img\//)) { // img
+    res.redirect(301, req.url.replace(/.*\/(img\/.*)$/, "/docs/hpe/$1"));
+  } else if (req.url.match(/\/img\//)) { // img
+    next();
+  } else if (req.url.match(/.+\/font\//)) { // font
+    res.redirect(301, req.url.replace(/.*\/(font\/.*)$/, "/docs/hpe/$1"));
+  } else if (req.url.match(/\/font\//)) { // font
+    next();
+  } else if (req.url.match(/.+\/.*\.[^\/]*$/)) { // file
+    res.redirect(301, req.url.replace(/.*\/([^\/]*)$/, "/docs/hpe/$1"));
+  } else {
+    next();
+  }
+}
+
+router.use('/hpe', translateStatics);
 router.use('/hpe', express.static('../../docs/dist/hpe'));
-router.get('/hpe/|/styleguide|/documentation|/downloads', function (req, res) {
+router.get('/hpe/*', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../../docs/dist/hpe/index.html'));
 });
 
+router.use('/', translateStatics);
 router.use('/', express.static('../../docs/dist'));
-router.get('/|/styleguide|/documentation|/downloads', function (req, res) {
+router.get('/*', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../../docs/dist/index.html'));
 });
 
