594b120bf941335ca414ce7059fc834856e7ac05
Eric Soderberg <eric.soderberg@hp.com>
2015-04-21 14:20:27 -0700

    fix lint issues in Chart

--- src/js/components/Chart.js
+++ src/js/components/Chart.js
@@ -79,7 +79,7 @@ var Chart = React.createClass({
     for (var i=0; i<=BASE_WIDTH; i = i + step) {
       grid.push(<path key={i} fill="none" d={"M" + i + ",0L" + i + "," + BASE_HEIGHT} />);
     }
-    var step = BASE_HEIGHT / 5;
+    step = BASE_HEIGHT / 5;
     for (i=BASE_HEIGHT; i>=0; i = i - step) {
       grid.push(<path key={100 + i} fill="none" d={"M0," + i + "L" + BASE_WIDTH + "," + i} />);
     }
@@ -100,11 +100,11 @@ var Chart = React.createClass({
         <path fill="none" className={"color-index-" + item.colorIndex} d={commands} />
       );
 
-      if (this.props.key) {
+      /*if (this.props.key) {
 
         var keyItemClasses = ["chart__key-item"];
 
-        keys[colorIndex] = (
+        keys[index] = (
           <li key={item.className} className={keyItemClasses.join(' ')}>
             <svg className={"chart__key-item-swatch"} viewBox="0 0 10 10">
               <path className={item.className} d="M 5 0 l 0 10" />
@@ -112,7 +112,7 @@ var Chart = React.createClass({
             <span className="chart__key-item-label">{item.label}</span>
           </li>
         );
-      }
+      }*/
     }, this);
 
     return (
