f7c0c613d0488fc46450e0bcd5e2fb0398aba27e
Alan Souza <alansouzati@gmail.com>
2015-05-04 10:58:34 -0700

    Correct regex for windows.

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -50,13 +50,13 @@ var opts = {
     module: {
     	loaders: [
 	      {
-	        test: /style_guide\/[^\/]*\.htm$/,
-	        loader: 'jsx-loader!imports?React=react,Router=react-router,Link=>Router.Link!html-jsx-loader?group=true'
-	      },
-	      {
-	        test: /documentation\/.*\.htm$|downloads\/.*\.htm$|style_guide\/.*\/.*\.htm$/,
-	        loader: 'jsx-loader!imports?React=react,Router=react-router,Link=>Router.Link!html-jsx-loader'
-	      }
+          test: /style_guide(\/|\\)[^\/]*\.htm$/,
+          loader: 'jsx-loader!imports?React=react,Router=react-router,Link=>Router.Link!html-jsx-loader?group=true'
+        },
+        {
+          test: /documentation(\/|\\).*\.htm$|downloads(\/|\\).*\.htm$|style_guide(\/|\\).*\/.*\.htm$/,
+          loader: 'jsx-loader!imports?React=react,Router=react-router,Link=>Router.Link!html-jsx-loader'
+        }
     	]
     }	
   },
