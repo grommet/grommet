b03172941866ae8f5641a7ae71020fcde27d4f84
Eric Soderberg <eric.soderberg@hp.com>
2015-05-27 11:10:18 -0700

    Improved coverage of Chart-test.

--- test/components/Chart-test.js
+++ test/components/Chart-test.js
@@ -7,19 +7,45 @@ var expect = require('expect');
 
 var testSeries = [
   {
-    label: 'first',
-    values: [[5,2], [4,3], [3,3], [2,2], [1,1]],
+    label: 'ascending',
+    values: [[3,3], [2,2], [1,1]],
     colorIndex: "graph-1"
   },
   {
-    label: 'second',
-    values: [[5,3], [4,2], [3,0], [2,0], [1,0]],
+    label: 'descending',
+    values: [[3,1], [2,2], [1,3]],
     colorIndex: "graph-2"
+  },
+  {
+    label: 'peak',
+    values: [[3,1], [2,2], [1,1]],
+    colorIndex: "graph-3"
   }
 ];
 
+var testXAxis = [
+  {value: 1, label: 'one'},
+  {value: 2, label: 'two'},
+  {value: 3, label: 'three'}
+];
+
 describe('Grommet Chart', function() {
-  it('loads a basic line Chart component', function() {
+  it('loads an empty Chart', function() {
+    var Component = GrommetTestUtils.getComponent(__path__, undefined, {
+      series: []
+    });
+
+    GrommetTestUtils.componentShouldExist(Component, 'chart');
+
+    var React = require('react/addons');
+    var TestUtils = React.addons.TestUtils;
+    var linePaths = TestUtils.scryRenderedDOMComponentsWithClass(Component,
+                      'chart__values-line');
+
+    expect(linePaths.length).toBe(0);
+  });
+
+  it('loads a line Chart', function() {
     var Component = GrommetTestUtils.getComponent(__path__, undefined, {
       series: testSeries
     });
@@ -31,13 +57,16 @@ describe('Grommet Chart', function() {
     var linePaths = TestUtils.scryRenderedDOMComponentsWithClass(Component,
                       'chart__values-line');
 
-    expect(linePaths.length).toBe(2);
+    expect(linePaths.length).toBe(3);
   });
 
-  it('loads a basic line Chart component with threshold', function() {
+  it('loads a line Chart with threshold, min, max, small', function() {
     var Component = GrommetTestUtils.getComponent(__path__, undefined, {
       series: testSeries,
-      threshold: 1
+      threshold: 1,
+      min: 0,
+      max: 10,
+      small: true
     });
 
     GrommetTestUtils.componentShouldExist(Component, 'chart');
@@ -50,11 +79,13 @@ describe('Grommet Chart', function() {
     expect(linePaths.length).toBe(1);
   });
 
-  it('loads a basic line Chart component with legend', function() {
+  it('loads a line Chart with legend, xAxis, large, important', function() {
     var Component = GrommetTestUtils.getComponent(__path__, undefined, {
       series: testSeries,
       legend: true,
-      xAxis: ['one', 'two']
+      xAxis: testXAxis,
+      large: true,
+      important: 1
     });
 
     GrommetTestUtils.componentShouldExist(Component, 'chart');
@@ -65,10 +96,10 @@ describe('Grommet Chart', function() {
     var xAxis = TestUtils.scryRenderedDOMComponentsWithClass(Component,
                       'chart__xaxis-index');
 
-    expect(xAxis.length).toBe(5);
+    expect(xAxis.length).toBe(3);
   });
 
-  it('loads a basic area Chart component', function() {
+  it('loads an area Chart', function() {
     var Component = GrommetTestUtils.getComponent(__path__, undefined, {
       series: testSeries,
       type: 'area'
@@ -81,10 +112,27 @@ describe('Grommet Chart', function() {
     var areaPaths = TestUtils.scryRenderedDOMComponentsWithClass(Component,
                       'chart__values-area');
 
-    expect(areaPaths.length).toBe(2);
+    expect(areaPaths.length).toBe(3);
   });
 
-  it('loads a basic bar Chart component', function() {
+  it('loads a smooth area Chart', function() {
+    var Component = GrommetTestUtils.getComponent(__path__, undefined, {
+      series: testSeries,
+      type: 'area',
+      smooth: true
+    });
+
+    GrommetTestUtils.componentShouldExist(Component, 'chart');
+
+    var React = require('react/addons');
+    var TestUtils = React.addons.TestUtils;
+    var areaPaths = TestUtils.scryRenderedDOMComponentsWithClass(Component,
+                      'chart__values-area');
+
+    expect(areaPaths.length).toBe(3);
+  });
+
+  it('loads a bar Chart', function() {
     var Component = GrommetTestUtils.getComponent(__path__, undefined, {
       series: testSeries,
       type: 'bar'
@@ -97,6 +145,6 @@ describe('Grommet Chart', function() {
     var areaPaths = TestUtils.scryRenderedDOMComponentsWithClass(Component,
                       'chart__values-bar');
 
-    expect(areaPaths.length).toBe(10);
+    expect(areaPaths.length).toBe(9);
   });
 });
