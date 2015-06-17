3173270ac0eb9913e0720c1dcbc7893b3b6e1e32
Eric Soderberg <eric.soderberg@hp.com>
2015-06-17 09:24:38 -0700

    Updated CalendarDoc to align options documentation.

--- docs/src/documentation/components/CalendarDoc.js
+++ docs/src/documentation/components/CalendarDoc.js
@@ -29,21 +29,14 @@ var CalendarDoc = React.createClass({
         <section>
           <h2>Options</h2>
           <dl>
-            <dt><code>defaultValue  {"{value: , label: }|{string}"}</code></dt>
-            <dd>What text to start with in the input.</dd>
             <dt><code>id            {"{string}"}</code></dt>
             <dd>The id attribute of the input.</dd>
             <dt><code>name          {"{string}"}</code></dt>
             <dd>The name attribute of the input.</dd>
-            <dt><code>onChange      {"function ({value: , label: }|{string}) {...}"}</code></dt>
-            <dd>Function that will be called when the user types some text into the input.</dd>
-            <dt><code>onSearch      {"function ({text}) {...}"}</code></dt>
-            <dd>Function that will be called when the user types some text into the search.</dd>
-            <dt><code>placeHolder   {"{string}"}</code></dt>
-            <dd>Placeholder text to use when the input is empty.</dd>
-            <dt><code>suggestions   {"[{value: , label: }|{string}, ...]"}</code></dt>
-            <dd>Suggestions</dd>
-            <dt><code>value         {"{value: , label: }|{string}"}</code></dt>
+            <dt><code>onChange      {"function ({string}) {...}"}</code></dt>
+            <dd>Function that will be called when the user types some text into
+              the input or selects a date from the drop down.</dd>
+            <dt><code>value         {"{string}"}</code></dt>
             <dd>What text to put in the input.</dd>
           </dl>
         </section>
