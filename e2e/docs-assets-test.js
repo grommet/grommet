b0f6e22ce636dbe9e5b8d686ac5d31786c1d91bd
Alan Souza <alansouzati@gmail.com>
2015-06-25 18:23:13 -0700

    Added multiremote option to sauce labs.

--- e2e/docs-assets-test.js
+++ e2e/docs-assets-test.js
@@ -3,30 +3,49 @@ var should = require('should');
 
 var options = {
   desiredCapabilities: {
-    logLevel: 'silent',
     browserName: 'phantomjs'
   }
 };
 
+var client;
 if (process.env.TRAVIS) {
-  options.host = 'ondemand.saucelabs.com';
-  options.port = 80;
-  options.user = process.env.SAUCE_USERNAME;
-  options.key = process.env.SAUCE_ACCESS_KEY;
+  options = {
+    host: 'ondemand.saucelabs.com',
+    port: 80,
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    browserA: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        version: '27',
+        platform: ['Windows 7', 'Windows 8']
+      }
+    },
+    browserB: {
+      desiredCapabilities: {
+        browserName: 'internet explorer',
+        version: '9',
+        platform: 'Windows 7'
+      }
+    }
+
+  };
+
+  client = require('webdriverio').multiremote(options).init();
+} else {
+  client = require('webdriverio').remote(options).init();
 }
 
-var client = require('webdriverio').remote(options).init();
+describe('Docs website e2e', function() {
+  this.timeout(1000000);
 
-describe('Docs website e2e', function () {
-  this.timeout(5000);
-
-  before(function (done) {
+  before(function(done) {
     client.url('http://localhost:8000/docs/', done);
   });
 
-  it('loads the title for the docs home page', function (done) {
+  it('loads the title for the docs home page', function(done) {
     client
-      .getTitle(function (err, title) {
+      .getTitle(function(err, title) {
         if (err) {
           should.fail('Expected the title to be returned.');
         }
@@ -35,7 +54,7 @@ describe('Docs website e2e', function () {
       });
   });
 
-  after(function (done) {
+  after(function(done) {
     client.end(done);
   });
 });
