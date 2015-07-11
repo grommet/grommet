5d02e1839c039ce0e9ab08c05b0edda59e4b692b
Sam Ogami <sam.ogami@hp.com>
2015-07-11 08:53:06 -0700

    Update ErrorStatus.js
    Fix indentation

--- src/js/components/icons/status/ErrorStatus.js
+++ src/js/components/icons/status/ErrorStatus.js
@@ -14,7 +14,7 @@ var ErrorStatus = React.createClass({
     }
     return (
       <svg className={className} viewBox="0 0 24 24" aria-labelledby="title-icon-error" role="img" version="1.1">
-	<title id="title-icon-error">{this.getGrommetIntlMessage('Error')}</title>
+        <title id="title-icon-error">{this.getGrommetIntlMessage('Error')}</title>
         <g className={"status-icon__base"} stroke="none">
           <path role="presentation" d="M12,0 L24,12 L12,24 L0,12 Z"></path>
         </g>
