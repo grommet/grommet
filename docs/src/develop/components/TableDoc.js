ed15be9d94060093131db03b6cb3440b6ab42511
Kevin Woldt <kevin.woldt@hp.com>
2015-07-23 17:26:22 +0200

    issue #99: fixed table doc to mention all options

--- docs/src/develop/components/TableDoc.js
+++ docs/src/develop/components/TableDoc.js
@@ -35,9 +35,15 @@ var TableDoc = React.createClass({
         <section>
           <h2>Options</h2>
           <dl>
-            <dt><code>defaultSelection</code></dt>
-            <dt><code>onSelection</code></dt>
-            <dt><code>selectable</code></dt>
+            <dt><code>onMore        {"function () {...}"}</code></dt>
+            <dd>Function that will be called when more data is needed.</dd>
+            <dt><code>onSelect      {"function (selection) {...}"}</code></dt>
+            <dd>Function that will be called when the user selects a row.</dd>
+            <dt><code>scrollable    true|false</code></dt>
+            <dt><code>selectable    true|false</code></dt>
+            <dd>Whether rows are selectable.</dd>
+            <dt><code>selection     number</code></dt>
+            <dd>The currently selected item.</dd>
           </dl>
         </section>
 
@@ -62,7 +68,7 @@ var TableDoc = React.createClass({
 
           <h3>Selectable</h3>
           <div className="example">
-            <Table selectable={true} defaultSelection={0}>
+            <Table selectable={true} selection={0}>
               <thead>
                 <tr>
                   <th>header 1</th>
