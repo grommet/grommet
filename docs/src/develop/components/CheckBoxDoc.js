e132a158280e3d742fe5355278d9be74afe0251f
Eric Soderberg <eric.soderberg@hp.com>
2015-08-25 06:23:06 -0700

    Added disabled= to CheckBox.

--- docs/src/develop/components/CheckBoxDoc.js
+++ docs/src/develop/components/CheckBoxDoc.js
@@ -6,6 +6,14 @@ var CheckBox = require('grommet/components/CheckBox');
 
 var CheckBoxDoc = React.createClass({
 
+  getInitialState: function () {
+    return {checked: false};
+  },
+
+  _onChange: function () {
+    this.setState({checked: ! this.state.checked});
+  },
+
   render: function() {
     var inline = [
       "<CheckBox id=\"item2\" label=\"Item 1\">"
@@ -48,15 +56,27 @@ var CheckBoxDoc = React.createClass({
 
           <h3>Basic</h3>
           <div className="example">
-            <CheckBox id="item2" label="Item 2" />
+            <CheckBox id="item2" name="item2" label="Item 2" checked={this.state.checked} onChange={this._onChange} />
           </div>
-          <pre><code className="html">{"<CheckBox id=\"\{item2\}\" label=\"Item 2\" />"}</code></pre>
+          <pre><code className="html">{"<CheckBox id=\"item2\" name=\"item2\" label=\"Item 2\" />"}</code></pre>
 
           <h3>Toggle</h3>
           <div className="example">
-            <CheckBox id="item3" label="Item 3" toggle={true} />
+            <CheckBox id="item3" name="item3" label="Item 3" toggle={true} />
           </div>
-          <pre><code className="html">{"<CheckBox id=\"\{item3\}\" label=\"Item 3\" toggle={true} />"}</code></pre>
+          <pre><code className="html">{"<CheckBox id=\"item3\" name=\"item3\" label=\"Item 3\" toggle={true} />"}</code></pre>
+
+          <h3>Disabled</h3>
+          <div className="example">
+            <CheckBox id="item4" name="item4" label="Item 4" disabled={true} checked={this.state.checked} />
+          </div>
+          <pre><code className="html">{"<CheckBox id=\"item4\" name=\"item4\" label=\"Item 4\" disabled=\{true\} />"}</code></pre>
+
+          <h3>Disabled Toggle</h3>
+          <div className="example">
+            <CheckBox id="item5" name="item5" label="Item 5" toggle={true} disabled={true} checked={this.state.checked} />
+          </div>
+          <pre><code className="html">{"<CheckBox id=\"item5\" name=\"item5\" label=\"Item 5\" toggle={true} disabled={true} />"}</code></pre>
 
         </section>
 
