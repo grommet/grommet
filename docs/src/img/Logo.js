d0695a44426e48dd8469fda3b50cbb4463d3295c
Eric Soderberg <eric.soderberg@hp.com>
2015-09-04 17:29:06 -0700

    Minor responsive styling improvement to Title.

--- docs/src/img/Logo.js
+++ docs/src/img/Logo.js
@@ -10,7 +10,7 @@ var Logo = React.createClass({
       className += ' ' + this.props.className;
     }
     return (
-      <svg className={className} viewBox="0 0 30 27" version="1.1">
+      <svg className={className} height="27" viewBox="0 0 30 27" version="1.1">
         <g strokeWidth="4" fill="none" fillRule="evenodd">
           <rect stroke="#4A4A4A" x="9" y="10" width="19" height="15"></rect>
           <g className="doc-brand">
