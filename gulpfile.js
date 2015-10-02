9d5a161b7fd07fe1c43cee0dfdb1843c31e02c6b
Alan Souza <alansouzati@gmail.com>
2015-10-01 21:13:44 -0700

    Adding proper configuration for the new loader.

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,9 @@ var opts = {
       library: 'Grommet'
     },
     resolve: {
+      alias: {
+        'grommet': path.resolve(__dirname, 'src/js')
+      },
       modulesDirectories: ['node_modules', 'src/js', 'src/scss']
     },
     externals: {
