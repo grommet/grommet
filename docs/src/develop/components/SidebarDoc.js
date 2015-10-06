644d8711b50f9aaec37d2d633435f110565893d8
Eric Soderberg <eric.soderberg@hp.com>
2015-10-06 14:18:13 -0700

    Use Box for Sidebar. Deprecated primary= for Sidebar in favor of colorIndex= for Box. Added -light-colors for light color backgrounds.

--- docs/src/develop/components/SidebarDoc.js
+++ docs/src/develop/components/SidebarDoc.js
@@ -1,6 +1,11 @@
 var React = require('react');
+var Link = require('react-router').Link;
 var DocsArticle = require('../../DocsArticle');
 var Sidebar = require('grommet/components/Sidebar');
+var Header = require('grommet/components/Header');
+var Title = require('grommet/components/Title');
+var Menu = require('grommet/components/Menu');
+var Anchor = require('grommet/components/Anchor');
 
 var inline =
       "<Sidebar>\n" +
@@ -22,24 +27,31 @@ var SidebarDoc = React.createClass({
             <dd>Whether any contained Header and Footer should be fixed to
               the top and bottom.</dd>
             <dt><code>primary    true|false</code></dt>
-            <dd>Whether this is the primary application sidebar or not.</dd>
+            <dd>Whether this is the primary application sidebar or not.
+              Deprecated, use <Link to="develop_box">Box colorIndex</Link>.</dd>
             <dt><code>size       small|medium|large</code></dt>
             <dd>The size of the Sidebar. Defaults to <code>medium</code>.</dd>
             <dt><code>small      true|false</code></dt>
             <dd>Smaller sized version. Deprecated, use <code>size</code>.</dd>
           </dl>
+          <p>Options for <Link to="develop_box">Box</Link> area also available.</p>
         </section>
 
         <section>
           <h2>Example</h2>
 
           <div className="example">
-            <Sidebar primary={true}>
-              <p>Sample Content One</p>
+            <Sidebar colorIndex="light-1">
+              <Header pad="medium">
+                <Title>Title</Title>
+              </Header>
+              <Menu pad="medium">
+                <Anchor>Sample Content One</Anchor>
+              </Menu>
             </Sidebar>
           </div>
           <pre><code className="html hljs xml">
-            {"<Sidebar primary={true}>\n  <p>\n    Sample Content One\n  </p>\n</Sidebar>"}
+            {"<Sidebar colorIndex=\"light-1\">\n  <p>\n    Sample Content One\n  </p>\n</Sidebar>"}
           </code></pre>
 
         </section>
--- examples/medium-app/src/js/MediumMain.js
+++ examples/medium-app/src/js/MediumMain.js
@@ -24,7 +24,7 @@ var MediumMain = React.createClass({
 
   render: function() {
     return (
-      <Sidebar primary={this.props.primary} fixed={true}>
+      <Sidebar colorIndex="light-1" fixed={true}>
         <Header large={true} justify="between" pad={{horizontal: 'medium'}}>
           <Title>
             <Logo />
