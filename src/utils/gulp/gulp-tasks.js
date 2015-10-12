56af07dc90af187e92b31dc04a04c967313aed11
Alan Souza <alansouzati@gmail.com>
2015-10-12 11:34:53 -0700

    Removed path.isAbsolute to be node 0.10 compliant

--- src/utils/gulp/gulp-tasks.js
+++ src/utils/gulp/gulp-tasks.js
@@ -7,6 +7,7 @@ var path = require('path');
 var fs = require('fs');
 var loader = require('grommet-icon-loader');
 var mkdirp = require('mkdirp');
+var pathIsAbsolute = require('path-is-absolute');
 
 String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
@@ -106,7 +107,7 @@ module.exports = function(gulp, opts) {
     var iconsConfig = options.icons || {};
     var iconInputFolder = iconsConfig.source;
     if (iconInputFolder) {
-      if (!path.isAbsolute(iconsConfig.source)) {
+      if (!pathIsAbsolute(iconsConfig.source)) {
         iconInputFolder = path.resolve(basePath, iconsConfig.source || 'src/img/icons');
       }
 
@@ -128,7 +129,7 @@ module.exports = function(gulp, opts) {
                   async: function() {
                     return function(err, result) {
                       var iconDestFolder = iconsConfig.destination;
-                      if (!path.isAbsolute(iconsConfig.destination)) {
+                      if (!pathIsAbsolute(iconsConfig.destination)) {
                         iconDestFolder = path.resolve(basePath, iconsConfig.destination);
                       }
 
