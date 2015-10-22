dfbb71b431cf8105a400e6251c26543436d903af
Alan Souza <alansouzati@gmail.com>
2015-10-22 15:49:39 -0700

    Removed cloneElement from App as react 14 is out.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -49,17 +49,10 @@ var App = React.createClass({
       classes.push(this.props.className);
     }
 
-    //remove this when React 0.14 is released. This is required because context props are not being propagated to children.
-    var children = React.Children.map(this.props.children, function(child) {
-      return React.isValidElement(child) ?
-        React.cloneElement(child, this.getChildContext()) :
-        child;
-    }.bind(this));
-
     return (
       <div lang={this.state.lang} className={classes.join(' ')}>
         <SkipLinks/>
-        {children}
+        {this.props.children}
       </div>
     );
   }
