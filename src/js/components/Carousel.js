d561ed358365e830f0e14623852294e64b588152
Allison Day <day.allison@gmail.com>
2015-11-08 19:58:07 -0800

    Working carousel with navigation dots
    Signed-off-by: Allison Day <day.allison@gmail.com>

--- src/js/components/Carousel.js
+++ src/js/components/Carousel.js
@@ -1,6 +1,7 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Box = require('./Box');
 
 var CLASS_ROOT = "carousel";
@@ -8,12 +9,22 @@ var CLASS_ROOT = "carousel";
 var Carousel = React.createClass({
 
   getInitialState: function () {
-    return {activeIndex: 0, priorIndex: 0, sequence: 1};
+    return {
+      activeIndex: 0,
+      priorIndex: 0,
+      sequence: 1,
+      width: 0
+    };
+  },
+
+  componentDidMount: function() {
+    this.setState({ width: ReactDOM.findDOMNode(this.refs.carousel).offsetWidth });
   },
 
   _onSelect: function (index) {
     if (index !== this.state.activeIndex) {
       console.log(index);
+      this.setState({ activeIndex: index });
     }
   },
 
@@ -26,13 +37,18 @@ var Carousel = React.createClass({
 
     var index = -1;
     const children = this.props.children.slice();
-    var slides = this.props.children.slice();
-    slides.unshift(React.cloneElement(children[children.length - 1], {
-      key: -1
-    }));
-    slides.push(React.cloneElement(children[1], {
-      key: children.length
-    }));
+    // var slides = this.props.children.slice();
+    // slides.unshift(React.cloneElement(children[children.length - 1], {
+    //   key: -1
+    // }));
+    // slides.push(React.cloneElement(children[1], {
+    //   key: children.length
+    // }));
+
+    var width = this.state.width;
+    var trackWidth = width * children.length;
+
+    var trackPosition = -(width * this.state.activeIndex);
 
     var controls = React.Children.map(this.props.children, function (child) {
       index += 1;
@@ -50,10 +66,10 @@ var Carousel = React.createClass({
     }, this);
 
     return (
-      <Box className={classes.join(' ')}>
-        <Box direction="row">
-          {slides}
-        </Box>
+      <Box ref="carousel" className={classes.join(' ')}>
+        <div className={CLASS_ROOT + "__track"} style={{ width: trackWidth, left: trackPosition }}>
+          {children}
+        </div>
         <Box className={CLASS_ROOT + "__controls"} direction="row" justify="center">
           {controls}
         </Box>
