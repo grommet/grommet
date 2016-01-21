8c8ce072b32a453a03232d96105e27c48f49e50b
Son <phuson@gmail.com>
2016-01-20 22:33:36 -0800

    Replaced width and height props with type prop for Brick component to avoid confusion.
    Signed-off-by: Son <phuson@gmail.com>

--- src/js/components/Brick.js
+++ src/js/components/Brick.js
@@ -3,12 +3,33 @@
 import React, { Component } from 'react';
 import Anchor from './Anchor';
 
-const CLASS_ROOT = "brick";
+const CLASS_ROOT = 'brick';
+const TYPE_SMALL = 'small';
+const TYPE_LARGE = 'large';
+const TYPE_WIDE = 'wide';
+const TYPE_TALL = 'tall';
 
 export default class Brick extends Component {
   render() {
+    let widthUnit = 1;
+    let heightUnit = 1;
+    switch (this.props.type) {
+      case TYPE_LARGE:
+        widthUnit = 2;
+        heightUnit = 2;
+        break;
+      case TYPE_WIDE:
+        widthUnit = 2;
+        heightUnit = 1;
+        break;
+      case TYPE_TALL:
+        widthUnit = 1;
+        heightUnit = 2;
+        break;
+    }
+
     let classes = [CLASS_ROOT];
-    classes.push(`${CLASS_ROOT}--${this.props.width}-${this.props.height}`);
+    classes.push(`${CLASS_ROOT}--${widthUnit}-${heightUnit}`);
 
     if (this.props.colorIndex) {
       classes.push(`background-color-index-${this.props.colorIndex}`);
@@ -45,12 +66,10 @@ export default class Brick extends Component {
 Brick.propTypes = {
   label: React.PropTypes.string,
   href: React.PropTypes.string,
-  width: React.PropTypes.oneOf([1, 2]),
-  height: React.PropTypes.oneOf([1, 2]),
+  type: React.PropTypes.oneOf([TYPE_SMALL, TYPE_LARGE, TYPE_WIDE, TYPE_TALL]),
   colorIndex: React.PropTypes.string
 };
 
 Brick.defaultProps = {
-  width: 1,
-  height: 1
+  type: TYPE_SMALL
 };
