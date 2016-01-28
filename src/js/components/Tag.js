047a6741dcd3c632defb75b76ad884bc7520df61
Erhan Abay <erhanabay@gmail.com>
2016-01-28 19:28:26 +0200

    Convert Tags and Tag to stateless functions

--- src/js/components/Tag.js
+++ src/js/components/Tag.js
@@ -1,35 +1,31 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 import Anchor from './Anchor';
-import pick from 'lodash/object/pick';
-import keys from 'lodash/object/keys';
+import Props from '../utils/Props';
 
-const CLASS_ROOT = "tag";
+const CLASS_ROOT = 'tag';
 
-export default class Tag extends Component {
+const Tag = props => {
+  let classes = classnames(CLASS_ROOT, props.className);
+  let anchorProps = Props.pick(props, Anchor);
 
-  render () {
-    let classes = [CLASS_ROOT];
-
-    if (this.props.className) {
-      classes.push(this.props.className);
-    }
-
-    var other = pick(this.props, keys(Anchor.propTypes));
-
-    return (
-      <div className={classes.join(' ')} onClick={this.props.onClick}>
-        {this.props.children}
-        <Anchor {...other} className={`${CLASS_ROOT}--label`}>
-          <span>{this.props.label}</span>
-        </Anchor>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes} onClick={props.onClick}>
+      {props.children}
+      <Anchor {...anchorProps} className={`${CLASS_ROOT}--label`}>
+        <span>{props.label}</span>
+      </Anchor>
+    </div>
+  );
+};
 
 Tag.propTypes = {
-  label: React.PropTypes.string,
+  label: PropTypes.string,
   ...Anchor.propTypes
 };
+
+Tag.displayName = 'Tag';
+
+export default Tag;
