318cbbaea7f17541607933f175e1d2ea7e8ba6bf
Erhan Abay <erhanabay@gmail.com>
2016-01-28 17:25:40 +0200

    Convert ListItem to a stateless function

--- src/js/components/ListItem.js
+++ src/js/components/ListItem.js
@@ -1,58 +1,54 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-import React, { Component, PropTypes } from 'react';
-import pick from 'lodash/object/pick';
-import keys from 'lodash/object/keys';
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 import Box from './Box';
+import Props from '../utils/Props';
 
-const CLASS_ROOT = "list-item";
+const CLASS_ROOT = 'list-item';
 
-export default class ListItem extends Component {
-
-  render () {
-    let classes = [CLASS_ROOT];
-    var other = pick(this.props, keys(Box.propTypes));
-    if (this.props.selected) {
-      classes.push(CLASS_ROOT + "--selected");
+const ListItem = props => {
+  let classes = classnames(
+    CLASS_ROOT,
+    props.className,
+    {
+      [`${CLASS_ROOT}--selected`]: props.selected,
+      [`${CLASS_ROOT}--selectable`]: props.onClick
     }
-    if (this.props.onClick) {
-      classes.push(CLASS_ROOT + "--selectable");
-    }
-    if (this.props.className) {
-      classes.push(this.props.className);
-    }
+  );
 
-    let children;
-    if (this.props.label) {
-      let image;
-      if (this.props.image) {
-        image = (
-          <span className={CLASS_ROOT + "__image"}>
-            {this.props.image}
-          </span>
-        );
-      }
-      children = [
-        image,
-        <span key="label" className={CLASS_ROOT + "__label"}>
-          {this.props.label}
-        </span>,
-        <span key="annotation" className={CLASS_ROOT + "__annotation"}>
-          {this.props.annotation}
+  let children;
+
+  if (props.label) {
+    let image;
+    if (props.image) {
+      image = (
+        <span className={`${CLASS_ROOT}__image`}>
+          {props.image}
         </span>
-      ];
-    } else {
-      children = this.props.children;
+      );
     }
-
-    return (
-      <Box tag="li" className={classes.join(' ')} {...other}
-        onClick={this.props.onClick}>
-        {children}
-      </Box>
-    );
+    children = [
+      image,
+      <span key="label" className={`${CLASS_ROOT}__label`}>
+        {props.label}
+      </span>,
+      <span key="annotation" className={`${CLASS_ROOT}__annotation`}>
+        {props.annotation}
+      </span>
+    ];
+  } else {
+    children = props.children;
   }
-}
+
+  let boxProps = Props.pick(props, Box);
+
+  return (
+    <Box {...boxProps} tag="li" className={classes} onClick={props.onClick}>
+      {children}
+    </Box>
+  );
+};
 
 ListItem.propTypes = {
   onClick: PropTypes.func,
@@ -70,3 +66,7 @@ ListItem.defaultProps = {
   pad: {horizontal: 'medium', vertical: 'small'},
   separator: 'bottom'
 };
+
+ListItem.displayName = 'ListItem';
+
+export default ListItem;
