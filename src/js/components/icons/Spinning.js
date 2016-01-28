d2d27752cc56a2a9034cae72c34875e9c69ff4e0
Erhan Abay <erhanabay@gmail.com>
2016-01-28 19:53:17 +0200

    Convert Spinning to a stateless function

--- src/js/components/icons/Spinning.js
+++ src/js/components/icons/Spinning.js
@@ -1,23 +1,27 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-import React, { Component } from 'react';
+import React from 'react';
+import classnames from 'classnames';
 
-var CLASS_ROOT = "icon-spinning";
+var CLASS_ROOT = 'icon-spinning';
 
-export default class Spinning extends Component {
-  render () {
-    var classes = [CLASS_ROOT];
-    if (this.props.small) {
-      classes.push(CLASS_ROOT + "--small");
+const Spinning = props => {
+  let classes = classnames(
+    CLASS_ROOT,
+    props.className,
+    {
+      [`${CLASS_ROOT}--small}`]: props.small
     }
-    if (this.props.className) {
-      classes.push(this.props.className);
-    }
-    return (
-      <svg className={classes.join(' ')} viewBox="0 0 48 48" version="1.1" >
-        <circle stroke="#ddd" strokeWidth="4" strokeDasharray="24px 8px" fill="none" cx="24" cy="24" r="20"></circle>
-        <circle stroke="#333" strokeWidth="4" strokeDasharray="24px 104px" fill="none" cx="24" cy="24" r="20"></circle>
-      </svg>
-    );
-  }
-}
+  );
+
+  return (
+    <svg className={classes} viewBox="0 0 48 48" version="1.1" >
+      <circle stroke="#ddd" strokeWidth="4" strokeDasharray="24px 8px" fill="none" cx="24" cy="24" r="20"></circle>
+      <circle stroke="#333" strokeWidth="4" strokeDasharray="24px 104px" fill="none" cx="24" cy="24" r="20"></circle>
+    </svg>
+  );
+};
+
+Spinning.displayName = 'Spinning';
+
+export default Spinning;
