6eca504b524d1706b061c982e407cc7260320904
Erhan Abay <erhanabay@gmail.com>
2016-01-28 14:35:42 +0200

    Convert Brick to a stateless function

--- src/js/components/Brick.js
+++ src/js/components/Brick.js
@@ -1,6 +1,7 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 import Anchor from './Anchor';
 
 const CLASS_ROOT = 'brick';
@@ -9,67 +10,67 @@ const TYPE_LARGE = 'large';
 const TYPE_WIDE = 'wide';
 const TYPE_TALL = 'tall';
 
-export default class Brick extends Component {
-  render() {
-    let widthUnit = 1;
-    let heightUnit = 1;
-    switch (this.props.type) {
-      case TYPE_LARGE:
-        widthUnit = 2;
-        heightUnit = 2;
-        break;
-      case TYPE_WIDE:
-        widthUnit = 2;
-        heightUnit = 1;
-        break;
-      case TYPE_TALL:
-        widthUnit = 1;
-        heightUnit = 2;
-        break;
-    }
+const Brick = props => {
+  let widthUnit = 1;
+  let heightUnit = 1;
 
-    let classes = [CLASS_ROOT];
-    classes.push(`${CLASS_ROOT}--${widthUnit}-${heightUnit}`);
+  switch (props.type) {
+    case TYPE_LARGE:
+      widthUnit = 2;
+      heightUnit = 2;
+      break;
+    case TYPE_WIDE:
+      widthUnit = 2;
+      heightUnit = 1;
+      break;
+    case TYPE_TALL:
+      widthUnit = 1;
+      heightUnit = 2;
+      break;
+  }
 
-    if (this.props.colorIndex) {
-      classes.push(`background-color-index-${this.props.colorIndex}`);
-    }
-    if (this.props.className) {
-      classes.push(this.props.className);
-    }
+  let classes = classnames(
+    CLASS_ROOT,
+    `${CLASS_ROOT}--${widthUnit}-${heightUnit}`,
+    {
+      [`background-color-index-${props.colorIndex}`]: props.colorIndex
+    },
+    props.className
+  );
 
-    let label = (
-      <div className={`${CLASS_ROOT}--label`}>
-        <span>{this.props.label}</span>
-      </div>
-    );
+  let label = (
+    <div className={`${CLASS_ROOT}--label`}>
+      <span>{props.label}</span>
+    </div>
+  );
 
-    if (this.props.href) {
-      label = (
-        <Anchor href={this.props.href} className={`${CLASS_ROOT}--label`}>
-          <span>{this.props.label}</span>
-        </Anchor>
-      );
-    }
-
-    return (
-      <div className={classes.join(' ')} onClick={this.props.onClick}>
-        <div className={`${CLASS_ROOT}--content-wrapper`}>
-          {this.props.children}
-        </div>
-        {label}
-      </div>
+  if (props.href) {
+    label = (
+      <Anchor href={props.href} className={`${CLASS_ROOT}--label`}>
+        <span>{props.label}</span>
+      </Anchor>
     );
   }
-}
+
+  return (
+    <div className={classes} onClick={props.onClick}>
+      <div className={`${CLASS_ROOT}--content-wrapper`}>
+        {props.children}
+      </div>
+      {label}
+    </div>
+  );
+};
 
 Brick.propTypes = {
-  label: React.PropTypes.string,
-  href: React.PropTypes.string,
-  type: React.PropTypes.oneOf([TYPE_SMALL, TYPE_LARGE, TYPE_WIDE, TYPE_TALL]),
-  colorIndex: React.PropTypes.string
+  colorIndex: PropTypes.string,
+  href: PropTypes.string,
+  label: PropTypes.string,
+  type: PropTypes.oneOf([TYPE_SMALL, TYPE_LARGE, TYPE_WIDE, TYPE_TALL])
 };
 
 Brick.defaultProps = {
   type: TYPE_SMALL
 };
+
+export default Brick;
