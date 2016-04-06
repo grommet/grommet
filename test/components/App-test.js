86f14c65315800f178fc9cca47627e99114ab2ec
Alan Souza <alansouzati@gmail.com>
2016-04-06 11:19:52 -0700

    Adios grommet utils. Welcome grommet-toolbox.

--- test/components/App-test.js
+++ test/components/App-test.js
@@ -1,53 +1,72 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-import {expect} from 'chai';
+import {test} from 'tape';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
 import App from '../../src/js/components/App';
 
-describe('Grommet App', function() {
-  it('loads a basic App', () => {
-    const shallowRenderer = TestUtils.createRenderer();
-    shallowRenderer.render(React.createElement(App));
-    const appElement = shallowRenderer.getRenderOutput();
+test('loads a basic App', (t) => {
+  t.plan(1);
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(React.createElement(App));
+  const appElement = shallowRenderer.getRenderOutput();
 
-    expect(appElement.props.className).to.contain('app');
-  });
+  if (appElement.props.className.indexOf('app') > -1) {
+    t.pass('App has class');
+  } else {
+    t.fail('App does not have app class');
+  }
+});
 
-  it('loads an inline App', () => {
-    const shallowRenderer = TestUtils.createRenderer();
-    shallowRenderer.render(React.createElement(App, {inline: true}));
-    const appElement = shallowRenderer.getRenderOutput();
+test('loads an inline App', (t) => {
+  t.plan(1);
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(React.createElement(App, {inline: true}));
+  const appElement = shallowRenderer.getRenderOutput();
 
-    expect(appElement.props.className).to.contain('app--inline');
-  });
+  if (appElement.props.className.indexOf('app--inline') > -1) {
+    t.pass('App has inline class');
+  } else {
+    t.fail('App does not have inline class');
+  }
+});
 
-  it('loads a custom className App', () => {
-    const shallowRenderer = TestUtils.createRenderer();
+test('loads a custom className App', (t) => {
+  t.plan(1);
+  const shallowRenderer = TestUtils.createRenderer();
 
-    shallowRenderer.render(React.createElement(App, {className: 'testing'}));
-    const appElement = shallowRenderer.getRenderOutput();
+  shallowRenderer.render(React.createElement(App, {className: 'testing'}));
+  const appElement = shallowRenderer.getRenderOutput();
 
-    expect(appElement.props.className).to.contain('testing');
-  });
+  if (appElement.props.className.indexOf('testing') > -1) {
+    t.pass('App has testing class');
+  } else {
+    t.fail('App does not have testing class');
+  }
+});
 
-  it('loads an App with body', () => {
-    const shallowRenderer = TestUtils.createRenderer();
+test('loads an App with body', (t) => {
+  t.plan(2);
+  const shallowRenderer = TestUtils.createRenderer();
 
-    var AppWithBody = (
-      <App><h2>App Body</h2></App>
-    );
+  var AppWithBody = (
+    <App><h2>App Body</h2></App>
+  );
 
-    shallowRenderer.render(
-      AppWithBody
-    );
-    const appElement = shallowRenderer.getRenderOutput();
+  shallowRenderer.render(
+    AppWithBody
+  );
+  const appElement = shallowRenderer.getRenderOutput();
 
-    expect(appElement.props.className).to.contain('app');
+  if (appElement.props.className.indexOf('app') > -1) {
+    t.pass('App has class');
+  } else {
+    t.fail('App does not have app class');
+  }
 
-    expect(appElement.props.children[0].props.children).to.equal(
-      "App Body"
-    );
-  });
+  t.equal(
+    appElement.props.children[0].props.children, 'App Body',
+    'App has body'
+  );
 });
