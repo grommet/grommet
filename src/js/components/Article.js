09b6a54536d467e019a4adf8e6483a1b3d4332db
Eric Soderberg <eric.soderberg@hp.com>
2016-04-14 13:55:13 -0700

    Improve Article horizontal scroll step wheel sensitivity.

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -142,7 +142,7 @@ export default class Article extends Component {
       // Horizontal scrolling.
       if (! this.state.ignoreScroll) {
         // Only step if the user isn't scrolling vertically, bias vertically
-        if (Math.abs(event.deltaY) < Math.abs(event.deltaX * 2)) {
+        if (Math.abs(event.deltaY * 4) < Math.abs(event.deltaX)) {
           event.preventDefault();
           // Constrain scrolling to lock on each section.
           if (event.deltaX > 0) {
