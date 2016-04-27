d023f37f409d87d97e21348b45d52dfa07a1ee31
Candy374 <lingling374@qq.com>
2016-04-27 17:27:00 +0800

    Fix Issue: Table selection in IE 11 is wrong
    The function matches and matchesElement is not supported in IE 11, so, just msMatchesSelector instead
    
    Issue details: #246

--- src/js/utils/Selection.js
+++ src/js/utils/Selection.js
@@ -69,6 +69,10 @@ function onClick (event, options) {
     while (item && ! item.matchesElement(options.childSelector)) {
       item = item.parentNode;
     }
+  } else if (item.msMatchesSelector) {
+    while (item.msMatchesSelector && !item.msMatchesSelector(options.childSelector)) {
+      item = item.parentNode;
+    }
   }
 
   // determine the index of the clicked element
