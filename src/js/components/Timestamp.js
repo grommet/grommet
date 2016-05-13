2592bf4845b011647d0a85696e9bdf597dbfafe5
Eric Soderberg <eric.soderberg@hp.com>
2016-05-12 17:40:06 -0700

    Don't lookup locale in Timestamp render.

--- src/js/components/Timestamp.js
+++ src/js/components/Timestamp.js
@@ -5,7 +5,7 @@ import { getCurrentLocale } from '../utils/Locale';
 
 const CLASS_ROOT = 'timestamp';
 
-function showField(field, fields) {
+function _showField(field, fields) {
   let result = true;
   if (fields) {
     if (Array.isArray(fields)) {
@@ -19,37 +19,55 @@ function showField(field, fields) {
 
 export default class Timestamp extends Component {
 
+  constructor () {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount () {
+    this._formatForLocale(this.props);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this._formatForLocale(nextProps);
+  }
+
+  _formatForLocale (props) {
+    const locale = getCurrentLocale();
+    const value = (typeof props.value === 'string') ? new Date(props.value) : props.value;
+
+    let date;
+    if (_showField('date', props.fields)) {
+      const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+      date = value.toLocaleDateString(locale, dateOptions);
+    }
+
+    let time;
+    if (_showField('time', props.fields)) {
+      const timeOptions = { hour: '2-digit', minute: '2-digit' };
+      time = value.toLocaleTimeString(locale, timeOptions);
+    }
+
+    this.setState({ date: date, time: time });
+  }
+
   render () {
-    const { fields } = this.props;
     var classes = [CLASS_ROOT];
     classes.push(CLASS_ROOT + '--' + this.props.align);
     if (this.props.className) {
       classes.push(this.props.className);
     }
 
-    let locale = getCurrentLocale();
-    let value = typeof this.props.value === 'string' ?
-      new Date(this.props.value) : this.props.value;
-
     let date;
-    if (showField('date', fields)) {
-      const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
-      date = (
-        <span className={`${CLASS_ROOT}__date`}>
-          {value.toLocaleDateString(locale, dateOptions)}
-        </span>
-      );
+    if (this.state.date) {
+      date = <span className={`${CLASS_ROOT}__date`}>{this.state.date}</span>;
     }
 
     let time;
-    if (showField('time', fields)) {
-      const timeOptions = { hour: '2-digit', minute: '2-digit' };
-      time = (
-        <span className={`${CLASS_ROOT}__time`}>
-          {value.toLocaleTimeString(locale, timeOptions)}
-        </span>
-      );
+    if (this.state.time) {
+      time = <span className={`${CLASS_ROOT}__time`}>{this.state.time}</span>;
     }
+
     return (
       <span className={classes.join(' ')}>
         {date} {time}
@@ -68,8 +86,8 @@ Timestamp.propTypes = {
     FIELD_TYPES
   ]),
   value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object
+    PropTypes.string, // ISO-8601 string
+    PropTypes.object  // Date object
   ]).isRequired
 };
 
