0892f1c0f832f71f5b28e5ce0f4548e9dc7eed0b
David Pinheiro <davrodpin@gmail.com>
2016-06-26 20:36:32 -0700

    Add command to create empty grommet project
    This change adds a new task to the Grommet command line tool that
    creates a directory layout, html, js and scss files and all
    dependencies required to start writing a Grommet application.
    The main difference between 'init' and 'new' commands is that a project
    created with 'new' will not contain any component other than a <Main>
    that contains an <App>.
    
    Other minor changes:
      - Remove gulp signal function (done function) that enables gulp task
        dependency since there is no dependency to be managed
      - Using absolute paths so 'process.chdir' call could be removed
    
    Signed-off-by: David Pinheiro <davrodpin@gmail.com>

---
+++ templates/new/grommet-toolbox.config.js
@@ -0,0 +1,17 @@
+import path from 'path';
+
+export default {
+  copyAssets: [
+    'src/index.html',
+    {
+      asset: 'src/img/**',
+      dist: 'dist/img/'
+    }
+  ],
+  jsAssets: ['src/js/**/*.js'],
+  mainJs: 'src/js/index.js',
+  mainScss: 'src/scss/index.scss',
+  devServerPort: 9000,
+  eslintOverride: path.resolve(__dirname, 'customEslintrc'),
+  scsslint: true
+};
