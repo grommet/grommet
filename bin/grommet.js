9eaec825027200c0c94beeb585bf90fd3c0394e4
David Pinheiro <davrodpin@gmail.com>
2016-06-27 18:12:59 -0700

    Minimize use of global vars in gulp task callbacks
    Signed-off-by: David Pinheiro <davrodpin@gmail.com>

--- bin/grommet.js
+++ bin/grommet.js
@@ -6,6 +6,7 @@ var install = require('gulp-install');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var shelljs = require('shelljs');
+var fs = require('fs');
 
 String.prototype.capitalize = function() {
   var words = this.split(' ');
@@ -17,76 +18,120 @@ String.prototype.capitalize = function() {
   return words.join(' ');
 };
 
-function nodeVersionSupported() {
-  return Number(process.version.match(/^v(\d+\.\d+)/)[1]) >= 0.10;
+function fileExists(filePath) {
+  try {
+    result = fs.statSync(filePath);
+    return true;
+  } catch(error) {
+  }
+
+  return false;
+}
+
+function nodeVersionSupported(nodeVersion) {
+  return nodeVersion >= Number(supportedNodeVersion);
 }
 
 function npmVersionSupported(npmVersion) {
-  return npmVersion >= 1.4;
+  return npmVersion >= Number(supportedNpmVersion);
+}
+
+function dependenciesSupported(nodeVersion, npmVersion) {
+  if (!nodeVersionSupported(nodeVersion) || !npmVersionSupported(npmVersion)) {
+    console.error(
+      '[grommet] Grommet requires Node v'+supportedNodeVersion+
+      '+ and NPM '+supportedNpmVersion+'+.');
+    console.error(
+      '[grommet] Currently you have Node ' + process.version +
+      ' and NPM ' + npmVersion
+    );
+    return false;
+  }
+
+  return true;
+}
+
+function copyAssetsAndInstall(from, to, command) {
+  var installDependencies = function() {
+    console.log("[grommet] Installing Grommet dependencies...");
+    return install();
+  }
+
+  var templateVars = {
+    appName: command.options.app,
+    appTitle: command.options.title,
+    appDescription: command.options.description,
+    grommetVersion: command.env.grommetVersion
+  };
+
+  gulp.src(from, { dot: true })
+    .pipe(template(templateVars))
+    .pipe(gulp.dest(to))
+    .pipe(installDependencies());
 }
 
 var tasks = {
-  'init': function(done) {
-    if (!nodeVersionSupported() || !npmVersionSupported(npmVersion)) {
-      console.error('[grommet] Grommet requires Node v0.10+ and NPM 1.4.x+.');
-      console.error(
-        '[grommet] Currently you have Node ' + process.version +
-        ' and NPM ' + npmVersion
-      );
-      process.exit(1);
-    }
-
-    mkdirp('./' + app, function(err) {
-      if (err) {
-        console.log('Error trying to create project: ' + err);
-      } else {
-        process.chdir('./' + app);
-
-        var templateFolder = path.join(grommetPath, 'templates/init/**');
-        var mobileIcon = path.join(grommetPath, 'img/mobile-app-icon.png');
-        var shortcutIcon = path.join(grommetPath, 'img/shortcut-icon.png');
-
-        gulp.src(mobileIcon).pipe(gulp.dest('./src/img'));
-        gulp.src(shortcutIcon).pipe(gulp.dest('./src/img'));
-        gulp.src(templateFolder, { dot: true })
-        .pipe(template({
-          appName: app,
-          appTitle: title,
-          grommetVersion: packageJSON.version
-        }))
-        .pipe(gulp.dest('./'))
-        .pipe(install()).on('finish', function() {
-          done();
-        });
+  'init': function(command) {
+    return function() {
+      if (!dependenciesSupported(command.env.nodeVersion, command.env.npmVersion)) {
+        process.exit(1);
       }
-    });
+
+      var newAppPath = path.join(process.cwd(), command.options.app);
+
+      mkdirp(newAppPath, function(err) {
+        if (err) {
+          console.log('Error trying to create project: ' + err);
+        } else {
+          var templateFolder = path.join(command.env.templatePath, 'init/**');
+          var mobileIcon = path.join(command.env.grommetPath, 'img/mobile-app-icon.png');
+          var shortcutIcon = path.join(command.env.grommetPath, 'img/shortcut-icon.png');
+
+          gulp.src(mobileIcon).pipe(gulp.dest('./src/img'));
+          gulp.src(shortcutIcon).pipe(gulp.dest('./src/img'));
+          copyAssetsAndInstall(templateFolder, newAppPath, command);
+        }
+      })
+    }
   }
 };
 
+var supportedNodeVersion = '0.10';
+var supportedNpmVersion = '1.4';
 var grommetPath = path.join(__dirname, '..');
-var packageJSON = require(path.join(grommetPath, 'package.json'));
-var npmVersion = Number(shelljs.exec('npm --version', {silent:true})
-  .stdout.toString().match(/^(\d+\.\d+)/)[1]);
 var argv = require('yargs').boolean('version').argv;
+var options = argv._;
+var command = {
+  task: options[0] || 'init',
+  env: {
+    grommetPath: grommetPath,
+    grommetVersion: require(path.join(grommetPath, 'package.json')).version,
+    templatePath: path.join(grommetPath, 'templates'),
+    npmVersion: Number(shelljs.exec('npm --version', {silent:true}).stdout.toString().match(/^(\d+\.\d+)/)[1]),
+    nodeVersion: Number(process.version.match(/^v(\d+\.\d+)/)[1]),
+    processVersion: process.version
+  },
+  options: {}
+}
 
 if (argv.version) {
-  console.log(packageJSON.version);
+  console.log(command.env.grommetVersion);
   process.exit(0);
 }
 
-var options = argv._;
-var command = options[0] || 'init';
-if (command === 'init') {
-  var app = options[1] || 'app-name';
-  var title = app.replace(/-|_/g, ' ').capitalize();
+switch(command.task) {
+  case 'init':
+    command.options['app'] = options[1] || 'app-name';
+    command.options['title'] = command.options.app.replace(/-|_/g, ' ').capitalize();
+    break;
 }
 
-if (command in tasks) {
-  gulp.task(command, tasks[command]);
-  gulp.start(command);
+if (command.task in tasks) {
+  gulp.task(command.task, tasks[command.task](command));
+  gulp.start(command.task);
 } else {
   var allTaskNames = Object.keys(tasks).join('|');
-  console.log('[grommet] Command "' + command + '" not supported.');
+  console.log('[grommet] Command "' + command.task + '" not supported.');
   console.log('[grommet] Usage: grommet <' + allTaskNames + '>');
   process.exit(1);
 }
