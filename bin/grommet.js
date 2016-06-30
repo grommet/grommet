c3ce1410075333e1c51baf670d4bb83a7ae2f621
David Pinheiro <davrodpin@gmail.com>
2016-06-29 17:15:39 -0700

    Add a new grommet cli command to create components
    This change adds a new command line options to create the code skeleton
    for new components. Includes the ability to create empty methods as
    well.
    
    Signed-off-by: David Pinheiro <davrodpin@gmail.com>

--- bin/grommet.js
+++ bin/grommet.js
@@ -3,10 +3,24 @@
 var gulp = require('gulp');
 var template = require('gulp-template');
 var install = require('gulp-install');
+var rename = require('gulp-rename');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var shelljs = require('shelljs');
 var fs = require('fs');
+
+Array.prototype.unique = function() {
+  var that = this;
+  return this.filter(function(elem, pos) {
+    return that.indexOf(elem) == pos;
+  });
+};
+
+Array.prototype.remove = function(values) {
+  return this.filter(function(value) {
+    return values.indexOf(value) == -1;
+  });
+}
 
 String.prototype.capitalize = function() {
   var words = this.split(' ');
@@ -123,8 +137,112 @@ var tasks = {
         }
       })
     }
+  },
+  'create-component': function(command) {
+    return function() {
+      var methodBlacklist = [ 'constructor', 'render' ];
+      var componentTemplate = 'component_template.js';
+      var from = path.join(command.env.templatePath, componentTemplate);
+      var to = path.join(process.cwd(), 'src/js/components');
+
+      var grommetFiles = [
+        'grommet-toolbox.config.js',
+        'src/js',
+        'src/js/components'
+      ]
+
+      var isAGrommetProject = function() {
+        var cwd = process.cwd();
+        return grommetFiles.every(function(filePath) {
+          var filePath = path.join(cwd, filePath);
+          return fileExists(filePath)
+        });
+      }();
+
+      if (!isAGrommetProject) {
+        console.error(
+          '[grommet] Error while creating component. '+
+          'It seems the current directory is not a Grommet project.');
+        process.exit(1);
+      }
+
+      var componentTemplateExists = fileExists(from);
+      if (!componentTemplateExists) {
+        console.error('[grommet] Error while creating component. Could not find template file ['+from+'].');
+        process.exit(1);
+      }
+
+      var methodsTemplate = "";
+      if (command.options.methodList) {
+        methodsTemplate = command.options.methodList
+          .split(',')
+          .map(function(methodName) {
+            return methodName.trim();
+          })
+          .unique()
+          .remove(methodBlacklist)
+          .map(function(methodName) {
+            return '\n  '+methodName+'() {\n    return();\n  }\n';
+        }).join('');
+      }
+
+      gulp.src(from)
+        .pipe(template({
+          componentName: command.options.component,
+          methods: methodsTemplate
+        }))
+        .pipe(rename(command.options.component+'.js'))
+        .pipe(gulp.dest(to));
+
+      console.log('[grommet] Component '+command.options.component+' has been successfully created.');
+    }
   }
 };
+
+var parseOpts = {
+  'init': function(command, options) {
+    command.options['app'] = options[1] || 'app-name';
+    command.options['title'] = command.options.app.replace(/-|_/g, ' ').capitalize();
+    return command;
+  },
+  'new': function(command, options) {
+    command.options['app'] = options[1];
+    if (!command.options.app) {
+      console.error('[grommet] Usage: grommet new <app-name> [description]');
+      process.exit(1);
+    }
+    command.options['title'] = command.options.app.replace(/-|_/g, ' ').capitalize();
+    command.options['description'] = options[2] || "";
+    return command;
+  },
+  'create': function(command, options) {
+    var usage = '[grommet] Usage: grommet create component <component-name> [method1,method2,...,methodN]'
+    var entity = options[1];
+    if (!entity) {
+      console.error(usage);
+      process.exit(1);
+    }
+    switch(entity) {
+      case 'component':
+        var component = options[2];
+        if (!component) {
+            console.error(usage);
+            process.exit(1);
+        }
+        console.log('[grommet] Creating component '+component+'...');
+        command.task = command.task+'-'+entity;
+        command.options.component = component;
+        command.options.methodList = options[3] || '';
+        break;
+      default:
+        console.error('[grommet] Do not know how to create '+entity+'.');
+        console.error(usage);
+        process.exit(1);
+        break;
+    }
+    return command;
+  }
+}
 
 var supportedNodeVersion = '0.10';
 var supportedNpmVersion = '1.4';
@@ -149,28 +267,16 @@ if (argv.version) {
   process.exit(0);
 }
 
-switch(command.task) {
-  case 'init':
-    command.options['app'] = options[1] || 'app-name';
-    command.options['title'] = command.options.app.replace(/-|_/g, ' ').capitalize();
-    break;
-  case 'new':
-    command.options['app'] = options[1];
-    if (!command.options.app) {
-      console.error('[grommet] Usage: grommet new <app-name> [description]');
-      process.exit(1);
-    }
-    command.options['title'] = command.options.app.replace(/-|_/g, ' ').capitalize();
-    command.options['description'] = options[2] || "";
-    break;
-}
-
-if (command.task in tasks) {
-  gulp.task(command.task, tasks[command.task](command));
-  gulp.start(command.task);
-} else {
+if (!command.task in tasks) {
   var allTaskNames = Object.keys(tasks).join('|');
-  console.error('[grommet] Command "' + command + '" not supported.');
+  console.error('[grommet] Command "' + command.task + '" not supported.');
   console.error('[grommet] Usage: grommet <' + allTaskNames + '>');
   process.exit(1);
 }
+
+if (command.task in parseOpts) {
+  command = parseOpts[command.task](command, options);
+}
+
+gulp.task(command.task, tasks[command.task](command));
+gulp.start(command.task);
