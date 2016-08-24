a8ad19ab2fa8c1e1acf0aacbc1c8ab40baa6e6b4
Eric Soderberg <eric.soderberg@hp.com>
2016-08-23 17:45:45 -0700

    Convert Markdown-test to use enzyme.

--- test/components/Markdown-test.js
+++ test/components/Markdown-test.js
@@ -2,32 +2,25 @@
 
 import {test} from 'tape';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 
 import Markdown from '../../src/js/components/Markdown';
 import Paragraph from '../../src/js/components/Paragraph';
 
 test('loads a paragraph Markdown', (t) => {
   t.plan(3);
-  const shallowRenderer = TestUtils.createRenderer();
-  shallowRenderer.render(React.createElement(Markdown, {
-    content: 'test',
-    components: {
-      p: {
-        props: {
-          className: 'testing',
-          size: 'large'
-        }
-      }
-    }
-  }));
-  const markdownElement = shallowRenderer.getRenderOutput();
 
-  t.equal(markdownElement.type, Paragraph, 'Markdown uses Paragraph');
-  if (markdownElement.props.className.indexOf('testing') > -1) {
+  const markdownElement = shallow(
+    <Markdown content='test'
+      components={{ p: { props: { className: 'testing', size: 'large' } } }} />
+  );
+
+  t.equal(markdownElement.find(Paragraph).length, 1);
+  const props = markdownElement.find(Paragraph).props();
+  if (props.className.indexOf('testing') > -1) {
     t.pass('Markdown paragraph has custom class');
   } else {
     t.fail('Markdown paragraph does not have custom class');
   }
-  t.equal(markdownElement.props.size, 'large', 'Markdown paragraph is large');
+  t.equal(props.size, 'large', 'Markdown paragraph is large');
 });
