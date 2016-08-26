5b18230fe9303d48e962c50d7c0426277a28b0cd
Alan Souza <alansouzati@gmail.com>
2016-08-25 19:41:57 -0700

    Fixed case sensitive issue for checkBox component

--- __tests__/components/CheckBox-test.js
+++ __tests__/components/CheckBox-test.js
@@ -1,6 +1,6 @@
 // Checkbox-test.js
 import React from 'react';
-import Checkbox from '../../src/js/components/Checkbox.js';
+import Checkbox from '../../src/js/components/CheckBox';
 import renderer from 'react/lib/ReactTestRenderer';
 
 describe('Checkbox', () => {
