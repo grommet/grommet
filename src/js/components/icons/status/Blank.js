a2d0278423247646cddf075d4bdf8c08aba5672d
Emmanuel Pilande <epilande@gmail.com>
2016-10-03 16:46:14 -1000

    Clean up & transfer props to status Blank

--- src/js/components/icons/status/Blank.js
+++ src/js/components/icons/status/Blank.js
@@ -1,27 +1,23 @@
 // (C) Copyright 2014-2016 Hewlett Packard Enterprise Development LP
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import FormattedMessage from '../../FormattedMessage';
 import CSSClassnames from '../../../utils/CSSClassnames';
 
-const STATUS_ICON = CSSClassnames.STATUS_ICON;
+const CLASS_ROOT = CSSClassnames.STATUS_ICON;
 
 export default class Blank extends Component {
   render() {
-    var className = `${STATUS_ICON} ${STATUS_ICON}-blank`;
-    var a11yTitle = this.props.a11yTitle;
-    if (this.props.className) {
-      className += ' ' + this.props.className;
-    }
-    if (typeof this.props.a11yTitle === "undefined") {
-      // this.props.a11yTitle empty string is an acceptable value.
-      // Only if undefined
-      // should use the default title value.
-      a11yTitle = 'Blank';
-    }
-    var blankTitleId = 'blank-title';
+    const { a11yTitle, className, ...props } = this.props;
+    const classes = classnames(
+      CLASS_ROOT,
+      `${CLASS_ROOT}-blank`,
+      className
+    );
+    const blankTitleId = 'blank-title';
     return (
-      <svg className={className} viewBox="0 0 24 24" role="img"
+      <svg {...props} className={classes} viewBox="0 0 24 24" role="img"
         aria-labelledby={blankTitleId} version="1.1">
         <title id={blankTitleId}>
           <FormattedMessage id={a11yTitle} defaultMessage={a11yTitle} />
@@ -30,3 +26,11 @@ export default class Blank extends Component {
     );
   }
 }
+
+Blank.defaultProps = {
+  a11yTitle: 'Blank'
+};
+
+Blank.propTypes = {
+  a11yTitle: PropTypes.string
+};
