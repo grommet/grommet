0cc4c6dec714f76790a7a59b735d6cc4b694c404
Emmanuel Pilande <epilande@gmail.com>
2016-10-03 16:41:56 -1000

    Clean up & transfer props to status OK icon

--- src/js/components/icons/status/OK.js
+++ src/js/components/icons/status/OK.js
@@ -1,35 +1,31 @@
 // (C) Copyright 2014-2016 Hewlett Packard Enterprise Development LP
 
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import FormattedMessage from '../../FormattedMessage';
 import CSSClassnames from '../../../utils/CSSClassnames';
 
-const STATUS_ICON = CSSClassnames.STATUS_ICON;
+const CLASS_ROOT = CSSClassnames.STATUS_ICON;
 
 export default class OK extends Component {
   render() {
-    var className = `${STATUS_ICON} ${STATUS_ICON}-ok`;
-    var a11yTitle = this.props.a11yTitle;
-    if (this.props.className) {
-      className += ' ' + this.props.className;
-    }
-    if (typeof this.props.a11yTitle === "undefined") {
-      // this.props.a11yTitle emplty string is an acceptable value
-      // only if undefined
-      // should it use the default title value
-      a11yTitle = 'OK';
-    }
-    var okTitleId = 'ok-title';
+    const { a11yTitle, className, ...props } = this.props;
+    const classes = classnames(
+      CLASS_ROOT,
+      `${CLASS_ROOT}-ok`,
+      className
+    );
+    const okTitleId = 'ok-title';
     return (
-      <svg className={className} viewBox="0 0 24 24" role="img"
+      <svg {...props} className={classes} viewBox="0 0 24 24" role="img"
         aria-labelledby={okTitleId} version="1.1">
         <title id={okTitleId}>
           <FormattedMessage id={a11yTitle} defaultMessage={a11yTitle} />
         </title>
-        <g className={`${STATUS_ICON}__base`}>
+        <g className={`${CLASS_ROOT}__base`}>
           <circle role="presentation" cx="12" cy="12" r="12" stroke="none" />
         </g>
-        <g className={`${STATUS_ICON}__detail`}>
+        <g className={`${CLASS_ROOT}__detail`}>
           <path role="presentation"
             d={"M10,17.4 L5.3,12.7 L6.7,11.3 L10,14.6 L17.3,7.3 L18.7,8.7 " +
               "L10,17.4 Z"} stroke="none" />
@@ -38,6 +34,10 @@ export default class OK extends Component {
     );
   }
 }
+
+OK.defaultProps = {
+  a11yTitle: 'OK'
+};
 
 OK.propTypes = {
   a11yTitle: PropTypes.string
