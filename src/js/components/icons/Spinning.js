3eb59a319566b77d441c5ee7bbf20c437e8eb5b1
Alan Souza <alansouzati@gmail.com>
2016-10-06 02:30:13 -0700

    Table and Spinning: refactored component and added accessibility.

--- src/js/components/icons/Spinning.js
+++ src/js/components/icons/Spinning.js
@@ -3,12 +3,14 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import CSSClassnames from '../../utils/CSSClassnames';
+import Intl from '../../utils/Intl';
 
 const CLASS_ROOT = CSSClassnames.SPINNING;
 
 export default class Spinning extends Component {
   render () {
-    const { className, small, ...props } = this.props;
+    const { a11yTitle, className, small, ...props } = this.props;
+    const { intl } = this.context;
     const classes = classnames(
       CLASS_ROOT,
       {
@@ -18,18 +20,23 @@ export default class Spinning extends Component {
     );
 
     return (
-      <svg {...props} className={classes} viewBox="0 0 48 48" version="1.1"
-        role="img">
-        <title>Spinning</title>
-        <circle stroke="#ddd" strokeWidth="4" strokeDasharray="24px 8px"
-          fill="none" cx="24" cy="24" r="20" />
-        <circle stroke="#333" strokeWidth="4" strokeDasharray="24px 104px"
-          fill="none" cx="24" cy="24" r="20" />
+      <svg {...props} className={classes} viewBox='0 0 48 48' version='1.1'
+        role='img' aria-label={a11yTitle || Intl.getMessage(intl, 'Spinning')}>
+        <circle stroke='#ddd' strokeWidth='4' strokeDasharray='24px 8px'
+          fill='none' cx='24' cy='24' r='20' />
+        <circle stroke='#333' strokeWidth='4' strokeDasharray='24px 104px'
+          fill='none' cx='24' cy='24' r='20' />
       </svg>
     );
   }
 }
 
+Spinning.contextTypes = {
+  intl: PropTypes.object
+};
+
 Spinning.propTypes = {
+  a11yTitle: PropTypes.string,
+  className: PropTypes.string,
   small: PropTypes.bool
 };
