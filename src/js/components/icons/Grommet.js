77c04d2b0844f2b1a91bd8befff2099a8cb6e889
Eric Soderberg <eric.soderberg@hpe.com>
2016-11-19 08:43:51 +0100

    Added invert= to Grommet logo.

--- src/js/components/icons/Grommet.js
+++ src/js/components/icons/Grommet.js
@@ -9,21 +9,23 @@ const CLASS_ROOT = CSSClassnames.LOGO_ICON;
 
 export default class Grommet extends Component {
   render () {
-    const { a11yTitle, className, large, size, small } = this.props;
+    const { a11yTitle, className, invert, large, size, small } = this.props;
     const { intl } = this.context;
     const classes = classnames(
       CLASS_ROOT,
-      className, {
+      {
         [`${CLASS_ROOT}--small`]: small,
         [`${CLASS_ROOT}--large`]: large,
         [`${CLASS_ROOT}--${size}`]: size
-      }
+      },
+      className
     );
+    const stroke = (invert ? '#fff' : '#865CD6');
     return (
       <svg className={classes} viewBox='0 0 182 182'
         width='182' height='182'
         version='1.1' role='img' aria-label={Intl.getMessage(intl, a11yTitle)}>
-        <path role='presentation' strokeWidth='18' stroke='#865CD6' fill='none'
+        <path role='presentation' strokeWidth='18' stroke={stroke} fill='none'
           d='M 91,91 m 0,-82 a 82,82 0 1,1 0,164 a 82,82 0 1,1 0,-164' />
       </svg>
     );
@@ -40,6 +42,6 @@ Grommet.defaultProps = {
 
 Grommet.propTypes = {
   a11yTitle: React.PropTypes.string,
-  colorIndex: PropTypes.string,
+  invert: PropTypes.bool,
   size: React.PropTypes.oneOf(['small', 'medium', 'large', 'xlarge'])
 };
