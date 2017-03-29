7b8a4ec0caabf49b01756981e65ad7961c9d3d23
Chris Carlozzi <L0ZZI@users.noreply.github.com>
2017-03-28 18:52:43 -0700

    Fix svg that had rendering problems (#1275)
    * Fix svg that had rendering problems
    
    * Added react icons. updated snapshot

--- src/js/components/icons/base/Aed.js
+++ src/js/components/icons/base/Aed.js
@@ -29,7 +29,7 @@ export default class Icon extends Component {
     a11yTitle = a11yTitle || Intl.getMessage(intl, 'aed');
 
     const restProps = Props.omit(this.props, Object.keys(Icon.propTypes));
-    return <svg {...restProps} version="1.1" viewBox="0 0 24 24" width="24px" height="24px" role="img" className={classes} aria-label={a11yTitle}><g fill="none" fillRule="evenodd"><path stroke="#000" strokeWidth="2" d="M2,8.4 C2,4 5,3 7,3 C9,3 11,5 12,6.5 C13,5 15,3 17,3 C19,3 22,4 22,8.4 C22,15 12,21 12,21 C12,21 2,15 2,8.4 Z" fill="none"/><polygon fill="#000" points="12 9 9 15 12 14.143 12 18 15 12 12 13" stroke="none"/></g></svg>;
+    return <svg {...restProps} version="1.1" viewBox="0 0 24 24" width="24px" height="24px" role="img" className={classes} aria-label={a11yTitle}><path fill="none" stroke="#000" strokeWidth="2" d="M2,8.4 C2,4 5,3 7,3 C9,3 11,5 12,6.5 C13,5 15,3 17,3 C19,3 22,4 22,8.4 C22,15 12,21 12,21 C12,21 2,15 2,8.4 Z M12,6 C11.5,5.5 10,10 10,10 L12,10 L12,12.5 L14,9 L12,9 C12,9 13,3 17,3 C13,3 12,6 12,6 Z"/></svg>;
   }
 };
 
--- src/js/components/icons/base/Attraction.js
+++ src/js/components/icons/base/Attraction.js
@@ -29,7 +29,7 @@ export default class Icon extends Component {
     a11yTitle = a11yTitle || Intl.getMessage(intl, 'attraction');
 
     const restProps = Props.omit(this.props, Object.keys(Icon.propTypes));
-    return <svg {...restProps} version="1.1" viewBox="0 0 24 24" width="24px" height="24px" role="img" className={classes} aria-label={a11yTitle}><g fill="none" fillRule="evenodd"><polygon stroke="#000" strokeWidth="2" points="12 4 21 10 21 12 3 12 3 10" fill="none"/><polygon fill="#000" points="12 0 15.5 1.25 12 2.5" stroke="none"/><path stroke="#000" strokeWidth="2" d="M4.5,12 L19.5,12 C19.5,16.1666667 21,22 21,22 L3,22 C3,22 4.5,16.1666667 4.5,12 Z" fill="none"/><polygon fill="#000" stroke="none" strokeWidth="2" points="14 15.5 16 22 12 22"/></g></svg>;
+    return <svg {...restProps} version="1.1" viewBox="0 0 24 24" width="24px" height="24px" role="img" className={classes} aria-label={a11yTitle}><path fill="none" stroke="#000" strokeWidth="2" d="M13,2 L14.5,2.5 L13,3 L13,2 Z M14,18 L15,23 L13,23 L14,18 Z M14,16.5 L16,23 L12,23 L14,16.5 Z M12,6 L21,11 L21,13 L3,13 L3,11 L12,6 Z M4.5,13 L19.5,13 C19.5,17.1666667 21,23 21,23 L3,23 C3,23 4.5,17.1666667 4.5,13 Z M4.5,13 L19.5,13 C19.5,17.1666667 21,23 21,23 L3,23 C3,23 4.5,17.1666667 4.5,13 Z"/></svg>;
   }
 };
 
