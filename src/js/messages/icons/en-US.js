e789690b0c2c9b677de7ccc78e1c9fe3514d3f93
woodnt <woodm1979@gmail.com>
2017-05-11 12:57:12 -0600

    The FormEdit icon needs to be localized

--- src/js/messages/icons/en-US.js
+++ src/js/messages/icons/en-US.js
@@ -162,6 +162,7 @@ export default {
   'folder-cycle': 'folder cycle', 
   'folder-open': 'folder open', 
   'folder': 'folder', 
+  'form-edit': 'form edit',
   'forward-ten': 'forward ten', 
   'gallery': 'gallery', 
   'gamepad': 'gamepad', 
