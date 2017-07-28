a442e55f3a0bd4dd1b29db7a2e985db507ada226
Shai <shailevi32@gmail.com>
2017-07-29 00:58:37 +0300

    Update he.js

--- src/js/messages/he.js
+++ src/js/messages/he.js
@@ -54,7 +54,7 @@ export default {
     ' והנמוך הינו {smallest}'
   ),
   hour: 'שעה',
-  'Grommet Logo': 'Grommet Logo',
+  'Grommet Logo': 'Grommet לוגו',
   'Layer': 'שכבה',
   List: 'רשימה',
   line: 'קו',
