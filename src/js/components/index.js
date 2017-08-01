6f59a4efc281fbdd8a295d555debaa3f3bfb820e
Alan Souza <alansouzati@gmail.com>
2017-08-01 14:47:51 -0700

    First pass of TextInput

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,6 +2,5 @@ export * from './box';
 export * from './button';
 export * from './grid';
 export * from './grommet';
-export * from './heading';
 export * from './paragraph';
 export * from './text-input';
