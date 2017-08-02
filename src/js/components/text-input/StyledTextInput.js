6f4ed7aa6af1273280fcdba8419523f3cf85e682
Alan Souza <alansouzati@gmail.com>
2017-08-02 13:25:13 -0700

    Added suggestions to text input (#1541)

--- src/js/components/text-input/StyledTextInput.js
+++ src/js/components/text-input/StyledTextInput.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-import { focusStyle, inputStyle } from '../utils';
+import { focusStyle, inputStyle, parseMetricToInt } from '../utils';
 
 const placeholderColor = css`
   color: ${props => props.theme.global.placeholder.color};
@@ -28,6 +28,32 @@ const StyledTextInput = styled.input`
   &:focus {
     ${focusStyle}
   }
+`;
+
+// TODO: remove cursor pointer and hover when using button
+const suggestionBackgroundStyle = css`
+  background-color: ${props => props.theme.global.hover.backgroundColor};
+`;
+
+export const StyledSuggestion = styled.li`
+  padding: ${props => (
+    `${parseMetricToInt(props.theme.global.spacing) / 4}px ${props.theme.global.spacing}`
+  )};
+  cursor: pointer;
+
+  &:hover {
+    ${suggestionBackgroundStyle}
+  }
+
+  ${props => props.active && suggestionBackgroundStyle}
+`;
+
+export const StyledSuggestions = styled.ol`
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
 `;
 
 export default StyledTextInput.extend`
---
+++ src/js/components/utils/styles.js
@@ -0,0 +1,60 @@
+import { css } from 'styled-components';
+
+export function parseMetricToInt(fontAsString) {
+  return parseInt(fontAsString.replace(/[^0-9]/g, ''), 10);
+}
+
+export const baseStyle = css`
+  font-family: ${props => props.theme.global.font.family};
+  font-size: ${props => `${(parseMetricToInt(props.theme.global.font.size) / 16) * 1}em`};
+  line-height: ${props => (
+    parseMetricToInt(props.theme.global.lineHeight) / parseMetricToInt(props.theme.global.font.size)
+  )};
+  color: ${props => props.theme.global.colors.text};
+  background-color: ${props => props.theme.global.colors.background};
+
+  box-sizing: border-box;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+
+  * {
+    box-sizing: inherit;
+  }
+`;
+
+export const focusStyle = css`
+  border-color: ${
+    props => (
+      props.theme.global.focus.border.color ||
+      props.theme.global.colors.accent[0]
+    )
+  };
+  box-shadow: 0 0 1px 1px ${
+    props => (
+      props.theme.global.focus.border.color ||
+      props.theme.global.colors.accent[0]
+    )
+  };
+`;
+
+export const inputStyle = css`
+  padding: ${props => (
+    (parseMetricToInt(props.theme.global.spacing) / 2) -
+    parseMetricToInt(props.theme.global.input.border.width)
+  )}px;
+  border: ${props => props.theme.global.input.border.width} solid ${props => props.theme.global.input.border.color};
+  border-radius: ${props => props.theme.global.input.border.radius};
+  outline: none;
+  background-color: transparent;
+  color: inherit;
+  font: inherit;
+  margin: 0;
+
+  ${props => props.focus && focusStyle}
+`;
+
+export default {
+  baseStyle, inputStyle, focusStyle,
+};
