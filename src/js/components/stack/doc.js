5a944c09c2cd3d8af4cb2be8058d393a335d7471
Eric Soderberg <eric.soderberg@hpe.com>
2017-08-22 06:32:12 -0700

    Removed dummy prop

--- src/js/components/stack/doc.js
+++ src/js/components/stack/doc.js
@@ -1,10 +1,9 @@
-import { schema, PropTypes } from 'react-desc';
+import { schema } from 'react-desc';
 
 export default Stack => schema(Stack, {
   description: 'Stacks components on top of the first child component.',
   usage: `import { Stack } from 'grommet';
   <Stack/>`,
   props: {
-    none: [PropTypes.any, 'not needed'],
   },
 });
