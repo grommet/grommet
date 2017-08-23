188858b43d4d0ecfcab4e87fa5aaa2a52e70db1b
Eric Soderberg <eric.soderberg@hpe.com>
2017-08-23 14:46:44 -0700

    Enhanced WorldMap to support latitude and longitude coordinates for places, added zoom property, added flag and onHover to series objects. (#1570)

--- src/js/components/WorldMap.js
+++ src/js/components/WorldMap.js
@@ -113,21 +113,50 @@ const FACTOR = 10;
 const clickableSeries = (props) =>
   props.series && props.series.some((serie) => serie.onClick);
 
+const maxCoordinate = (a, b) =>
+  [Math.max(a[0], b[0]), Math.max(a[1], b[1])];
+const minCoordinate = (a, b) =>
+  [Math.min(a[0], b[0]), Math.min(a[1], b[1])];
+
+// Based on https://stackoverflow.com/a/43861247
+const MAP_LAT_BOTTOM = -50.0; // empirically determined
+const latLonCoord = (latitude, longitude, origin, extent) => {
+  const mapLatBottomRad = MAP_LAT_BOTTOM * Math.PI / 180;
+  const latitudeRad = latitude * Math.PI / 180;
+  const mapLngLeft = -171.0; // empirically determined
+  const mapLngRight = 184.0; // empirically determined
+  const mapLngDelta = (mapLngRight - mapLngLeft);
+
+  const worldMapWidth = ((extent[0] / mapLngDelta) * 360) / (2 * Math.PI);
+  const mapOffsetY = Math.round(worldMapWidth / 2 *
+    Math.log((1 + Math.sin(mapLatBottomRad)) /
+      (1 - Math.sin(mapLatBottomRad))));
+
+  const x = Math.round((longitude - mapLngLeft) * (extent[0] / mapLngDelta));
+  const y = extent[1] -
+    Math.round((worldMapWidth / 2 *
+      Math.log((1 + Math.sin(latitudeRad)) /
+        (1 - Math.sin(latitudeRad)))) - mapOffsetY);
+
+  return [x, y]; // the coordinate value of this point on the map image
+};
+
 export default class WorldMap extends Component {
 
   constructor(props, context) {
     super(props, context);
+    this._renderPlace = this._renderPlace.bind(this);
     this._activateContinent = this._activateContinent.bind(this);
     this._activatePlace = this._activatePlace.bind(this);
-    this._renderContinent = this._renderContinent.bind(this);
-    this._renderPlace = this._renderPlace.bind(this);
     this._onEnter = this._onEnter.bind(this);
     this._onMouseOver = this._onMouseOver.bind(this);
     this._onMouseMove = this._onMouseMove.bind(this);
     this._onMouseLeave = this._onMouseLeave.bind(this);
 
     this.state = this._buildState();
+    this.state = { ...this.state, ...this._updateState(props) };
     this.state.clickable = clickableSeries(props);
+    this._flagRefs = {};
   }
 
   componentDidMount () {
@@ -138,6 +167,8 @@ export default class WorldMap extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    this.setState({ ...this._updateState(nextProps) });
+
     const { clickable } = this.state;
     const nextClickable = clickableSeries(nextProps);
     if (nextClickable !== clickable) {
@@ -150,6 +181,38 @@ export default class WorldMap extends Component {
     }
   }
 
+  componentDidUpdate() {
+    // place flags
+    const { series } = this.props;
+    const { continents, places, zoomedOrigin, width } = this.state;
+    const flagged = (series || []).filter(s => s.flag);
+    if (flagged.length > 0) {
+      const rect = this._worldMapRef.getBoundingClientRect();
+      const scale = rect.width / width;
+      flagged.forEach((serie, index) => {
+        const placeState = places[index];
+        let x;
+        let y;
+        if (serie.continent) {
+          const continentState = continents[serie.continent];
+          x = ((continentState.mid[0] - zoomedOrigin[0]) * FACTOR * scale);
+          y = ((continentState.mid[1] - zoomedOrigin[1]) * FACTOR * scale);
+        } else if (serie.place) {
+          x = placeState.place[0] * FACTOR * scale;
+          y = placeState.place[1] * FACTOR * scale;
+        }
+        const flag = this._flagRefs[index];
+        const flagRect = flag.getBoundingClientRect();
+        const xOffset = (x > (rect.width / 2)) ? (FACTOR / 2) :
+          -(flagRect.width + (FACTOR / 2));
+        const yOffset = (y > (rect.height / 2)) ? (FACTOR / 2) :
+          -(flagRect.height + (FACTOR / 2));
+        flag.style.top = `${y + yOffset}px`;
+        flag.style.left = `${x + xOffset}px`;
+      });
+    }
+  }
+
   componentWillUnmount () {
     const { clickable } = this.state;
     if (clickable) {
@@ -159,42 +222,95 @@ export default class WorldMap extends Component {
 
   _buildState () {
     let state = {
-      activeContinent: undefined, activePlace: undefined, dots: {}, area: {}
+      activeContinent: undefined, activePlace: undefined, continents: {}
     };
 
     // Build the SVG paths describing the individual dots
-    let width = 0;
-    let height = 0;
+    let totalExtent = [0, 0];
     CONTINENTS.forEach(continent => {
       const origin = continent.origin;
 
-      state.dots[continent.id] = continent.dots.map(segment => {
+      let extent = [...origin];
+      const dots = continent.dots.map(segment => {
         const dots = Array.apply(null, Array(segment[2]))
           .map(() => {
             return 'h0';
           }).join(' m10,0 ');
         const x = FACTOR * (origin[0] + segment[0] + 1);
         const y = FACTOR * (origin[1] + segment[1] + 1);
-        width = Math.max(width, FACTOR * (origin[0] + segment[0] + segment[2]));
-        height = Math.max(height, y);
+        extent = maxCoordinate(extent,
+          [(origin[0] + segment[0] + segment[2]),
+            (origin[1] + segment[1])]);
         return `M${x},${y} ${dots}`;
       }).join(' ');
 
-      state.area[continent.id] = continent.area.map((point, index) => {
+      const area = continent.area.map((point, index) => {
         const x = FACTOR * (point[0] + origin[0] + 1);
         const y = FACTOR * (point[1] + origin[1] + 1);
         return `${index === 0 ? 'M' : 'L'}${x},${y}`;
-      }).join(' ');
+      }).join(' ') + ' Z';
 
-      if (state.area[continent.id]) {
-        state.area[continent.id] += ' Z';
-      }
+      const mid = [
+        origin[0] + ((extent[0] - origin[0]) / 2),
+        origin[1] + ((extent[1] - origin[1]) / 2)
+      ];
+      state.continents[continent.id] = { area, dots, origin, extent, mid };
+      totalExtent = maxCoordinate(totalExtent, extent);
     });
 
-    state.width = width + FACTOR;
-    state.height = height + FACTOR;
+    state.origin = [0, 0];
+    state.extent = totalExtent;
 
     return state;
+  }
+
+  _updateState(props) {
+    const { series, zoom } = props;
+    const continents = { ...this.state.continents };
+
+    // convert places using lat,lon
+    const places = (series || []).filter(s => s.place).map((serie) => {
+      let place = serie.place;
+      if (place[0] % 1) {
+        place = latLonCoord(place[0], place[1],
+          this.state.origin, this.state.extent);
+      }
+      return { place, id: place.join(',') };
+    });
+
+    // update how much of the map to show
+    const haveSomeContinents =
+      (series || []).filter(s => s.continent).length > 0;
+    let origin = [...this.state.extent];
+    let extent = [...this.state.origin];
+    CONTINENTS.forEach(continent => {
+      const continentState = continents[continent.id];
+      const serie = (series || []).filter(s => s.continent === continent.id)[0];
+      // see if any places are within the continent
+      const includedPlaces = places.filter(s => (
+        s.place[0] >= continentState.origin[0] &&
+        s.place[0] <= continentState.extent[0] &&
+        s.place[1] >= continentState.origin[1] &&
+        s.place[1] <= continentState.extent[1]
+      ));
+      let visible = ((!haveSomeContinents && !zoom) ||
+        serie || includedPlaces.length > 0);
+      if (visible || !zoom) {
+        origin = minCoordinate(origin, continentState.origin);
+        extent = maxCoordinate(extent, continentState.extent);
+      }
+      continents[continent.id].visible = visible;
+      continents[continent.id].serie = serie;
+    });
+
+    return {
+      continents,
+      places,
+      zoomedOrigin: origin,
+      x: (origin[0] * FACTOR), y: (origin[1] * FACTOR),
+      width: ((extent[0] - origin[0] + 1) * FACTOR),
+      height: ((extent[1] - origin[1] + 2) * FACTOR)
+    };
   }
 
   _startKeyboardListening() {
@@ -259,26 +375,47 @@ export default class WorldMap extends Component {
     this.setState({ over: false, activePlace: undefined });
   }
 
-  _interactiveProps (serie, defaultLabel, activeFunc, activeValue) {
+  _interactiveProps (serie, defaultLabel, activeFunc, activeValue, active) {
     return {
       role: 'button',
       'aria-label': serie.label || defaultLabel,
       tabIndex: '0',
       onClick: serie.onClick,
-      onMouseOver: () => activeFunc(activeValue),
-      onMouseLeave: () => activeFunc(undefined),
+      onMouseOver: () => {
+        if (!active) {
+          activeFunc(activeValue);
+          if (serie.onHover) {
+            serie.onHover(true);
+          }
+        }
+      },
+      onMouseLeave: () => {
+        if (active) {
+          activeFunc(undefined);
+          if (serie.onHover) {
+            serie.onHover(false);
+          }
+        }
+      },
       onFocus: () => {
         // This moves the map unnecessarily. Instead, we should check
         // the position and scroll if it isn't already visible
         // this._worldMapRef.scrollIntoView();
-        activeFunc(activeValue);
+        if (!active) {
+          activeFunc(activeValue);
+        }
       },
-      onBlur: () => activeFunc(undefined)
+      onBlur: () => {
+        if (active) {
+          activeFunc(undefined);
+        }
+      }
     };
   }
 
   _renderContinent (continent, index, mapColorIndex, serie) {
-    const { activeContinent } = this.state;
+    const { activeContinent, continents } = this.state;
+    const active = continent.id === activeContinent;
     // only graph color if they explicitly asked for this continent
     const colorIndex =
       (serie || {}).colorIndex || mapColorIndex ||
@@ -287,47 +424,53 @@ export default class WorldMap extends Component {
     const classes = classnames(
       `${CLASS_ROOT}__continent`,
       `${COLOR_INDEX}-${colorIndex}`, {
-        [`${CLASS_ROOT}__continent--active`]: continent.id === activeContinent
+        [`${CLASS_ROOT}__continent--active`]: active
       }
     );
     let area;
     let interactiveProps = {};
-    if (serie && serie.onClick) {
+    if (serie && (serie.onClick || serie.onHover)) {
       area = (
         <path stroke='none' fill='#fff' fillOpacity='0.01'
-          d={this.state.area[continent.id]} />
+          d={continents[continent.id].area} />
       );
       interactiveProps = this._interactiveProps(
-        serie, continent.id, this._activateContinent, continent.id);
+        serie, continent.id, this._activateContinent, continent.id, active);
     }
     // We add the area so the mouse events work for the whole region,
     // not just the dots
     return (
       <g key={continent.id} className={classes} {...interactiveProps}>
         {area}
-        <path d={this.state.dots[continent.id]} />
+        <path d={continents[continent.id].dots} />
       </g>
     );
   }
 
   _renderPlace(serie, index) {
     const { colorIndex: mapColorIndex } = this.props;
-    const { activePlace } = this.state;
-    const { colorIndex: serieColorIndex, onClick, place, ...rest } = serie;
+    const { activePlace, places } = this.state;
+    const {
+      colorIndex: serieColorIndex, onClick, onHover, place, ...rest
+    } = serie;
+    delete rest.flag;
+    const placeState = places[index];
+    const active = (activePlace && activePlace.join(',') === placeState.id);
     const colorIndex =
       serieColorIndex || mapColorIndex || `graph-${index}`;
     const classes = classnames(
       `${CLASS_ROOT}__place`,
       `${COLOR_INDEX}-${colorIndex}`, {
-        [`${CLASS_ROOT}__place--active`]:
-        (activePlace && activePlace.join(',') === place.join(','))
+        [`${CLASS_ROOT}__place--active`]: active
       }
     );
-    const d = `M${FACTOR * place[0]},${FACTOR * place[1]} h0`;
+    const d =
+      `M${FACTOR * placeState.place[0]},${FACTOR * placeState.place[1]} h0`;
     let interactiveProps = {};
-    if (onClick) {
+    if (onClick || onHover) {
       interactiveProps =
-        this._interactiveProps(serie, 'place', this._activatePlace, place);
+        this._interactiveProps(serie, 'place', this._activatePlace, place,
+          active);
     }
     return (
       <path key={place.join(',')} {...rest} className={classes}
@@ -339,18 +482,18 @@ export default class WorldMap extends Component {
     const {
       className, colorIndex, onSelectPlace, series, ...props
     } = this.props;
-    const { activePlace, over, width, height } = this.state;
+    delete props.zoom;
+    const { activePlace, over, x, y, width, height } = this.state;
     const classes = classnames(
       CLASS_ROOT,
       className
     );
 
-    const haveSomeContinents =
-      (series || []).filter(s => s.continent).length > 0;
     const continents = [];
     CONTINENTS.forEach((continent, index) => {
-      const serie = (series || []).filter(s => s.continent === continent.id)[0];
-      if (!haveSomeContinents || serie) {
+      const continentState = this.state.continents[continent.id];
+      if (continentState.visible) {
+        const serie = continentState.serie;
         continents.push(
           this._renderContinent(continent, index, colorIndex, serie));
       }
@@ -394,12 +537,13 @@ export default class WorldMap extends Component {
       }
     }
 
-    return (
+    let contents = (
       <svg {...props} {...interactiveProps}
         ref={(ref) => this._worldMapRef = ref}
         className={classes} version='1.1'
         preserveAspectRatio='xMidYMid meet'
-        width={`${width}px`} viewBox={`0 0 ${width} ${height}`}>
+        width={`${width}px`}
+        viewBox={`${x} ${y} ${width} ${height}`}>
         <g stroke='none' fill='none' fillRule='evenodd'>
           {continents}
         </g>
@@ -407,6 +551,21 @@ export default class WorldMap extends Component {
         {placesGroup}
       </svg>
     );
+
+    const flagged = (series || []).filter(s => s.flag);
+    if (flagged.length > 0) {
+      contents = (
+        <div className={`${CLASS_ROOT}__container`}>
+          {contents}
+          {flagged.map((serie, index) => (
+            <div key={index} className={`${CLASS_ROOT}__flag`}
+              ref={ref => (this._flagRefs[index] = ref)}>{serie.flag}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return contents;
   }
 }
 
@@ -418,8 +577,11 @@ WorldMap.propTypes = {
   series: PropTypes.arrayOf(PropTypes.shape({
     continent: PropTypes.oneOf(CONTINENTS.map(c => c.id)),
     colorIndex: PropTypes.string,
+    flag: PropTypes.node,
     label: PropTypes.string, // for a11y aria-label
     onClick: PropTypes.func,
+    onHover: PropTypes.func,
     place: PropTypes.arrayOf(PropTypes.number)
-  }))
+  })),
+  zoom: PropTypes.bool
 };
