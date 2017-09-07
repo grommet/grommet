13198c9597813fd1379a09b31ded767ab9c22a18
Eric Soderberg <eric.soderberg@hpe.com>
2017-09-07 16:49:13 -0700

    Renamed component directories to match component name case (#1603)
    * Renamed component directories to match component name case
    
    * Renamed component directories to match component name case missing files

---
+++ src/js/components/Heading/__tests__/Heading-test.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import { Grommet } from '../../Grommet';
+import { Heading } from '../';
+
+test('Heading renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Heading level renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading level={1} />
+      <Heading level={2} />
+      <Heading level={3} />
+      <Heading level={4} />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Heading size renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading level={1} size='small' />
+      <Heading level={1} size='medium' />
+      <Heading level={1} size='large' />
+      <Heading level={2} size='small' />
+      <Heading level={2} size='medium' />
+      <Heading level={2} size='large' />
+      <Heading level={3} size='small' />
+      <Heading level={3} size='medium' />
+      <Heading level={3} size='large' />
+      <Heading level={4} size='small' />
+      <Heading level={4} size='medium' />
+      <Heading level={4} size='large' />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Heading textAlign renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading textAlign='start' />
+      <Heading textAlign='center' />
+      <Heading textAlign='end' />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Heading margin renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading margin='small' />
+      <Heading margin='medium' />
+      <Heading margin='large' />
+      <Heading margin='none' />
+      <Heading margin={{ bottom: 'small' }} />
+      <Heading margin={{ top: 'small' }} />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+const LONG = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
+
+test('Heading truncate renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Heading truncate={false}>{LONG}</Heading>
+      <Heading truncate={true}>{LONG}</Heading>
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/js/components/Markdown/__tests__/Markdown-test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import { Grommet } from '../../Grommet';
+import { Markdown } from '../';
+
+const CONTENT = `
+# H1
+
+Paragraph
+
+## H2
+
+### H3
+
+#### H4
+`;
+
+test('Markdown renders', () => {
+  const component = renderer.create(
+    <Grommet>
+      <Markdown content={CONTENT} />
+    </Grommet>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
