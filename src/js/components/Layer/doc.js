4e8399280f771b60c8f637947b7f10fee50011f2
Alan Souza <alansouzati@gmail.com>
2017-09-08 18:17:05 -0700

    Layer docs

--- src/js/components/Layer/doc.js
+++ src/js/components/Layer/doc.js
@@ -12,9 +12,13 @@ export default Layer => schema(Layer, {
         defaultProp: 'center',
       },
     ],
+    context: [
+      PropTypes.object,
+      'Object with the context variables to be passed to the Layer.',
+    ],
     onEsc: [
       PropTypes.func,
-      'Function that will be called when the user presses the escape key inside the Layer',
+      'Function that will be called when the user presses the escape key inside the Layer.',
     ],
     size: [
       PropTypes.oneOf(
