a082624a398332a3f34921738fa0376fff48a11d
Ledion Bitincka <lbitincka@gmail.com>
2017-09-15 23:04:40 -0700

    do not show the menu's control if when expanded the menu doesn't overlap with the control (e.g. when someone wants *only* the menu items to display below the control, ie top: bottom)

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -166,13 +166,23 @@ class MenuDrop extends Component {
     });
 
     let contents = [
-      React.cloneElement(control, {key: 'control', fill: true}),
       <Box {...restProps} key="nav" ref={ref => this.navContainerRef = ref}
         tag="nav" className={`${CLASS_ROOT}__contents`}
         primary={false}>
         {menuDropChildren}
       </Box>
     ];
+
+    // do not show the control if menu doesn't overlap with it when expanded
+    const showControl =
+        ('top' === dropAlign.top || 'bottom' === dropAlign.bottom) && 
+        ('left' === dropAlign.left || 'right' === dropAlign.right);
+    
+    if(showControl) {
+      contents.unshift(
+                  React.cloneElement(control, {key: 'control', fill: true}));
+    }
+
 
     if (dropAlign.bottom) {
       contents.reverse();
