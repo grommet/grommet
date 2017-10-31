cbe08eb13a47d763aeb0e5773951da0c5c1719b4
Alan Souza <alansouzati@gmail.com>
2017-10-31 12:50:00 -0700

    Updated layer doc

--- src/js/components/Layer/doc.js
+++ src/js/components/Layer/doc.js
@@ -20,7 +20,9 @@ export default (Layer) => {
     onEsc: PropTypes.func.description(
       'Function that will be called when the user presses the escape key inside the Layer.'
     ),
-    plain: PropTypes.bool.description('Whether this is a plain Layer with not background color.'),
+    plain: PropTypes.bool.description(
+      'Whether this is a plain Layer with no background color or border.'
+    ),
   };
 
   return DocumentedLayer;
