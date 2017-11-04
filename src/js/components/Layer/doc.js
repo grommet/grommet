d7637e0245cc89da4b47d31f35381ea1b635ec74
Alan Souza <alansouzati@gmail.com>
2017-11-03 17:23:58 -0700

    Added layer plain. (#1708)
    * Added layer plain.
    
    * Updated layer doc
    
    * Removed display flex from stack

--- src/js/components/Layer/doc.js
+++ src/js/components/Layer/doc.js
@@ -20,19 +20,8 @@ export default (Layer) => {
     onEsc: PropTypes.func.description(
       'Function that will be called when the user presses the escape key inside the Layer.'
     ),
-    size: PropTypes.oneOf(
-      [
-        'xxsmall',
-        'xsmall',
-        'small',
-        'medium',
-        'large',
-        'xlarge',
-        'xxlarge',
-        'full',
-      ]
-    ).description(
-      'Size for the Layer.'
+    plain: PropTypes.bool.description(
+      'Whether this is a plain Layer with no background color or border.'
     ),
   };
 
