c5337762c1b891ad58caa57b4f3b352363c0eb57
Eric Soderberg <eric.soderberg@hpe.com>
2018-01-17 07:59:40 -0800

    Re-implemented Cookies to avoid license issues (#1795)

--- src/js/utils/Cookies.js
+++ src/js/utils/Cookies.js
@@ -1,68 +1,20 @@
-// (C) Copyright 2015 Hewlett Packard Enterprise Development LP
-
-// Slightly modified version of the Mozilla Developer Network version.
-// renamed the module and the functions.
-
-/*\
-|*|
-|*|  :: cookies.js ::
-|*|
-|*|  A complete cookies reader/writer framework with full unicode support.
-|*|
-|*|  Revision #1 - September 4, 2014
-|*|
-|*|  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
-|*|  https://developer.mozilla.org/User:fusionchess
-|*|
-|*|  This framework is released under the GNU Public License, version 3 or later.
-|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
-|*|
-|*|  Syntaxes:
-|*|
-|*|  * Cookies.set(name, value[, end[, path[, domain[, secure]]]])
-|*|  * Cookies.get(name)
-|*|  * Cookies.remove(name[, path[, domain]])
-|*|  * Cookies.has(name)
-|*|  * Cookies.keys()
-|*|
-\*/
+// (C) Copyright 2015-2018 Hewlett Packard Enterprise Development LP
+import JSCookie from 'js-cookie';
 
 export default {
-  get (sKey) {
-    if (!sKey) { return null; }
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+  get (name) {
+    return JSCookie.get(name);
   },
-  set (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toUTCString();
-          break;
-      }
-    }
-    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-    return true;
+  set (name, value, expires, path, domain, secure) {
+    return JSCookie.set(name, value, { expires, path, domain, secure } );
   },
-  remove (sKey, sPath, sDomain) {
-    if (!this.has(sKey)) { return false; }
-    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-    return true;
+  remove (name, path, domain) {
+    JSCookie.remove(name, { path, domain });
   },
-  has (sKey) {
-    if (!sKey) { return false; }
-    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  has (name) {
+    return JSCookie.get(name) ? true : false;
   },
   keys () {
-    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-    return aKeys;
+    return Object.keys(JSCookie.get());
   }
 };
