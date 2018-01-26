1a140016aacec806dc650be36a377e02a4592e6a
atanasster <atanasster@gmail.com>
2018-01-26 11:16:16 -0500

    fix for #1812

--- src/js/utils/DOM.js
+++ src/js/utils/DOM.js
@@ -32,7 +32,7 @@ export function findScrollParents (element, horizontal) {
     }
     parent = parent.parentNode;
   }
-  // last scrollable element will be the document 
+  // last scrollable element will be the document
   // if nothing else is scrollable in the page
   if (result.length === 0) {
     result.push(document);
@@ -185,4 +185,13 @@ export function checkDarkBackground (colorIndex, element, handler) {
     }
   }
   return { stop: () => clearTimeout(timer) };
+}
+
+export function findVisibleParent (element) {
+  if (element) {
+    return element.offsetParent ?
+      element : findVisibleParent(element.parentElement);
+  } else {
+    return null;
+  }
 }
