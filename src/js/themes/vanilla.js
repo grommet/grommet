0b3dbf3f501841d421b090708063633e01368746
Eric Soderberg <eric.soderberg@hpe.com>
2018-02-16 14:23:36 -0800

    Added elevation to Box (#1881)
    * Added elevation to Box
    
    * Fixed a documentation issue

--- src/js/themes/vanilla.js
+++ src/js/themes/vanilla.js
@@ -87,6 +87,14 @@ export default deepFreeze({
       large: `${baseSpacing * 2}px`,
       xlarge: `${baseSpacing * 4}px`,
     },
+    elevation: {
+      none: 'none',
+      xsmall: '0px 1px 2px rgba(100, 100, 100, 0.50)',
+      small: '0px 2px 4px rgba(100, 100, 100, 0.50)',
+      medium: '0px 3px 8px rgba(100, 100, 100, 0.50)',
+      large: '0px 6px 12px rgba(100, 100, 100, 0.50)',
+      xlarge: '0px 8px 16px rgba(100, 100, 100, 0.50)',
+    },
     focus: {
       border: {
         color: css`${props => colorForName('accent-1', props.theme)}`,
