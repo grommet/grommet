2a1fee05e0f7edd83d730d00ae807ed4d5b577db
Eric Soderberg <eric.soderberg@hpe.com>
2018-02-18 14:34:01 -0800

    Changed RadioButton to require 'name'

--- src/js/components/RadioButton/doc.js
+++ src/js/components/RadioButton/doc.js
@@ -24,7 +24,7 @@ with the same name so form submissions work.`
     label: PropTypes.node.description('Label text to place next to the control.'),
     name: PropTypes.string.description(
       'The DOM name attribute value to use for the underlying <input/> element.'
-    ),
+    ).isRequired,
     onChange: PropTypes.func.description(
       `Function that will be called when the user clicks the radio button. It
       will be passed a React event object. The current state can be accessed
