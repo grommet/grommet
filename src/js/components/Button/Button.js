2e674b7e4d99cecacc14a9e19feb2f4f4d777107
Eric Soderberg <eric.soderberg@hpe.com>
2018-02-21 10:10:01 -0800

    Remove unnecessary tabindexes

--- src/js/components/Button/Button.js
+++ src/js/components/Button/Button.js
@@ -61,11 +61,8 @@ class Button extends Component {
       ['reset', 'submit'].indexOf(type) === -1
     );
 
-    // tabIndex is needed because you may use button inside an svg
-    // without tabIndex it does not work in that scenario
     return (
       <Tag
-        tabIndex='0'
         {...rest}
         aria-label={a11yTitle}
         disabled={disabled}
