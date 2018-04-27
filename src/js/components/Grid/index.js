8243871c903d2d80122a2c87592880397c95da68
Eric Soderberg <eric.soderberg@hpe.com>
2018-04-27 12:05:02 -0700

    Added Grid.available

--- src/js/components/Grid/index.js
+++ src/js/components/Grid/index.js
@@ -2,4 +2,7 @@ import Grid from './Grid';
 
 export { default as Grid } from './Grid';
 
+Grid.available = window && window.CSS && window.CSS.supports &&
+  window.CSS.supports('display', 'grid');
+
 export default Grid;
