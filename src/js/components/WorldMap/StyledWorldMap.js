3652cc86fe3a54edb8010f5e845f0cf864a90081
Sean Kim <nogever@gmail.com>
2018-06-07 15:26:43 -0400

    Remove old IE11 style fix
    Signed-off-by: Sean Kim <nogever@gmail.com>

--- src/js/components/WorldMap/StyledWorldMap.js
+++ src/js/components/WorldMap/StyledWorldMap.js
@@ -2,13 +2,6 @@ import styled from 'styled-components';
 
 const StyledWorldMap = styled.svg`
   width: 100%;
-
-  // IE11 fix world map height
-  @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
-    // The padding value below comes from svg height / width
-    padding-bottom: 49%; // 460 / 940 = .489361702 rounding off to 49
-    height: 1px;
-  }
 `;
 
 export default StyledWorldMap.extend`
