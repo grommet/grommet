0eb5eab9cf03589874486b6fd43404401fa621b3
Eric Soderberg <eric.soderberg@hpe.com>
2018-06-14 15:05:33 -0700

    Fixed an issue with icon only Button padding

--- src/js/components/Button/button.stories.js
+++ src/js/components/Button/button.stories.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Add } from 'grommet-icons';
 
 import Button from '../Button/Button';
 import Grommet from '../Grommet/Grommet';
+import Box from '../Box/Box';
+import Text from '../Text/Text';
 
-class SimpleButton extends Component {
-  render() {
-    return (
-      <Grommet>
-        <Button label='Submit' onClick={() => {}} />
-      </Grommet>
-    );
-  }
-}
+const SimpleButton = () => (
+  <Grommet>
+    <Button label='Submit' onClick={() => {}} />
+  </Grommet>
+);
+
+const IconButton = () => (
+  <Grommet>
+    <Button icon={<Add />} hoverIndicator={true} onClick={() => {}} />
+  </Grommet>
+);
+
+const PlainButton = () => (
+  <Grommet>
+    <Button hoverIndicator={true} onClick={() => {}}>
+      <Box pad='small' direction='row' align='center' gap='small'>
+        <Add />
+        <Text>Add</Text>
+      </Box>
+    </Button>
+  </Grommet>
+);
 
 storiesOf('Button', module)
-  .add('Simple Button', () => <SimpleButton />);
+  .add('Simple Button', () => <SimpleButton />)
+  .add('Icon Button', () => <IconButton />)
+  .add('Plain Button', () => <PlainButton />);
