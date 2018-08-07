530b0408851bb798839f8b3795bbda26fdb00cb8
luvsharma19 <38468453+luvsharma19@users.noreply.github.com>
2018-08-07 23:02:14 +0530

    Added defaultMessage as an argument
    Adding "defaultMessage" argument to the getMessage function so that the defaultMessage value is not always same as the key argument value which is passed to the getMessage function. 
    Due to the absence of "defaultMessage" argument,  developers are forced to keep the value of key argument as what they want the defaultMessage to be shown. Devs want to name their keys in a unique manner and sometimes component specific however the current code restricts it as only key argument is present in the function.

--- src/js/utils/Intl.js
+++ src/js/utils/Intl.js
@@ -1,10 +1,10 @@
 // (C) Copyright 2014 Hewlett Packard Enterprise Development LP
 export default {
-  getMessage (intl, key, values) {
+  getMessage (intl, key, values, defaultMessage) {
     if (intl) {
       return intl.formatMessage({
         id: key,
-        defaultMessage: key
+        defaultMessage: defaultMessage || key
       }, values);
     } else {
       return key;
