3acf8ef63e32acd52fcbead5ef0a98fdb203d46e
Alan Souza <alansouzati@gmail.com>
2018-08-20 19:30:22 -0700

    Added clean webpack plugin (#2188)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,9 @@
 import path from 'path';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const plugins = [
+  new CleanWebpackPlugin(['dist']),
   new CopyWebpackPlugin([
     { from: './README.md' },
     { from: './package.json' },
