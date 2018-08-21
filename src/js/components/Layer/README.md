6ba6db62fb465a558482d17a9c27201de65f1ba7
Eric Soderberg <eric.soderberg@hpe.com>
2018-08-21 09:08:39 -0700

    Added storybook example links (#2187)

--- src/js/components/Layer/README.md
+++ src/js/components/Layer/README.md
@@ -2,7 +2,7 @@
 A modal overlay. It is the caller's responsibility to provide a control for
       the user to close the layer.
 
-[![](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=layer&module=%2Fsrc%2FLayer.js)
+[![](https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png)](https://storybook.grommet.io/?selectedKind=Layer&full=0&addons=0&stories=1&panelRight=0) [![](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=layer&module=%2Fsrc%2FLayer.js)
 ## Usage
 
 ```javascript
--- src/js/utils/mixins.js
+++ src/js/utils/mixins.js
@@ -47,10 +47,18 @@ export function findAllByType(component, type) {
 }
 
 export function getAvailableAtBadge(availableAt) {
-  return {
-    url: `https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=${availableAt.toLowerCase()}&module=%2Fsrc%2F${availableAt}.js`,
-    badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
-  };
+  return [
+    {
+      url: `https://storybook.grommet.io/?selectedKind=${availableAt}&full=0&addons=0&stories=1&panelRight=0`,
+      badge: 'https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png',
+      label: 'Storybook',
+    },
+    {
+      url: `https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=${availableAt.toLowerCase()}&module=%2Fsrc%2F${availableAt}.js`,
+      badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
+      label: 'CodeSandbox',
+    },
+  ];
 }
 
 export default {
