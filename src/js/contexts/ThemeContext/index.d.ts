e7632a3ce068011cb55ac9fe7b92d09a8b728e64
Frank Thomschke <frank@thomschke.de>
2018-09-03 08:20:02 +0200

    add missing typescript definitions

---
+++ src/js/contexts/ThemeContext/index.d.ts
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export type ThemeValue = object;
+
+export interface ThemeContext extends React.Context<ThemeValue> {
+  Extend: React.ComponentType<{ value: ThemeValue }>
+}
+
+declare const ThemeContext: ThemeContext;
+
+export { ThemeContext };
+
