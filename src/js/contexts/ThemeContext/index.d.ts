db54a03077e0b7ff7abffc5c7ead75c9419cec9e
Frank Thomschke <frank@thomschke.de>
2018-09-05 10:53:12 +0200

    ts declarations based on React.SFC (manual)

--- src/js/contexts/ThemeContext/index.d.ts
+++ src/js/contexts/ThemeContext/index.d.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 export type ThemeValue = object;
 
 export interface ThemeContext extends React.Context<ThemeValue> {
-  Extend: React.ComponentType<{ value: ThemeValue }>
+  Extend: React.StatelessComponent<{ value: ThemeValue }>
 }
 
 declare const ThemeContext: ThemeContext;
