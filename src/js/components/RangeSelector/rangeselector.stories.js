0a622dc33dd0f2db7eb84170c5de761da44f8bdb
Eric Soderberg <eric.soderberg@hpe.com>
2018-11-05 19:23:23 -0800

    Changed RangeSelect to fix issues with step and vertical

--- src/js/components/RangeSelector/rangeselector.stories.js
+++ src/js/components/RangeSelector/rangeselector.stories.js
@@ -5,38 +5,55 @@ import { Grommet, Box, RangeSelector, Stack, Text } from 'grommet';
 import { grommet } from 'grommet/themes';
 
 class SimpleRangeSelector extends Component {
-  state = { values: [2, 8] };
+  static defaultProps = {
+    direction: 'horizontal',
+  };
+
+  state = { values: [12, 16] };
 
   onChange = values => this.setState({ values });
 
   render() {
+    const { direction, ...rest } = this.props;
     const { values } = this.state;
     return (
       <Grommet theme={grommet}>
-        <Stack>
-          <Box direction="row" justify="between">
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(value => (
-              <Box key={value} pad="small" border={false}>
-                <Text style={{ fontFamily: 'monospace' }}>{value}</Text>
-              </Box>
-            ))}
-          </Box>
-          <RangeSelector
-            direction="horizontal"
-            invert={false}
-            min={0}
-            max={9}
-            size="full"
-            round="small"
-            values={values}
-            onChange={this.onChange}
-          />
-        </Stack>
+        <Box align="start">
+          <Stack>
+            <Box
+              direction={direction === 'vertical' ? 'column' : 'row'}
+              justify="between"
+            >
+              {[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(value => (
+                <Box
+                  key={value}
+                  width="xxsmall"
+                  height="xxsmall"
+                  align="center"
+                  pad="small"
+                  border={false}
+                >
+                  <Text style={{ fontFamily: 'monospace' }}>{value}</Text>
+                </Box>
+              ))}
+            </Box>
+            <RangeSelector
+              direction={direction}
+              min={10}
+              max={20}
+              size="full"
+              values={values}
+              onChange={this.onChange}
+              {...rest}
+            />
+          </Stack>
+        </Box>
       </Grommet>
     );
   }
 }
 
-storiesOf('RangeSelector', module).add('Simple RangeSelector', () => (
-  <SimpleRangeSelector />
-));
+storiesOf('RangeSelector', module)
+  .add('Simple', () => <SimpleRangeSelector />)
+  .add('Step', () => <SimpleRangeSelector step={2} />)
+  .add('Vertical', () => <SimpleRangeSelector direction="vertical" />);
