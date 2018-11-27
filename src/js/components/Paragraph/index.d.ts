1357497aac3876a240c319ac3f12f4ee91235bfc
shimisun <shimrit.yacobi@gmail.com>
2018-11-27 12:17:32 -0700

    adding responsive docs for paragraph

--- src/js/components/Paragraph/index.d.ts
+++ src/js/components/Paragraph/index.d.ts
@@ -6,6 +6,7 @@ export interface ParagraphProps {
   gridArea?: string;
   margin?: "none" | "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | {bottom: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,horizontal: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,left: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,right: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,top: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,vertical: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string} | string;
   color?: string;
+  responsive?: boolean;
   size?: "small" | "medium" | "large" | "xlarge" | "xxlarge" | string;
   textAlign?: "start" | "center" | "end";
 }
