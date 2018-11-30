c7c677bd92cde5d076b37d8c68448c28ac7d00d9
Alan Souza <alansouzati@gmail.com>
2018-11-30 10:22:28 -0800

    Added tests for default theme props.

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,13 @@
 import { defaultProps } from './default-props';
+import { deepMerge } from './utils';
+import { base } from './themes';
 
 export * from './components';
 export * from './contexts';
+export * from './default-props';
 export * from './themes';
 
-let initialized = false;
-export const initializeDefaults = props => {
-  if (initialized) {
-    console.warn(
-      '[grommet]: default theme has been previously initialized. Make sure to only intialize the default theme once.',
-    );
-  }
-  Object.keys(props).forEach(key => (defaultProps[key] = props[key])); // eslint-disable-line no-return-assign
-  initialized = true;
-};
+/* eslint-disable no-return-assign */
+export const extendDefaultTheme = theme =>
+  (defaultProps.theme = deepMerge(base, theme));
+/* eslint-enable no-return-assign */
