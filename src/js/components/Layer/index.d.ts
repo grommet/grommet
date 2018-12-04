791aa53bad2b279cb255e85927cf8d85794a3d94
Orestis Ioannou <oorestisime@gmail.com>
2018-12-04 10:22:37 +0100

    Update react-desc dependency and TS definitions for optional shape properties (#2524)
    Closes: #2464
    
    Signed-off-by: Orestis Ioannou <oorestisime@gmail.com>

--- src/js/components/Layer/index.d.ts
+++ src/js/components/Layer/index.d.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export interface LayerProps {
   full?: boolean | "vertical" | "horizontal";
-  margin?: "none" | "xxsmall" | "xsmall" | "small" | "medium" | "large" | {bottom: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,horizontal: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,left: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,right: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,top: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,vertical: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string} | string;
+  margin?: "none" | "xxsmall" | "xsmall" | "small" | "medium" | "large" | {bottom?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,horizontal?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,left?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,right?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,top?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string,vertical?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | string} | string;
   modal?: boolean;
   onClickOutside?: (...args: any[]) => any;
   onEsc?: (...args: any[]) => any;
