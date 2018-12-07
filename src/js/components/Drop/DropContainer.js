d0dde11d43b78e1295a89e6e7376850ea6ce25f0
Alan Souza <alansouzati@gmail.com>
2018-12-06 17:24:56 -0800

    Added just the integration with grommet icons v4

--- src/js/components/Drop/DropContainer.js
+++ src/js/components/Drop/DropContainer.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import { compose } from 'recompose';
-
-import { withTheme } from 'styled-components';
-
-import { ThemeContext as IconThemeContext } from 'grommet-icons/contexts';
 
 import { defaultProps } from '../../default-props';
 import { ThemeContext } from '../../contexts';
@@ -30,6 +25,7 @@ const preventLayerClose = event => {
 };
 
 class DropContainer extends Component {
+  static contextType = ThemeContext;
   static defaultProps = {
     align: {
       top: 'top',
@@ -37,38 +33,6 @@ class DropContainer extends Component {
     },
     stretch: 'width',
   };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // Since the drop background can be different from the current theme context,
-    // we update the theme to set the dark background context.
-    const { theme: propsTheme } = nextProps;
-    const { theme: stateTheme, priorTheme } = prevState;
-
-    const dark = backgroundIsDark(
-      propsTheme.global.drop.background,
-      propsTheme,
-    );
-
-    if (dark === propsTheme.dark && stateTheme) {
-      return { theme: undefined, priorTheme: undefined };
-    }
-    if (
-      dark !== propsTheme.dark &&
-      (!stateTheme || dark !== stateTheme.dark || propsTheme !== priorTheme)
-    ) {
-      return {
-        theme: {
-          ...propsTheme,
-          dark,
-          icon: dark ? propsTheme.iconThemes.dark : propsTheme.iconThemes.light,
-        },
-        priorTheme: propsTheme,
-      };
-    }
-    return null;
-  }
-
-  state = {};
 
   dropRef = React.createRef();
 
@@ -274,13 +238,12 @@ class DropContainer extends Component {
       onClickOutside,
       onEsc,
       onKeyDown,
-      theme: propsTheme,
       elevation,
       plain,
+      theme: propsTheme,
       ...rest
     } = this.props;
-    const { theme: stateTheme } = this.state;
-    const theme = stateTheme || propsTheme;
+    const theme = this.context || propsTheme;
 
     let content = (
       <StyledDrop
@@ -300,19 +263,15 @@ class DropContainer extends Component {
       </StyledDrop>
     );
 
-    if (stateTheme) {
-      if (stateTheme.dark !== propsTheme.dark && stateTheme.icon) {
+    if (theme.global.drop.background) {
+      const dark = backgroundIsDark(theme.global.drop.background, theme);
+      if (dark !== theme.dark) {
         content = (
-          <IconThemeContext.Provider value={stateTheme.icon}>
+          <ThemeContext.Provider value={{ ...theme, dark }}>
             {content}
-          </IconThemeContext.Provider>
+          </ThemeContext.Provider>
         );
       }
-      content = (
-        <ThemeContext.Provider value={stateTheme}>
-          {content}
-        </ThemeContext.Provider>
-      );
     }
 
     return (
@@ -331,6 +290,4 @@ class DropContainer extends Component {
 
 Object.setPrototypeOf(DropContainer.defaultProps, defaultProps);
 
-const DropContainerWrapper = compose(withTheme)(DropContainer);
-
-export { DropContainerWrapper as DropContainer };
+export { DropContainer };
--- src/js/themes/base.js
+++ src/js/themes/base.js
@@ -2,6 +2,7 @@ import { rgba } from 'polished';
 import { css } from 'styled-components';
 
 import {
+  base as iconBase,
   Actions,
   ClosedCaption,
   Expand,
@@ -18,7 +19,7 @@ import {
   VolumeLow,
 } from 'grommet-icons';
 
-import { normalizeColor, deepFreeze } from '../utils';
+import { deepMerge, deepFreeze, normalizeColor } from '../utils';
 
 const brandColor = '#7D4CDB';
 const accentColors = ['#6FFFB0', '#FD6FFF', '#6FFFB0', '#FFCA58'];
@@ -68,6 +69,10 @@ const colors = {
     dark: '#f8f8f8',
     light: '#444444',
   },
+  icon: {
+    dark: '#f8f8f8',
+    light: '#666666',
+  },
   white: '#FFFFFF',
 };
 
@@ -99,7 +104,7 @@ export const generate = (baseSpacing = 24, scale = 6) => {
 
   const borderWidth = 2;
 
-  const result = {
+  const result = deepMerge(iconBase, {
     global: {
       animation: {
         duration: '1s',
@@ -531,10 +536,6 @@ export const generate = (baseSpacing = 24, scale = 6) => {
       responsiveBreakpoint: 'small', // when we scale the font size down
       weight: 600,
     },
-    icon: {
-      colors,
-    },
-    iconThemes: {},
     layer: {
       background: 'white',
       border: {
@@ -743,7 +744,7 @@ export const generate = (baseSpacing = 24, scale = 6) => {
         base: '8px',
       },
     },
-  };
+  });
 
   return deepFreeze(result);
 };
