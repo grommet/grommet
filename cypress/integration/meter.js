9e7033dd98d3b0ae08801641c4070becb04f890c
Orestis Ioannou <oorestisime@gmail.com>
2018-12-13 14:44:24 +0100

    Bootstrap cypress visual regression testing

---
+++ cypress/integration/meter.js
@@ -0,0 +1,19 @@
+describe('Meter storybook', () => {
+  beforeEach(() => {
+      // Visiting the storybook each test removes any state build up from other tests
+      // This should probably default to index story and then have each context pick
+      // its own component and test it
+      cy.visit('https://storybook.grommet.io/?selectedKind=Meter&selectedStory=Bar')
+  })
+  // Let's build some tests around the Meter story
+  context('Default meter', () => {
+    it('capture default meter', () => {
+      cy.get("#storybook-preview-iframe").screenshot()
+    })
+
+    it('capture circle meter', () => {
+      cy.get('a[href="/?selectedKind=Meter&selectedStory=Circle"]').click();
+      cy.get("#storybook-preview-iframe").screenshot()
+    })
+  })
+})
