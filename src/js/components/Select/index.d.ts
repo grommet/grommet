5bd2964e91b4dc7c91ed34d715cbb140b4cd3fdf
Eric Soderberg <eric.soderberg@hpe.com>
2019-01-04 10:09:52 -0800

    Updated react-desc and fixed some typescript typings

--- src/js/components/Select/index.d.ts
+++ src/js/components/Select/index.d.ts
@@ -7,7 +7,7 @@ export interface SelectProps {
   margin?: "none" | "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | {bottom?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,horizontal?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,left?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,right?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,top?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,vertical?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string} | string;
   children?: ((...args: any[]) => any);
   closeOnChange?: boolean;
-  disabled?: boolean | number | string | object[];
+  disabled?: boolean | (number | string | object)[];
   disabledKey?: string | ((...args: any[]) => any);
   dropAlign?: {top?: "top" | "bottom",bottom?: "top" | "bottom",right?: "left" | "right",left?: "left" | "right"};
   dropHeight?: "xsmall" | "small" | "medium" | "large" | "xlarge" | string;
@@ -20,13 +20,13 @@ export interface SelectProps {
   onClose?: ((...args: any[]) => any);
   onOpen?: ((...args: any[]) => any);
   onSearch?: ((...args: any[]) => any);
-  options: string | JSX.Element | object[];
+  options: (string | JSX.Element | object)[];
   placeholder?: string | React.ReactNode;
   plain?: boolean;
   searchPlaceholder?: string;
   selected?: number | number[];
   size?: "small" | "medium" | "large" | "xlarge" | string;
-  value?: string | JSX.Element | object | string | object[];
+  value?: string | JSX.Element | object | (string | object)[];
   valueLabel?: React.ReactNode;
   valueKey?: string | ((...args: any[]) => any);
   emptySearchMessage?: string;
