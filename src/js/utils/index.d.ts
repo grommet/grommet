4d698949bce5d8300ae31f667619737ea36154ab
Jakub Wadas <vadistic@gmail.com>
2019-01-12 04:57:59 +0100

    typescript: grommet/utils object.js typings

---
+++ src/js/utils/index.d.ts
@@ -0,0 +1,25 @@
+// object.js
+export type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K]
+};
+
+export type NonUndefined<T> = T extends undefined ? never: T
+
+export type NonUndefinedProps<T extends object> = {
+  [K in keyof T]?: NonUndefined<T[K]>
+}
+export type DeepFreeze = <T extends object>(obj: T) => DeepReadonly<T>
+
+// overload because generic variadic solution has messy result and all/most mergings are binary
+export interface DeepMerge {
+  <T extends object, S extends object>(target: T, source: S): T & S
+  <T extends object, S extends object[]>(target: T, ...sources: S): T & S[number]
+}
+
+declare const isObject: (item:any) => boolean
+declare const deepFreeze: DeepFreeze
+declare const deepMerge: DeepMerge
+declare const removeUndefined: <T extends object>(obj: T) => NonUndefinedProps<T>
+
+export {isObject, deepFreeze, deepMerge, removeUndefined}
+
