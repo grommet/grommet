2c9c6f076df5f1ded5b90bc8e5316e887616bed1
shimisun <shimrit.yacobi@gmail.com>
2019-01-12 20:40:08 -0800

    update docs

--- src/js/contexts/ResponsiveContext/doc.js
+++ src/js/contexts/ResponsiveContext/doc.js
@@ -16,10 +16,53 @@ export const doc = ResponsiveContext => {
   DocumentedResponsiveContext.propTypes = {
     children: PropTypes.func.description(
       `Render function that will be called with the current screen resolution
-      size (e.g 'wide', 'narrow'). The size value will be derived from global.breakpoints entry
+      size (e.g our base theme of size 'small', 'medium', 'large'). The size value will be derived from global.breakpoints entry
       in the theme object.`,
     ),
   };
 
   return DocumentedResponsiveContext;
+};
+
+export const themeDoc = {
+  'global.breakpoints': {
+    description:
+      'The possible breakpoints that could affect border, direction, gap, margin, pad, and round.',
+    type: 'object',
+    defaultValue: `{
+    small: {
+      value: '768px',
+      borderSize: {
+        xsmall: '1px',
+        small: '2px',
+        medium: '4px',
+        large: '6px',
+        xlarge: '12px',
+      },
+      edgeSize: {
+        none: '0px',
+        hair: '1px',
+        xxsmall: '2px',
+        xsmall: '3px',
+        small: '6px',
+        medium: '12px',
+        large: '24px',
+        xlarge: '48px',
+      },
+      size: {
+        xxsmall: '24px',
+        xsmall: '48px',
+        small: '96px',
+        medium: '192px',
+        large: '384px',
+        xlarge: '768px',
+        full: '100%',
+      },
+    },
+    medium: {
+      value: '1536px',
+    },
+    large: {},
+  }`,
+  },
 };
