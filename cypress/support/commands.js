a098901f12c62c1bf1241cea926b712c8026c4bc
Orestis Ioannou <oorestisime@gmail.com>
2019-01-16 00:41:17 +0100

    Try again to increase threshold

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,5 +27,8 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
   customDiffDir: 'cypress/snapshots/diff',
-  failureThreshold: 0.3,
+  failureThreshold: 0.03,
+  failureThresholdType: 'percent',
+  customDiffConfig: { threshold: 0.1 },
+  capture: 'viewport',
 });
