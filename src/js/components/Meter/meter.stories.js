3a6cb2d191ebb4d3d513e71fe2f7f5070aac5c97
Eric Soderberg <eric.soderberg@hpe.com>
2019-01-25 16:35:16 -0800

    Changed Meter to fix #2683

--- src/js/components/Meter/meter.stories.js
+++ src/js/components/Meter/meter.stories.js
@@ -12,13 +12,35 @@ const BarMeter = () => (
   </Grommet>
 );
 
-const CircleMeter = () => (
-  <Grommet theme={grommet}>
-    <Box align="center" pad="large">
-      <Meter type="circle" background="light-2" values={[{ value: 30 }]} />
-    </Box>
-  </Grommet>
-);
+class CircleMeter extends React.Component {
+  state = { value: 20 };
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      const { value } = this.state;
+      this.setState({ value: value < 100 ? value + 8 : 20 });
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <Grommet theme={grommet}>
+        <Box align="center" pad="large">
+          <Meter
+            type="circle"
+            background="light-2"
+            values={[{ value, color: value > 50 ? 'accent-2' : 'accent-1' }]}
+          />
+        </Box>
+      </Grommet>
+    );
+  }
+}
 
 const LabelledMeter = () => (
   <Grommet theme={grommet}>
