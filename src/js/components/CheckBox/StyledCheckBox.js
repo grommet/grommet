35cbca4675318b3888576ee4a9b3c69d54270faa
Eric Soderberg <eric.soderberg@hpe.com>
2019-01-30 09:46:53 -0800

    Changed CheckBox to fix z-index issue #2653 (#2705)

--- src/js/components/CheckBox/StyledCheckBox.js
+++ src/js/components/CheckBox/StyledCheckBox.js
@@ -54,7 +54,6 @@ const StyledCheckBoxInput = styled.input`
   width: 100%;
   height: 100%;
   margin: 0;
-  z-index: 1;
   ${props => !props.disabled && 'cursor: pointer;'} :checked + span > span {
     left: calc(
       ${props => props.theme.checkBox.toggle.size} -
@@ -119,9 +118,7 @@ const StyledCheckBoxKnob = styled.span`
 StyledCheckBoxKnob.defaultProps = {};
 Object.setPrototypeOf(StyledCheckBoxKnob.defaultProps, defaultProps);
 
-const StyledCheckBox = styled.div`
-  position: relative;
-`;
+const StyledCheckBox = styled.div``;
 
 StyledCheckBox.defaultProps = {};
 Object.setPrototypeOf(StyledCheckBox.defaultProps, defaultProps);
