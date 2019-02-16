1aeb4da1aef6e95cca03be0a04788c5734083b80
shimisun <shimrit.yacobi@gmail.com>
2019-02-15 20:39:05 -0700

    adding custome theme and docs to rangeInput

--- src/js/components/RangeInput/rangeinput.stories.js
+++ src/js/components/RangeInput/rangeinput.stories.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Box, Grommet, RangeInput } from 'grommet';
+import { Volume } from 'grommet-icons';
 import { grommet } from 'grommet/themes';
+import { deepMerge } from 'grommet/utils';
 
 class SimpleRangeInput extends Component {
   state = { value: 5 };
@@ -21,6 +23,51 @@ class SimpleRangeInput extends Component {
   }
 }
 
-storiesOf('RangeInput', module).add('Simple RangeInput', () => (
-  <SimpleRangeInput />
-));
+const customThemeRangeInput = deepMerge(grommet, {
+  global: {
+    spacing: '12px',
+  },
+  rangeInput: {
+    track: {
+      color: 'accent-2',
+      height: '12px',
+      extend: () => `border-radius: 10px`,
+    },
+    thumb: {
+      color: 'neutral-2',
+      extend: () => `
+        margin: 0;
+        `,
+    },
+  },
+});
+
+class CustomRangeInput extends Component {
+  state = { value: 0.4 };
+
+  onChange = event => this.setState({ value: event.target.value });
+
+  render() {
+    const { value } = this.state;
+    return (
+      <Grommet theme={customThemeRangeInput}>
+        <Box direction="row" align="center" pad="large" gap="small">
+          <Volume color="neutral-2" />
+          <Box align="center">
+            <RangeInput
+              min={0}
+              max={1}
+              step={0.1}
+              value={value}
+              onChange={this.onChange}
+            />
+          </Box>
+        </Box>
+      </Grommet>
+    );
+  }
+}
+
+storiesOf('RangeInput', module)
+  .add('Simple RangeInput', () => <SimpleRangeInput />)
+  .add('Custom RangeInput', () => <CustomRangeInput />);
