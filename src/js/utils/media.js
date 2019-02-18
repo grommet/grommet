11ed3a5a4f410953e076a01da0d683966da93c31
shimisun <shimrit.yacobi@gmail.com>
2019-02-18 09:54:07 -0700

    fix NaN dispaly for both Video and Audio

--- src/js/utils/media.js
+++ src/js/utils/media.js
@@ -1,13 +1,17 @@
+// return the time in a format of minutes:seconds. if time is not defined, return '' to avoid NaN.
 export const formatTime = time => {
-  let minutes = Math.round(time / 60);
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+  if (time) {
+    let minutes = Math.round(time / 60);
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let seconds = Math.round(time) % 60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
   }
-  let seconds = Math.round(time) % 60;
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  return `${minutes}:${seconds}`;
+  return '';
 };
 
 export const mediaEvents = [
