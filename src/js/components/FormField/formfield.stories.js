b128940609d13deaa25b05486c63d1080c08d0db
Alan Souza <alansouzati@gmail.com>
2019-02-19 16:18:34 -0800

    Fixed warning for text in formfield story

--- src/js/components/FormField/formfield.stories.js
+++ src/js/components/FormField/formfield.stories.js
@@ -147,8 +147,8 @@ const customFormFieldTheme = {
     label: {
       color: 'dark-3',
       size: 'xsmall',
-      margin: { vertical: 0, bottom: 'small', horizontal: 0 },
-      weight: '600',
+      margin: { vertical: '0', bottom: 'small', horizontal: '0' },
+      weight: 600,
     },
     border: false,
     margin: 0,
