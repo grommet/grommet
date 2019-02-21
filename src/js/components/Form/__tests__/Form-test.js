00af6f09a0af16c4396d30c12218a56c475ec4e8
Orestis Ioannou <oorestisime@gmail.com>
2019-02-21 19:37:06 +0100

    wip

--- src/js/components/Form/__tests__/Form-test.js
+++ src/js/components/Form/__tests__/Form-test.js
@@ -74,4 +74,33 @@ describe('Form', () => {
       expect.objectContaining({ value: { test: 'value', test2: 'value-2' } }),
     );
   });
+
+  test('regexp validation', () => {
+    const onSubmit = jest.fn();
+    const { getByPlaceholderText, getByText, queryByText } = render(
+      <Grommet>
+        <Form onSubmit={onSubmit}>
+          <FormField
+            name="test"
+            required
+            validate={{ regexp: /^[a-z]/i }}
+            placeholder="test input"
+          />
+          <Button type="submit" primary label="Submit" />
+        </Form>
+      </Grommet>,
+    );
+
+    fireEvent.change(getByPlaceholderText('test input'), {
+      target: { value: '1' },
+    });
+    fireEvent.click(getByText('Submit'));
+    expect(getByText('invalid')).toMatchSnapshot();
+
+    fireEvent.change(getByPlaceholderText('test input'), {
+      target: { value: 'a' },
+    });
+    fireEvent.click(getByText('Submit'));
+    expect(queryByText('invalid')).toBeNull();
+  });
 });
