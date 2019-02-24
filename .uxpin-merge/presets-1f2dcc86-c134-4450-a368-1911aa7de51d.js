c002ab6568407e3c8c418f803564f9fe8356fd89
Marcin Treder <marcin@uxpin.com>
2019-02-23 18:21:51 -0800

    Updated Tests

--- .uxpin-merge/presets-1f2dcc86-c134-4450-a368-1911aa7de51d.js
+++
@@ -1,5 +0,0 @@
-import Preset6f1d7671f37959dbab5a92cb10e03c6e from '../src/js/components/Button/presets/0-default.jsx';
-
-export {
-  Preset6f1d7671f37959dbab5a92cb10e03c6e,
-};
--- .uxpin-merge/presets-2071d90c-d2b9-46e5-935d-52164bc0977e.js
+++
@@ -1,9 +0,0 @@
-import Presetd35c38fe9aeb56f083dbcb64d90b103a from '../src/js/components/Accordion/presets/0-default.jsx';
-import Preset069e528d30995af2a64bd68713cea1f3 from '../src/js/components/AccordionPanel/presets/0-default.jsx';
-import Preset6f1d7671f37959dbab5a92cb10e03c6e from '../src/js/components/Button/presets/0-default.jsx';
-
-export {
-  Presetd35c38fe9aeb56f083dbcb64d90b103a,
-  Preset069e528d30995af2a64bd68713cea1f3,
-  Preset6f1d7671f37959dbab5a92cb10e03c6e,
-};
