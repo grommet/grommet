b78bdec2434c4ea78c437cdc1a2fdfb7e7430591
Marcin Treder <marcin@uxpin.com>
2019-02-25 19:18:35 -0800

    Change of theme to hpe

--- tools/UXPinWrapper.js
+++ tools/UXPinWrapper.js
@@ -7,7 +7,7 @@ import { Grommet } from '../src/js/components/Grommet';
 
 export default function UXPinWrapper({ children }) {
   return (
-    <Grommet theme={dxc}>
+    <Grommet theme={hpe}>
       {children}
     </Grommet>
   );
