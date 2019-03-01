4d0b6ae51d13b16934b3c8a32c0b9822083281be
Eric Soderberg <eric.soderberg@hpe.com>
2019-02-28 19:30:08 -0800

    Added Drop Tooltip story (#2860)

--- src/js/components/Drop/drop.stories.js
+++ src/js/components/Drop/drop.stories.js
@@ -444,9 +444,46 @@ class PlainDrop extends Component {
   }
 }
 
+class TooltipDrop extends Component {
+  state = {};
+
+  ref = createRef();
+
+  render() {
+    const { over } = this.state;
+    return (
+      <Grommet theme={grommet} full>
+        <Box fill align="center" justify="center">
+          <Button
+            label="Button"
+            ref={this.ref}
+            onMouseOver={() => this.setState({ over: true })}
+            onMouseOut={() => this.setState({ over: false })}
+            onFocus={() => {}}
+            onBlur={() => {}}
+          />
+          {this.ref.current && over && (
+            <Drop align={{ left: 'right' }} target={this.ref.current} plain>
+              <Box
+                margin="xsmall"
+                pad="small"
+                background="dark-3"
+                round={{ size: 'medium', corner: 'left' }}
+              >
+                tooltip contents
+              </Box>
+            </Drop>
+          )}
+        </Box>
+      </Grommet>
+    );
+  }
+}
+
 storiesOf('Drop', module)
   .add('Simple', () => <SimpleDrop />)
   .add('All not stretch', () => <AllDrops />)
   .add('Progressive', () => <ProgressiveDrop />)
   .add('Lazy', () => <LazyDrop />)
-  .add('Plain', () => <PlainDrop />);
+  .add('Plain', () => <PlainDrop />)
+  .add('Tooltip', () => <TooltipDrop />);
