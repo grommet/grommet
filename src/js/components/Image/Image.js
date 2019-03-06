3da8b28267323f6c78af98e338eb6ff972f89b5c
Orestis Ioannou <oorestisime@gmail.com>
2019-03-06 22:08:18 +0100

    Image fallback (#2873)
    * Add fallback image into image component
    
    Fix: #2791
    
    * Remove unused var
    
    * Reword description

--- src/js/components/Image/Image.js
+++ src/js/components/Image/Image.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import { StyledImage } from './StyledImage';
+
+const Image = ({ src, fallback, ...rest }) => {
+  const [imageMissing, setImageMissing] = useState(false);
+  return (
+    <StyledImage
+      {...rest}
+      onError={() => setImageMissing(true)}
+      src={!imageMissing ? src : fallback}
+    />
+  );
+};
 
 let ImageDoc;
 if (process.env.NODE_ENV !== 'production') {
-  ImageDoc = require('./doc').doc(StyledImage); // eslint-disable-line global-require
+  ImageDoc = require('./doc').doc(Image); // eslint-disable-line global-require
 }
-const ImageWrapper = ImageDoc || StyledImage;
+const ImageWrapper = ImageDoc || Image;
 ImageWrapper.displayName = 'Image';
 
 export { ImageWrapper as Image };
