2de8cdeaf05bdc06154f5533d3e3dbb554c27479
Marcin Treder <marcin@uxpin.com>
2019-03-06 17:44:12 -0800

    Modification of button prest. Test

--- src/js/components/Button/presets/0-default.jsx
+++ src/js/components/Button/presets/0-default.jsx
@@ -2,4 +2,4 @@
 import * as React from 'react';
 import Button from '../Button';
 
-export default <Button uxpId="button0" label="hello" fill />;
+export default <Button uxpId="button0" label="hello" fill primary />;
