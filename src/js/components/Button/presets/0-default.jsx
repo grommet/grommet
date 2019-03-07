d7529b6e66529bb19200bcb1caac3da5889381a9
Marcin Treder <marcin@uxpin.com>
2019-03-06 17:30:43 -0800

    Update of Button preset

--- src/js/components/Button/presets/0-default.jsx
+++ src/js/components/Button/presets/0-default.jsx
@@ -2,6 +2,4 @@
 import * as React from 'react';
 import Button from '../Button';
 
-export default (
-  <Button uxpId="button0" label="hello" fill />
-)
+export default <Button uxpId="button0" label="merge!" fill primary />;
