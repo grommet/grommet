1484ce572877f6683639d05087cb7f4327f08351
Marcin Treder <marcin@uxpin.com>
2019-03-07 19:29:59 -0800

    Change of the default theme to Aruba

--- tools/UXPinWrapper.js
+++ tools/UXPinWrapper.js
@@ -7,7 +7,7 @@ import { Grommet } from '../src/js/components/Grommet';
 
 export default function UXPinWrapper({ children }) {
   return (
-    <Grommet theme={hpe}>
+    <Grommet theme={aruba}>
       {children}
     </Grommet>
   );
