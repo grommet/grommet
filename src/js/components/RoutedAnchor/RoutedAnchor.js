5531549fcb30648225ba20de532d04ae16dccd9b
Orestis Ioannou <oorestisime@gmail.com>
2019-03-30 16:47:40 +0100

    Warn on the usage of Routed components

--- src/js/components/RoutedAnchor/RoutedAnchor.js
+++ src/js/components/RoutedAnchor/RoutedAnchor.js
@@ -14,6 +14,11 @@ class RoutedAnchor extends Component {
 
   render() {
     const { path, method, ...rest } = this.props;
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(
+        `This component will be deprecated in the 3.0 release. Please refer to https://github.com/grommet/grommet/issues/2855 for more information.`,
+      );
+    }
     return (
       <Anchor
         {...rest}
