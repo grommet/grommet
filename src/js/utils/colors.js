0c9558dcca585b05177d9c9fcd14c10d4ac69663
Eric Soderberg <eric.soderberg@hpe.com>
2019-04-23 12:03:18 -0700

    Added support for hsl() background color

--- src/js/utils/colors.js
+++ src/js/utils/colors.js
@@ -22,7 +22,44 @@ const parseHexToRGB = color =>
     : // https://stackoverflow.com/a/42429333
       color.match(/[A-Za-z0-9]{2}/g).map(v => parseInt(v, 16));
 
-const canExtractRGBArray = color => /^#/.test(color) || /^rgb/.test(color);
+// From: https://stackoverflow.com/a/9493060/8513067
+// Converts an HSL color value to RGB. Conversion formula
+// adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+// Assumes h, s, and l are contained in the set [0, 1] and
+// returns r, g, and b in the set [0, 255].
+const hslToRGB = (h, s, l) => {
+  let r;
+  let g;
+  let b;
+
+  if (s === 0 || s === '0') {
+    // achromatic
+    r = l;
+    g = l;
+    b = l;
+  } else {
+    const hue2rgb = (p, q, inT) => {
+      let t = inT;
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 0.16666667) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 0.66666667) return p + (q - p) * (0.66666667 - t) * 6;
+      return p;
+    };
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 0.33333333);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 0.33333333);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+};
+
+const canExtractRGBArray = color =>
+  /^#/.test(color) || /^rgb/.test(color) || /^hsl/.test(color);
 
 const getRGBArray = color => {
   if (/^#/.test(color)) {
@@ -30,8 +67,16 @@ const getRGBArray = color => {
   }
   if (/^rgb/.test(color)) {
     return color
-      .match(/rgba?\((\s?[0-9]*\s?),(\s?[0-9]*\s?),(\s?[0-9]*\s?).*?\)/)
+      .match(/rgba?\(\s?([0-9]*)\s?,\s?([0-9]*)\s?,\s?([0-9]*)\s?.*?\)/)
       .splice(1);
+  }
+  if (/^hsl/.test(color)) {
+    // e.g. hsl(240, 60%, 50%)
+    const [h, s, l] = color
+      .match(/hsla?\(\s?([0-9]*)\s?,\s?([0-9]*)%?\s?,\s?([0-9]*)%?\s?.*?\)/)
+      .splice(1)
+      .map(v => parseInt(v, 10));
+    return hslToRGB(h / 360.0, s / 100.0, l / 100.0);
   }
   return color;
 };
