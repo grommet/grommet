a722e91f0c66d2efc4c99ab6fc8d4f0fc10f1e1e
Shimi <shimrit.yacobi@gmail.com>
2019-04-30 17:12:59 -0600

    Custom edge-control for RangeSelector  (#3066)
    * fix alignment of edge controls
    
    * decoupling storybook stories
    
    * allowing node as an edge control option
    
    * roll back git ignore
    
    * roll back git ignore
    
    * removing leftover code

---
+++ src/js/components/RangeSelector/stories/Basics.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Grommet, Box, RangeSelector, Stack, Text } from 'grommet';
+import { grommet } from 'grommet/themes';
+
+class SimpleRangeSelector extends Component {
+  static defaultProps = {
+    direction: 'horizontal',
+  };
+
+  state = { values: [12, 16] };
+
+  onChange = values => this.setState({ values });
+
+  render() {
+    const { direction, ...rest } = this.props;
+    const { values } = this.state;
+    return (
+      <Grommet theme={grommet}>
+        <Box align="center" pad="large">
+          <Stack>
+            <Box
+              direction={direction === 'vertical' ? 'column' : 'row'}
+              justify="between"
+            >
+              {[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(value => (
+                <Box
+                  key={value}
+                  width="xxsmall"
+                  height="xxsmall"
+                  align="center"
+                  pad="small"
+                  border={false}
+                >
+                  <Text style={{ fontFamily: 'monospace' }}>{value}</Text>
+                </Box>
+              ))}
+            </Box>
+            <RangeSelector
+              direction={direction}
+              min={10}
+              max={20}
+              size="full"
+              values={values}
+              onChange={this.onChange}
+              {...rest}
+            />
+          </Stack>
+        </Box>
+      </Grommet>
+    );
+  }
+}
+
+storiesOf('RangeSelector', module)
+  .add('Simple', () => <SimpleRangeSelector />)
+  .add('Step', () => <SimpleRangeSelector step={2} />)
+  .add('Vertical', () => <SimpleRangeSelector direction="vertical" />);
