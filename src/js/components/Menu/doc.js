05812fb4e1636b26e0ebac5c4f23d83a464d57db
Orestis Ioannou <oorestisime@gmail.com>
2019-04-30 05:42:45 +0200

    Add open prop in Menu (#2985)
    * Add open prop in Menu
    
    * Improve wording

--- src/js/components/Menu/doc.js
+++ src/js/components/Menu/doc.js
@@ -84,6 +84,9 @@ The object values can be any Button prop, for example: label and onClick.`,
     })
       .description('Custom messages. Used for accessibility by screen readers.')
       .defaultValue({ openMenu: 'Open Menu', closeMenu: 'Close Menu' }),
+    open: PropTypes.bool
+      .description('Whether the state of the component should be open')
+      .defaultValue(false),
     size: PropTypes.oneOfType([
       PropTypes.oneOf(['small', 'medium', 'large', 'xlarge']),
       PropTypes.string,
