954066ad218e4cb29dd59393fae424fd285bcdc4
Shimi <shimrit.yacobi@gmail.com>
2019-05-02 11:01:43 -0600

    RadioButtonGroup key warning (#3075)
    * fixing index key usage to be more explicit
    
    * Update src/js/components/Box/Box.js

--- src/js/components/Box/Box.js
+++ src/js/components/Box/Box.js
@@ -53,7 +53,7 @@ class Box extends Component {
             contents.push(
               <StyledBoxGap
                 // eslint-disable-next-line react/no-array-index-key
-                key={index}
+                key={`gap-${index}`}
                 gap={gap}
                 directionProp={direction}
                 responsive={responsive}
