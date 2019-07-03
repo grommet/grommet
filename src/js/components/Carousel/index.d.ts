49446f6dd06192b48ddc8b1cfe0a911c3dcaf410
Yuriy Kovalchuk <yuriy.kovalchuk@cloudtp.com>
2019-07-03 18:46:07 +0300

    changed the order of properties in .ts file

--- src/js/components/Carousel/index.d.ts
+++ src/js/components/Carousel/index.d.ts
@@ -3,11 +3,11 @@ import * as React from "react";
 export interface CarouselProps {
   a11yTitle?: string;
   alignSelf?: "start" | "center" | "end" | "stretch";
+  controls?: "true" | "false" | "arrows" | "selectors";
   gridArea?: string;
   margin?: "none" | "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | {bottom?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,horizontal?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,left?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,right?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,top?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string,vertical?: "xxsmall" | "xsmall" | "small" | "medium" | "large" | "xlarge" | string} | string;
   fill?: boolean;
   play?: number;
-  controls?: "true" | "false" | "arrows" | "selectors"
 }
 
 declare const Carousel: React.ComponentClass<CarouselProps & JSX.IntrinsicElements['div']>;
