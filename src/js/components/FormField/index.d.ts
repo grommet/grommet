6353c600da230f65d415ed62b1bebec23caf5984
britt6612 <42451602+britt6612@users.noreply.github.com>
2019-07-30 10:02:05 -0600

    Addit omit for placeholder in Formfield to work with TS

--- src/js/components/FormField/index.d.ts
+++ src/js/components/FormField/index.d.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Omit } from "../../utils";
 
 export interface FormFieldProps {
   error?: string | React.ReactNode;
@@ -7,11 +8,12 @@ export interface FormFieldProps {
   label?: string | React.ReactNode;
   name?: string;
   pad?: boolean;
+  placeholder?: React.ReactNode;
   required?: boolean;
   component?: any;
   validate?: {regexp?: object,message?: string} | ((...args: any[]) => any);
 }
 
-declare const FormField: React.ComponentClass<FormFieldProps & JSX.IntrinsicElements['input']>;
+declare const FormField: React.ComponentClass<FormFieldProps & Omit<JSX.IntrinsicElements['input'], 'placeholder'>>;
 
 export { FormField };
