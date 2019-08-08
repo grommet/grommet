05dbb6256118ebaf95a57afe8150d923ea021fa2
britt6612 <42451602+britt6612@users.noreply.github.com>
2019-08-08 15:08:54 -0600

    Drop button to also accept HTML button props

--- src/js/components/DropButton/index.d.ts
+++ src/js/components/DropButton/index.d.ts
@@ -12,6 +12,6 @@ export interface DropButtonProps {
   open?: boolean;
 }
 
-declare const DropButton: React.ComponentClass<DropButtonProps & ButtonProps>;
+declare const DropButton: React.ComponentClass<DropButtonProps & ButtonProps <JSX.IntrinsicElements['button']>>;
 
 export { DropButton };
