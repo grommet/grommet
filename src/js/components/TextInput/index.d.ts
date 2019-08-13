4e32016f6eede31ae446b1ea487bddecaca8ae03
Stevche Radevski <sradevski@live.com>
2019-08-13 13:25:54 +0200

    Fix type import for PlaceHolderType
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- src/js/components/TextInput/index.d.ts
+++ src/js/components/TextInput/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Omit, PlaceholderType, } from "../../utils";
+import { Omit, PlaceHolderType, } from "../../utils";
 import { DropProps } from "../Drop";
 
 export interface TextInputProps {
