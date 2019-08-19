c6610b9b5330583d0b633adc3f8b6da641ee3a07
Taylor Seamans <taylor.seamans@hpe.com>
2019-08-19 11:59:09 -0700

    Add ColorType to WorldMap index.d.ts

--- src/js/components/WorldMap/index.d.ts
+++ src/js/components/WorldMap/index.d.ts
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { A11yTitleType, AlignSelfType, GridAreaType, MarginType } from "../../utils";
+import { A11yTitleType, AlignSelfType, ColorType, GridAreaType, MarginType } from "../../utils";
 
 export interface WorldMapProps {
   a11yTitle?: A11yTitleType;
   alignSelf?: AlignSelfType;
   gridArea?: GridAreaType;
   margin?: MarginType;
-  color?: string | {dark?: string,light?: string};
+  color?: ColorType;
   continents?: {color?: string | {dark?: string,light?: string},name: "Africa" | "Asia" | "Australia" | "Europe" | "North America" | "South America",onClick?: ((...args: any[]) => any),onHover?: ((...args: any[]) => any)}[];
   onSelectPlace?: ((...args: any[]) => any);
   places?: {color?: string | {dark?: string,light?: string},name?: string,location: number[],onClick?: ((...args: any[]) => any),onHover?: ((...args: any[]) => any)}[];
