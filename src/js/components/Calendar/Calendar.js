fd829c6240f1702caa5ea54b1a6c1ffec53227e6
Sharon Shabtai <sharons@spectralogic.com>
2019-08-21 11:32:24 -0600

    code review changes

--- src/js/components/Calendar/Calendar.js
+++ src/js/components/Calendar/Calendar.js
@@ -41,12 +41,10 @@ const buildStartEnd = (reference, firstDayOfWeek) => {
   let start = new Date(reference);
   start.setDate(1); // first of month
 
-  // in case Sunday is the first day of the month, and the user asked for Monday to be the first day of the week, we need to include Sunday and six days prior.
-  if (start.getDay() === 0 && firstDayOfWeek === 1) {
-    start = subtractDays(start, 6);
-  } else {
-    start = subtractDays(start, start.getDay() - firstDayOfWeek); // beginning of week
-  }
+  start =
+    start.getDay() === 0 && firstDayOfWeek === 1
+      ? (start = subtractDays(start, 6)) // in case Sunday is the first day of the month, and the user asked for Monday to be the first day of the week, we need to include Sunday and six days prior.
+      : (start = subtractDays(start, start.getDay() - firstDayOfWeek)); // beginning of week
 
   const end = addDays(start, 7 * 5 + 7); // 5 weeks to end of week
   return { start, end };
