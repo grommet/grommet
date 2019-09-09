1a640e10ae1c65e9ffe0d5a35c09ef0aefb45638
britt6612 <42451602+britt6612@users.noreply.github.com>
2019-09-09 15:00:59 -0600

    reverted story back

--- src/js/components/Calendar/stories/Simple.js
+++ src/js/components/Calendar/stories/Simple.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
-import { deepMerge } from 'grommet/utils';
 
-import { Box, Calendar, Grommet, grommet } from 'grommet';
-
-const customTheme = deepMerge(grommet,{
-  calendar:{
-    heading: {
-      level: '3' },
-  },
-});
-
+import { Box, Calendar, Grommet } from 'grommet';
+import { grommet } from 'grommet/themes';
 
 class SimpleCalendar extends Component {
   state = {};
@@ -23,11 +15,12 @@ class SimpleCalendar extends Component {
   render() {
     const { date } = this.state;
     return (
-      <Grommet theme={customTheme}>
+      <Grommet theme={grommet}>
         <Box align="center" pad="large">
           <Calendar
             date={date}
             onSelect={this.onSelect}
+            size="small"
             bounds={['2018-09-08', '2020-12-13']}
           />
         </Box>
