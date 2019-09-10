e0b7df6d630fdca9c3a96349e857b5982d3bd8e7
Taylor Seamans <taylor.seamans@yahoo.com>
2019-09-10 11:14:32 -0700

    Add comments to all files that escape special characters

--- src/js/components/Select/stories/ObjectMultiple.js
+++ src/js/components/Select/stories/ObjectMultiple.js
@@ -42,7 +42,11 @@ class ObjectMultiSelect extends Component {
             }
             onClose={() => this.setState({ options: objectOptions })}
             onSearch={text => {
+              // The line below escapes regular expression special characters
               const escapedText = text.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+
+              // Create the regular expression with modified value which handles escaping special characters
+              // Without escaping special characters, errors will appear in the console
               const exp = new RegExp(escapedText, 'i');
               this.setState({
                 options: objectOptions.filter(o => exp.test(o.lab)),
