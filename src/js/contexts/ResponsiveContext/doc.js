ffef24e9f84af3da78e9670872318aa2a43c1032
Taylor Seamans <taylor.seamans@yahoo.com>
2019-09-23 11:32:39 -0700

    Correct quotation to single.

--- src/js/contexts/ResponsiveContext/doc.js
+++ src/js/contexts/ResponsiveContext/doc.js
@@ -26,7 +26,8 @@ export const doc = ResponsiveContext => {
 
 export const themeDoc = {
   'global.breakpoints': {
-    description: `The possible breakpoints that could affect border, direction, gap, margin, pad, and round. The default values help to optimize content for mobile, tablet, and computer.`,
+    description:
+      'The possible breakpoints that could affect border, direction, gap, margin, pad, and round. The default values help to optimize content for mobile, tablet, and computer.',
     type: 'object',
     defaultValue: `{
     small: {
