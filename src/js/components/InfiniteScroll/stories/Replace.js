26f49f9a4736b9244cc2f9cc1b77b884e8bf299b
shimisun <shimrit.yacobi@gmail.com>
2019-10-01 10:00:13 -0600

    decoupling the story for Replace

---
+++ src/js/components/InfiniteScroll/stories/Replace.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { grommet } from 'grommet/themes';
+
+import { Grommet, Box, InfiniteScroll, Text } from 'grommet';
+
+const allItems = Array(1000)
+  .fill()
+  .map((_, i) => `item ${i + 1}`);
+
+const InfiniteScrollReplace = props => (
+  <Grommet theme={grommet}>
+    <InfiniteScroll items={allItems} {...props}>
+      {item => (
+        <Box key={item} pad="medium" border={{ side: 'bottom' }} align="center">
+          <Text>{item}</Text>
+        </Box>
+      )}
+    </InfiniteScroll>
+  </Grommet>
+);
+
+storiesOf('InfiniteScroll', module).add('Replace', () => (
+  <InfiniteScrollReplace replace />
+));
