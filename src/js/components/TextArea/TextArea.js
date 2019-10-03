d959602bdc7f1c5559232c87d3755f3f7267ce1b
Sharon Shabtai <sharons@spectralogic.com>
2019-10-03 16:09:40 -0600

    Refactor TextArea.js class component and split stories (#3393)
    * Refactor TextArea.js
    
    * Split TextArea stories

--- src/js/components/TextArea/TextArea.js
+++ src/js/components/TextArea/TextArea.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { compose } from 'recompose';
 
 import { Keyboard } from '../Keyboard';
@@ -6,23 +6,20 @@ import { withFocus, withForwardRef } from '../hocs';
 
 import { StyledTextArea } from './StyledTextArea';
 
-class TextArea extends Component {
-  onEsc = event => {
+const TextArea = ({ fill, forwardRef, ...rest }) => {
+  const onEsc = event => {
     // we have to stop both synthetic events and native events
     // drop and layer should not close by pressing esc on this input
     event.stopPropagation();
     event.nativeEvent.stopImmediatePropagation();
   };
 
-  render() {
-    const { fill, forwardRef, ...rest } = this.props;
-    return (
-      <Keyboard onEsc={this.onEsc}>
-        <StyledTextArea ref={forwardRef} fillArg={fill} {...rest} />
-      </Keyboard>
-    );
-  }
-}
+  return (
+    <Keyboard onEsc={onEsc}>
+      <StyledTextArea ref={forwardRef} fillArg={fill} {...rest} />
+    </Keyboard>
+  );
+};
 
 let TextAreaDoc;
 if (process.env.NODE_ENV !== 'production') {
