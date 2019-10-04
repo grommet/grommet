8c4d357048f5fe624c4c026eb229ada0f58edee8
Victor Kunzler <victor_kunzler@hotmail.com>
2019-10-04 15:54:33 -0300

    Refactoring AccordionPanel to use hooks

--- src/js/components/AccordionPanel/AccordionPanel.js
+++ src/js/components/AccordionPanel/AccordionPanel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { compose } from 'recompose';
 
 import { withTheme } from 'styled-components';
@@ -14,133 +14,125 @@ import { withForwardRef } from '../hocs';
 
 import { AccordionContext } from '../Accordion/AccordionContext';
 
-class AccordionPanel extends Component {
-  state = {
-    hover: undefined,
-  };
+const onHandleMouseOver = ({ onMouseOver, theme, setHover }) => (...args) => {
+  const { dark } = theme;
 
-  onMouseOver = (...args) => {
-    const {
-      onMouseOver,
-      theme: { dark },
-    } = this.props;
-    this.setState({ hover: dark ? 'light-4' : 'dark-3' });
-    if (onMouseOver) {
-      onMouseOver(args);
-    }
-  };
+  setHover(dark ? 'light-4' : 'dark-3');
 
-  onMouseOut = (...args) => {
-    const { onMouseOut } = this.props;
-    this.setState({ hover: undefined });
-    if (onMouseOut) {
-      onMouseOut(args);
-    }
-  };
+  if (onMouseOver) onMouseOver(args);
+};
 
-  onFocus = (...args) => {
-    const {
-      onFocus,
-      theme: { dark },
-    } = this.props;
-    this.setState({ hover: dark ? 'light-4' : 'dark-3' });
-    if (onFocus) {
-      onFocus(args);
-    }
-  };
+const onHandleMouseOut = ({ onMouseOut, setHover }) => (...args) => {
+  setHover(undefined);
 
-  onBlur = (...args) => {
-    const { onBlur } = this.props;
-    this.setState({ hover: undefined });
-    if (onBlur) {
-      onBlur(args);
-    }
-  };
+  if (onMouseOut) onMouseOut(args);
+};
 
-  render() {
-    const {
-      children,
-      header,
-      label,
-      theme,
-      onMouseOut,
-      onMouseOver,
-      onFocus,
-      onBlur,
-      ...rest
-    } = this.props;
-    const { hover } = this.state;
+const onHandleFocus = ({ onFocus, theme, setHover }) => (...args) => {
+  const { dark } = theme;
 
-    const iconColor = normalizeColor(
-      theme.accordion.icons.color || 'control',
-      theme,
-    );
+  setHover(dark ? 'light-4' : 'dark-3');
 
-    return (
-      <AccordionContext.Consumer>
-        {panelContext => {
-          const { active, animate, onPanelChange } = panelContext;
-          const AccordionIcon = active
-            ? theme.accordion.icons.collapse
-            : theme.accordion.icons.expand;
+  if (onFocus) onFocus(args);
+};
 
-          return (
-            <Box flex={false}>
-              <Button
-                role="tab"
-                aria-selected={active}
-                aria-expanded={active}
-                onClick={onPanelChange}
-                onMouseOver={this.onMouseOver}
-                onMouseOut={this.onMouseOut}
-                onFocus={this.onFocus}
-                onBlur={this.onBlur}
-              >
-                {header || (
-                  <Box
-                    align="center"
-                    direction="row"
-                    justify="between"
-                    {...rest}
-                  >
-                    {typeof label === 'string' ? (
-                      <Box pad={{ horizontal: 'xsmall' }}>
-                        <Heading
-                          level={
-                            (theme.accordion.heading &&
-                              theme.accordion.heading.level) ||
-                            4
-                          }
-                          color={hover}
-                        >
-                          {label}
-                        </Heading>
-                      </Box>
-                    ) : (
-                      label
-                    )}
-                    {AccordionIcon && (
-                      <Box pad={{ horizontal: 'small' }}>
-                        <AccordionIcon color={iconColor} />
-                      </Box>
-                    )}
-                  </Box>
-                )}
-              </Button>
-              <Box border={theme.accordion.border}>
-                {animate ? (
-                  <Collapsible open={active}>{children}</Collapsible>
-                ) : (
-                  active && children
-                )}
-              </Box>
+const onHandleBlur = ({ onBlur, setHover }) => (...args) => {
+  setHover(undefined);
+
+  if (onBlur) onBlur(args);
+};
+
+const AccordionPanel = ({
+  children,
+  header,
+  label,
+  theme,
+  onMouseOut,
+  onMouseOver,
+  onFocus,
+  onBlur,
+  ...rest
+}) => {
+  const [hover, setHover] = useState(undefined);
+
+  const iconColor = normalizeColor(
+    theme.accordion.icons.color || 'control',
+    theme,
+  );
+
+  return (
+    <AccordionContext.Consumer>
+      {panelContext => {
+        const { active, animate, onPanelChange } = panelContext;
+        const AccordionIcon = active
+          ? theme.accordion.icons.collapse
+          : theme.accordion.icons.expand;
+
+        return (
+          <Box flex={false}>
+            <Button
+              role="tab"
+              aria-selected={active}
+              aria-expanded={active}
+              onClick={onPanelChange}
+              onMouseOver={onHandleMouseOver({
+                onMouseOver,
+                theme,
+                setHover,
+              })}
+              onMouseOut={onHandleMouseOut({
+                onMouseOut,
+                setHover,
+              })}
+              onFocus={onHandleFocus({
+                onFocus,
+                theme,
+                setHover,
+              })}
+              onBlur={onHandleBlur({
+                onBlur,
+                setHover,
+              })}
+            >
+              {header || (
+                <Box align="center" direction="row" justify="between" {...rest}>
+                  {typeof label === 'string' ? (
+                    <Box pad={{ horizontal: 'xsmall' }}>
+                      <Heading
+                        level={
+                          (theme.accordion.heading &&
+                            theme.accordion.heading.level) ||
+                          4
+                        }
+                        color={hover}
+                      >
+                        {label}
+                      </Heading>
+                    </Box>
+                  ) : (
+                    label
+                  )}
+                  {AccordionIcon && (
+                    <Box pad={{ horizontal: 'small' }}>
+                      <AccordionIcon color={iconColor} />
+                    </Box>
+                  )}
+                </Box>
+              )}
+            </Button>
+            <Box border={theme.accordion.border}>
+              {animate ? (
+                <Collapsible open={active}>{children}</Collapsible>
+              ) : (
+                active && children
+              )}
             </Box>
-          );
-        }}
-      </AccordionContext.Consumer>
-    );
-  }
-}
+          </Box>
+        );
+      }}
+    </AccordionContext.Consumer>
+  );
+};
 
 AccordionPanel.defaultProps = {};
 Object.setPrototypeOf(AccordionPanel.defaultProps, defaultProps);
