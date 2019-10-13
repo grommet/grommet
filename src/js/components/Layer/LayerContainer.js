01f1844e582e2906a264672168f32803b27985ce
husseyexplores <husseyexplores@gmail.com>
2019-10-13 09:59:56 +0500

    Refactor LayerContainer.js to use hooks
    Signed-off-by: husseyexplores <husseyexplores@gmail.com>

--- src/js/components/Layer/LayerContainer.js
+++ src/js/components/Layer/LayerContainer.js
@@ -1,4 +1,4 @@
-import React, { createRef, Component } from 'react';
+import React, { useRef, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { defaultProps } from '../../default-props';
@@ -17,137 +17,120 @@ const HiddenAnchor = styled.a`
   position: absolute;
 `;
 
-class LayerContainer extends Component {
-  static contextType = ThemeContext;
+const LayerContainer = ({
+  children,
+  id,
+  modal,
+  onClickOutside,
+  onEsc,
+  plain,
+  position,
+  responsive,
+  theme: propsTheme,
+  ...rest
+}) => {
+  const themeContext = useContext(ThemeContext);
+  const [prevPosition, setPrevPosition] = useState();
+  const anchorRef = useRef();
+  const containerRef = useRef();
+  const layerRef = useRef();
 
-  static defaultProps = {
-    full: false,
-    margin: 'none',
-    modal: true,
-    position: 'center',
+  const makeLayerVisible = () => {
+    const node = layerRef.current || containerRef.current;
+    if (node && node.scrollIntoView) {
+      node.scrollIntoView();
+    }
   };
 
-  anchorRef = createRef();
-
-  containerRef = React.createRef();
-
-  layerRef = React.createRef();
-
-  componentDidMount() {
-    const { position } = this.props;
-    if (position !== 'hidden') {
-      this.makeLayerVisible();
+  useEffect(() => {
+    if (prevPosition !== position && position !== 'hidden') {
+      makeLayerVisible();
+      setPrevPosition(position);
       // Once layer is open we make sure it has focus so that you
       // can start tabbing inside the layer. If the caller put focus
       // on an element already, we honor that. Otherwise, we put
       // the focus in the hidden anchor.
       let element = document.activeElement;
       while (element) {
-        if (element === this.containerRef.current) {
+        if (element === containerRef.current) {
           // already have focus inside the container
           break;
         }
         element = element.parentElement;
       }
-      if (!element && this.anchorRef.current) {
-        this.anchorRef.current.focus();
+      if (!element && anchorRef.current) {
+        anchorRef.current.focus();
       }
     }
-  }
+  }, [position]);
 
-  componentDidUpdate(prevProps) {
-    const { position } = this.props;
-    if (prevProps.position !== position && position !== 'hidden') {
-      this.makeLayerVisible();
-    }
-  }
+  const theme = themeContext || propsTheme;
 
-  makeLayerVisible = () => {
-    const node = this.layerRef.current || this.containerRef.current;
-    if (node && node.scrollIntoView) {
-      node.scrollIntoView();
-    }
-  };
+  let content = (
+    <StyledContainer
+      id={id}
+      {...rest}
+      position={position}
+      plain={plain}
+      responsive={responsive}
+      ref={containerRef}
+    >
+      <HiddenAnchor ref={anchorRef} tabIndex="-1" aria-hidden="true" />
+      {children}
+    </StyledContainer>
+  );
 
-  render() {
-    const {
-      children,
-      id,
-      modal,
-      onClickOutside,
-      onEsc,
-      plain,
-      position,
-      responsive,
-      theme: propsTheme,
-      ...rest
-    } = this.props;
-    const theme = this.context || propsTheme;
-
-    let content = (
-      <StyledContainer
+  if (modal) {
+    content = (
+      <StyledLayer
         id={id}
-        {...rest}
-        position={position}
         plain={plain}
+        position={position}
         responsive={responsive}
-        ref={this.containerRef}
+        tabIndex="-1"
+        ref={layerRef}
       >
-        {/* eslint-disable max-len */}
-        {/* eslint-disable jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */}
-        <HiddenAnchor ref={this.anchorRef} tabIndex="-1" aria-hidden="true" />
-        {/* eslint-enable jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */}
-        {/* eslint-enable max-len */}
-        {children}
-      </StyledContainer>
-    );
-
-    if (modal) {
-      content = (
-        <StyledLayer
-          id={id}
+        <StyledOverlay
           plain={plain}
-          position={position}
+          onMouseDown={onClickOutside}
           responsive={responsive}
-          tabIndex="-1"
-          ref={this.layerRef}
-        >
-          <StyledOverlay
-            plain={plain}
-            onMouseDown={onClickOutside}
-            responsive={responsive}
-          />
-          {content}
-        </StyledLayer>
-      );
-      /* eslint-enable jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */
-    }
-
-    if (onEsc) {
-      content = <Keyboard onEsc={onEsc}>{content}</Keyboard>;
-    }
-
-    if (theme.layer.background) {
-      const dark = backgroundIsDark(theme.layer.background, theme);
-      if (dark !== theme.dark) {
-        content = (
-          <ThemeContext.Provider value={{ ...theme, dark }}>
-            {content}
-          </ThemeContext.Provider>
-        );
-      }
-    }
-
-    if (modal) {
-      content = (
-        <FocusedContainer hidden={position === 'hidden'} restrictScroll>
-          {content}
-        </FocusedContainer>
-      );
-    }
-    return content;
+        />
+        {content}
+      </StyledLayer>
+    );
   }
-}
+
+  if (onEsc) {
+    content = <Keyboard onEsc={onEsc}>{content}</Keyboard>;
+  }
+
+  if (theme.layer.background) {
+    const dark = backgroundIsDark(theme.layer.background, theme);
+    if (dark !== theme.dark) {
+      content = (
+        <ThemeContext.Provider value={{ ...theme, dark }}>
+          {content}
+        </ThemeContext.Provider>
+      );
+    }
+  }
+
+  if (modal) {
+    content = (
+      <FocusedContainer hidden={position === 'hidden'} restrictScroll>
+        {content}
+      </FocusedContainer>
+    );
+  }
+  return content;
+};
+
+LayerContainer.defaultProps = {
+  full: false,
+  margin: 'none',
+  modal: true,
+  position: 'center',
+};
 
 Object.setPrototypeOf(LayerContainer.defaultProps, defaultProps);
 
