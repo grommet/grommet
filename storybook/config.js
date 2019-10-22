3adf1b2de8fd0299b4fd2f5109e8c8c0ea828d7c
Shimi <shimrit.yacobi@gmail.com>
2019-10-22 01:02:39 -0600

    Changing config of storybook (#3487)

--- storybook/config.js
+++ storybook/config.js
@@ -17,19 +17,6 @@ addParameters({
     theme: grommetLight,
     showNav: true,
     showPanel: true, // show the code panel by default
-    storySort: (a, b) => {
-      console.log('a', a);
-      console.log('b', b);
-      if (a[1].kind === 'All') return -1;
-      console.log(
-        'accordion--dark-no-animation'.localeCompare('accordion--rich', {
-          numeric: true,
-        }),
-      );
-      return a[1].kind === b[1].kind
-        ? 0
-        : a[1].id.localeCompare(b[1].id , { numeric: true });
-    },
   },
 });
 
