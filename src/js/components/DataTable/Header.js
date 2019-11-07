37948852c15e4c1220c307bf83adbc112aaed18e
Eric Soderberg <eric.soderberg@hpe.com>
2019-11-07 12:03:41 -0800

    Changed DataTable to fix issues with header layout

--- src/js/components/DataTable/Header.js
+++ src/js/components/DataTable/Header.js
@@ -71,26 +71,37 @@ const Header = ({
           }
 
           if (search || onResize) {
+            const resizer = onResize ? (
+              <Resizer property={property} onResize={onResize} />
+            ) : null;
+            const searcher =
+              search && filters ? (
+                <Searcher
+                  filtering={filtering}
+                  filters={filters}
+                  property={property}
+                  onFilter={onFilter}
+                  onFiltering={onFiltering}
+                />
+              ) : null;
             content = (
               <Box
                 direction="row"
                 align="center"
-                justify={align}
+                justify={!align || align === 'start' ? 'between' : align}
                 gap="small"
                 fill="vertical"
                 style={onResize ? { position: 'relative' } : undefined}
               >
                 {content}
-                {search && filters && (
-                  <Searcher
-                    filtering={filtering}
-                    filters={filters}
-                    property={property}
-                    onFilter={onFilter}
-                    onFiltering={onFiltering}
-                  />
+                {searcher && resizer ? (
+                  <Box flex={false} direction="row" align="center" gap="small">
+                    {searcher}
+                    {resizer}
+                  </Box>
+                ) : (
+                  searcher || resizer
                 )}
-                <Resizer property={property} onResize={onResize} />
               </Box>
             );
           }
