7d0047ec4db1bb6a70622aeffd7eba6fd23fd9b6
shimisun <shimrit.yacobi@gmail.com>
2019-12-04 12:28:52 -0800

    Update Header docs

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Box } from '../Box';
 
-const Header = ({ children, ...rest }) => (
+const Header = ({ ...rest }) => (
   <Box
     align="center"
     as="header"
