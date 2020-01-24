bb5822e560f272c8def046f1b13d777e4f8b514d
Eric Soderberg <eric.soderberg@hpe.com>
2020-01-23 21:17:32 -0800

    Fixed an issue with Layer RTL story

--- src/js/components/Layer/stories/RTL.js
+++ src/js/components/Layer/stories/RTL.js
@@ -5,7 +5,7 @@ import { Box, Grommet, Layer } from 'grommet';
 import { grommet } from 'grommet/themes';
 
 const RTLLayer = () => (
-  <Grommet theme={grommet} direction="rtl">
+  <Grommet theme={grommet} dir="rtl">
     <Layer
       position="start"
       margin={{ vertical: 'small', start: 'xlarge', end: 'medium' }}
