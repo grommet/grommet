d37e48d7eddaa5ab2fc7b5afc4b40ab99e83d4b2
thaydds <thayronedds@gmail.com>
2020-01-29 08:59:13 -0300

    add storybook for options from an object

---
+++ src/js/components/Select/stories/OptionsFromObject.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { Box, Select } from 'grommet';
+
+const objectOptions = [
+  {
+    lab: 'Male',
+    val: 1,
+  },
+  {
+    lab: 'Female',
+    value: 2,
+  },
+  {
+    lab: 'Non Binary',
+    value: 3,
+  },
+  {
+    lab: 'Other',
+    value: 4,
+  },
+];
+
+const OptionsFromObject = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <Box fill align="center" justify="start" pad="large">
+      <Select
+        id="select"
+        name="select"
+        placeholder="Select"
+        labelKey="lab"
+        value={value}
+        options={objectOptions}
+        onChange={({ option }) => setValue(option)}
+      />
+    </Box>
+  );
+};
+
+storiesOf('Select', module).add('OptionsFromObject', () => (
+  <OptionsFromObject />
+));
