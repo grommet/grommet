2347b2dc6c2a797c20c2cc87f5a990386ba2e131
Shimi <shimrit.yacobi@gmail.com>
2020-02-04 17:08:52 -0700

    Added text colors to theme (#3722)
    * Added text colors to theme
    
    * added opacity to disabled background onFormField

--- src/js/themes/base.js
+++ src/js/themes/base.js
@@ -64,6 +64,7 @@ const colors = {
     light: '#FFFFFF08',
   },
   'active-background': 'background-contrast',
+  'active-text': 'text-strong',
   black: '#000000',
   border: {
     dark: rgba(255, 255, 255, 0.33),
@@ -81,15 +82,28 @@ const colors = {
   // "graph-3": 'neutral-2',
   placeholder: '#AAAAAA',
   selected: 'brand',
-  'selected-background': 'brand',
   text: {
     dark: '#f8f8f8',
     light: '#444444',
+  },
+  'text-strong': {
+    dark: '#FFFFFF',
+    light: '#000000',
+  },
+  'text-weak': {
+    dark: '#CCCCCC',
+    light: '#555555',
+  },
+  'text-xweak': {
+    dark: '#BBBBBB',
+    light: '#666666',
   },
   icon: {
     dark: '#f8f8f8',
     light: '#666666',
   },
+  'selected-background': 'brand',
+  'selected-text': 'text-strong',
   white: '#FFFFFF',
 };
 
@@ -534,7 +548,10 @@ export const generate = (baseSpacing = 24, scale = 6) => {
         pad: 'small',
       },
       disabled: {
-        // background: undefined,
+        background: {
+          color: 'status-disabled',
+          opacity: 'medium',
+        },
       },
       error: {
         color: {
