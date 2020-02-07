424237b2ab495169893a63db6690dd5fd4faa12a
sivakar12 <sivakar12@outlook.com>
2020-02-07 08:55:11 +0530

    Change Webpack loader's test to work on both Windows and *nix (#3713)
    * Change Webpack loader's test to work on both Windows and *nix
    
    Signed-off-by: Sivakar Sithamparanathan <sivakar12@outlook.com>
    
    * Change regex for storybook/addon-storysource to work on Windows
    
    Signed-off-by: Sivakar Sithamparanathan <sivakar12@outlook.com>
    
    Co-authored-by: Shimi <shimrit.yacobi@gmail.com>

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = async ({ config }) => {
   config.module.rules.push({
-    test: /\.stories\.js$|\/stories\/.*\.js$/,
+    test: /\.stories\.js$|(\/|\\)stories(\/|\\).*\.js$/,
     loaders: [
       {
         loader: require.resolve('@storybook/addon-storysource/loader'),
@@ -17,7 +17,7 @@ module.exports = async ({ config }) => {
   });
 
   config.module.rules.push({
-    test: /stories\/.*\.(ts|tsx)$/,
+    test: /stories(\\|\/).*\.(ts|tsx)$/,
     loaders: [
       {
         loader: require.resolve('awesome-typescript-loader'),
