5cc223a3954a8f10e58423f74c393764b3e8ddb6
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-12 15:59:43 -0800

    Move grommet background story to separate file

--- src/js/components/Grommet/grommet.stories.js
+++ src/js/components/Grommet/grommet.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { grommet, Grommet, Anchor, Box } from 'grommet';
-import { hpe } from 'grommet-theme-hpe';
 import { Add } from 'grommet-icons';
 
 const customTheme = {
@@ -36,57 +35,6 @@ const Plain = () => (
   </>
 );
 
-const Background = () => {
-  const themeColor = 'background-back';
-  const hexValue = '#DCD0FF';
-  const cssColor = 'gold';
-  return (
-    <Box gap="medium">
-      <Grommet>
-        <Box pad="medium">
-          <p>Grommet with no theme or background prop</p>
-        </Box>
-      </Grommet>
-      <Grommet theme={hpe} themeMode="dark">
-        <Box pad="medium">
-          <p>Grommet with theme & themeMode but no background prop</p>
-        </Box>
-      </Grommet>
-      <Grommet theme={hpe} themeMode="light" background={themeColor}>
-        <Box pad="medium">
-          <p>
-            Grommet with background as theme color of &apos;{themeColor}&apos;
-          </p>
-        </Box>
-      </Grommet>
-      <Grommet theme={grommet} background={hexValue}>
-        <Box pad="medium">
-          <p>Grommet with background as HEX value of &apos;{hexValue}&apos;</p>
-        </Box>
-      </Grommet>
-      <Grommet theme={grommet} background={cssColor}>
-        <Box pad="medium">
-          <p>
-            Grommet with background as CSS color name of &apos;{cssColor}&apos;
-          </p>
-        </Box>
-      </Grommet>
-      <Grommet
-        theme={grommet}
-        background={{
-          color: 'pink',
-          image:
-            'url(http://librelogo.org/wp-content/uploads/2014/04/gradient2.png)',
-        }}
-      >
-        <Box pad="medium">
-          <p>Grommet with background as object containing color and image</p>
-        </Box>
-      </Grommet>
-    </Box>
-  );
-};
-
 const GrommetVars = () => (
   <Grommet theme={grommet} cssVars>
     <Box pad="medium" background="var(--accent-2)" gap="medium">
@@ -103,5 +51,4 @@ const GrommetVars = () => (
 storiesOf('Grommet', module)
   .add('Plain', () => <Plain />)
   .add('Theme', () => <Themed />)
-  .add('Vars', () => <GrommetVars />)
-  .add('Background', () => <Background />);
+  .add('Vars', () => <GrommetVars />);
