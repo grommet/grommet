e1f2103f86c13e4c7b5033e7fec5359d8a1f6ce1
Lukáš Vasek <bilaak@gmail.com>
2020-03-17 18:55:57 +0100

    Feature: add BoxTypes to TableRow (fixes #3865)

--- src/js/components/TableRow/index.d.ts
+++ src/js/components/TableRow/index.d.ts
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { BoxTypes } from "../Box";
 
 export interface TableRowProps {
   
 }
 
-declare const TableRow: React.FC<TableRowProps & JSX.IntrinsicElements['tr']>;
+declare const TableRow: React.FC<TableRowProps & BoxTypes & JSX.IntrinsicElements['tr']>;
 
 export { TableRow };
