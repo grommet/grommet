afa9e09a961501bab540115641d088db5d889cbc
Eric Soderberg <eric.soderberg@hpe.com>
2020-03-19 10:59:13 -0700

    Changed Select to fix an issue with value propagation. Fixes issue #3864.

---
+++ src/js/components/Select/stories/Multiple.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Box, Grommet, Select } from 'grommet';
+import { grommet } from 'grommet/themes';
+
+const options = ['one', 'two'];
+
+const Example = () => {
+  const [value, setValue] = useState([]);
+
+  return (
+    <Grommet full theme={grommet}>
+      <Box fill align="center" justify="start" pad="large">
+        <Select
+          placeholder="Select"
+          multiple
+          closeOnChange={false}
+          value={value}
+          selected={[0]}
+          options={options}
+          onChange={({ value: nextValue }) => setValue(nextValue)}
+        />
+      </Box>
+    </Grommet>
+  );
+};
+
+storiesOf('Select', module).add('Multiple', () => <Example />);
