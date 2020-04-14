9aec6ecd18281fc8e9f3705a094fb9c691080528
Eric Soderberg <eric.soderberg@hpe.com>
2020-04-14 07:27:20 -0700

    Make applyKey more defensive

--- src/js/components/Select/utils.js
+++ src/js/components/Select/utils.js
@@ -1,4 +1,5 @@
 export const applyKey = (option, key) => {
+  if (!option) return undefined;
   if (typeof key === 'object') return applyKey(option, key.key);
   if (typeof key === 'function') return key(option);
   if (key) return option[key];
