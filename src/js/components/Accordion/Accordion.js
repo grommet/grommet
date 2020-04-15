da2f4108387bae01c0dcb7e8fb376571cf72c2a5
Eric Soderberg <eric.soderberg@hpe.com>
2020-04-15 14:24:03 -0700

    Changed Accordion to fix an issue with wrapped children panels. Fixes #3960

--- src/js/components/Accordion/Accordion.js
+++ src/js/components/Accordion/Accordion.js
@@ -1,67 +1,73 @@
-import React, { Children, cloneElement, useState } from 'react';
+import React, { Children, forwardRef, useState } from 'react';
 
 import { Box } from '../Box';
+
+import { AccordionContext } from './AccordionContext';
 
 const activeAsArray = active =>
   typeof active === 'number' ? [active] : active;
 
-const Accordion = ({
-  activeIndex,
-  animate = true,
-  children,
-  multiple,
-  onActive,
-  ...rest
-}) => {
-  const [activeIndexes, setActiveIndexes] = useState([]);
-  const [stateActiveIndex, setStateActiveIndex] = useState();
+const Accordion = forwardRef(
+  (
+    { activeIndex, animate = true, children, multiple, onActive, ...rest },
+    ref,
+  ) => {
+    const [activeIndexes, setActiveIndexes] = useState([]);
+    const [stateActiveIndex, setStateActiveIndex] = useState();
 
-  // Derived state from props
-  // https://reactjs.org/docs/hooks-faq.html#how-do-i-implement-getderivedstatefromprops
-  const derivedActiveIndexes = activeAsArray(activeIndex) || [];
-  if (
-    (typeof activeIndex !== 'undefined' || activeIndex !== stateActiveIndex) &&
-    derivedActiveIndexes.join() !== activeIndexes.join()
-  ) {
-    setActiveIndexes(derivedActiveIndexes);
-    setStateActiveIndex(activeIndex);
-  }
-
-  const onPanelChange = index => {
-    let nextActiveIndexes = [...(activeIndexes || [])];
-
-    const nextActiveIndex = nextActiveIndexes.indexOf(index);
-    if (nextActiveIndex > -1) {
-      nextActiveIndexes.splice(nextActiveIndex, 1);
-    } else if (multiple) {
-      nextActiveIndexes.push(index);
-    } else {
-      nextActiveIndexes = [index];
+    // Derived state from props
+    // https://reactjs.org/docs/hooks-faq.html#how-do-i-implement-getderivedstatefromprops
+    const derivedActiveIndexes = activeAsArray(activeIndex) || [];
+    if (
+      (typeof activeIndex !== 'undefined' ||
+        activeIndex !== stateActiveIndex) &&
+      derivedActiveIndexes.join() !== activeIndexes.join()
+    ) {
+      setActiveIndexes(derivedActiveIndexes);
+      setStateActiveIndex(activeIndex);
     }
 
-    setActiveIndexes(nextActiveIndexes);
-    if (onActive) {
-      onActive(nextActiveIndexes);
-    }
-  };
+    const onPanelChange = index => {
+      let nextActiveIndexes = [...(activeIndexes || [])];
 
-  return (
-    <Box role="tablist" {...rest}>
-      {Children.toArray(children)
-        .filter(child => child)
-        .map((child, index) => {
-          if (child) {
-            return cloneElement(child, {
-              active: activeIndexes.indexOf(index) > -1,
-              animate,
-              onPanelChange: () => onPanelChange(index),
-            });
-          }
-          return child;
-        })}
-    </Box>
-  );
-};
+      const nextActiveIndex = nextActiveIndexes.indexOf(index);
+      if (nextActiveIndex > -1) {
+        nextActiveIndexes.splice(nextActiveIndex, 1);
+      } else if (multiple) {
+        nextActiveIndexes.push(index);
+      } else {
+        nextActiveIndexes = [index];
+      }
+
+      setActiveIndexes(nextActiveIndexes);
+      if (onActive) {
+        onActive(nextActiveIndexes);
+      }
+    };
+
+    return (
+      <Box ref={ref} role="tablist" {...rest}>
+        {Children.toArray(children)
+          .filter(child => child)
+          .map((child, index) => (
+            <AccordionContext.Provider
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              value={{
+                active: activeIndexes.indexOf(index) > -1,
+                animate,
+                onPanelChange: () => onPanelChange(index),
+              }}
+            >
+              {child}
+            </AccordionContext.Provider>
+          ))}
+      </Box>
+    );
+  },
+);
+
+Accordion.displayName = 'Accordion';
 
 let AccordionDoc;
 if (process.env.NODE_ENV !== 'production') {
