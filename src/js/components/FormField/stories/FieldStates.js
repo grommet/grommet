18718531ae2f80cec3e077fdf3aa76007720a4d8
Matt Glissmann <mdglissmann@gmail.com>
2020-04-20 14:56:52 -0600

    add form to stories

--- src/js/components/FormField/stories/FieldStates.js
+++ src/js/components/FormField/stories/FieldStates.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { grommet, Box, FormField, TextInput, Grommet } from 'grommet';
+import { grommet, Box, Form, FormField, TextInput, Grommet } from 'grommet';
 
 const FormFieldStates = () => {
   const inputRef = useRef();
@@ -13,62 +13,64 @@ const FormFieldStates = () => {
   return (
     <Grommet theme={grommet}>
       <Box align="center" pad="large">
-        <Box border gap="medium" pad="large" width="medium">
-          <FormField htmlFor="enabled-id" name="enabled" label="Default">
-            <TextInput
-              id="enabled-id"
-              name="enabled"
-              placeholder="Enter a username"
-            />
-          </FormField>
-          <FormField htmlFor="focus-id" name="focus" label="Focus State">
-            <TextInput
-              id="focus-id"
-              name="focus"
-              placeholder="Enter a username"
-              ref={inputRef}
-            />
-          </FormField>
-          <FormField
-            htmlFor="error-id"
-            name="enabled"
-            label="Info State"
-            info="Unique name. No spaces. May include '-' as a separator."
-          >
-            <TextInput
-              id="error-id"
-              name="enabled"
-              placeholder="Enter a username"
-              value="fluffyKi"
-            />
-          </FormField>
-          <FormField
-            htmlFor="error-id"
-            name="enabled"
-            label="Error State"
-            error="It looks like that username is already taken. Bummer."
-          >
-            <TextInput
-              id="error-id"
-              name="enabled"
-              placeholder="Enter a username"
-              value="fluffyKitty123"
-            />
-          </FormField>
-          <FormField
-            htmlFor="disabled-id"
-            name="disabled"
-            label="Disabled State"
-            disabled
-          >
-            <TextInput
-              id="disabled-id"
+        <Form>
+          <Box border gap="medium" pad="large" width="medium">
+            <FormField htmlFor="enabled-id" name="enabled" label="Default">
+              <TextInput
+                id="enabled-id"
+                name="enabled"
+                placeholder="Enter a username"
+              />
+            </FormField>
+            <FormField htmlFor="focus-id" name="focus" label="Focus State">
+              <TextInput
+                id="focus-id"
+                name="focus"
+                placeholder="Enter a username"
+                ref={inputRef}
+              />
+            </FormField>
+            <FormField
+              htmlFor="info-id"
+              name="info-demo"
+              label="Info State"
+              info="Unique name. No spaces. May include '-' as a separator."
+            >
+              <TextInput
+                id="info-id"
+                name="info-demo"
+                placeholder="Enter a username"
+                value="fluffyKi"
+              />
+            </FormField>
+            <FormField
+              htmlFor="error-id"
+              name="error-demo"
+              label="Error State"
+              error="It looks like that username is already taken. Bummer."
+            >
+              <TextInput
+                id="error-id"
+                name="error-demo"
+                placeholder="Enter a username"
+                value="fluffyKitty123"
+              />
+            </FormField>
+            <FormField
+              htmlFor="disabled-id"
               name="disabled"
-              placeholder="Enter a username"
+              label="Disabled State"
               disabled
-            />
-          </FormField>
-        </Box>
+            >
+              <TextInput
+                id="disabled-id"
+                name="disabled"
+                placeholder="Enter a username"
+                disabled
+              />
+            </FormField>
+          </Box>
+        </Form>
       </Box>
     </Grommet>
   );
