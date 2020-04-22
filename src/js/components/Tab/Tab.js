463268ffd66bc49879310827bf347b284a9ec0cf
Taylor Seamans <taylor.seamans@yahoo.com>
2020-04-22 08:09:20 -0700

    Set default state

--- src/js/components/Tab/Tab.js
+++ src/js/components/Tab/Tab.js
@@ -27,7 +27,7 @@ const Tab = forwardRef(
   ) => {
     const theme = useContext(ThemeContext) || defaultProps.theme;
     const [over, setOver] = useState(undefined);
-    const [focus, setFocus] = useState();
+    const [focus, setFocus] = useState(undefined);
     let normalizedTitle = title;
     const tabStyles = {};
 
@@ -135,7 +135,7 @@ const Tab = forwardRef(
           if (onMouseOver) onMouseOver();
         }}
         onBlur={() => {
-          setFocus();
+          setFocus(undefined);
           if (onMouseOut) onMouseOut();
         }}
         // ensure focus outline is not covered by hover styling
