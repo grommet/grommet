0555cf7f1926dfe4c011081c8a5da08a72d2fde7
Eduardo Moreira <contato@eduardoem.com.br>
2020-05-04 13:09:52 -0300

    Add markdown-to-jsx reference in doc (#3734)
    Co-authored-by: Orestis Ioannou <oorestisime@gmail.com>
    Co-authored-by: Shimi <shimrit.yacobi@gmail.com>

--- src/js/components/Markdown/doc.js
+++ src/js/components/Markdown/doc.js
@@ -6,6 +6,10 @@ export const doc = Markdown => {
   const DocumentedMarkdown = describe(Markdown)
     .availableAt(getAvailableAtBadge('Markdown'))
     .description('Markdown formatting using Grommet components.')
+    .details(
+      `Grommet uses 'markdown-to-jsx' in Markdown component,
+      you can see all the options in the documentation.`,
+    )
     .usage(
       `import { Markdown } from 'grommet';
       <Markdown>{content}</Markdown>`,
