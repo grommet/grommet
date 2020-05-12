5bc6d3b2248e4285314da1e178e77ef94596a03e
Eric Soderberg <eric.soderberg@hpe.com>
2020-05-12 09:59:34 -0700

    Updated story to be protocol agnostic

--- src/js/components/Video/stories/Simple.js
+++ src/js/components/Video/stories/Simple.js
@@ -9,19 +9,19 @@ const SimpleVideo = props => (
     <Box align="center" pad="large">
       <Video {...props}>
         <source
-          src="http://techslides.com/demos/sample-videos/small.webm"
+          src="//techslides.com/demos/sample-videos/small.webm"
           type="video/webm"
         />
         <source
-          src="http://techslides.com/demos/sample-videos/small.ogv"
+          src="//techslides.com/demos/sample-videos/small.ogv"
           type="video/ogg"
         />
         <source
-          src="http://techslides.com/demos/sample-videos/small.mp4"
+          src="//techslides.com/demos/sample-videos/small.mp4"
           type="video/mp4"
         />
         <source
-          src="http://techslides.com/demos/sample-videos/small.3gp"
+          src="//techslides.com/demos/sample-videos/small.3gp"
           type="video/3gp"
         />
       </Video>
