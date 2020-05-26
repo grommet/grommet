fd5cf0518c432c3c2cc77b866b987d4ca961a2af
Eric Soderberg <eric.soderberg@hpe.com>
2020-05-26 10:22:17 -0700

    Change Menu Box responsive to preserve backwards compatibility

--- src/js/components/Menu/Menu.js
+++ src/js/components/Menu/Menu.js
@@ -191,6 +191,7 @@ const Menu = forwardRef((props, ref) => {
       align="center"
       pad={theme.button.default ? undefined : 'small'}
       gap={label && icon !== false ? 'small' : undefined}
+      responsive={false}
     >
       <Text size={size}>{label}</Text>
       {icon !== false
