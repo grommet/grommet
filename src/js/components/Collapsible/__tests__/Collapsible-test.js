aa95acceacc19827a3745a0be1f2973f43c55341
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-06-10 15:49:00 -0400

    Cleaned up code with beforeEach, added accessibility test

--- src/js/components/Collapsible/__tests__/Collapsible-test.js
+++ src/js/components/Collapsible/__tests__/Collapsible-test.js
@@ -2,92 +2,73 @@ import React from 'react';
 import 'jest-styled-components';
 import { cleanup, render } from '@testing-library/react';
 
+import { axe } from 'jest-axe';
 import { Collapsible } from '..';
-import { Box } from '../../Box';
 import { Grommet } from '../../Grommet';
 import { Text } from '../../Text';
+import 'jest-axe/extend-expect';
+import 'regenerator-runtime/runtime';
 
 describe('Collapsible', () => {
+  let App;
+  let open;
+  let direction;
+  beforeEach(() => {
+    App = () => {
+      return (
+        <Grommet>
+          <Collapsible open={open} direction={direction}>
+            <Text>Example</Text>
+          </Collapsible>
+        </Grommet>
+      );
+    };
+  });
   afterEach(cleanup);
 
+  test('Collapsible Accessibility', async () => {
+    const { container } = render(<App />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
   test('open', () => {
-    const { container } = render(
-      <Grommet>
-        <Collapsible open>
-          <Box>Box Body</Box>
-        </Collapsible>
-      </Grommet>,
-    );
+    open = true;
+    const { container } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test('onClick open default', () => {
-    let open = false;
-    const { container, rerender } = render(
-      <Grommet>
-        <Collapsible open={open}>
-          <Text>Example</Text>
-        </Collapsible>
-      </Grommet>,
-    );
+    open = false;
+    const { container, rerender } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
     open = !open;
     expect(open).toBe(true);
-    rerender(
-      <Grommet>
-        <Collapsible open={open}>
-          <Text>Example</Text>
-        </Collapsible>
-      </Grommet>,
-    );
+    rerender(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test('direction vertical', () => {
-    let open = false;
-    const { container, rerender } = render(
-      <Grommet>
-        <Collapsible open={open} direction="vertical">
-          <Box>
-            <Text>Example</Text>
-          </Box>
-        </Collapsible>
-      </Grommet>,
-    );
+    open = false;
+    direction = 'vertical';
+    const { container, rerender } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
     open = !open;
     expect(open).toBe(true);
     // Rerendering because props were updated. Want to render with new props
-    rerender(
-      <Grommet>
-        <Collapsible open={open} direction="vertical">
-          <Text>Example</Text>
-        </Collapsible>
-      </Grommet>,
-    );
+    rerender(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test('direction horizontal', () => {
-    let open = false;
-    const { container, rerender } = render(
-      <Grommet>
-        <Collapsible open={open} direction="horizontal">
-          <Text>Example</Text>
-        </Collapsible>
-      </Grommet>,
-    );
+    open = false;
+    direction = 'horizontal';
+    const { container, rerender } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
     open = !open;
     expect(open).toBe(true);
     // Rerendering because props were updated. Want to render with new props
-    rerender(
-      <Grommet>
-        <Collapsible open={open} direction="horizontal">
-          <Text>Example</Text>
-        </Collapsible>
-      </Grommet>,
-    );
+    rerender(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 });
