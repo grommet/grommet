876e97eecd10b4c6cc60c78abdd9e5190e0fd07a
shimisun <shimrit.yacobi@gmail.com>
2020-06-18 21:03:56 -0600

    Fixed breakpoint logic

--- src/js/utils/responsive.js
+++ src/js/utils/responsive.js
@@ -1,7 +1,7 @@
 export const getBreakpoint = (windowWidth, theme) => {
   let result;
-  Object.keys(theme.global.breakpoints)
-    .sort((a, b) => {
+  const sortedBreakpoints = Object.keys(theme.global.breakpoints).sort(
+    (a, b) => {
       const first = theme.global.breakpoints[a];
       const second = theme.global.breakpoints[b];
       if (!first) return 1;
@@ -9,17 +9,24 @@ export const getBreakpoint = (windowWidth, theme) => {
       if (!first.value) return 1;
       if (!second.value) return -1;
       return first.value - second.value;
-    })
-    .some(name => {
-      const breakpoint = theme.global.breakpoints[name];
-      if (breakpoint) {
-        if (!breakpoint.value || breakpoint.value >= windowWidth) {
-          result = name;
-          return true;
-        }
+    },
+  );
+  const lastBreakpoint = sortedBreakpoints[sortedBreakpoints.length - 1];
+
+  sortedBreakpoints.some(name => {
+    const breakpoint = theme.global.breakpoints[name];
+    if (breakpoint) {
+      if (
+        !breakpoint.value ||
+        lastBreakpoint === name ||
+        breakpoint.value >= windowWidth
+      ) {
+        result = name;
+        return true;
       }
-      return false;
-    });
+    }
+    return false;
+  });
   return result;
 };
 
