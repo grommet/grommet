c060e5c5e3cc793b5047839dc5f125f7d8c6a311
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-06-19 16:01:52 -0400

    Added in other video formats

--- src/js/components/Video/stories/Simple.js
+++ src/js/components/Video/stories/Simple.js
@@ -9,6 +9,18 @@ const SimpleVideo = props => (
     <Box align="center" pad="large">
       <Video {...props}>
         <source src="small.mp4" type="video/mp4" />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.webm"
+          type="video/webm"
+        />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.ogv"
+          type="video/ogg"
+        />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.3gp"
+          type="video/3gp"
+        />
       </Video>
     </Box>
   </Grommet>
