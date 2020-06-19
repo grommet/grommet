a2fc8ff90226718bbe93fdcbdab03965f96f7876
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-06-19 16:05:48 -0400

    Added in other video formats to all story

--- src/js/all/stories/AllComponents.js
+++ src/js/all/stories/AllComponents.js
@@ -260,6 +260,18 @@ const Components = () => {
     <Box key="video" alignSelf="start">
       <Video>
         <source src="small.mp4" type="video/mp4" />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.webm"
+          type="video/webm"
+        />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.ogv"
+          type="video/ogg"
+        />
+        <source
+          src="http://techslides.com/demos/sample-videos/small.3gp"
+          type="video/3gp"
+        />
       </Video>
     </Box>,
   ];
