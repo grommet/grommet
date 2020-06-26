5ae985ea3990a937efbe0c08bba947117911ccd5
amit.mish <amitm@media.net>
2020-06-26 20:00:07 +0530

    [Tooltip] Added tooplip component

--- src/js/themes/base.js
+++ src/js/themes/base.js
@@ -1101,6 +1101,12 @@ export const generate = (baseSpacing = 24, scale = 6) => {
         pad: 'xsmall',
       },
     },
+    tooptip: {
+      background: 'dark-1',
+      color: 'white',
+      tipSize: '5px',
+      round: 'small',
+    },
   });
 
   return deepFreeze(result);
