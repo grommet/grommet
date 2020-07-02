f5986cf4e09eae2b9a624faac66e3919e80b6586
Eric Soderberg <eric.soderberg@hpe.com>
2020-07-01 20:43:18 -0700

    Enhanced documentation

--- src/js/components/DateInput/doc.js
+++ src/js/components/DateInput/doc.js
@@ -33,7 +33,7 @@ export const doc = DateInput => {
       `The date format to use. If not specified, the date value will not
       be displayed as a text string and the user will not be able to enter
       a date by typing. For example: 'mm/dd/yyyy', or for a range:
-      'mm/dd/yyyy-mm/dd/yyyy`,
+      'mm/dd/yyyy-mm/dd/yyyy'. This property should be used when in a Form.`,
     ),
     id: PropTypes.string.description('The id of the input.'),
     inline: PropTypes.bool
