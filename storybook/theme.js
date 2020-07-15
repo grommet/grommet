15eaa3619a5db6ae915b2a7dfaee8d91cc227e07
Leonardo Negreiros <negreirosleo12@gmail.com>
2020-07-15 14:14:25 -0300

    Fix storybook theme that was mistakenly deleted

---
+++ storybook/theme.js
@@ -0,0 +1,26 @@
+import { create } from '@storybook/theming/create';
+
+// https://storybook.js.org/docs/configurations/theming/
+
+export default create({
+  base: 'light',
+
+  colorSecondary: '#7D4CDB',
+
+  appBg: '#F8F8F8',
+  appBorderColor: '#EDEDED',
+  appBorderRadius: 6,
+
+  barTextColor: '#999999',
+  barSelectedColor: '#7D4CDB',
+  barBg: '#F2F2F2',
+
+  inputBg: 'white',
+  inputBorder: 'rgba(0,0,0,.1)',
+  inputTextColor: '#333333',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Grommet',
+  brandImage: 'grommet-logo.svg',
+  brandUrl: 'https://v2.grommet.io/',
+});
