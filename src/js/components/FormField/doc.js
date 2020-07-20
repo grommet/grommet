1894b5c4e658fa95c626e7b5766dabbe8337987a
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-07-20 14:10:57 -0400

    Updated sentence for a11yTitle

--- src/js/components/FormField/doc.js
+++ src/js/components/FormField/doc.js
@@ -18,9 +18,10 @@ export const doc = FormField => {
 
   DocumentedFormField.propTypes = {
     a11yTitle: PropTypes.string.description(
-      `Custom label to be used by screen readers. When provided and FormField
-        has no child component an aria-label will be added to the element.
-        Should not be provided if FormField has child.`,
+      `Custom label to be used by screen readers.
+       Should only be provided if FormField has no children.
+       When a11yTitle is provided an aria-label will be added to the element
+       if it has no children.`,
     ),
     component: PropTypes.oneOfType([
       PropTypes.func,
