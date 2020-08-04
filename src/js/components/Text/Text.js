968803bcb29a4ccd3ab499154043e10557f4c373
Jason Long <j@jaslong.com>
2020-08-04 09:28:15 -0700

    Allow Text and Heading to accept ref (#4371)
    * Add "accepts ref" test to Text and Heading
    
    Signed-off-by: Jason Long <j@jaslong.com>
    
    * Wrap Text and Heading with React.forwardRef.
    
    Signed-off-by: Jason Long <j@jaslong.com>
    
    * Add displayName so the README is generated properly
    
    * Fix components-test

--- src/js/components/Text/Text.js
+++ src/js/components/Text/Text.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { StyledText } from './StyledText';
 
-const Text = ({ color, tag, as, a11yTitle, ...rest }) => (
+const Text = forwardRef(({ color, tag, as, a11yTitle, ...rest }, ref) => (
   <StyledText
     as={!as && tag ? tag : as}
     colorProp={color}
     aria-label={a11yTitle}
     {...rest}
+    ref={ref}
   />
-);
+));
 
+Text.displayName = 'Text';
 Text.defaultProps = {
   level: 1,
 };
