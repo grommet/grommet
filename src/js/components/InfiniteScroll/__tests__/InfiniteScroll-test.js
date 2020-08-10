c49ece6536125a0bdd901f51ad95c4ba3aa70fc1
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-08-10 16:20:35 -0400

    MixedCOntent

--- src/js/components/InfiniteScroll/__tests__/InfiniteScroll-test.js
+++ src/js/components/InfiniteScroll/__tests__/InfiniteScroll-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import 'jest-styled-components';
 
-import { Grommet } from '../../Grommet';
+import { Grommet, Image } from '../..';
 import { InfiniteScroll } from '..';
 
 describe('InfiniteScroll', () => {
@@ -69,6 +69,34 @@ describe('InfiniteScroll', () => {
     const { container } = render(
       <Grommet>
         <InfiniteScroll items={items} step={2} replace>
+          {(item, index) => <div key={index}>{item}</div>}
+        </InfiniteScroll>
+      </Grommet>,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('mixed items', () => {
+    const tempItems = [
+      1,
+      'Hello World',
+      <Image
+        a11yTitle="Gremlin"
+        src="https://v2.grommet.io/img/stak-hurrah.svg"
+      />,
+      99,
+      `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod temporincididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia 
+      deserunt mollit anim id est laborum.`,
+    ];
+    const { container } = render(
+      <Grommet>
+        <InfiniteScroll items={tempItems}>
           {(item, index) => <div key={index}>{item}</div>}
         </InfiniteScroll>
       </Grommet>,
