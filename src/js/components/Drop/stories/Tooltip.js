b5ae2af2c7af6f7e77b146741350ae8c9e9293e3
iankbovard <iankbovard@gmail.com>
2020-08-11 11:06:28 -0500

    changed tooltip story added trapfocus to docs

--- src/js/components/Drop/stories/Tooltip.js
+++ src/js/components/Drop/stories/Tooltip.js
@@ -1,40 +1,188 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Box, Button, Drop, Grommet } from 'grommet';
-import { grommet } from 'grommet/themes';
+import {
+  Grommet,
+  grommet,
+  Box,
+  Button,
+  Drop,
+  DropButton,
+  Text,
+  Nav,
+  Heading,
+  TextInput,
+  Form,
+  FormField,
+} from 'grommet';
+import {
+  Calculator,
+  Bar,
+  Bug,
+  Alert,
+  Close,
+  Achievement,
+  Action,
+} from 'grommet-icons';
 
-const TooltipDrop = () => {
-  const [over, setOver] = useState();
+const icons = [
+  { name: 'Bar', icon: <Bar /> },
+  { name: 'Bug', icon: <Bug /> },
+  { name: 'Calculator', icon: <Calculator /> },
+];
+const menuIcons = [
+  { name: 'Alert', icon: <Alert /> },
+  { name: 'Achievement', icon: <Achievement /> },
+];
+
+const DropContent = ({ onClose }) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.focus();
+  });
+  return (
+    <Box pad="small" background="slategray">
+      <Box direction="row" justify="between" align="center">
+        <Heading level={3} margin="small" color="white">
+          Heading
+        </Heading>
+        <Button icon={<Close color="white" />} onClick={onClose} />
+      </Box>
+      <Text color="white">Content</Text>
+      <Form>
+        <FormField>
+          <TextInput ref={ref} />
+        </FormField>
+      </Form>
+    </Box>
+  );
+};
+
+const SidebarButton = ({ icon, index, name }) => {
+  const [over, setOver] = useState(false);
   const ref = useRef();
 
   return (
-    <Grommet theme={grommet} full>
-      <Box fill align="center" justify="center">
-        <Button
-          label="Button"
-          ref={ref}
-          onMouseOver={() => setOver(true)}
-          onMouseOut={() => setOver(false)}
-          onFocus={() => {}}
-          onBlur={() => {}}
-        />
+    <Box>
+      <Button
+        ref={ref}
+        onMouseOver={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
+        onFocus={() => setOver(true)}
+        onBlur={() => setOver(false)}
+        plain
+        onClick={() => console.log(`Clicked me ${index}`)}
+      >
+        <Box pad={{ vertical: 'small' }} align="center">
+          {icon}
+        </Box>
+      </Button>
+      {ref.current && over && (
+        <Drop
+          align={{ left: 'right' }}
+          target={ref.current}
+          plain
+          trapFocus={false}
+        >
+          <Box pad="small" background="pink">
+            <Text color="white">{name}</Text>
+          </Box>
+        </Drop>
+      )}
+    </Box>
+  );
+};
 
-        {ref.current && over && (
-          <Drop align={{ left: 'right' }} target={ref.current} plain>
-            <Box
-              margin="xsmall"
-              pad="small"
-              background="dark-3"
-              round={{ size: 'medium', corner: 'left' }}
-            >
-              tooltip contents
-            </Box>
-          </Drop>
-        )}
+const SidebarDropButton = ({ icon, name }) => {
+  const [open, setOpen] = useState(false);
+  const [over, setOver] = useState(false);
+
+  const ref = useRef();
+
+  return (
+    <Box fill="horizontal">
+      <DropButton
+        ref={ref}
+        onMouseOver={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}
+        onFocus={() => setOver(true)}
+        onBlur={() => setOver(false)}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        dropAlign={{
+          top: 'top',
+          right: 'right',
+          left: 'right',
+          bottom: 'top',
+        }}
+        dropContent={<DropContent onClose={() => setOpen(false)} />}
+      >
+        <Box>{icon}</Box>
+      </DropButton>
+      {ref.current && over && (
+        <Drop
+          align={{ left: 'right' }}
+          target={ref.current}
+          plain
+          trapFocus={false}
+        >
+          <Box pad="small" background="pink">
+            <Text color="white">{name}</Text>
+          </Box>
+        </Drop>
+      )}
+    </Box>
+  );
+};
+const Sidebar = () => {
+  return (
+    <Box direction="row" fill>
+      <Box
+        direction="column"
+        basis="15%"
+        justify="between"
+        style={{ border: 'blue solid 4px' }}
+        align="center"
+      >
+        <Box>
+          <Nav>
+            {icons.map(({ icon, name }, index) => {
+              return (
+                <SidebarButton
+                  key={name}
+                  icon={icon}
+                  index={index}
+                  name={name}
+                />
+              );
+            })}
+            {menuIcons.map(({ icon, name }, index) => {
+              return (
+                <SidebarDropButton
+                  key={name}
+                  icon={icon}
+                  index={index}
+                  name={name}
+                />
+              );
+            })}
+          </Nav>
+        </Box>
+        <Box>
+          <SidebarButton icon={<Action />} index="Action" name="Action" />
+        </Box>
       </Box>
+    </Box>
+  );
+};
+
+const Tooltips = () => {
+  return (
+    <Grommet theme={grommet} full>
+      <Sidebar />
     </Grommet>
   );
 };
 
-storiesOf('Drop', module).add('Tooltip', () => <TooltipDrop />);
+storiesOf('Drop', module).add('Tooltips', () => <Tooltips />);
