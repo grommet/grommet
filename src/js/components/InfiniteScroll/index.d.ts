ecffa1d5aa9e2e850ddd24013680b8b877b16b12
Taylor Seamans <taylor.seamans@yahoo.com>
2020-08-17 11:00:40 -0700

    Update InfiniteScroll typescript

--- src/js/components/InfiniteScroll/index.d.ts
+++ src/js/components/InfiniteScroll/index.d.ts
@@ -1,12 +1,12 @@
-import * as React from "react";
+import * as React from 'react';
 
 export interface InfiniteScrollProps {
-  children?: ((...args: any[]) => any);
-  items?: any[];
-  onMore?: ((...args: any[]) => any);
-  renderMarker?: ((...args: any[]) => any);
+  children?: (...args: [string, number]) => React.ReactElement;
+  items?: (string | Record<string, any>)[];
+  onMore?: () => void;
+  renderMarker?: (marker: React.ReactElement) => React.ReactElement;
   replace?: boolean;
-  scrollableAncestor?: React.ReactNode | "window";
+  scrollableAncestor?: React.ReactNode | 'window';
   show?: number;
   step?: number;
 }
