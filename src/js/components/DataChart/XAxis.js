5d97577f2bc5ac7e5af6ab5fc4196fa9ff1d2256
Eric Soderberg <eric.soderberg@hpe.com>
2020-08-17 15:45:42 -0700

    Changed DataChart to add more functionality (#4226)
    * Changed DataChart to add more functionality
    
    * Added legend
    
    * Added legend interaction
    
    * Improved padding and detail. Added Everything story.
    
    * Added Keyboard interaction
    
    * Split up into multiple files
    
    * Fixed an issue with stacked bar colors
    
    * Fixed an issue with Legend driving Grid
    
    * Added Prediction example in DataChart
    
    * Changed DataChart to rename 'property' to 'series'
    
    * Data driven point charts
    
    * Test fixes
    
    * Removed console warning
    
    * Fixed an issue with Legend interaction
    
    * Refined Prediction example with recent DataChart enhancments
    
    * Removed color and point from series stories
    
    * Cleaned up documentation. Fixed an issue with stacked bar colors.
    
    * Added bars type to allow range charts
    
    * Enhanced documentation
    
    * Improved Legend Swatch style
    
    * Fixed calcs crossing 0 check
    
    * Fixed an issue with bars calcs
    
    * Documented series property
    
    Co-authored-by: shimisun <shimrit.yacobi@gmail.com>

---
+++ src/js/components/DataChart/XAxis.js
@@ -0,0 +1,32 @@
+import React, { forwardRef } from 'react';
+import { Box } from '../Box';
+
+const XAxis = forwardRef(
+  ({ chartProps, data, renderValue, serie }, ref) => {
+    // pull the x-axis values from the first chart, all should have the same
+    const [axisValues] = (Array.isArray(chartProps[0])
+      ? chartProps[0][0]
+      : chartProps[0]
+    ).axis;
+
+    return (
+      <Box ref={ref} gridArea="xAxis" direction="row" justify="between">
+        {axisValues.map((dataIndex, i) => {
+          let align;
+          if (axisValues.length === data.length) align = 'center';
+          else if (i === 0) align = 'start';
+          else if (i === axisValues.length - 1) align = 'end';
+          else align = 'center';
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <Box key={i} flex align={align}>
+              {serie ? renderValue(serie, dataIndex) : dataIndex}
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  },
+);
+
+export { XAxis };
