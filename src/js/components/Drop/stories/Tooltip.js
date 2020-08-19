585aa3ad5d85521d1cb47fb0f72d37fb935562a3
iankbovard <iankbovard@gmail.com>
2020-08-19 10:40:28 -0500

    changed tooltip story

--- src/js/components/Drop/stories/Tooltip.js
+++ src/js/components/Drop/stories/Tooltip.js
@@ -1,64 +1,10 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
-import {
-  Grommet,
-  grommet,
-  Box,
-  Button,
-  Drop,
-  DropButton,
-  Text,
-  Nav,
-  Heading,
-  TextInput,
-  Form,
-  FormField,
-} from 'grommet';
-import {
-  Calculator,
-  Bar,
-  Bug,
-  Alert,
-  Close,
-  Achievement,
-  Action,
-} from 'grommet-icons';
+import { Grommet, grommet, Box, Button, Drop, Text } from 'grommet';
+import { Calculator, Bug, Achievement } from 'grommet-icons';
 
-const icons = [
-  { name: 'Bar', icon: <Bar /> },
-  { name: 'Bug', icon: <Bug /> },
-  { name: 'Calculator', icon: <Calculator /> },
-];
-const menuIcons = [
-  { name: 'Alert', icon: <Alert /> },
-  { name: 'Achievement', icon: <Achievement /> },
-];
-
-const DropContent = ({ onClose }) => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current.focus();
-  });
-  return (
-    <Box pad="small" background="slategray">
-      <Box direction="row" justify="between" align="center">
-        <Heading level={3} margin="small" color="white">
-          Heading
-        </Heading>
-        <Button icon={<Close color="white" />} onClick={onClose} />
-      </Box>
-      <Text color="white">Content</Text>
-      <Form>
-        <FormField>
-          <TextInput ref={ref} />
-        </FormField>
-      </Form>
-    </Box>
-  );
-};
-
-const SidebarButton = ({ icon, index, name }) => {
+const TooltipButton = ({ icon, name }) => {
   const [over, setOver] = useState(false);
   const ref = useRef();
 
@@ -71,7 +17,6 @@ const SidebarButton = ({ icon, index, name }) => {
         onFocus={() => setOver(true)}
         onBlur={() => setOver(false)}
         plain
-        onClick={() => console.log(`Clicked me ${index}`)}
       >
         <Box pad={{ vertical: 'small' }} align="center">
           {icon}
@@ -82,6 +27,7 @@ const SidebarButton = ({ icon, index, name }) => {
           align={{ left: 'right' }}
           target={ref.current}
           plain
+          // trapFocus set to false allows tabbing through
           trapFocus={false}
         >
           <Box pad="small" background="pink">
@@ -93,96 +39,16 @@ const SidebarButton = ({ icon, index, name }) => {
   );
 };
 
-const SidebarDropButton = ({ icon, name }) => {
-  const [open, setOpen] = useState(false);
-  const [over, setOver] = useState(false);
-
-  const ref = useRef();
-
+const Tooltip = () => {
   return (
-    <Box fill="horizontal">
-      <DropButton
-        ref={ref}
-        onMouseOver={() => setOver(true)}
-        onMouseLeave={() => setOver(false)}
-        onFocus={() => setOver(true)}
-        onBlur={() => setOver(false)}
-        open={open}
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-        dropAlign={{
-          top: 'top',
-          right: 'right',
-          left: 'right',
-          bottom: 'top',
-        }}
-        dropContent={<DropContent onClose={() => setOpen(false)} />}
-      >
-        <Box>{icon}</Box>
-      </DropButton>
-      {ref.current && over && (
-        <Drop
-          align={{ left: 'right' }}
-          target={ref.current}
-          plain
-          trapFocus={false}
-        >
-          <Box pad="small" background="pink">
-            <Text color="white">{name}</Text>
-          </Box>
-        </Drop>
-      )}
-    </Box>
-  );
-};
-const Sidebar = () => {
-  return (
-    <Box direction="row" fill>
-      <Box
-        direction="column"
-        basis="15%"
-        justify="between"
-        style={{ border: 'blue solid 4px' }}
-        align="center"
-      >
-        <Box>
-          <Nav>
-            {icons.map(({ icon, name }, index) => {
-              return (
-                <SidebarButton
-                  key={name}
-                  icon={icon}
-                  index={index}
-                  name={name}
-                />
-              );
-            })}
-            {menuIcons.map(({ icon, name }, index) => {
-              return (
-                <SidebarDropButton
-                  key={name}
-                  icon={icon}
-                  index={index}
-                  name={name}
-                />
-              );
-            })}
-          </Nav>
-        </Box>
-        <Box>
-          <SidebarButton icon={<Action />} index="Action" name="Action" />
-        </Box>
+    <Grommet theme={grommet}>
+      <Box align="center" pad="large">
+        <TooltipButton icon={<Calculator />} name="Calculator" />
+        <TooltipButton icon={<Bug />} name="Bug" />
+        <TooltipButton icon={<Achievement />} name="Achievement" />
       </Box>
-    </Box>
-  );
-};
-
-const Tooltips = () => {
-  return (
-    <Grommet theme={grommet} full>
-      <Sidebar />
     </Grommet>
   );
 };
 
-storiesOf('Drop', module).add('Tooltips', () => <Tooltips />);
+storiesOf('Drop', module).add('Tooltip', () => <Tooltip />);
