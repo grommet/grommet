bad66a860b8176c4a63dfc2f8f7d12ed9d8612c3
Francisco Jimenez <jimenezff@gmail.com>
2020-08-26 11:12:48 -0500

    Omit color from JSX.IntrinsicElements['svg'] (#4453)
    Signed-off-by: Francisco Jimenez <jimenezff@gmail.com>

--- src/js/components/WorldMap/index.d.ts
+++ src/js/components/WorldMap/index.d.ts
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   A11yTitleType,
   AlignSelfType,
@@ -6,21 +6,39 @@ import {
   FillType,
   GridAreaType,
   MarginType,
-} from "../../utils";
+} from '../../utils';
 
 export interface WorldMapProps {
   a11yTitle?: A11yTitleType;
   alignSelf?: AlignSelfType;
   color?: ColorType;
-  continents?: {color?: string | {dark?: string,light?: string},name: "Africa" | "Asia" | "Australia" | "Europe" | "North America" | "South America",onClick?: ((...args: any[]) => any),onHover?: ((...args: any[]) => any)}[];
+  continents?: {
+    color?: string | { dark?: string; light?: string };
+    name:
+      | 'Africa'
+      | 'Asia'
+      | 'Australia'
+      | 'Europe'
+      | 'North America'
+      | 'South America';
+    onClick?: (...args: any[]) => any;
+    onHover?: (...args: any[]) => any;
+  }[];
   fill?: FillType;
   gridArea?: GridAreaType;
-  hoverColor?: string | {dark?: string,light?: string};
+  hoverColor?: string | { dark?: string; light?: string };
   margin?: MarginType;
-  onSelectPlace?: ((place: number[]) => void);
-  places?: {color?: string | {dark?: string,light?: string},name?: string,location: number[],onClick?: ((...args: any[]) => any),onHover?: ((...args: any[]) => any)}[];
+  onSelectPlace?: (place: number[]) => void;
+  places?: {
+    color?: string | { dark?: string; light?: string };
+    name?: string;
+    location: number[];
+    onClick?: (...args: any[]) => any;
+    onHover?: (...args: any[]) => any;
+  }[];
 }
 
-declare const WorldMap: React.ComponentClass<WorldMapProps & JSX.IntrinsicElements['svg']>;
+declare const WorldMap: React.ComponentClass<WorldMapProps &
+  Omit<JSX.IntrinsicElements['svg'], 'color'>>;
 
 export { WorldMap };
