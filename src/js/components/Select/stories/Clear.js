5b8920ed6aeb192c1330151677c6499654b94477
iankbovard <iankbovard@gmail.com>
2020-08-28 15:44:21 -0500

    added tests for clear prop

--- src/js/components/Select/stories/Clear.js
+++ src/js/components/Select/stories/Clear.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Box, Grommet, Select, grommet } from 'grommet';
+import {
+  Box,
+  Grommet,
+  Select,
+  grommet,
+  FormField,
+  ThemeContext,
+} from 'grommet';
 
-const options = ['one', 'two'];
+const options = ['one', 'two', 'three'];
 
 const ClearTop = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState();
   return (
     <Box fill align="center" justify="start" pad="large">
       <Select
         placeholder="Clear on top"
+        multiple
         value={value}
         options={options}
-        onChange={({ option }) => setValue(option)}
+        onChange={({ value: nextValue }) => setValue(nextValue)}
         clear
       />
     </Box>
@@ -21,14 +29,15 @@ const ClearTop = () => {
 };
 
 const ClearBottom = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState();
   return (
     <Box fill align="center" justify="start" pad="large">
       <Select
         placeholder="Clear on bottom"
+        multiple
         value={value}
         options={options}
-        onChange={({ option }) => setValue(option)}
+        onChange={({ value: nextValue }) => setValue(nextValue)}
         clear={{ position: 'bottom' }}
       />
     </Box>
@@ -36,26 +45,84 @@ const ClearBottom = () => {
 };
 
 const ClearLabel = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState();
   return (
     <Box fill align="center" justify="start" pad="large">
       <Select
         placeholder="Clear with label"
+        multiple
         value={value}
         options={options}
-        onChange={({ option }) => setValue(option)}
+        onChange={({ value: nextValue }) => setValue(nextValue)}
         clear={{ label: 'Click me!' }}
       />
     </Box>
   );
 };
 
+const ClearCustomTheme = () => {
+  const [value, setValue] = useState();
+  return (
+    <ThemeContext.Extend
+      value={{
+        select: {
+          clear: {
+            container: {
+              background: 'accent-4',
+            },
+            text: {
+              color: 'neutral-3',
+              weight: 900,
+            },
+          },
+        },
+      }}
+    >
+      <Box fill align="center" justify="start" pad="large">
+        <Select
+          placeholder="Clear with a custom theme"
+          multiple
+          value={value}
+          options={options}
+          onChange={({ value: nextValue }) => setValue(nextValue)}
+          clear
+        />
+      </Box>
+    </ThemeContext.Extend>
+  );
+};
+
+const ClearForm = () => {
+  const [value, setValue] = useState();
+  return (
+    <Box fill align="center" justify="start" pad="large">
+      <FormField label="Numbers" name="numbers">
+        <Select
+          name="numbers"
+          placeholder="Clear within formfield"
+          multiple
+          options={options}
+          value={value}
+          clear
+          onChange={({ value: nextValue }) => setValue(nextValue)}
+        />
+      </FormField>
+    </Box>
+  );
+};
+
 const ClearExamples = () => (
   <Grommet theme={grommet}>
-    <Box direction="row">
-      <ClearTop />
-      <ClearBottom />
-      <ClearLabel />
+    <Box direction="column" align="start">
+      <Box direction="row">
+        <ClearTop />
+        <ClearBottom />
+        <ClearLabel />
+      </Box>
+      <Box direction="row">
+        <ClearForm />
+        <ClearCustomTheme />
+      </Box>
     </Box>
   </Grommet>
 );
