824a6cf0a3463567504a720adcfdfb2d3ab44fd3
Eric Soderberg <eric.soderberg@hpe.com>
2020-09-02 13:46:12 -0700

    Improved Custom story sortable icon

--- src/js/components/DataTable/stories/Custom.js
+++ src/js/components/DataTable/stories/Custom.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Box, Grommet, DataTable } from 'grommet';
-import { FormNext } from 'grommet-icons';
+import { Blank } from 'grommet-icons';
 
 // Source code for the data can be found here
 // https://github.com/grommet/grommet/blob/master/src/js/components/DataTable/stories/data.js
 import { columns, DATA } from './data';
+
+const SortableIcon = () => (
+  <Blank color="text-xweak" opacity="0.5">
+    <g fill="none" stroke="#000" strokeWidth="2">
+      <path d="M 6 10 L 12 6 L 18 10" />
+      <path d="M 6 14 L 12 18 L 18 14" />
+    </g>
+  </Blank>
+);
 
 const customTheme = {
   global: {
@@ -16,7 +25,7 @@ const customTheme = {
   },
   dataTable: {
     icons: {
-      sortable: FormNext,
+      sortable: SortableIcon,
     },
   },
 };
