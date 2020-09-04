bf43930abfa14f115e0c940067059544f28e328c
iankbovard <iankbovard@gmail.com>
2020-09-04 11:40:17 -0500

    aded test in select for stretch

--- src/js/components/Select/__tests__/Select-test.js
+++ src/js/components/Select/__tests__/Select-test.js
@@ -884,5 +884,22 @@ describe('Select', () => {
     fireEvent.click(getByPlaceholderText('test select'));
     expectPortal('test-select__drop').toMatchSnapshot();
   });
+
+  test('stretch = align restricts drop width to its select container', () => {
+    const { getByPlaceholderText } = render(
+      <Grommet>
+        <Select
+          id="test-select"
+          placeholder="test select"
+          options={[
+            'test test test test test test test test test test test test test',
+            'two',
+          ]}
+        />
+      </Grommet>,
+    );
+    fireEvent.click(getByPlaceholderText('test select'));
+    expectPortal('test-select__drop').toMatchSnapshot();
+  });
   window.scrollTo.mockRestore();
 });
