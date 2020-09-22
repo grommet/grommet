a4430fa03969b21a9c6e0d3a48e8248b1b0410e6
Shimi <shimrit.yacobi@gmail.com>
2020-09-22 19:28:02 +0300

    Changed all ts files to follow prettier consistency (#4540)

--- src/js/components/TextInput/index.d.ts
+++ src/js/components/TextInput/index.d.ts
@@ -1,28 +1,42 @@
-import * as React from "react";
-import { Omit, PlaceHolderType, } from "../../utils";
-import { DropProps } from "../Drop";
+import * as React from 'react';
+import { Omit, PlaceHolderType } from '../../utils';
+import { DropProps } from '../Drop';
 
 export interface TextInputProps {
-  dropAlign?: {top?: "top" | "bottom",bottom?: "top" | "bottom",right?: "left" | "right",left?: "left" | "right"};
-  dropHeight?: "xsmall" | "small" | "medium" | "large" | "xlarge" | string;
+  dropAlign?: {
+    top?: 'top' | 'bottom';
+    bottom?: 'top' | 'bottom';
+    right?: 'left' | 'right';
+    left?: 'left' | 'right';
+  };
+  dropHeight?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | string;
   dropTarget?: object;
   dropProps?: DropProps;
   focusIndicator?: boolean;
-  icon?: JSX.Element,
+  icon?: JSX.Element;
   id?: string;
-  messages?: {enterSelect?: string,suggestionsCount?: string,suggestionsExist?: string,suggestionIsOpen?: string};
+  messages?: {
+    enterSelect?: string;
+    suggestionsCount?: string;
+    suggestionsExist?: string;
+    suggestionIsOpen?: string;
+  };
   name?: string;
-  onSelect?: ((x: { target: React.RefObject<HTMLElement>['current'], suggestion: any }) => void);
-  onSuggestionsOpen?: (() => void);
-  onSuggestionsClose?: (() => void);
+  onSelect?: (x: {
+    target: React.RefObject<HTMLElement>['current'];
+    suggestion: any;
+  }) => void;
+  onSuggestionsOpen?: () => void;
+  onSuggestionsClose?: () => void;
   placeholder?: PlaceHolderType;
   plain?: boolean;
   reverse?: boolean;
-  size?: "small" | "medium" | "large" | "xlarge" | string;
-  suggestions?: ({label?: React.ReactNode,value?: any} | string)[];
+  size?: 'small' | 'medium' | 'large' | 'xlarge' | string;
+  suggestions?: ({ label?: React.ReactNode; value?: any } | string)[];
   value?: string | number;
 }
 
-declare const TextInput: React.ComponentClass<TextInputProps & Omit<JSX.IntrinsicElements['input'], 'onSelect' | 'size' | 'placeholder'>>;
+declare const TextInput: React.ComponentClass<TextInputProps &
+  Omit<JSX.IntrinsicElements['input'], 'onSelect' | 'size' | 'placeholder'>>;
 
 export { TextInput };
