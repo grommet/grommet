1114c7c42ef4b370ebe2178348468bc8b070269b
shimisun <shimrit.yacobi@gmail.com>
2020-10-09 10:29:00 -0600

    Changed children of kind button to have plain

--- src/js/components/Button/stories/Plain.js
+++ src/js/components/Button/stories/Plain.js
@@ -5,19 +5,44 @@ import { Add } from 'grommet-icons';
 import { Box, Button, Grommet, Text } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-const PlainButton = props => (
-  <Grommet theme={grommet}>
-    <Box align="center" pad="large">
-      <Button hoverIndicator="light-1" onClick={() => {}} {...props}>
-        <Box pad="small" direction="row" align="center" gap="small">
-          <Add />
-          <Text>Add</Text>
-        </Box>
-      </Button>
-    </Box>
-  </Grommet>
+const PlainButtons = props => (
+  <Box pad="large" gap="large">
+    {/* Out of the Box Button */}
+    <Grommet theme={grommet}>
+      <Box align="center">
+        <Button hoverIndicator="light-1" onClick={() => {}} {...props}>
+          {/*  When Button include children, it is treated as plain */}
+          <Box pad="small" direction="row" align="center" gap="small">
+            <Add />
+            <Text>Add</Text>
+          </Box>
+        </Button>
+      </Box>
+    </Grommet>
+    {/* Kind Button */}
+    <Grommet
+      theme={{
+        global: {
+          font: {
+            family: `-apple-system, BlinkMacSystemFont`,
+          },
+        },
+        button: { default: {} }, // enabling kind button functionality
+      }}
+    >
+      <Box align="center">
+        <Button hoverIndicator="light-1" onClick={() => {}} {...props}>
+          {/*  When kind Button include children, it is treated as plain */}
+          <Box pad="small" direction="row" align="center" gap="small">
+            <Add />
+            <Text>Kind</Text>
+          </Box>
+        </Button>
+      </Box>
+    </Grommet>
+  </Box>
 );
 
 storiesOf('Button', module)
-  .add('Active', () => <PlainButton active />)
-  .add('Plain', () => <PlainButton />);
+  .add('Active', () => <PlainButtons active />)
+  .add('Plain', () => <PlainButtons />);
