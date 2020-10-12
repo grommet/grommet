0cff4f167435e7d9fa40337756eab48fce22fe45
Taylor Seamans <taylor.seamans@yahoo.com>
2020-10-12 14:21:35 -0700

    Fix story

--- src/js/components/Text/stories/All.js
+++ src/js/components/Text/stories/All.js
@@ -22,7 +22,7 @@ const All = () => (
           <Text size={size}>{`Text ${size}`}</Text>
         </Box>
       ))}
-      <Box background="light-3" align="start" width="small" pad="small">
+      <Box background="light-3" align="end" width="small" pad="small">
         <Text truncate>
           This is a long truncated string of text that is aligned to the end.
         </Text>
