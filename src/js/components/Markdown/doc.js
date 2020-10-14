fd657e65e6ff50df2898dd80352cee899b58062c
Jessica Filben <jcfilben@rams.colostate.edu>
2020-10-14 11:59:37 -0600

    updated markdown docs to include avaliable options for components

--- src/js/components/Markdown/doc.js
+++ src/js/components/Markdown/doc.js
@@ -20,7 +20,17 @@ export const doc = Markdown => {
     components: PropTypes.shape({}).description(
       `Custom components and props to override html elements such as 'img'
       or 'pre'. By default 'a', 'p', 'img', and table elements are overridden
-      with grommet components`,
+      with grommet components.\n\nAvailable options:\n
+      a: { component: Anchor },\n
+      img: { component: Image },\n
+      p: { component: Paragraph },\n
+      table: { component: Table },\n
+      td: { component: TableCell },\n
+      tbody: { component: TableBody },\n
+      tfoot: { component: TableFooter },\n
+      th: { component: TableCell },\n
+      thead: { component: TableHeader },\n
+      tr: { component: TableRow },\n`,
     ),
     options: PropTypes.shape({}).description(
       `Used to tune the jsx compiler to specific properties, available options on [markdown-to-jsx](https://github.com/probablyup/markdown-to-jsx).`,
