ff567919610ee505803cb2c860eea77ac5c3285a
Jessica Filben <jcfilben@rams.colostate.edu>
2020-10-14 12:10:25 -0600

    fixed spacing

--- src/js/components/Markdown/doc.js
+++ src/js/components/Markdown/doc.js
@@ -20,17 +20,18 @@ export const doc = Markdown => {
     components: PropTypes.shape({}).description(
       `Custom components and props to override html elements such as 'img'
       or 'pre'. By default 'a', 'p', 'img', and table elements are overridden
-      with grommet components.\n\nAvailable options:\n
-      a: { component: Anchor },\n
-      img: { component: Image },\n
-      p: { component: Paragraph },\n
-      table: { component: Table },\n
-      td: { component: TableCell },\n
-      tbody: { component: TableBody },\n
-      tfoot: { component: TableFooter },\n
-      th: { component: TableCell },\n
-      thead: { component: TableHeader },\n
-      tr: { component: TableRow },\n`,
+      with grommet components.
+      Available options:
+      a: { component: Anchor },
+      img: { component: Image },
+      p: { component: Paragraph },
+      table: { component: Table },
+      td: { component: TableCell },
+      tbody: { component: TableBody },
+      tfoot: { component: TableFooter },
+      th: { component: TableCell },
+      thead: { component: TableHeader },
+      tr: { component: TableRow }`,
     ),
     options: PropTypes.shape({}).description(
       `Used to tune the jsx compiler to specific properties, available options on [markdown-to-jsx](https://github.com/probablyup/markdown-to-jsx).`,
