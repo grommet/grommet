ed03f85145388d370feb43e2db8acf45c57894cc
iankbovard <iankbovard@gmail.com>
2020-10-16 13:19:31 -0500

    added logic to applyKey to handle options that are not objects

--- src/js/components/Select/utils.js
+++ src/js/components/Select/utils.js
@@ -2,6 +2,6 @@ export const applyKey = (option, key) => {
   if (option === undefined) return undefined;
   if (typeof key === 'object') return applyKey(option, key.key);
   if (typeof key === 'function') return key(option);
-  if (key !== undefined) return option[key];
+  if (key !== undefined && typeof option === 'object') return option[key];
   return option;
 };
