0677d62222b47dd6c48eaf38b65f9fca7c29b1fa
Sharon Shabtai <sharons@spectralogic.com>
2020-10-20 13:44:06 -0600

    add warn message

--- src/js/components/Chart/__tests__/utils-test.js
+++ src/js/components/Chart/__tests__/utils-test.js
@@ -2,26 +2,43 @@ import { areNormalizedBoundsEquals, areNormalizedValuesEquals } from '../utils';
 
 describe('utils', () => {
   test('areNormalizedValuesEquals', () => {
+    console.warn = jest.fn();
+    const warnSpy = jest.spyOn(console, 'warn');
+
     expect(
       areNormalizedValuesEquals([{ value: [1] }], [{ value: [1] }]),
     ).toBeTruthy();
+
     expect(areNormalizedValuesEquals([{ value: [1] }], undefined)).toBeFalsy();
+
     expect(
       areNormalizedValuesEquals(
         [{ value: [1] }],
         [{ value: [1] }, { value: [2] }],
       ),
     ).toBeFalsy();
+
     expect(areNormalizedValuesEquals([], [])).toBeTruthy();
+
     expect(
       areNormalizedValuesEquals([{ test: 'test' }], [{ value: [2] }]),
     ).toBeFalsy();
+
     expect(
       areNormalizedValuesEquals([{ value: [1] }], [{ test: 'test' }]),
     ).toBeFalsy();
+
+    expect(warnSpy).toBeCalledTimes(6);
+    expect(warnSpy).toHaveBeenCalledWith(
+      `This function will be removed in the upcoming releases.
+Please get in touch with us if you have concerns.`,
+    );
   });
 
   test('areNormalizedBoundsEquals', () => {
+    console.warn = jest.fn();
+    const warnSpy = jest.spyOn(console, 'warn');
+
     expect(
       areNormalizedBoundsEquals(
         [
@@ -34,6 +51,7 @@ describe('utils', () => {
         ],
       ),
     ).toBeTruthy();
+
     expect(
       areNormalizedBoundsEquals(
         [
@@ -43,12 +61,14 @@ describe('utils', () => {
         undefined,
       ),
     ).toBeFalsy();
+
     expect(
       areNormalizedBoundsEquals(undefined, [
         [0, 1],
         [0, 1],
       ]),
     ).toBeFalsy();
+
     expect(
       areNormalizedBoundsEquals(
         [],
@@ -58,6 +78,13 @@ describe('utils', () => {
         ],
       ),
     ).toBeFalsy();
+
     expect(areNormalizedBoundsEquals([], [])).toBeFalsy();
+
+    expect(warnSpy).toBeCalledTimes(5);
+    expect(warnSpy).toHaveBeenCalledWith(
+      `This function will be removed in the upcoming releases.
+Please get in touch with us if you have concerns.`,
+    );
   });
 });
