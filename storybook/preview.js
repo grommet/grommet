bef9d376515834e9692346486f7c1ab0c1282810
Jessica Filben <jcfilben@rams.colostate.edu>
2020-10-26 16:07:33 -0600

    remove commented out code

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,17 +1,3 @@
-// import { configure } from '@storybook/react';
-
-// const req = require.context(
-//   '../src/js',
-//   true,
-//   /\.stories\.js$|\/stories\/.*\.js$|\/stories\/.*\.ts$|\/stories\/.*\.tsx$/,
-// );
-
-// function loadStories() {
-//   req.keys().forEach(filename => req(filename));
-// }
-
-// configure(loadStories, module);
-
 import { addParameters } from '@storybook/react';
 
 addParameters({
