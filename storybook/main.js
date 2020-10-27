0bce402194e7e35808eeae15718dad4406f8602a
shimisun <shimrit.yacobi@gmail.com>
2020-10-27 07:38:58 -0600

    Updated loader of storybook to include context stories and 'All' stories.

--- storybook/main.js
+++ storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   addons: ['@storybook/addon-storysource'],
-  stories: ['../src/js/components/**/stories/*.(ts|tsx|js|jsx)'],
+  stories: [
+    '../src/js/components/**/stories/*.(ts|tsx|js|jsx)',
+    '../src/js/components/**/*stories.js',
+    '../src/js/contexts/**/*stories.js',
+    '../src/js/contexts/**/stories/*.(ts|tsx|js|jsx)',
+    '../src/js/all/**/stories/*.(ts|tsx|js|jsx)',
+  ],
 };
