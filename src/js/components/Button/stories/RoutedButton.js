6a7ccc94f97b58ff4cb34f2a06a7262fcc03a4a7
Sharon Shabtai <sharons@spectralogic.com>
2020-10-28 10:17:01 -0600

    feedback

--- src/js/components/Button/stories/RoutedButton.js
+++ src/js/components/Button/stories/RoutedButton.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { Box, Grommet, RoutedButton, Text } from 'grommet';
+import { Box, Grommet, RoutedButton as GrommetRoutedButton, Text } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-export const RouteButton = () => (
+export const RoutedButton = () => (
   <Grommet theme={grommet}>
     <Box align="center" pad="large">
       <Text margin="medium" size="small">
         Note: RoutedButton will soon be deprecated
       </Text>
-      <RoutedButton label="Go" path="/" />
+      <GrommetRoutedButton label="Go" path="/" />
     </Box>
   </Grommet>
 );
