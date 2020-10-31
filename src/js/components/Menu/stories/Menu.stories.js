d812eec68d491077e37ca27282d11befa3a62837
Leonardo Santiago <leossantiago1@gmail.com>
2020-10-31 18:57:44 -0300

    Menu stories changed to new structure

---
+++ src/js/components/Menu/stories/Menu.stories.js
@@ -0,0 +1,10 @@
+export { Children } from './typescript/Children.tsx';
+export { BottomControlButton } from './BottomControlButton';
+export { ItemWithIcon } from './ItemWithIcon';
+export { Reverse } from './Reverse';
+export { Simple } from './Simple';
+export { Themed } from './typescript/Themed.tsx';
+
+export default {
+  title: 'Controls/Menu',
+};
