3d6a846162f469a76b6827bd0dc0670be9e56991
Shimi <shimrit.yacobi@gmail.com>
2020-11-05 16:06:11 -0700

    Drop - Added DOM handling for elements without viewport position (#4705)
    * Added DOM handling for elements without viewport position
    
    * Removed the usage of offsetParent
    
    * disable chromatic on story

---
+++ src/js/components/Drop/stories/Meter.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import { Box, Drop, Grommet, Meter } from 'grommet';
+import { grommet } from 'grommet/themes';
+
+const TestDrop = () => {
+  const targetRef = useRef();
+
+  const [, setShowDrop] = useState(false);
+  useEffect(() => setShowDrop(true), []);
+  return (
+    <Grommet theme={grommet} full>
+      <Box fill align="center" justify="center" pad="large">
+        <Meter
+          ref={targetRef}
+          size="small"
+          background="light-2"
+          values={[{ value: 20, color: 'accent-1' }]}
+        />
+        {targetRef.current && (
+          <Drop
+            id="test-drop-with-svg"
+            plain
+            align={{ top: 'bottom', left: 'left' }}
+            target={targetRef.current}
+          >
+            <Box pad="large">target is an svg</Box>
+          </Drop>
+        )}
+      </Box>
+    </Grommet>
+  );
+};
+
+export const SVGChild = () => <TestDrop />;
+SVGChild.story = {
+  parameters: {
+    chromatic: { disable: true },
+  },
+};
