bd40cce90649a5194dc3ebe41e773267f57a5924
Zack Urben <zackurben@gmail.com>
2020-11-13 16:04:14 -0600

    Spelling fix
    Signed-off-by: Zack Urben <zackurben@gmail.com>

--- src/js/components/Box/doc.js
+++ src/js/components/Box/doc.js
@@ -179,7 +179,7 @@ export const doc = Box => {
     ]).description(`The amount of spacing between child elements. This
         should not be used in conjunction with 'wrap' as the gap elements
         will not wrap gracefully. If a child is a Fragment,
-        Box will not add a gap between the choldren of the Fragment.`),
+        Box will not add a gap between the children of the Fragment.`),
     height: PropTypes.oneOfType([
       PropTypes.oneOf([
         'xxsmall',
