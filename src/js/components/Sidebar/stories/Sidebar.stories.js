521de0b4f0def7883e307322dd4e6b1f7896abd5
Jessica Filben <jcfilben@rams.colostate.edu>
2020-11-13 09:37:44 -0700

    removed tooltip story from Sidebar

--- src/js/components/Sidebar/stories/Sidebar.stories.js
+++ src/js/components/Sidebar/stories/Sidebar.stories.js
@@ -1,6 +1,5 @@
 export { SidebarIcons } from './typescript/Simple.tsx';
 export { Labels } from './Labels';
-export { TooltipsSidebar } from './Tooltips';
 
 export default {
   title: 'Layout/Sidebar',
