85f812f81b1ffa66d2eb4a4a0a11fa4515c74078
Eric Soderberg <eric.soderberg@hpe.com>
2020-12-02 14:02:22 -0800

    Improved loading placeholder story

--- src/js/components/DataTable/stories/Placeholder.js
+++ src/js/components/DataTable/stories/Placeholder.js
@@ -18,9 +18,21 @@ export const Placeholder = () => (
             fill
             align="center"
             justify="center"
+            direction="row"
             pad="large"
+            gap="small"
             background={{ color: 'background-front', opacity: 'strong' }}
           >
+            <Box
+              direction="row"
+              border={[
+                { side: 'all', color: 'transparent', size: 'medium' },
+                { side: 'horizontal', color: 'brand', size: 'medium' },
+              ]}
+              pad="small"
+              round="full"
+              animation={{ type: 'rotateRight', duration: 1500 }}
+            />
             <Text weight="bold">Loading ...</Text>
           </Box>
         }
