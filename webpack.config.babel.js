d29d8a7f4c0bfae98d6adac8ba81d187bf6da548
Jessica Filben <jcfilben@rams.colostate.edu>
2020-12-18 13:38:42 -0700

    Upgrade webpack from v4 tov5

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,16 @@
 import path from 'path';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 const plugins = [
   new CleanWebpackPlugin(),
-  new CopyWebpackPlugin([
-    { from: './README.md' },
-    { from: './package.json' },
-    { from: './tools', to: 'tools' },
-  ]),
+  new CopyPlugin({
+    patterns: [
+      { from: './README.md' },
+      { from: './package.json' },
+      { from: './tools', to: 'tools' },
+    ],
+  }),
 ];
 
 export default {
@@ -27,13 +29,13 @@ export default {
   },
   resolve: {
     extensions: ['.js', '.json'],
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+    },
   },
   plugins,
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-  },
   module: {
     rules: [
       {
