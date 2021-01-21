3560ad4e6fe8e98615a6b3cd52ddce79addc021c
Shimi <shimrit.yacobi@gmail.com>
2021-01-21 14:35:57 -0700

    Upgrade major eslint version (#4889)

--- src/js/components/Anchor/stories/Disabled.js
+++ src/js/components/Anchor/stories/Disabled.js
@@ -3,16 +3,14 @@ import React from 'react';
 import { Anchor, Box, Grommet } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-const DisabledAnchor = () => {
-  return (
-    <Grommet theme={grommet}>
-      <Box align="center" pad="large">
-        <Box margin="small">
-          <Anchor disabled label="Disabled Anchor" />
-        </Box>
+const DisabledAnchor = () => (
+  <Grommet theme={grommet}>
+    <Box align="center" pad="large">
+      <Box margin="small">
+        <Anchor disabled label="Disabled Anchor" />
       </Box>
-    </Grommet>
-  );
-};
+    </Box>
+  </Grommet>
+);
 
 export const Disabled = () => <DisabledAnchor />;
--- src/js/components/Calendar/stories/Fill.js
+++ src/js/components/Calendar/stories/Fill.js
@@ -4,14 +4,12 @@ import { Box, Calendar, Grommet } from 'grommet';
 
 import { grommet } from 'grommet/themes';
 
-export const Fill = () => {
-  return (
-    <Grommet theme={grommet}>
-      <Box align="center">
-        <Box height="large" width="large" border>
-          <Calendar fill daysOfWeek />
-        </Box>
+export const Fill = () => (
+  <Grommet theme={grommet}>
+    <Box align="center">
+      <Box height="large" width="large" border>
+        <Calendar fill daysOfWeek />
       </Box>
-    </Grommet>
-  );
-};
+    </Box>
+  </Grommet>
+);
