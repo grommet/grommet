e5ec092f0b377c994323d4c56acbac32f96b811b
Eric Soderberg <eric.soderberg@hpe.com>
2021-02-05 12:47:26 -0800

    prettier cleanup

--- src/js/components/FileInput/stories/Custom.js
+++ src/js/components/FileInput/stories/Custom.js
@@ -25,28 +25,26 @@ const customTheme = {
   },
 };
 
-export const Custom = () => {
-  return (
-    <Grommet full theme={customTheme}>
-      <Box fill align="center" justify="start" pad="large">
-        <Box width="medium">
-          <FileInput
-            fileLabel={file => (
-              <Box direction="row" gap="small">
-                <Text weight="bold">{file.name}</Text>
-                <Text color="text-weak">{file.size} bytes</Text>
-              </Box>
-            )}
-            onChange={event => {
-              const fileList = event.target.files;
-              for (let i = 0; i < fileList.length; i += 1) {
-                const file = fileList[i];
-                console.log(file.name);
-              }
-            }}
-          />
-        </Box>
+export const Custom = () => (
+  <Grommet full theme={customTheme}>
+    <Box fill align="center" justify="start" pad="large">
+      <Box width="medium">
+        <FileInput
+          fileLabel={file => (
+            <Box direction="row" gap="small">
+              <Text weight="bold">{file.name}</Text>
+              <Text color="text-weak">{file.size} bytes</Text>
+            </Box>
+          )}
+          onChange={event => {
+            const fileList = event.target.files;
+            for (let i = 0; i < fileList.length; i += 1) {
+              const file = fileList[i];
+              console.log(file.name);
+            }
+          }}
+        />
       </Box>
-    </Grommet>
-  );
-};
+    </Box>
+  </Grommet>
+);
