27322d82405ed21926274be1b60ba54031124e9b
Eric Soderberg <eric.soderberg@hpe.com>
2021-02-08 09:13:05 -0800

    Removed defaultValue documentation and typing

--- src/js/components/CheckBox/index.d.ts
+++ src/js/components/CheckBox/index.d.ts
@@ -4,7 +4,6 @@ import { A11yTitleType } from '../../utils';
 export interface CheckBoxProps {
   a11yTitle?: A11yTitleType;
   checked?: boolean;
-  defaultChecked?: boolean;
   disabled?: boolean;
   id?: string;
   label?: React.ReactNode;
