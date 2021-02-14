aec8f49e43d0a150f44d0203d0012b12f9ac2267
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-02-13 23:46:26 -0300

    Fix ResponsiveContext document width

--- src/js/utils/responsive.js
+++ src/js/utils/responsive.js
@@ -1,29 +1,24 @@
-export const getBreakpoint = (windowWidth, theme) => {
-  let result;
+export const getBreakpoint = (viewportWidth, theme) => {
   const sortedBreakpoints = Object.keys(theme.global.breakpoints).sort(
     (a, b) => {
       const first = theme.global.breakpoints[a];
       const second = theme.global.breakpoints[b];
-      if (!first) return 1;
-      if (!second) return -1;
-      if (!first.value) return 1;
-      if (!second.value) return -1;
+      if (!first || !first.value) return 1;
+      if (!second || !second.value) return -1;
       return first.value - second.value;
     },
   );
+
   // the last breakpoint on the sorted array should have
   // no windowWidth boundaries
   const lastBreakpoint = sortedBreakpoints[sortedBreakpoints.length - 1];
-  sortedBreakpoints.some(name => {
+  const result = sortedBreakpoints.find(name => {
     const breakpoint = theme.global.breakpoints[name];
-    if (breakpoint) {
-      if (!breakpoint.value || breakpoint.value >= windowWidth) {
-        result = name;
-        return true;
-      }
-    }
-    return false;
+    return !breakpoint.value || breakpoint.value >= viewportWidth
+      ? name
+      : false;
   });
+
   return result || lastBreakpoint;
 };
 
