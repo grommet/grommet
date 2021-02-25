f243dcaa35e4b4ae2ba5a21b29ba95a5a8f455d1
Yashwanth Pinneka <ypinneka@nimblestorage.com>
2021-02-25 10:02:56 -0800

    Update valid story for a better end-end workflow picture

--- src/js/components/Form/stories/ValidateOnChange.js
+++ src/js/components/Form/stories/ValidateOnChange.js
@@ -1,58 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Box, Button, Grommet, Form, FormField } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-export const ValidateOnChange = () => (
-  <Grommet full theme={grommet}>
-    <Box fill align="center" justify="center">
-      <Box width="medium">
-        <Form
-          validate="change"
-          onReset={event => console.log(event)}
-          onSubmit={({ value }) => console.log('Submit', value)}
-          onValidate={validationResults => {
-            console.log('validationResults = ', validationResults);
-          }}
-        >
-          <FormField
-            label="First Name"
-            name="firstName"
-            required
-            validate={[
-              { regexp: /^[a-z]/i },
-              firstName => {
-                if (firstName && firstName.length === 1)
-                  return 'must be >1 character';
-                return undefined;
-              },
-            ]}
-          />
+export const ValidateOnChange = () => {
+  const [valid, setValid] = useState(false);
 
-          <FormField
-            label="Last Name"
-            name="lastName"
-            required
-            validate={[
-              { regexp: /^[a-z]/i },
-              lastName => {
-                if (lastName && lastName.length === 1)
-                  return 'must be >1 character';
-                return undefined;
-              },
-            ]}
-          />
+  return (
+    <Grommet full theme={grommet}>
+      <Box fill align="center" justify="center">
+        <Box width="medium">
+          <Form
+            validate="change"
+            onReset={event => console.log(event)}
+            onSubmit={({ value }) => console.log('Submit', value)}
+            onValidate={validationResults => {
+              console.log('validationResults = ', validationResults);
+              setValid(validationResults.valid);
+            }}
+          >
+            <FormField
+              label="First Name"
+              name="firstName"
+              required
+              validate={[
+                { regexp: /^[a-z]/i },
+                firstName => {
+                  if (firstName && firstName.length === 1)
+                    return 'must be >1 character';
+                  return undefined;
+                },
+              ]}
+            />
 
-          <Box direction="row" justify="between" margin={{ top: 'medium' }}>
-            <Button label="Cancel" />
-            <Button type="reset" label="Reset" />
-            <Button type="submit" label="Update" primary />
-          </Box>
-        </Form>
+            <FormField
+              label="Last Name"
+              name="lastName"
+              required
+              validate={[
+                { regexp: /^[a-z]/i },
+                lastName => {
+                  if (lastName && lastName.length === 1)
+                    return 'must be >1 character';
+                  return undefined;
+                },
+              ]}
+            />
+
+            <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+              <Button label="Cancel" />
+              <Button type="reset" label="Reset" />
+              <Button type="submit" label="Update" disabled={!valid} primary />
+            </Box>
+          </Form>
+        </Box>
       </Box>
-    </Box>
-  </Grommet>
-);
+    </Grommet>
+  );
+};
 
 ValidateOnChange.storyName = 'Validate on change';
 
