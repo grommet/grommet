86a5df169eeb4c97aa58c3e7d801e0eccf16d539
Taylor Seamans <taylor.seamans@yahoo.com>
2021-02-25 14:48:28 -0800

    Don't touch story

--- src/js/components/Menu/stories/Simple.js
+++ src/js/components/Menu/stories/Simple.js
@@ -2,30 +2,19 @@ import React from 'react';
 
 import { Grommet, Box, Menu } from 'grommet';
 import { grommet } from 'grommet/themes';
-import { deepMerge } from 'grommet/utils';
 
-const myTheme = deepMerge(grommet, {
-  menu: {
-    drop: {
-      align: {
-        top: 'top',
-        left: 'right',
-      },
-    },
-  },
-});
 const SimpleMenu = () => (
-  <Grommet theme={myTheme}>
+  <Grommet theme={grommet}>
     <Box align="center" pad="large">
       <Menu
-        // dropProps={{
-        //   align: { top: 'bottom', left: 'left' },
-        //   elevation: 'xlarge',
-        // }}
+        dropProps={{
+          align: { top: 'bottom', left: 'left' },
+          elevation: 'xlarge',
+        }}
         label="actions"
         items={[
-          { label: 'Launch Something', onClick: () => {} },
-          { label: 'Abort Something', onClick: () => {} },
+          { label: 'Launch', onClick: () => {} },
+          { label: 'Abort', onClick: () => {} },
           { label: 'Disabled', disabled: true },
         ]}
       />
