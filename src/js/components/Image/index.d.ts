6cc3a22978d6f847e45b5599cfbef71a1027688e
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-03-08 17:18:39 -0300

    Add ImageExtendedProps type

--- src/js/components/Image/index.d.ts
+++ src/js/components/Image/index.d.ts
@@ -18,6 +18,8 @@ export interface ImageProps {
   opacity?: 'weak' | 'medium' | 'strong' | string | boolean;
 }
 
-declare const Image: React.FC<ImageProps & JSX.IntrinsicElements['img']>;
+export type ImageExtendedProps = ImageProps & JSX.IntrinsicElements['img'];
+
+declare const Image: React.FC<ImageExtendedProps>;
 
 export { Image };
