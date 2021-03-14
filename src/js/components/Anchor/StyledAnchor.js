53c715ded8f4efe3c1d75b06690b88d77a0139a4
Mihai Ro <mihairo@192-168-0-102.rdsnet.ro>
2021-03-14 14:15:36 +0200

    fix(styledanchor): prevent custom props from bleeding into DOM and throwing warnings in console

--- src/js/components/Anchor/StyledAnchor.js
+++ src/js/components/Anchor/StyledAnchor.js
@@ -24,7 +24,13 @@ const sizeStyle = props => {
   `;
 };
 
-const StyledAnchor = styled.a`
+const StyledAnchor = styled.a.withConfig({
+  // prevent custom props from bleeding into DOM
+  // https://styled-components.com/docs/api#shouldforwardprop
+  shouldForwardProp: (prop, defaultValidatorFn) =>
+    !['as', 'colorProp', 'hasIcon', 'hasLabel'].includes(prop) &&
+    defaultValidatorFn(prop),
+})`
   box-sizing: border-box;
   ${props => sizeStyle(props)}
   color: ${props =>
