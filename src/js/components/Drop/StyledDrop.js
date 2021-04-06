8fad895341197bdf13b00d6bf1436314b6e44244
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-04-05 20:57:14 -0600

    clean up if statements

--- src/js/components/Drop/StyledDrop.js
+++ src/js/components/Drop/StyledDrop.js
@@ -27,80 +27,22 @@ const dropKeyFrames = keyframes`
   }
 `;
 
-const isObject = object => object != null && typeof object === 'object';
-
-const deepEqual = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < keys1.length; i += 1) {
-    const val1 = object1[keys1[i]];
-    const val2 = object2[keys1[i]];
-    const areObjects = isObject(val1) && isObject(val2);
-    if (
-      (areObjects && !deepEqual(val1, val2)) ||
-      (!areObjects && val1 !== val2)
-    ) {
-      return false;
-    }
-  }
-  return true;
-};
-
 // The desired margin may be adjusted depending on drops alignment
 const marginStyle = (theme, align, data, responsive, marginProp) => {
   const margin = theme.global.edgeSize[data] || data;
-  let customCSS;
-  let adjustedMargin;
+  let adjustedMargin = {};
   // if user provides CSS string such as '50px 12px', apply that always
-  if (typeof margin === 'string') {
-    customCSS = margin.split(' ').length > 1;
-  }
+  const customCSS = typeof margin === 'string' && margin.split(' ').length > 1;
   if (
     theme.global.drop.intelligentMargin === true &&
     !customCSS &&
     typeof margin === 'string'
   ) {
-    if (deepEqual(align, { top: 'bottom' })) {
-      adjustedMargin = { top: margin };
-    } else if (deepEqual(align, { bottom: 'top' })) {
-      adjustedMargin = { bottom: margin };
-    } else if (deepEqual(align, { right: 'left' })) {
-      adjustedMargin = { left: `-${margin}` };
-    } else if (deepEqual(align, { left: 'right' })) {
-      adjustedMargin = { left: margin };
-    } else if (deepEqual(align, { top: 'top', left: 'right' })) {
-      adjustedMargin = { left: margin };
-    } else if (deepEqual(align, { top: 'top', right: 'left' })) {
-      adjustedMargin = { left: `-${margin}` };
-    } else if (deepEqual(align, { top: 'bottom', right: 'left' })) {
-      adjustedMargin = { top: margin, left: `-${margin}` };
-    } else if (deepEqual(align, { top: 'bottom', right: 'right' })) {
-      adjustedMargin = { top: margin };
-    } else if (deepEqual(align, { top: 'bottom', left: 'right' })) {
-      adjustedMargin = { top: margin, left: margin };
-    } else if (deepEqual(align, { top: 'bottom', left: 'left' })) {
-      adjustedMargin = { top: margin };
-    } else if (deepEqual(align, { bottom: 'top', right: 'left' })) {
-      adjustedMargin = { bottom: margin, left: `-${margin}` };
-    } else if (deepEqual(align, { right: 'right', bottom: 'top' })) {
-      adjustedMargin = { bottom: margin };
-    } else if (deepEqual(align, { bottom: 'top', left: 'left' })) {
-      adjustedMargin = { bottom: margin };
-    } else if (deepEqual(align, { bottom: 'top', left: 'right' })) {
-      adjustedMargin = { bottom: margin, left: margin };
-    } else if (deepEqual(align, { bottom: 'top', right: 'left' })) {
-      adjustedMargin = { bottom: margin, left: `-${margin}` };
-    } else if (deepEqual(align, { bottom: 'bottom', left: 'right' })) {
-      adjustedMargin = { left: margin };
-    } else if (deepEqual(align, { bottom: 'bottom', right: 'left' })) {
-      adjustedMargin = { left: `-${margin}` };
-      // Drop will be contained inside of target, don't apply margin
-    } else adjustedMargin = 'none';
+    if (align.top === 'bottom') adjustedMargin.top = margin;
+    else if (align.bottom === 'top') adjustedMargin.bottom = margin;
+    if (align.right === 'left') adjustedMargin.left = `-${margin}`;
+    else if (align.left === 'right') adjustedMargin.left = margin;
+    if (!Object.keys(adjustedMargin)) adjustedMargin = 'none';
   } else {
     return edgeStyle(
       'margin',
