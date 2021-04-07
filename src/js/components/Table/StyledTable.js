6333cb49875c686b386231241aaebd86cebeaa46
taysea <taylor.seamans@yahoo.com>
2021-04-07 12:13:09 -0700

    Update DataTable Checkbox styling, allow plain column render (#5077)
    * Update DataTable Checkbox styling, allow plain column
    
    * Merge master
    
    * Fix height and master changes
    
    * Update docs
    
    * Fix height calc
    
    * Pad default to undefined
    
    * Dont include border in cell height
    
    * Merge master
    
    * Prop value noPad
    
    * Update docs
    
    * Dont adjust default pad value
    
    * Update docs and prop passing
    
    * Incorporate review feedback
    
    Co-authored-by: Shimi <shimrit.yacobi@gmail.com>

--- src/js/components/Table/StyledTable.js
+++ src/js/components/Table/StyledTable.js
@@ -19,9 +19,23 @@ const SIZE_MAP = {
 
 const sizeStyle = css`
   width: ${props =>
-    SIZE_MAP[props.size] || props.theme.global.size[props.size] || props.size};
+    props.size === 'auto'
+      ? // setting width to a small value will allow
+        // the cell to fit width of its content. this
+        // is a commonly implemented CSS pattern to
+        // allow an auto-width behavior on fixed table
+        // layouts (which is what DataTable applies)
+        // https://stackoverflow.com/questions/4757844/css-table-column-autowidth?noredirect=1&lq=1
+        '1px'
+      : SIZE_MAP[props.size] ||
+        props.theme.global.size[props.size] ||
+        props.size};
   max-width: ${props =>
-    SIZE_MAP[props.size] || props.theme.global.size[props.size] || props.size};
+    props.size !== 'auto'
+      ? SIZE_MAP[props.size] ||
+        props.theme.global.size[props.size] ||
+        props.size
+      : undefined};
   overflow: hidden;
 `;
 
