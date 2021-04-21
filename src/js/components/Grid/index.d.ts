88a3b2286872d4f347f9ae051c1f18b51b5f8376
Andrew Covenant <andrewcovenant@Andrews-Mac-mini-2.local>
2021-04-21 09:30:43 +0300

    Type object added as a type on row grid prop

--- src/js/components/Grid/index.d.ts
+++ src/js/components/Grid/index.d.ts
@@ -98,6 +98,26 @@ export interface GridProps {
     | 'medium'
     | 'large'
     | 'xlarge'
+    | {
+      count?: 'fit' | 'fill' | number;
+      size?:
+        | 'xsmall'
+        | 'small'
+        | 'medium'
+        | 'large'
+        | 'xlarge'
+        | 'full'
+        | '1/2'
+        | '1/3'
+        | '2/3'
+        | '1/4'
+        | '2/4'
+        | '3/4'
+        | 'flex'
+        | 'auto'
+        | string
+        | string[];
+    }
     | string;
   tag?: PolymorphicType;
 }
