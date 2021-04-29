01db58c5b2a18404ff14d40beab0bc132aa35cae
Bojan Durdevic <bojan88@gmail.com>
2021-04-29 15:31:34 -0400

    Revert "PR comment addressed - using event.constructor for both event types"
    This reverts commit f6dc658f2f04efd14f02d6e5f34ceda81b5ccfcd.
    
    Signed-off-by: Bojan Durdevic <bojan88@gmail.com>

--- src/js/components/Select/Select.js
+++ src/js/components/Select/Select.js
@@ -186,26 +186,23 @@ const Select = forwardRef(
         setValue(nextValue);
         if (onChange) {
           event.persist();
-          const adjustedEvent =
-            event instanceof Event
-              ? // native event
-                new event.constructor(event.type, event)
-              : // react SyntheticBaseEvent
-                new event.constructor(
-                  // eslint-disable-next-line no-underscore-dangle
-                  event._reactName,
-                  event.type,
-                  // eslint-disable-next-line no-underscore-dangle
-                  event._targetInst,
-                  event.nativeEvent,
-                  event.target,
-                );
-          Object.defineProperties(adjustedEvent, {
-            target: { value: inputRef.current },
-            value: { value: nextValue },
-            option: { value: option },
-            selected: { value: nextSelected },
-          });
+          let adjustedEvent;
+          // support for native event used by Preact
+          if (event instanceof Event) {
+            adjustedEvent = new event.constructor(event.type, event);
+            Object.defineProperties(adjustedEvent, {
+              target: { value: inputRef.current },
+              value: { value: nextValue },
+              option: { value: option },
+              selected: { value: nextSelected },
+            });
+          } else {
+            adjustedEvent = event;
+            adjustedEvent.target = inputRef.current;
+            adjustedEvent.value = nextValue;
+            adjustedEvent.option = option;
+            adjustedEvent.selected = nextSelected;
+          }
           onChange(adjustedEvent);
         }
         setSearch();
