c71ab474ffe5044ea125efd65824891490910cfc
gurkiran_singh <gurkiransinghk@gmail.com>
2021-05-07 18:03:40 -0400

    refactored the codebase as eric suggested
    Signed-off-by: gurkiran_singh <gurkiransinghk@gmail.com>

--- src/js/components/Meter/Bar.js
+++ src/js/components/Meter/Bar.js
@@ -13,65 +13,36 @@ const Bar = forwardRef((props, ref) => {
     max,
     round,
     size,
-    thickness,
+    thickness: thicknessProp,
     direction,
     values,
     ...rest
   } = props;
   const theme = useContext(ThemeContext) || defaultProps.theme;
-  const width =
+  const length =
     size === 'full' ? 288 : parseMetricToNum(theme.global.size[size] || size);
-  const height = parseMetricToNum(
-    theme.global.edgeSize[thickness] || thickness,
+  const thickness = parseMetricToNum(
+    theme.global.edgeSize[thicknessProp] || thicknessProp,
   );
   // account for the round cap, if any
-  const capOffset = round ? height / 2 : 0;
-  const mid = height / 2;
+  const capOffset = round ? thickness / 2 : 0;
+  const mid = thickness / 2;
 
   const someHighlight = (values || []).some(v => v.highlight);
-  let start = capOffset;
+  let start =
+    direction === 'horizontal'
+      ? capOffset
+      : (max * (length - 2 * capOffset)) / max;
 
-  // suppose if the user only passes the
-  // two values, 50, 20 and max value provided
-  // by user is 100 then the bar starts value from top.
-  // It means in the top its shows
-  // the value for 20 then 50. Here I am creating a dumpy
-  // value for the leftover of max
-  // value which will show empty value in the top.
-  if (direction === 'vertical') {
-    const res = values.reduce((acc, curr) => acc + curr.value, 0);
-    if (res !== max) {
-      values.push({ value: max - res, color: '#F2F2F2' });
-      values.reverse();
-    }
-  }
   const paths = (values || []).reduce((acc, valueArg, index) => {
     if (valueArg.value > 0) {
       const { color, highlight, label, onHover, value, ...pathRest } = valueArg;
       const key = `p-${index}`;
-      let delta;
-      let d;
-      if (direction === 'horizontal') {
-        delta = (value * (width - 2 * capOffset)) / max;
-        d = `M ${start},${mid} L ${start + delta},${mid}`;
-      } else if (direction === 'vertical') {
-        //  if there is only one value provided by
-        //  user like value = 30, previous logic
-        //  display the value in the top of bar but by
-        //  providing the height in the M of the d
-        //  ,the value starts from bottom but instead of 30,
-        //  the stroke shows value for 70,
-        //  so here (max - value) keeps the logic right
-        //  for the value given by user.
-        if (values.length === 1) {
-          delta = ((max - value) * (width - 2 * capOffset)) / max;
-          d = `M ${mid},${width} L ${mid},${start + delta}`;
-        } else {
-          delta = (value * (width - 2 * capOffset)) / max;
-          d = `M ${mid},${start} L ${mid},${start + delta}`;
-        }
-      }
-
+      const delta = (value * (length - 2 * capOffset)) / max;
+      const d =
+        direction === 'horizontal'
+          ? `M ${start},${mid} L ${start + delta},${mid}`
+          : `M ${mid},${start} L ${mid},${start - delta}`;
       const colorName =
         color || defaultColor(index, theme, values ? values.length : 0);
       let hoverProps;
@@ -81,7 +52,12 @@ const Bar = forwardRef((props, ref) => {
           onMouseLeave: () => onHover(false),
         };
       }
-      start += delta;
+      if (direction === 'horizontal') {
+        start += delta;
+      } else {
+        start -= delta;
+      }
+
       const result = (
         <path
           key={key}
@@ -91,7 +67,7 @@ const Bar = forwardRef((props, ref) => {
             someHighlight && !highlight ? background : colorName,
             theme,
           )}
-          strokeWidth={direction === 'horizontal' ? height : width}
+          strokeWidth={direction === 'horizontal' ? thickness : length}
           strokeLinecap={round ? 'round' : 'butt'}
           {...hoverProps}
           {...pathRest}
@@ -103,37 +79,37 @@ const Bar = forwardRef((props, ref) => {
     return acc;
   }, []);
 
-  let dForFirstPath;
-  let viewBox;
-  let widthStyledMeter;
-  let heightStyledMeter;
+  let width;
   if (direction === 'horizontal') {
-    dForFirstPath = `M ${capOffset},${mid} L ${width - capOffset},${mid}`;
-    viewBox = `0 0 ${width} ${height}`;
-    widthStyledMeter = size === 'full' ? '100%' : width;
-    heightStyledMeter = height;
+    width = size === 'full' ? '100%' : length;
   } else {
-    dForFirstPath = `M ${mid},${capOffset} L ${mid},${width - capOffset}`;
-    viewBox = `0 0 ${height} ${width}`;
-    widthStyledMeter = size === 'full' ? '100%' : height;
-    heightStyledMeter = width;
+    width = size === 'full' ? '100%' : thickness;
   }
+
+  const backgroundPath =
+    direction === 'horizontal'
+      ? `M ${capOffset},${mid} L ${length - capOffset},${mid}`
+      : `M ${mid},${capOffset} L ${mid},${length - capOffset}`;
 
   return (
     <StyledMeter
       ref={ref}
-      viewBox={viewBox}
+      viewBox={
+        direction === 'horizontal'
+          ? `0 0 ${length} ${thickness}`
+          : `0 0 ${thickness} ${length}`
+      }
       preserveAspectRatio="none"
-      width={widthStyledMeter}
-      height={heightStyledMeter}
-      round={round ? { size: thickness } : undefined}
+      width={width}
+      height={direction === 'horizontal' ? thickness : length}
+      round={round ? { size: thicknessProp } : undefined}
       {...rest}
     >
       <path
-        d={dForFirstPath}
+        d={backgroundPath}
         fill="none"
         {...strokeProps(background, theme)}
-        strokeWidth={height}
+        strokeWidth={thickness}
         strokeLinecap={round ? 'round' : 'square'}
       />
       {paths}
