c79669e757ff384337067eed81c00aa999ec6082
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-05-17 12:08:35 -0400

    Pre-Commit Checks: fixes for the warnings given

--- src/js/components/Diagram/StyledDiagram.js
+++ src/js/components/Diagram/StyledDiagram.js
@@ -1,12 +1,74 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 import { defaultProps } from '../../default-props';
+
+const PULSE_SIZES = {
+  xsmall: 1.001,
+  small: 1.01,
+  medium: 1.1,
+  large: 1.5,
+  xlarge: 2,
+};
+
+const normalizeTiming = (time, defaultTiming) =>
+  time ? `${time / 1000.0}s` : defaultTiming;
+
+const animationEnding = type => {
+  if (type === 'pulse') {
+    return 'alternate infinite';
+  }
+  return 'fowards';
+};
+
+const animationBounds = (type, size = 'medium') => {
+  if (type === 'pulse') {
+    return ['transform: scale(1);', `transform: scale(${PULSE_SIZES[size]})`];
+  }
+  return [];
+};
+
+const animationObjectStyle = (animation, theme) => {
+  const bounds = animationBounds(animation.type, animation.size);
+
+  if (bounds) {
+    const animationTransition = css`
+      from {
+        ${bounds[0]};
+      }
+      to {
+        ${bounds[1]};
+      }
+    `;
+    return css`
+    ${keyframes`${animationTransition}`}
+    ${normalizeTiming(
+      animation.duration,
+      (theme.global.animation[animation.type]
+        ? theme.global.animation[animation.type].duration
+        : undefined) || theme.global.animation.duration,
+    )}
+      ${normalizeTiming(animation.delay, '0s')}
+      ${animationEnding(animation.type)}
+    `;
+  }
+  return '';
+};
+
+const animationItemStyle = (item, theme) =>
+  animationObjectStyle({ type: item }, theme);
+
+const animationStyle = css`
+  ${props => css`
+    animation: ${animationItemStyle(props.animation, props.theme)};
+  `};
+`;
 
 const StyledDiagram = styled.svg`
   max-width: 100%;
   width: 100%;
   height: 100%;
 
+  ${props => props.animation && animationStyle}
   ${props => props.theme.diagram && props.theme.diagram.extend};
 `;
 
