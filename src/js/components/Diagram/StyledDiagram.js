e6b549a0ba22d2cef7bc37e80b50fdbe183b4606
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-05-20 16:03:15 -0400

    Animation Utils: created animation utils to be used by both Box and Diagram as well as tested the animations on Diagram after transferring the code to utils

--- src/js/components/Diagram/StyledDiagram.js
+++ src/js/components/Diagram/StyledDiagram.js
@@ -1,65 +1,7 @@
-import styled, { css, keyframes } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { defaultProps } from '../../default-props';
-
-const PULSE_SIZES = {
-  xsmall: 1.001,
-  small: 1.01,
-  medium: 1.1,
-  large: 1.5,
-  xlarge: 2,
-};
-
-const normalizeTiming = (time, defaultTiming) =>
-  time ? `${time / 1000.0}s` : defaultTiming;
-
-const animationEnding = type => {
-  if (type === 'pulse') {
-    return 'alternate infinite';
-  }
-  if (type === 'draw') {
-    return 'linear forwards';
-  }
-  return 'fowards';
-};
-
-const animationBounds = (type, size = 'medium') => {
-  if (type === 'pulse') {
-    return ['transform: scale(1);', `transform: scale(${PULSE_SIZES[size]})`];
-  }
-  if (type === 'draw') {
-    return ['', `stroke-dashoffset: 0`];
-  }
-  return [];
-};
-
-const animationObjectStyle = (animation, theme) => {
-  const bounds = animationBounds(animation.type, animation.size);
-
-  if (bounds) {
-    const animationTransition = css`
-      from {
-        ${bounds[0]};
-      }
-      to {
-        ${bounds[1]};
-      }
-    `;
-    return css`
-    ${keyframes`${animationTransition}`}
-    ${normalizeTiming(
-      animation.duration,
-      (theme.global.animation[animation.type]
-        ? theme.global.animation[animation.type].duration
-        : undefined) || theme.global.animation.duration,
-    )}
-      ${normalizeTiming(animation.delay, '0s')}
-      ${animationEnding(animation.type)}
-      
-    `;
-  }
-  return '';
-};
+import { animationObjectStyle } from '../../utils';
 
 const animationItemStyle = (item, theme) =>
   animationObjectStyle({ type: item }, theme);
