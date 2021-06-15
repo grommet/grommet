45b3bde78c355c7b68e117e522c5b2469b3fe9f6
kid-icarus <ryan.kois@gmail.com>
2021-06-14 21:07:35 -0700

    Add default title for truncated Text. (#5138)
    * Add default title for truncated Text.
    
    This is a minor UX improvement. It ensures that truncated text is
    still visible if the user hovers over it.
    
    * Incorporate feedback
    
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>
    Co-authored-by: Shimi <shimrit.yacobi@gmail.com>

--- src/js/components/Text/Text.js
+++ src/js/components/Text/Text.js
@@ -1,16 +1,41 @@
 import React, { forwardRef } from 'react';
 
 import { StyledText } from './StyledText';
+import { Tip } from '../Tip';
 
-const Text = forwardRef(({ color, tag, as, a11yTitle, ...rest }, ref) => (
-  <StyledText
-    as={!as && tag ? tag : as}
-    colorProp={color}
-    aria-label={a11yTitle}
-    {...rest}
-    ref={ref}
-  />
-));
+const Text = forwardRef(
+  (
+    {
+      color,
+      tag,
+      as,
+      tip,
+      // can't alphabetize a11yTitle before tip is defined
+      a11yTitle = typeof tip === 'string' ? tip : undefined,
+      ...rest
+    },
+    ref,
+  ) => {
+    const styledTextResult = (
+      <StyledText
+        as={!as && tag ? tag : as}
+        colorProp={color}
+        aria-label={a11yTitle}
+        {...rest}
+        ref={ref}
+      />
+    );
+
+    if (tip) {
+      if (typeof tip === 'string') {
+        return <Tip content={tip}>{styledTextResult}</Tip>;
+      }
+      return <Tip {...tip}>{styledTextResult}</Tip>;
+    }
+
+    return styledTextResult;
+  },
+);
 
 Text.displayName = 'Text';
 Text.defaultProps = {
