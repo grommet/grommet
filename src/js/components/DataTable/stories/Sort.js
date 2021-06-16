dacf4d3fe87da2981674bae6161bf0e049d44378
Samuel Goff <samuel@defpix.com>
2021-06-16 12:19:25 -0500

    incorporates separate data for sorting story & reverting shared data for DataTable, per feedback from @ericsoderberghp
    Signed-off-by: Samuel Goff <samuel@defpix.com>

--- src/js/components/DataTable/stories/Sort.js
+++ src/js/components/DataTable/stories/Sort.js
@@ -3,9 +3,89 @@ import React from 'react';
 import { Grommet, Box, DataTable } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-// Source code for the data can be found here
-// https://github.com/grommet/grommet/blob/master/src/js/components/DataTable/stories/data.js
-import { columns, DATA } from './data';
+import { columns } from './data';
+
+// This story uses intentionally-messy values (e.g. mixed case)
+// to demonstrate case-insensitive sorting
+const DATA = [
+  {
+    name: 'zoey',
+    location: '',
+    date: '',
+    percent: 0,
+    paid: 0,
+  },
+  {
+    name: 'aaron',
+    location: '',
+    date: '',
+    percent: 0,
+    paid: 0,
+  },
+  {
+    name: 'Zelda',
+    location: '',
+    date: '',
+    percent: 0,
+    paid: 0,
+  },
+  {
+    name: 'Alan',
+    location: '',
+    date: '',
+    percent: 0,
+    paid: 0,
+  },
+  {
+    name: 'Bryan',
+    location: 'Fort Collins',
+    date: '2018-06-10',
+    percent: 30,
+    paid: 1234,
+  },
+  {
+    name: 'Chris',
+    location: 'Palo Alto',
+    date: '2018-06-09',
+    percent: 40,
+    paid: 2345,
+  },
+  {
+    name: 'Eric',
+    location: 'Palo Alto',
+    date: '2018-06-11',
+    percent: 80,
+    paid: 3456,
+  },
+  {
+    name: 'Doug',
+    location: 'Fort Collins',
+    date: '2018-06-10',
+    percent: 60,
+    paid: 1234,
+  },
+  {
+    name: 'Jet',
+    location: 'Palo Alto',
+    date: '2018-06-09',
+    percent: 40,
+    paid: 3456,
+  },
+  {
+    name: 'Michael',
+    location: 'Boise',
+    date: '2018-06-11',
+    percent: 50,
+    paid: 1234,
+  },
+  {
+    name: 'Tracy',
+    location: 'San Francisco',
+    date: '2018-06-10',
+    percent: 10,
+    paid: 2345,
+  },
+];
 
 export const Sort = () => {
   const [sort, setSort] = React.useState({
