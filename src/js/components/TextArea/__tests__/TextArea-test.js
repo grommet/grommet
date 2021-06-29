061a755cbb59abae0fe5ddd4f5369bac7982caea
Lucas Vieira <lucas10.vieira.lv@gmail.com>
2021-06-28 23:36:25 -0300

    Refactor TextArea test to remove react-test-renderer

--- src/js/components/TextArea/__tests__/TextArea-test.js
+++ src/js/components/TextArea/__tests__/TextArea-test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { cleanup, fireEvent, render } from '@testing-library/react';
-import 'jest-styled-components';
 import { axe } from 'jest-axe';
+import 'jest-styled-components';
 import 'jest-axe/extend-expect';
 import 'regenerator-runtime/runtime';
 
@@ -23,86 +22,86 @@ describe('TextArea', () => {
   });
 
   test('basic', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea id="item" name="item" />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('placeholder', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea id="item" name="item" placeholder="placeholder" />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('plain', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea id="item" name="item" plain />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('disabled', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea disabled id="item" name="item" plain />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('focusIndicator', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea id="item" name="item" focusIndicator />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('fill', () => {
-    const component = renderer.create(
+    const { container } = render(
       <Grommet>
         <TextArea id="item" name="item" fill />
       </Grommet>,
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   [true, false, 'horizontal', 'vertical'].forEach(resize => {
     test(`resize ${resize}`, () => {
-      const component = renderer.create(
+      const { container } = render(
         <Grommet>
           <TextArea id="item" name="item" resize={resize} />
         </Grommet>,
       );
-      const tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
+
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 
   ['small', 'medium', 'large'].forEach(size => {
     test(`size ${size}`, () => {
-      const component = renderer.create(
+      const { container } = render(
         <Grommet>
           <TextArea id="item" name="item" size={size} />
         </Grommet>,
       );
-      const tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
+
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 
