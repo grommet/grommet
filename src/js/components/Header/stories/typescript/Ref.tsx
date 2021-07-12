4c02c32aead004b4ddf641f10d6fda453de887bf
Tanner B. Hess Webber <tb@hesswebber.com>
2021-07-12 17:20:11 -0400

    story(header): fixes ref creation in Ref

--- src/js/components/Header/stories/typescript/Ref.tsx
+++ src/js/components/Header/stories/typescript/Ref.tsx
@@ -6,19 +6,18 @@ import { grommet } from 'grommet/themes';
 const gravatarLink =
   '//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80';
 
-export const Ref = () => {
-  const ref = React.createRef<HTMLDivElement>();
-  const [color, setColor] = React.useState('');
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 
-  const setRandomColor = () => {
-    const red = Math.floor(Math.random() * 255).toString(16);
-    const green = Math.floor(Math.random() * 255).toString(16);
-    const blue = Math.floor(Math.random() * 255).toString(16);
-    setColor(`#${red}${green}${blue}`);
+export const Ref = () => {
+  const ref = React.useRef<HTMLDivElement>();
+  const [colorIndex, setColorIndex] = React.useState(0);
+
+  const changeColor = () => {
+    setColorIndex((i) => (i + 1) % colors.length);
   };
 
   React.useEffect(() => {
-    if (ref.current) ref.current.style.backgroundColor = color;
+    if (ref.current) ref.current.style.backgroundColor = colors[colorIndex];
   });
 
   return (
@@ -26,7 +25,7 @@ export const Ref = () => {
       <Header background="light-4" pad="small" ref={ref}>
         <Avatar src={gravatarLink} />
         <Nav direction="row">
-          <Button primary label="Change Color" onClick={setRandomColor} />
+          <Button primary label="Change Color" onClick={changeColor} />
         </Nav>
       </Header>
     </Grommet>
