64858998a7f1365a5f5c2a96f4e68003a079c87b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-08-11 15:36:09 -0600

    add default values for usecontext

--- src/js/contexts/RootsContext.js
+++ src/js/contexts/RootsContext.js
@@ -3,4 +3,4 @@ import React from 'react';
 // When toggling aria-hidden values, we only want to affect elements
 // in the DOM that come from Grommet, so we track those elements in this
 // context value. See FocusedContainer.js
-export const RootsContext = React.createContext(undefined);
+export const RootsContext = React.createContext([]);
