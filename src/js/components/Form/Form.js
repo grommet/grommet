6b26945c4cf112fffcace4e8216dbc9b69ebc1d6
britt6612 <42451602+britt6612@users.noreply.github.com>
2021-08-23 21:33:39 -0600

    add disabled for form context to errors be undefined (#5530)
    * add disabled for form context to errors be undefined
    
    * pass through prop and change naming
    
    * add disabledProp to input
    
    * add disabled to validationResult
    
    * add the story updating state
    
    * add disable to use effect
    
    * take out story
    
    * Chnage prop back to disabled
    
    Co-authored-by: Eric Soderberg <eric.soderberg@hpe.com>

--- src/js/components/Form/Form.js
+++ src/js/components/Form/Form.js
@@ -342,9 +342,12 @@ const Form = forwardRef(
       info: infoArg,
       name,
       required,
+      disabled,
       validate: validateArg,
     }) => {
-      const error = errorArg || validationResults.errors[name];
+      const error = disabled
+        ? undefined
+        : errorArg || validationResults.errors[name];
       const info = infoArg || validationResults.infos[name];
 
       useEffect(() => {
@@ -394,12 +397,15 @@ const Form = forwardRef(
         } else if (index !== -1) requiredFields.current.splice(index, 1);
 
         if (validateArg || required) {
+          if (disabled) {
+            return undefined;
+          }
           validations.current[name] = validateField;
           return () => delete validations.current[name];
         }
 
         return undefined;
-      }, [error, name, required, validateArg]);
+      }, [error, name, required, validateArg, disabled]);
 
       return {
         error,
