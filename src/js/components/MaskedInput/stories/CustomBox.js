4dcf1b0d6f012c3a480ea1b9ee157b01749498ac
Matt Glissmann <mdglissmann@gmail.com>
2021-08-25 15:13:09 -0600

    ensure drop target is defined prior to calling Drop

--- src/js/components/MaskedInput/stories/CustomBox.js
+++ src/js/components/MaskedInput/stories/CustomBox.js
@@ -4,15 +4,7 @@ import { grommet } from 'grommet/themes';
 
 export const CustomBoxMaskedInput = () => {
   const [value, setValue] = React.useState('');
-  const [dropProps, setDropProps] = React.useState({});
   const boxRef = React.useRef();
-
-  // Update the DropProps target once ref exists
-  React.useEffect(() => {
-    const nextDropProps = { ...dropProps };
-    nextDropProps.target = boxRef.current;
-    setDropProps(nextDropProps);
-  }, [boxRef]);
 
   return (
     <Grommet full theme={grommet}>
@@ -30,7 +22,7 @@ export const CustomBoxMaskedInput = () => {
         <Box flex width="medium" gap="medium">
           <MaskedInput
             plain
-            dropProps={dropProps}
+            dropProps={{ target: boxRef.current }}
             mask={[
               {
                 length: [1, 4],
