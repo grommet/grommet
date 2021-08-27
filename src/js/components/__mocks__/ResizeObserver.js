350d3614df01a719ebc122a94f34e64abc08840f
Brice Deguigne <brice@lecomptoirdespharmacies.fr>
2021-08-27 17:41:31 +0200

    resize observer mock

---
+++ src/js/components/__mocks__/ResizeObserver.js
@@ -0,0 +1,15 @@
+class ResizeObserver {
+    // eslint-disable-next-line class-methods-use-this
+    observe() {
+        // do nothing
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    unobserve() {
+        // do nothing
+    }
+}
+
+window.ResizeObserver = ResizeObserver;
+
+export default ResizeObserver;
