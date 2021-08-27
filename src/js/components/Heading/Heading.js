61fe86e873584de70d5b01c2817dcddb94a3e84e
britt6612 <42451602+britt6612@users.noreply.github.com>
2021-08-26 18:25:23 -0600

    Update Heading.js

--- src/js/components/Heading/Heading.js
+++ src/js/components/Heading/Heading.js
@@ -3,20 +3,22 @@ import React, { forwardRef } from 'react';
 import { StyledHeading } from './StyledHeading';
 import { HeadingPropTypes } from './propTypes';
 
-const Heading = forwardRef((
-  { color, fill, level, ...rest },
-  ref, // munged to avoid styled-components putting it in the DOM
-) => (
-  // enforce level to be a number
-  <StyledHeading
-    as={`h${level}`}
-    colorProp={color}
-    fillProp={fill}
-    level={+level}
-    {...rest}
-    ref={ref}
-  />
-));
+const Heading = forwardRef(
+  (
+    { color, fill, level, ...rest },
+    ref, // munged to avoid styled-components putting it in the DOM
+  ) => (
+    // enforce level to be a number
+    <StyledHeading
+      as={`h${level}`}
+      colorProp={color}
+      fillProp={fill}
+      level={+level}
+      {...rest}
+      ref={ref}
+    />
+  ),
+);
 
 Heading.displayName = 'Heading';
 Heading.defaultProps = {
