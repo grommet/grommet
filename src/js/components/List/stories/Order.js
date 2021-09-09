8f36331486c10e38b76e500bec64f64298dd2744
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-09-09 11:59:48 -0600

    revert story changes

--- src/js/components/List/stories/Order.js
+++ src/js/components/List/stories/Order.js
@@ -4,13 +4,7 @@ import { Grommet, Box, List } from 'grommet';
 import { grommet } from 'grommet/themes';
 
 const locations = [
-  `Lorem ipsum, or lipsum as it is sometimes known,
-   is dummy text used in laying out print, graphic
-   or web designs. The passage is attributed to an
-   unknown typesetter in the 15th century who is
-   thought to have scrambled parts of Cicero's De Finibus
-   Bonorum et Malorum for use in a type specimen book.
-   It usually begins with:`,
+  'Boise',
   'Fort Collins',
   'Los Gatos',
   'Palo Alto',
