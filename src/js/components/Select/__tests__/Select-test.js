db2330532af0ab7ae2eb9a6e7f7110ed32fa5f83
Jessica Filben <jessica.filben@gmail.com>
2021-09-14 15:12:22 -0600

    Disable nested-interactive rule in select accessibility tests

--- src/js/components/Select/__tests__/Select-test.js
+++ src/js/components/Select/__tests__/Select-test.js
@@ -22,7 +22,20 @@ describe('Select', () => {
       </Grommet>,
     );
 
-    const results = await axe(container);
+    const results = await axe(container, {
+      rules: {
+        /* This rule is flagged because Select is built using a 
+        TextInput within a DropButton. According to Dequeue and 
+        WCAG 4.1.2 "interactive controls must not have focusable 
+        descendants". Jest-axe is assuming that the input is focusable
+        and since the input is a descendant of the button the rule is 
+        flagged. However, the TextInput is built so that it is read 
+        only and cannot receive focus. Select is accessible 
+        according to the WCAG specification, but jest-axe is flagging
+        it so we are disabling this rule. */
+        'nested-interactive': { enabled: false }
+      }
+    });
     expect(results).toHaveNoViolations();
 
     expect(container.firstChild).toMatchSnapshot();
