8e1bcbd2ba24cea208b742f57659309032fc5b2f
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-09-24 11:59:44 -0600

    add types

--- src/js/components/NameValueList/index.d.ts
+++ src/js/components/NameValueList/index.d.ts
@@ -1,8 +1,27 @@
 import * as React from 'react';
 import { A11yTitleType } from '../../utils';
+import { BoxTypes } from '../Box';
+import { TextProps } from '../Text';
 
 export interface NameValueListProps {
   a11yTitle?: A11yTitleType;
+  data?: [
+    {
+      name?: string;
+      value?: string;
+      nameVisual?: React.ReactNode;
+      valueVisual?: React.ReactNode;
+    },
+  ];
+  layout?: string;
+  pairProps?: BoxTypes;
+  nameProps?: TextProps;
+  listProps?: BoxTypes;
+  valueProps?: TextProps;
+  align?: {
+    name?: string;
+    value?: string;
+  };
 }
 
 declare const NameValueListProps: React.ComponentClass<NameValueListProps>;
