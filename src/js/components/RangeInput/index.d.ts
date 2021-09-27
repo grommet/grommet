52e0538b24b300372cc1c14e55b4b36852604ba3
Matt Glissmann <mdglissmann@gmail.com>
2021-09-27 09:41:53 -0600

    fix typescript extend and naming

--- src/js/components/RangeInput/index.d.ts
+++ src/js/components/RangeInput/index.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { A11yTitleType, ColorType } from '../../utils';
 
-interface ColorI {
+interface ColorInterface {
   color: ColorType;
   value: number;
   opacity?: number;
@@ -14,13 +14,13 @@ export interface RangeInputProps {
   max?: number | string;
   name?: string;
   step?: number;
-  color?: ColorType | ColorI[];
+  color?: ColorType | ColorInterface[];
   value?: number | string;
 }
 
 export interface RangeInputExtendedProps
   extends RangeInputProps,
-    Omit<JSX.IntrinsicElements['input'], 'step' | 'value'> {}
+    Omit<JSX.IntrinsicElements['input'], 'color' | 'step' | 'value'> {}
 
 declare const RangeInput: React.FC<RangeInputExtendedProps>;
 
