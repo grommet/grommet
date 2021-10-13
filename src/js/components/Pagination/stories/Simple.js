1a2cd17b67e7990c0499a844c362e6ab682d8c61
Taylor Seamans <taylor.seamans@yahoo.com>
2021-10-13 12:48:25 -0700

    Revert story to original

--- src/js/components/Pagination/stories/Simple.js
+++ src/js/components/Pagination/stories/Simple.js
@@ -1,63 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Box, Button, DataTable, Grommet, Text } from 'grommet';
+import { Box, Grommet, Pagination, Text } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-const defaultData = [
-  {
-    id: 'OID257383',
-    orderName: 'My Test Order',
-    state: 'Created',
-  },
-];
-
-const columns = [
-  {
-    property: 'orderName',
-    header: 'Name',
-    render: (datum) => <Text>{datum.orderName}</Text>,
-    size: 'small',
-    search: true,
-  },
-  {
-    property: 'state',
-    header: 'State',
-    render: (datum) => <Text>{datum.state}</Text>,
-    size: 'xsmall',
-  },
-];
-
-export const Simple = () => {
-  const [data, setData] = useState(defaultData);
-
-  const removeData = () => {
-    const newArray = [...data];
-    newArray.pop();
-    setData(newArray);
-  };
-  const addData = () => {
-    const newArray = [...data];
-    newArray.push({
-      id: Math.floor(Math.random() * 10000),
-      orderName: `NEW_${Math.floor(Math.random() * 10)}`,
-      state: `State_${Math.floor(Math.random() * 10)}`,
-    });
-    setData(newArray);
-  };
-
-  return (
-    <Grommet theme={grommet}>
-      <Box gap="small">
-        <DataTable columns={columns} data={data} paginate step={1} />
+export const Simple = () => (
+  <Grommet theme={grommet}>
+    <Box align="start" pad="small" gap="medium">
+      <Box>
+        <Text>Default</Text>
+        <Pagination numberItems={237} />
       </Box>
-      <Box width="medium">
-        <Button onClick={addData} label="Add row" />
-        <br />
-        <Button onClick={removeData} label="Remove last item" />
+      <Box>
+        <Text>Box Props</Text>
+        <Pagination
+          numberItems={1237}
+          page={24}
+          background="brand"
+          pad="medium"
+          margin="small"
+        />
       </Box>
-    </Grommet>
-  );
-};
+    </Box>
+  </Grommet>
+);
+
 export default {
   title: 'Controls/Pagination/Simple',
 };
