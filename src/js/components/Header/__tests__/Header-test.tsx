be0dcffc1bde8a4a8d631f3659668416d1c8c1fd
Marcin Rogoza <proth7@hotmail.com>
2021-10-18 23:33:00 +0200

    Migrate Header component tests to typescript

---
+++ src/js/components/Header/__tests__/Header-test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Grommet } from '../../Grommet';
+import { Header } from '../index';
+
+describe('Header', () => {
+  it('should forward a ref to the underlying header', () => {
+    const ref = React.createRef<HTMLDivElement>();
+
+    render(
+      <Grommet>
+        <Header ref={ref}>Hello, World!</Header>
+      </Grommet>,
+    );
+
+    expect(ref.current).not.toBe(null);
+    expect(ref.current?.nodeName).toBe('HEADER');
+  });
+});
