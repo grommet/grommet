a0bc7a66bce6261436f76b5c58a6bf679b8632b4
Gurkiran Singh <61521805+g4rry420@users.noreply.github.com>
2021-10-20 16:15:22 -0400

    Update index.d.ts

--- src/js/components/FileInput/index.d.ts
+++ src/js/components/FileInput/index.d.ts
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { BorderType, PadType, MarginType, BackgroundType } from '../../utils';
-
 export interface FileInputProps {
   disabled?: boolean;
   id?: string;
