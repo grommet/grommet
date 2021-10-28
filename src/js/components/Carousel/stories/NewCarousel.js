9d958251fcba4f4288344bb74d9482f4c2fa16ee
kevingill55 <kevin.gill@hpe.com>
2021-10-28 10:20:44 -0600

    Updating NewCarousel storybook stories

--- src/js/components/Carousel/stories/NewCarousel.js
+++ src/js/components/Carousel/stories/NewCarousel.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Attraction, Car, TreeOption, Cloud, Amazon } from 'grommet-icons';
-import { Grommet, Box } from 'grommet';
+import { Box, Text } from 'grommet';
 
 import { NewCarousel } from '../NewCarousel';
 
 export const Simple = () => (
-  <Grommet>
-    <Box align="center" pad="large" background="white">
-      <NewCarousel height="600px" width="600px">
+  <Box direction="row" align="center" justify="center" fill="horizontal">
+    <Box
+      height="500px"
+      width="500px"
+      align="center"
+      gap="small"
+      justify="center"
+    >
+      <Text size="small">Carousel on white background</Text>
+      <NewCarousel height="400px" width="400px">
         <Box fill align="center" justify="center" background="accent-1">
           <Attraction size="xlarge" />
         </Box>
@@ -25,7 +32,66 @@ export const Simple = () => (
         </Box>
       </NewCarousel>
     </Box>
-  </Grommet>
+    <Box
+      height="500px"
+      width="500px"
+      gap="small"
+      align="center"
+      justify="center"
+      background="lavender"
+    >
+      <Text size="small">Carousel light mode on light background</Text>
+      <NewCarousel
+        height="400px"
+        width="400px"
+        initialChild={3}
+        background="lavender"
+      >
+        <Box fill align="center" justify="center" background="accent-1">
+          <Attraction size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="black">
+          <TreeOption size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="accent-3">
+          <Car size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="accent-4">
+          <Cloud size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center">
+          <Amazon size="xlarge" />
+        </Box>
+      </NewCarousel>
+    </Box>
+    <Box
+      height="500px"
+      width="500px"
+      align="center"
+      gap="small"
+      justify="center"
+      background="#263040"
+    >
+      <Text size="small">Carousel dark mode on dark background</Text>
+      <NewCarousel height="400px" width="400px" dark initialChild={2}>
+        <Box fill align="center" justify="center" background="accent-1">
+          <Attraction size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="black">
+          <TreeOption size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="accent-3">
+          <Car size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center" background="accent-4">
+          <Cloud size="xlarge" />
+        </Box>
+        <Box fill align="center" justify="center">
+          <Amazon size="xlarge" />
+        </Box>
+      </NewCarousel>
+    </Box>
+  </Box>
 );
 
 export default {
--- src/js/components/Carousel/stories/NewCarouselClickableChild.js
+++ src/js/components/Carousel/stories/NewCarouselClickableChild.js
@@ -1,51 +1,52 @@
 import React from 'react';
-import { Grommet, Text, Button, Box } from 'grommet';
+import { Text, Button, Box } from 'grommet';
 
 import { NewCarousel } from '../NewCarousel';
 
 export const ClickableChild = () => (
-  <Grommet>
-    <Box align="center" pad="large">
-      <NewCarousel height="600px" width="600px">
-        <Box
-          fill
-          align="center"
-          justify="center"
-          background="light-4"
-          gap="large"
-        >
-          <Text weight="bold" size="xlarge">
-            Slide One
-          </Text>
-          <Button label="Click Me!" color="black" />
-        </Box>
-        <Box
-          fill
-          align="center"
-          justify="center"
-          background="light-4"
-          gap="large"
-        >
-          <Text weight="bold" size="xlarge">
-            Slide Two
-          </Text>
-          <Button color="black" label="Click Me!" />
-        </Box>
-        <Box
-          fill
-          align="center"
-          justify="center"
-          background="light-4"
-          gap="large"
-        >
-          <Text weight="bold" size="xlarge">
-            Slide Three
-          </Text>
-          <Button label="Click Me!" color="black" />
-        </Box>
-      </NewCarousel>
-    </Box>
-  </Grommet>
+  <Box align="center" pad="large">
+    <NewCarousel height="600px" width="600px">
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="lavender"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 1
+        </Text>
+        <Button label="Button" />
+      </Box>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="light-3"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 2
+        </Text>
+        <Button label="Button" />
+        <Button label="Button" />
+      </Box>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="light-5"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 3
+        </Text>
+        <Button label="Button" />
+        <Button label="Button" />
+        <Button label="Button" />
+      </Box>
+    </NewCarousel>
+  </Box>
 );
 
 ClickableChild.storyName = 'Clickable Child';
