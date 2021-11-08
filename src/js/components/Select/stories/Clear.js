ac847d508c9caedc862beec1429cc2fb6d42e134
Chris Valmonte <cvalmonte@bittitan.com>
2021-11-08 12:27:30 -0800

    Update storybook for Clear

--- src/js/components/Select/stories/Clear.js
+++ src/js/components/Select/stories/Clear.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
-
-import { Box, Grommet, Select, grommet } from 'grommet';
+import { Box, Select } from 'grommet';
 
 const options = ['one', 'two', 'three'];
 
 export const Top = () => {
   const [value, setValue] = useState(options[0]);
   return (
-    <Grommet theme={grommet}>
-      <Box fill align="center" justify="start" pad="large">
-        <Select
-          placeholder="Clear Options"
-          multiple
-          value={value}
-          options={options}
-          onChange={({ value: nextValue }) => setValue(nextValue)}
-          clear
-        />
-      </Box>
-    </Grommet>
+    // Uncomment <Grommet> lines when using outside of storybook
+    // <Grommet theme={...}>
+    <Box fill align="center" justify="start" pad="large">
+      <Select
+        placeholder="Clear Options"
+        multiple
+        value={value}
+        options={options}
+        onChange={({ value: nextValue }) => setValue(nextValue)}
+        clear
+      />
+    </Box>
+    // </Grommet>
   );
 };
 Top.parameters = {
@@ -28,18 +28,19 @@ Top.parameters = {
 export const Bottom = () => {
   const [value, setValue] = useState(options[1]);
   return (
-    <Grommet theme={grommet}>
-      <Box fill align="center" justify="start" pad="large">
-        <Select
-          placeholder="Clear Options"
-          multiple
-          value={value}
-          options={options}
-          onChange={({ value: nextValue }) => setValue(nextValue)}
-          clear={{ position: 'bottom' }}
-        />
-      </Box>
-    </Grommet>
+    // Uncomment <Grommet> lines when using outside of storybook
+    // <Grommet theme={...}>
+    <Box fill align="center" justify="start" pad="large">
+      <Select
+        placeholder="Clear Options"
+        multiple
+        value={value}
+        options={options}
+        onChange={({ value: nextValue }) => setValue(nextValue)}
+        clear={{ position: 'bottom' }}
+      />
+    </Box>
+    // </Grommet>
   );
 };
 Bottom.parameters = {
