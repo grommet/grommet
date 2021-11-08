51f3fc233963125293af577a1e730efa306be211
Chris Valmonte <cvalmonte@bittitan.com>
2021-11-08 12:39:01 -0800

    Add CustomThemed storybook for Search

--- src/js/components/Select/stories/CustomThemed/Search.js
+++ src/js/components/Select/stories/CustomThemed/Search.js
@@ -1,9 +1,124 @@
 import React, { useRef, useState, useEffect } from 'react';
 
-import { FormClose } from 'grommet-icons';
+import {
+  FormClose,
+  CaretDownFill,
+  CaretUpFill,
+  FormCheckmark,
+} from 'grommet-icons';
 import { Box, Button, CheckBox, Grommet, Select, Text } from 'grommet';
+import { normalizeColor } from '../../../../utils';
+import { SearchInput } from '../components/SearchInput';
+
 // https://github.com/grommet/grommet/blob/master/src/js/components/Select/stories/theme.js
-import { theme as customSearchTheme } from './theme';
+const customSearchTheme = {
+  global: {
+    colors: {
+      selected: 'neutral-3',
+      border: '#e0e0e0',
+      focus: '#2196F3',
+      gray: 'rgba(0, 0, 0, 0.54)',
+    },
+    control: {
+      border: {
+        radius: '24px',
+      },
+    },
+    drop: {
+      background: '#ffffff',
+    },
+    elevation: {
+      light: {
+        small: '0 2px 2px 0 rgba(0,0,0,0.19)',
+        medium: '0 3px 3px 0 rgba(0,0,0,0.18)',
+        large: '0 4px 4px 0 rgba(0,0,0,0.17)',
+        xlarge: '0 24px 24px 0 rgba(0, 0, 0, 0.12)',
+      },
+    },
+    font: {
+      family: 'Arial',
+      size: '12px',
+    },
+    input: {
+      weight: 400,
+    },
+    size: {
+      xxsmall: '24px',
+    },
+  },
+  text: {
+    medium: '13px',
+  },
+  checkBox: {
+    border: {
+      color: {
+        light: 'brand',
+      },
+      radius: '2px',
+    },
+    color: {
+      light: 'brand',
+    },
+    check: {
+      extend: ({ theme: extendTheme, checked }) => `
+        ${
+          checked &&
+          `background-color: ${normalizeColor('brand', extendTheme)};`
+        }
+      `,
+    },
+    hover: {
+      border: {
+        color: undefined,
+      },
+    },
+    icon: {
+      size: '18px',
+      extend: 'stroke: white;',
+    },
+    icons: {
+      checked: FormCheckmark,
+    },
+    gap: 'small',
+    size: '18px',
+    extend: `
+      color: #9C9C9C;
+    `,
+  },
+  drop: {
+    maxHeight: '384px',
+  },
+  select: {
+    control: {
+      extend: 'padding: 3px 6px;',
+      open: {
+        background: '#ece0fa',
+        border: '1px solid #7D4CDB',
+      },
+    },
+    icons: {
+      down: CaretDownFill,
+      up: CaretUpFill,
+      color: 'dark-1',
+      margin: 'small',
+    },
+    searchInput: SearchInput,
+    container: {
+      text: {
+        size: 'small',
+      },
+      extend: 'max-height: 250px;',
+    },
+  },
+  textInput: {
+    extend: (props) => `
+      color: ${normalizeColor('gray', props.theme)};
+      font-weight: 400;
+      font-size: 13px;
+      padding: 14px;
+    `,
+  },
+};
 
 const allContentPartners = [
   {
@@ -56,7 +171,7 @@ const allContentPartners = [
   },
 ];
 
-export const CustomSearch = () => {
+export const Search = () => {
   const [selectedContentPartners, setSelectedContentPartners] = useState([]);
   const [contentPartners, setContentPartners] = useState(allContentPartners);
   const [searching, setSearching] = useState(false);
@@ -70,7 +185,7 @@ export const CustomSearch = () => {
 
   useEffect(() => {
     const filterContentPartners = allContentPartners.filter(
-      s => s.name.toLowerCase().indexOf(searchQuery.toLowerCase()) >= 0,
+      (s) => s.name.toLowerCase().indexOf(searchQuery.toLowerCase()) >= 0,
     );
 
     setTimeout(() => {
@@ -83,7 +198,9 @@ export const CustomSearch = () => {
     <Box direction="row" align="center" pad="small" flex={false}>
       <CheckBox
         tabIndex="-1"
-        checked={selectedContentPartners.some(partner => partner.name === name)}
+        checked={selectedContentPartners.some(
+          (partner) => partner.name === name,
+        )}
         label={<Text size="small">{name}</Text>}
         onChange={() => {}}
       />
@@ -117,8 +234,8 @@ export const CustomSearch = () => {
       </Box>
       <Button
         href="#"
-        onFocus={event => event.stopPropagation()}
-        onClick={event => {
+        onFocus={(event) => event.stopPropagation()}
+        onClick={(event) => {
           event.preventDefault();
           event.stopPropagation();
           clearContentPartners();
@@ -132,7 +249,7 @@ export const CustomSearch = () => {
     </Box>
   );
 
-  const sortContentPartners = selectedPartnerNames => (p1, p2) => {
+  const sortContentPartners = (selectedPartnerNames) => (p1, p2) => {
     const p1Exists = selectedPartnerNames.includes(p1.name);
     const p2Exists = selectedPartnerNames.includes(p2.name);
 
@@ -162,7 +279,7 @@ export const CustomSearch = () => {
           value={
             selectedContentPartners.length ? renderContentPartners() : undefined
           }
-          selected={selectedContentPartners.map(option =>
+          selected={selectedContentPartners.map((option) =>
             contentPartners.indexOf(option),
           )}
           options={contentPartners}
@@ -185,7 +302,7 @@ export const CustomSearch = () => {
             setSelectedContentPartners(newSelectedPartners);
             setContentPartners(sortedContentPartners);
           }}
-          onSearch={query => {
+          onSearch={(query) => {
             setSearching(true);
             setSearchQuery(query);
           }}
@@ -197,8 +314,6 @@ export const CustomSearch = () => {
   );
 };
 
-CustomSearch.storyName = 'Custom search';
-
 export default {
-  title: 'Input/Select/Custom search',
+  title: 'Input/Select/Custom Themed/Search',
 };
