de92107b4f2d60f4ee028620d3efdb5081ea6230
Jessica Filben <jessica.filben@gmail.com>
2021-11-22 13:34:20 -0700

    added comment

--- src/js/components/Grommet/StyledGrommet.js
+++ src/js/components/Grommet/StyledGrommet.js
@@ -5,6 +5,9 @@ import { defaultProps } from '../../default-props';
 
 const fullStyle = (full) => {
   if (full === 'min')
+    /* for min-height to take up the whole viewport
+    we need to set height to 1px in addition to setting
+    min-height to 100vh */
     return css`
       min-height: 100vh;
       height: 1px;
