d818458f945fc375e792c7558a3f874f41651916
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-12-22 12:36:27 -0500

    PR Review: safari cross browser issues solved

--- src/js/components/Video/stories/typescript/TheaterMode.tsx
+++ src/js/components/Video/stories/typescript/TheaterMode.tsx
@@ -5,22 +5,17 @@ import { Monitor } from 'grommet-icons/icons/Monitor';
 
 const TheaterMode = (props) => {
   const [theaterMode, setTheaterMode] = useState(false);
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
   return (
     <Box
       background={theaterMode ? 'dark-1' : 'light-1'}
       overflow="hidden"
       width={theaterMode ? null : 'small'}
-      height={isSafari && { max: theaterMode ? null : '237px' }}
-      fill="vertical"
       align="center"
-      flex={true}
-      pad="large"
     >
       <Video
+        margin="large"
         src="large_video.mp4"
-        fit="cover"
         controls={{
           position: theaterMode ? 'over' : 'below',
           items: [
