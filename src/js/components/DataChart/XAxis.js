8ca2fb3354179bde3f1d0fdc7273f82bf874daf8
Eric Soderberg <eric.soderberg@hpe.com>
2022-02-17 14:20:44 -0800

    Changed DataChart to fix issues with x-axis alignment

--- src/js/components/DataChart/XAxis.js
+++ src/js/components/DataChart/XAxis.js
@@ -1,28 +1,28 @@
 import React, { forwardRef } from 'react';
 import { Box } from '../Box';
 
-const XAxis = forwardRef(({ chartProps, data, renderValue, serie }, ref) => {
+const XAxis = forwardRef(({ chartProps, pad, renderValue, serie }, ref) => {
   // pull the x-axis values from the first chart, all should have the same
-  const [axisValues] = (Array.isArray(chartProps[0])
-    ? chartProps[0][0]
-    : chartProps[0]
+  const [axisValues] = (
+    Array.isArray(chartProps[0]) ? chartProps[0][0] : chartProps[0]
   ).axis;
 
+  // When there are only labels at the end of the axis, let them take as much
+  // space as they like. If there are more, align their container to the
+  // data/guide lines and then let their content overflow that.
+  const itemProps =
+    axisValues.length === 2
+      ? {}
+      : { width: '1px', overflow: 'visible', align: 'center' };
+
   return (
-    <Box ref={ref} gridArea="xAxis" direction="row" justify="between">
-      {axisValues.map((dataIndex, i) => {
-        let align;
-        if (axisValues.length === data.length) align = 'center';
-        else if (i === 0) align = 'start';
-        else if (i === axisValues.length - 1) align = 'end';
-        else align = 'center';
-        return (
-          // eslint-disable-next-line react/no-array-index-key
-          <Box key={i} flex align={align}>
-            {serie ? renderValue(serie, dataIndex) : dataIndex}
-          </Box>
-        );
-      })}
+    <Box ref={ref} gridArea="xAxis" direction="row" justify="between" pad={pad}>
+      {axisValues.map((dataIndex, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Box key={i} {...itemProps}>
+          {serie ? renderValue(serie, dataIndex) : dataIndex}
+        </Box>
+      ))}
     </Box>
   );
 });
