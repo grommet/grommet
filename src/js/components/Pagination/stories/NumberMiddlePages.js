03addd16d504f8517a182f24a96d4c1a412fa36a
besmaili <MFCDeveloper@yahoo.com>
2022-02-19 15:51:14 +0330

    Paginate story enhancements

--- src/js/components/Pagination/stories/NumberMiddlePages.js
+++ src/js/components/Pagination/stories/NumberMiddlePages.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import { Box, Grommet, Pagination, Text } from 'grommet';
-import { grommet } from 'grommet/themes';
+import { Box, Pagination, Text } from 'grommet';
 
 export const NumberMiddlePages = () => (
-  <Grommet theme={grommet}>
-    <Box pad="small" gap="medium">
-      <Box>
-        <Text>numberMiddlePages = 4 (number of pages in the middle)</Text>
-        <Pagination numberItems={237} page={10} numberMiddlePages={4} />
-      </Box>
-      <Box>
-        <Text>numberMiddlePages = 5 (number of pages in the middle)</Text>
-        <Pagination numberItems={237} page={10} numberMiddlePages={5} />
-      </Box>
+  // Uncomment <Grommet> lines when using outside of storybook
+  // <Grommet theme={...}>
+  <Box pad="small" gap="medium">
+    <Box>
+      <Text>numberMiddlePages = 4 (number of pages in the middle)</Text>
+      <Pagination numberItems={237} page={10} numberMiddlePages={4} />
     </Box>
-  </Grommet>
+    <Box>
+      <Text>numberMiddlePages = 5 (number of pages in the middle)</Text>
+      <Pagination numberItems={237} page={10} numberMiddlePages={5} />
+    </Box>
+  </Box>
+  // </Grommet>
 );
 
 NumberMiddlePages.storyName = 'Number middle pages';
