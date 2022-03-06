7861f1244d59cc8ed851d911a407a1e73b973777
Eric Soderberg <eric.soderberg@hpe.com>
2022-03-05 19:30:53 -0800

    Changed DataChart to align placeholder with DataTable

--- src/js/components/DataChart/stories/Placeholder.js
+++ src/js/components/DataChart/stories/Placeholder.js
@@ -6,18 +6,6 @@ export const Placeholder = () => (
   // Uncomment <Grommet> lines when using outside of storybook
   // <Grommet theme={grommet}>
   <Box align="center" justify="start" pad="large">
-    {/* <DataChart
-      data={[
-        { date: '2022-01-02' },
-        { date: '2022-02-02' },
-        { date: '2022-03-02' },
-      ]}
-      series={['date', 'percent']}
-      bounds={{ y: [0, 100] }}
-      axis
-      guide
-      placeholder="no data available"
-    /> */}
     <DataChart
       data={[
         { date: '2022-01-02' },
@@ -26,6 +14,7 @@ export const Placeholder = () => (
       ]}
       series={['date', 'percent']}
       bounds={{ y: [0, 100] }}
+      guide={{ y: { granularity: 'medium' } }}
       placeholder="no data"
     />
   </Box>
