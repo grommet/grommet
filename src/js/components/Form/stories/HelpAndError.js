860d63f4f43d2f664aafed96ab996f6d10cdb91c
brittany archibeque <brittany.archibeque@hpe.com>
2022-03-16 16:46:10 -0600

    feedback changes

--- src/js/components/Form/stories/HelpAndError.js
+++ src/js/components/Form/stories/HelpAndError.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Box, Form, FormField, Text, TextInput } from 'grommet';
-import { Alert } from 'grommet-icons';
 
 export const HelpAndError = () => (
   // Uncomment <Grommet> lines when using outside of storybook
@@ -27,19 +26,13 @@ export const HelpAndError = () => (
         label="Label"
         htmlFor="text-input"
         help={
-          <Text weight="bold" size="small">
+          <Text weight="lighter" size="small">
             Enter HPE email
           </Text>
         }
         error={
-          <Box
-            align="center"
-            gap="small"
-            direction="row"
-            background="background-front"
-          >
-            <Alert />
-            <Text align="center" size="small">
+          <Box align="center" background="background-front">
+            <Text weight="bolder" align="center" size="small">
               Custom Text to call attention to an issue with this field
             </Text>
           </Box>
