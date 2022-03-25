c0501cde5c2964483bd7fe201dd416b4da650b11
brittany archibeque <brittany.archibeque@hpe.com>
2022-03-25 14:29:04 -0600

    clean up code

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -4,54 +4,43 @@ import { useForwardedRef } from '../../utils';
 
 const Header = React.forwardRef(({ sticky, ...rest }, ref) => {
   const containerRef = useForwardedRef(ref);
-  // show header as default then if scroll down showHeader is false
-  // else is scroll up showHeader is true.
-  const [height, setHeight] = useState();
-  const [width, setWidth] = useState();
-  const [right, setRight] = useState();
-  const [left, setLeft] = useState();
+  const [stickyStyles, setStickyStyles] = useState();
   const [scrollUp, setScrollUp] = useState(false);
 
   useEffect(() => {
-    //   // threshold inital value set to 0
+    // threshold inital value set to 0
     const threshold = 0;
-    //   // number of pixels the document is currently scrolled
+    // number of pixels the document is currently scrolled
     let lastScrollY = window.pageYOffset;
-    //   // run event listener callback once
-    let ticking = false;
-
-    // check if threshold is met which will
-    // determine if the user is scrolling up or down
-    // use ref here to get height and width
+    // run event listener callback once
+    let runEventListener = false;
 
     const updateScrollDir = () => {
       const scrollY = window.pageYOffset;
 
       if (Math.abs(scrollY - lastScrollY) < threshold) {
-        ticking = false;
+        runEventListener = false;
         return;
       }
       if (scrollY === 0) {
         setScrollUp(false);
-        ticking = false;
+        runEventListener = false;
       } else setScrollUp(scrollY < lastScrollY);
-      // calcualte left, right, width, height
-      setHeight(`${containerRef.current.getBoundingClientRect().height}px`);
-      setWidth(`${containerRef.current.getBoundingClientRect().width}px`);
-      setLeft(`${containerRef.current.getBoundingClientRect().left}px`);
-      setRight(`${containerRef.current.getBoundingClientRect().right}px`);
+      setStickyStyles({
+        width: `${containerRef.current.getBoundingClientRect().width}px`,
+        height: `${containerRef.current.getBoundingClientRect().height}px`,
+        left: `${containerRef.current.getBoundingClientRect().left}px`,
+        right: `${containerRef.current.getBoundingClientRect().right}px`,
+      });
       lastScrollY = scrollY > 0 ? scrollY : 0;
-      ticking = false;
+      runEventListener = false;
     };
 
     const onScroll = () => {
-      if (!ticking) {
-        // perform an animation and requests that the browser
-        // calls a specified function to update an animation
-        // before the next repaint
+      if (!runEventListener) {
         // calculate the new offset after the page gets rendered
         window.requestAnimationFrame(updateScrollDir);
-        ticking = true;
+        runEventListener = true;
       }
     };
 
@@ -60,31 +49,36 @@ const Header = React.forwardRef(({ sticky, ...rest }, ref) => {
     return () => window.removeEventListener('scroll', onScroll);
   }, [containerRef, scrollUp]);
 
-  // normal header when you land on page and scroll down
-  // when scroll up when you need to show the sticky header
-  // back at 0 scroll position switch back to normal header
-
-  const stickyStyles = {
-    width: '100%',
+  const stickyStyle = {
     position: 'fixed',
-    height: { height },
     zIndex: '1',
-    left: { left },
-    right: { right },
+    top: '0',
+    left: stickyStyles && stickyStyles.left,
+    right: stickyStyles && stickyStyles.right,
   };
 
   return (
-    <Box
-      align="center"
-      as="header"
-      direction="row"
-      flex={false}
-      justify="between"
-      gap="medium"
-      style={sticky === 'scrollup' && scrollUp ? stickyStyles : undefined}
-      {...rest}
-      ref={containerRef}
-    />
+    <>
+      {sticky === 'scrollup' && scrollUp && (
+        <Box
+          height={stickyStyles && stickyStyles.height}
+          width={stickyStyles && stickyStyles.width}
+        />
+      )}
+      <Box
+        align="center"
+        as="header"
+        direction="row"
+        height={stickyStyles && stickyStyles.height}
+        width={stickyStyles && stickyStyles.width}
+        flex={false}
+        justify="between"
+        gap="medium"
+        style={sticky === 'scrollup' && scrollUp ? stickyStyle : undefined}
+        {...rest}
+        ref={containerRef}
+      />
+    </>
   );
 });
 Header.displayName = 'Header';
---
+++ src/js/components/Header/stories/Sticky.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  Anchor,
+  Avatar,
+  Card,
+  Grid,
+  Header,
+  Nav,
+  Page,
+  PageContent,
+  Paragraph,
+} from 'grommet';
+
+export const Sticky = () => (
+  // Uncomment <Grommet> lines when using outside of storybook
+  // <Grommet theme={...}>
+  <Page kind="narrow">
+    <PageContent background="light-3">
+      <Header sticky="scrollup" background="light-4" pad="small">
+        <Avatar background="brand">SY</Avatar>
+        <Nav direction="row">
+          <Anchor label="Home" href="#" />
+          <Anchor label="Profile" href="#" />
+        </Nav>
+      </Header>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo
+        gravida tincidunt. Nunc fringilla blandit tortor, id accumsan nisi
+        dictum quis. Aenean porttitor at mi id semper. Donec mattis bibendum
+        leo, interdum ullamcorper lectus ultrices vel. Fusce nec enim faucibus
+        nunc porta egestas. Fusce dapibus lobortis tincidunt.
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo
+        gravida tincidunt. Nunc fringilla blandit tortor, id accumsan nisi
+        dictum quis. Aenean porttitor at mi id semper. Donec mattis bibendum
+        leo, interdum ullamcorper lectus ultrices vel. Fusce nec enim faucibus
+        nunc porta egestas. Fusce dapibus lobortis tincidunt.
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo
+        gravida tincidunt. Nunc fringilla blandit tortor, id accumsan nisi
+        dictum quis. Aenean porttitor at mi id semper. Donec mattis bibendum
+        leo, interdum ullamcorper lectus ultrices vel. Fusce nec enim faucibus
+        nunc porta egestas. Fusce dapibus lobortis tincidunt.
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo
+        gravida tincidunt. Nunc fringilla blandit tortor, id accumsan nisi
+        dictum quis. Aenean porttitor at mi id semper. Donec mattis bibendum
+        leo, interdum ullamcorper lectus ultrices vel. Fusce nec enim faucibus
+        nunc porta egestas. Fusce dapibus lobortis tincidunt.
+      </Paragraph>
+      <Grid rows="small" columns={{ count: 'fit', size: 'small' }} gap="small">
+        <Card background="white" pad="large">
+          Card
+        </Card>
+        <Card background="white" pad="large">
+          Card
+        </Card>
+        <Card background="white" pad="large">
+          Card
+        </Card>
+      </Grid>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo
+        gravida tincidunt. Nunc fringilla blandit tortor, id accumsan nisi
+        dictum quis. Aenean porttitor at mi id semper. Donec mattis bibendum
+        leo, interdum ullamcorper lectus ultrices vel. Fusce nec enim faucibus
+        nunc porta egestas. Fusce dapibus lobortis tincidunt.
+      </Paragraph>
+    </PageContent>
+  </Page>
+  // </Grommet>
+);
+
+Sticky.storyName = 'Sticky';
+
+export default {
+  title: 'Layout/Header/Sticky',
+};
