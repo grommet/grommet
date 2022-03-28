dab2ce4cc1eee2bb199ff8917e42c01ef735e987
brittany archibeque <brittany.archibeque@hpe.com>
2022-03-28 13:28:53 -0600

    add ts and proptype

--- src/js/components/Header/index.d.ts
+++ src/js/components/Header/index.d.ts
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { BoxTypes } from '../Box';
+import { BoxTypes } from '../Box/index';
 
-declare const Header: React.FC<BoxTypes>;
+export interface HeaderProps {
+  sticky?: 'scrollup';
+}
+
+export interface HeaderExtendedProps extends BoxTypes, HeaderProps {}
+
+declare const Header: React.FC<HeaderExtendedProps>;
 
 export { Header };
