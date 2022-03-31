2bac211cc6ca4e2ad550b32370e0a91e41e29cd7
amandadupell <anddupell@gmail.com>
2022-03-31 14:00:00 -0400

    rework logic and variables

--- src/js/components/Menu/Menu.js
+++ src/js/components/Menu/Menu.js
@@ -84,7 +84,8 @@ const Menu = forwardRef((props, ref) => {
   // need to destructure the align otherwise it will get passed through
   // to DropButton and override prop values
   const { align: themeDropAlign, ...themeDropProps } = theme.menu.drop;
-  const aria = ariaLabel || a11yTitle || (dropProps && dropProps?.a11yTitle);
+  const a11y = ariaLabel || a11yTitle;
+  const dropA11y = dropProps && dropProps?.a11yTitle;
 
   const align = (dropProps && dropProps.align) || dropAlign || themeDropAlign;
   const controlButtonIndex = useMemo(() => {
@@ -269,7 +270,7 @@ const Menu = forwardRef((props, ref) => {
           // make it accessible at the end of all menu items
           buttonRefs.current[items.length] = r;
         }}
-        a11yTitle={aria || format({ id: 'menu.closeMenu', messages })}
+        a11yTitle={a11y || format({ id: 'menu.closeMenu', messages })}
         active={activeItemIndex === controlButtonIndex}
         focusIndicator={false}
         hoverIndicator="background"
@@ -301,7 +302,7 @@ const Menu = forwardRef((props, ref) => {
         ref={ref}
         {...rest}
         {...buttonProps}
-        a11yTitle={aria || format({ id: 'menu.openMenu', messages })}
+        a11yTitle={a11y || format({ id: 'menu.openMenu', messages })}
         aria-haspopup="menu"
         aria-expanded={isOpen}
         onAlign={setAlignControlMirror}
@@ -329,7 +330,7 @@ const Menu = forwardRef((props, ref) => {
               {alignControlMirror === 'top' && align.top === 'top'
                 ? controlMirror
                 : undefined}
-              <Box overflow="auto" role="menu" a11yTitle={aria}>
+              <Box overflow="auto" role="menu" a11yTitle={dropA11y || a11y}>
                 {items.map((item, index) => {
                   // Determine whether the label is done as a child or
                   // as an option Button kind property.
