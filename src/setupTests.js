c2b8ac51abdc888c87a73c7c72ab1a68227dc656
Jessica Filben <jessica.filben@gmail.com>
2022-04-04 18:34:19 -0600

    Bumped React version to v18

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,15 @@
-global.console.warn = message => {
+const originalErr = global.console.error.bind(global.console.error);
+beforeEach(() => {
+  global.console.error = (msg) =>
+    !msg.toString().includes('ReactDOM.render') && originalErr(msg);
+});
+afterEach(() => {
+  global.console.error = originalErr;
+});
+
+global.console.warn = (message) => {
   throw message;
 };
-global.console.error = message => {
+global.console.error = (message) => {
   throw message;
 };
