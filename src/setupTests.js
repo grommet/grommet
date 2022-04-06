fceec99968fce393a92a7ff77f9ebf709d804f52
Jessica Filben <jessica.filben@gmail.com>
2022-04-06 11:11:47 -0600

    upgrade testing-library/react

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,11 @@
-const originalErr = global.console.error.bind(global.console.error);
-beforeEach(() => {
-  global.console.error = (msg) =>
-    !msg.toString().includes('ReactDOM.render') && originalErr(msg);
-});
-afterEach(() => {
-  global.console.error = originalErr;
-});
+// const originalErr = global.console.error.bind(global.console.error);
+// beforeEach(() => {
+//   global.console.error = (msg) =>
+//     !msg.toString().includes('ReactDOM.render') && originalErr(msg);
+// });
+// afterEach(() => {
+//   global.console.error = originalErr;
+// });
 
 global.console.warn = (message) => {
   throw message;
