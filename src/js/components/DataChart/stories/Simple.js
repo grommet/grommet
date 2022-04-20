1dac60c60457f4a2bf25b3c2dfb44aba3a511dd1
Eric Soderberg <eric.soderberg@hpe.com>
2022-04-20 12:38:39 -0700

    Undid change to Simple story

--- src/js/components/DataChart/stories/Simple.js
+++ src/js/components/DataChart/stories/Simple.js
@@ -2,22 +2,19 @@ import React from 'react';
 
 import { Box, DataChart } from 'grommet';
 
-// const data = [];
-// for (let i = 1; i < 8; i += 1) {
-//   const v = Math.sin(i / 2.0);
-//   data.push({
-//     percent: Math.abs(v * 100),
-//   });
-// }
-
-const data = [{ date: '2020-08-22', amount: 33 }];
+const data = [];
+for (let i = 1; i < 8; i += 1) {
+  const v = Math.sin(i / 2.0);
+  data.push({
+    percent: Math.abs(v * 100),
+  });
+}
 
 export const Simple = () => (
   // Uncomment <Grommet> lines when using outside of storybook
   // <Grommet theme={grommet}>
   <Box align="center" justify="start" pad="large">
-    {/* <DataChart data={data} series={[{ property: 'percent' }, {}]} /> */}
-    <DataChart data={data} series={['date', 'amount']} />
+    <DataChart data={data} series={[{ property: 'percent' }, {}]} />
   </Box>
   // </Grommet>
 );
