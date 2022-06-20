1b7d713769b9d2df1624391493321682a4c02725
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-06-19 21:45:18 -0300

    Prevent scroll when pressing space on File Input

--- src/js/components/FileInput/FileInput.js
+++ src/js/components/FileInput/FileInput.js
@@ -294,6 +294,7 @@ const FileInput = forwardRef(
                   <Message {...theme.fileInput.message}>{message}</Message>
                   <Keyboard
                     onSpace={(event) => {
+                      event.preventDefault();
                       if (controlRef.current === event.target)
                         inputRef.current.click();
                     }}
