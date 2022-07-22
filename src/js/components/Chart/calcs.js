4f3531dec9b19e7257c59853fe490df0763f5b1e
Eric Soderberg <eric.soderberg@hpe.com>
2022-07-22 09:39:32 -0700

    Changed DataChart to fix an issue with negative values. Fixes #6231 (#6246)

--- src/js/components/Chart/calcs.js
+++ src/js/components/Chart/calcs.js
@@ -30,8 +30,10 @@ export const calcBounds = (values, options = {}) => {
   const coarseness = (Array.isArray(options.coarseness) &&
     options.coarseness) ||
     (options.coarseness && [undefined, options.coarseness]) || [undefined, 5];
+  const [coarseX, coarseY] = coarseness;
   // the number of steps is one less than the number of labels
   const steps = options.steps || [1, 1];
+  const [, stepsY] = steps;
   const calcValues = normalizeValues(values || []);
 
   // min and max values
@@ -42,8 +44,8 @@ export const calcBounds = (values, options = {}) => {
   if (calcValues.length) {
     // Calculate the max and min values.
     calcValues
-      .filter(value => value !== undefined)
-      .forEach(value => {
+      .filter((value) => value !== undefined)
+      .forEach((value) => {
         const x = value.value[0];
         if (x !== undefined) {
           minX = minX === undefined ? x : Math.min(minX, x);
@@ -75,16 +77,15 @@ export const calcBounds = (values, options = {}) => {
     // Calculate some reasonable bounds based on the max and min values.
     // This is so values like 87342.12 don't end up being displayed as the
     // graph axis labels.
-    const [cX, cY] = coarseness;
-    if (cX) {
+    if (coarseX) {
       const deltaX = maxX - minX;
-      const intervalX = Number.parseFloat((deltaX / cX).toPrecision(1));
+      const intervalX = Number.parseFloat((deltaX / coarseX).toPrecision(1));
       minX = alignMin(minX, intervalX);
       maxX = alignMax(maxX, intervalX);
     }
-    if (cY) {
+    if (coarseY) {
       const deltaY = maxY - minY;
-      const intervalY = Number.parseFloat((deltaY / cY).toPrecision(1));
+      const intervalY = Number.parseFloat((deltaY / coarseY).toPrecision(1));
       minY = alignMin(minY, intervalY);
       maxY = alignMax(maxY, intervalY);
     }
@@ -92,19 +93,25 @@ export const calcBounds = (values, options = {}) => {
     if (minY < 0 && maxY > 0 && Math.abs(minY) !== Math.abs(maxY)) {
       // Adjust min and max when crossing 0 to ensure 0 will be shown on
       // the Y axis based on the number of steps.
-
-      // const ratio = Math.abs(max) / Math.abs(min);
-      let stepInterval = (maxY - minY) / steps[1];
-      const minSteps = minY / stepInterval;
-      const maxSteps = maxY / stepInterval;
-      if (Math.abs(minSteps) < Math.abs(maxSteps)) {
-        stepInterval = maxY / Math.floor(maxSteps);
-        maxY = stepInterval * Math.floor(maxSteps);
-        minY = stepInterval * Math.floor(minSteps);
+      if (stepsY === 1) {
+        const largest = Math.max(Math.abs(minY), Math.abs(maxY));
+        minY = -largest;
+        maxY = largest;
       } else {
-        stepInterval = Math.abs(minY / Math.ceil(minSteps));
-        minY = stepInterval * Math.ceil(minSteps);
-        maxY = stepInterval * Math.ceil(maxSteps);
+        let stepInterval = (maxY - minY) / stepsY;
+        const minSteps = minY / stepInterval;
+        const maxSteps = maxY / stepInterval;
+        if (Math.abs(minSteps) < Math.abs(maxSteps)) {
+          // more above than below
+          stepInterval = maxY / Math.floor(maxSteps);
+          maxY = stepInterval * Math.floor(maxSteps);
+          minY = stepInterval * Math.floor(minSteps);
+        } else {
+          // more below than above
+          stepInterval = Math.abs(minY / Math.ceil(minSteps));
+          minY = stepInterval * Math.ceil(minSteps);
+          maxY = stepInterval * Math.ceil(maxSteps);
+        }
       }
     }
   }
@@ -122,30 +129,35 @@ export const calcBounds = (values, options = {}) => {
 export const calcs = (values = [], options = {}) => {
   // the number of steps is one less than the number of labels
   const steps = options.steps || [1, 1];
+  const [stepsX, stepsY] = steps;
   const bounds = options.bounds || calcBounds(values, options);
   if (options.min !== undefined) bounds[1][0] = options.min;
   if (options.max !== undefined) bounds[1][1] = options.max;
+  const [boundsX, boundsY] = bounds;
+  const [boundsXmin, boundsXmax] = boundsX;
+  const [boundsYmin, boundsYmax] = boundsY;
 
   const dimensions = [
-    round(bounds[0][1] - bounds[0][0], 2),
-    round(bounds[1][1] - bounds[1][0], 2),
+    round(boundsXmax - boundsXmin, 2),
+    round(boundsYmax - boundsYmin, 2),
   ];
+  const [dimensionsX, dimensionsY] = dimensions;
 
   // Calculate x and y axis values across the specfied number of steps.
   const yAxis = [];
-  let y = bounds[1][1];
+  let y = boundsYmax;
   // To deal with javascript math limitations, round the step with 4 decimal
   // places and then push the values with 2 decimal places
-  const yStepInterval = round(dimensions[1] / steps[1], 4);
-  while (round(y, 2) >= bounds[1][0]) {
+  const yStepInterval = round(dimensionsY / stepsY, 4);
+  while (round(y, 2) >= boundsYmin) {
     yAxis.push(round(y, 2));
     y -= yStepInterval;
   }
 
   const xAxis = [];
-  let x = bounds[0][0];
-  const xStepInterval = round(dimensions[0] / steps[0], 4);
-  while (round(x, 2) <= bounds[0][1]) {
+  let x = boundsXmin;
+  const xStepInterval = round(dimensionsX / stepsX, 4);
+  while (round(x, 2) <= boundsXmax) {
     xAxis.push(round(x, 2));
     x += xStepInterval;
   }
