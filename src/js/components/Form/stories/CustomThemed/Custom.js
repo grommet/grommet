6775edb501bc779f4b8501a2ebda660a45f1dd2b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-08-24 19:58:36 -0600

    take out previous changes

--- src/js/components/Form/stories/CustomThemed/Custom.js
+++ src/js/components/Form/stories/CustomThemed/Custom.js
@@ -15,126 +15,46 @@ import {
   TextInput,
 } from 'grommet';
 import { grommet } from 'grommet/themes';
-import { hpe } from 'grommet-theme-hpe';
 import { deepMerge } from 'grommet/utils';
 
-const customTheme = deepMerge(hpe, {
-  checkBox: {
-    hover: {
-      border: {
-        color: undefined,
-      },
-      background: {
-        color: 'background-contrast',
-      },
+const customTheme = {
+  global: {
+    font: {
+      size: '16px',
     },
-    color: 'background',
-    border: {
-      color: 'border',
-      width: '1px',
+    input: {
+      weight: 400,
     },
-    check: {
-      radius: '2px',
-      extend: ({ theme, checked, indeterminate }) => `
-      background-color: ${
-        checked || indeterminate
-          ? theme.global.colors.green[theme.dark ? 'dark' : 'light']
-          : theme.global.colors.background[theme.dark ? 'dark' : 'light']
-      };
-      ${(checked || indeterminate) && 'border: none;'}
-        `,
-    },
-    icon: {
-      extend: ({ theme }) => `stroke-width: 2px;
-      stroke: ${
-        theme.global.colors['text-strong'][theme.dark ? 'dark' : 'light']
-      }`,
-    },
-    gap: 'small',
-    label: {
-      align: 'start',
-    },
-    extend: ({ disabled, theme }) => `
-    ${
-      !disabled &&
-      `:hover {
-      background-color: ${
-        theme.global.colors['background-contrast'][
-          theme.dark ? 'dark' : 'light'
-        ]
-      };
-    }`
-    }
-    font-weight: 500;
-    width: auto;
-  };
-  `,
   },
   formField: {
-    checkBox: {
-      pad: {
-        vertical: 'xsmall',
-        horizontal: 'small',
-      },
-    },
-    content: {
-      margin: { vertical: 'xsmall' },
-      pad: undefined,
+    label: {
+      color: 'dark-3',
+      size: 'small',
+      margin: 'xsmall',
+      weight: 600,
     },
     border: {
-      error: {
-        color: 'border-strong',
-      },
-      color: 'border',
+      position: 'outer',
       side: 'all',
     },
     disabled: {
       background: {
-        color: undefined,
-      },
-      border: {
-        color: 'border-weak',
-      },
-      label: {
-        color: 'text-xweak',
+        color: 'status-disabled',
+        opacity: true,
       },
     },
-    focus: {
-      border: {
-        color: 'border-strong',
+    content: {
+      pad: 'small',
+    },
+    error: {
+      background: {
+        color: 'status-critical',
+        opacity: 'weak',
       },
     },
-    help: {
-      size: 'xsmall',
-      color: 'text',
-      margin: 'none',
-    },
-    info: {
-      size: 'xsmall',
-      color: 'text',
-      margin: {
-        bottom: 'xsmall',
-        top: 'none',
-        horizontal: 'none',
-      },
-    },
-    label: {
-      size: 'xsmall',
-      color: 'text',
-      margin: {
-        bottom: 'none',
-        top: 'xsmall',
-        horizontal: 'none',
-      },
-      requiredIndicator: true,
-      weight: 500,
-    },
-    margin: {
-      bottom: 'none',
-    },
-    round: '4px',
+    margin: 'none',
   },
-});
+};
 
 export const Custom = () => (
   <Grommet full theme={deepMerge(grommet, customTheme)}>
@@ -168,17 +88,12 @@ export const Custom = () => (
           <FormField label="Size" name="size">
             <Select name="size" options={['small', 'medium', 'large']} />
           </FormField>
-          <Select name="size" options={['small', 'medium', 'large']} />
           <FormField label="Comments" name="comments" disabled>
             <TextArea name="comments" disabled />
           </FormField>
           <FormField label="Age" name="age">
             <RangeInput name="age" min={15} max={75} />
           </FormField>
-          <FormField name="haveAlias">
-            <CheckBox name="haveAlias" label="alias?" />
-          </FormField>
-          <CheckBox name="haveAlias" label="alias?" />
           <Box direction="row" justify="between" margin={{ top: 'medium' }}>
             <Button label="Cancel" />
             <Button type="reset" label="Reset" />
