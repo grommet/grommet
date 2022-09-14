aeadd1ecc9c4d05796c90ead8f5f5bd0d858dc3b
Joy Bhalla <joybhalla9@gmail.com>
2022-09-14 08:45:05 +0530

    added font-variant to global font type

--- src/js/themes/base.d.ts
+++ src/js/themes/base.d.ts
@@ -392,6 +392,7 @@ export interface ThemeType {
       maxWidth?: string;
       size?: string;
       weight?: number | string;
+      variant?: string;
     };
     graph?: {
       colors?: GraphColorsType;
