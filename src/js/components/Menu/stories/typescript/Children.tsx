e14da6f2a137b19502d21f1a866565f2b1abc32a
julieg18 <juliannagal18@gmail.com>
2022-10-08 19:06:12 -0500

    Fix a11y violation in Menu/Children storybook

--- src/js/components/Menu/stories/typescript/Children.tsx
+++ src/js/components/Menu/stories/typescript/Children.tsx
@@ -30,12 +30,7 @@ const MenuWithChildren = (props) => (
 );
 
 const Example = () => (
-  <Box
-    align="center"
-    pad="large"
-    gap="small"
-    background={{ color: 'dark-3', opacity: 0.6 }}
-  >
+  <Box align="center" pad="large" gap="small" background="dark-1">
     <MenuWithChildren disabled />
     <MenuWithChildren />
   </Box>
