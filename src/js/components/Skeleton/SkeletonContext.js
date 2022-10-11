ede1f38d7d785fcd1b1636f6ee45cf7a2d9b3516
Mike Kingdom <michael.kingdom@hpe.com>
2022-10-11 14:42:41 -0600

    Fix context and tests

--- src/js/components/Skeleton/SkeletonContext.js
+++ src/js/components/Skeleton/SkeletonContext.js
@@ -1,7 +1,5 @@
 import React, { useContext } from 'react';
 
-export const SkeletonContext = React.createContext({
-  depth: 0,
-});
+export const SkeletonContext = React.createContext();
 
 export const useSkeleton = () => useContext(SkeletonContext);
