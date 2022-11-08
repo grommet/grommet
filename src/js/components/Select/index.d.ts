f85731151f302cb5e5893694a2ec56991bb1bc92
britt6612 <42451602+britt6612@users.noreply.github.com>
2022-11-08 13:55:22 -0700

    Update src/js/components/Select/index.d.ts
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>

--- src/js/components/Select/index.d.ts
+++ src/js/components/Select/index.d.ts
@@ -32,7 +32,7 @@ export interface BasicSelectProps {
   margin?: MarginType;
   messages?: { multiple?: string };
   name?: string;
-  onChange?: (event: React.ChangeEvent) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onClose?: (...args: any[]) => any;
   onMore?: (...args: any[]) => any;
   onOpen?: (...args: any[]) => any;
