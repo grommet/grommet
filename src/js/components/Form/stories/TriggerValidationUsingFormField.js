3fb2ed102ad14852d822e1562122e925d2fce8ef
GurkiranSingh <gurkiransinghk@gmail.com>
2022-11-09 21:08:07 -0500

    chore: increased bundlesize and removed grommet component from story example
    Signed-off-by: GurkiranSingh <gurkiransinghk@gmail.com>

--- src/js/components/Form/stories/TriggerValidationUsingFormField.js
+++ src/js/components/Form/stories/TriggerValidationUsingFormField.js
@@ -1,110 +1,107 @@
 import React from 'react';
 
 import { StatusGood } from 'grommet-icons';
-import { Box, Button, Grommet, Form, FormField, Heading } from 'grommet';
-import { grommet } from 'grommet/themes';
+import { Box, Button, Form, FormField, Heading } from 'grommet';
 
 export const TriggerValidationUsingFormField = () => (
-  <Grommet full theme={grommet}>
-    <Box fill align="center" justify="center">
-      <Box width="medium">
-        <Heading alignSelf="center" level="2">
-          Validate On
-        </Heading>
-        <Form
-          onReset={(event) => console.log(event)}
-          onSubmit={({ value }) => console.log('Submit', value)}
-        >
-          <FormField
-            label="Blur"
-            name="blur"
-            aria-label="blur"
-            required
-            validate={[
-              { regexp: /^[a-z]/i },
-              (name) => {
-                if (name && name.length === 1) return 'must be >1 character';
-                return undefined;
-              },
-              (name) => {
-                if (name === 'good')
-                  return {
-                    message: (
-                      <Box align="end">
-                        <StatusGood />
-                      </Box>
-                    ),
-                    status: 'info',
-                  };
-                return undefined;
-              },
-            ]}
-            validateOn="blur"
-          />
+  <Box fill align="center" justify="center">
+    <Box width="medium">
+      <Heading alignSelf="center" level="2">
+        Validate On
+      </Heading>
+      <Form
+        onReset={(event) => console.log(event)}
+        onSubmit={({ value }) => console.log('Submit', value)}
+      >
+        <FormField
+          label="Blur"
+          name="blur"
+          aria-label="blur"
+          required
+          validate={[
+            { regexp: /^[a-z]/i },
+            (name) => {
+              if (name && name.length === 1) return 'must be >1 character';
+              return undefined;
+            },
+            (name) => {
+              if (name === 'good')
+                return {
+                  message: (
+                    <Box align="end">
+                      <StatusGood />
+                    </Box>
+                  ),
+                  status: 'info',
+                };
+              return undefined;
+            },
+          ]}
+          validateOn="blur"
+        />
 
-          <FormField
-            label="Submit"
-            name="submit"
-            aria-label="submit"
-            required
-            validate={[
-              { regexp: /^[a-z]/i },
-              (name) => {
-                if (name && name.length === 1) return 'must be >1 character';
-                return undefined;
-              },
-              (name) => {
-                if (name === 'good')
-                  return {
-                    message: (
-                      <Box align="end">
-                        <StatusGood />
-                      </Box>
-                    ),
-                    status: 'info',
-                  };
-                return undefined;
-              },
-            ]}
-            validateOn="submit"
-          />
+        <FormField
+          label="Submit"
+          name="submit"
+          aria-label="submit"
+          required
+          validate={[
+            { regexp: /^[a-z]/i },
+            (name) => {
+              if (name && name.length === 1) return 'must be >1 character';
+              return undefined;
+            },
+            (name) => {
+              if (name === 'good')
+                return {
+                  message: (
+                    <Box align="end">
+                      <StatusGood />
+                    </Box>
+                  ),
+                  status: 'info',
+                };
+              return undefined;
+            },
+          ]}
+          validateOn="submit"
+        />
 
-          <FormField
-            label="Change"
-            name="change"
-            aria-label="change"
-            required
-            validate={[
-              { regexp: /^[a-z]/i },
-              (name) => {
-                if (name && name.length === 1) return 'must be >1 character';
-                return undefined;
-              },
-              (name) => {
-                if (name === 'good')
-                  return {
-                    message: (
-                      <Box align="end">
-                        <StatusGood />
-                      </Box>
-                    ),
-                    status: 'info',
-                  };
-                return undefined;
-              },
-            ]}
-            validateOn="change"
-          />
+        <FormField
+          label="Change"
+          name="change"
+          aria-label="change"
+          required
+          validate={[
+            { regexp: /^[a-z]/i },
+            (name) => {
+              if (name && name.length === 1) return 'must be >1 character';
+              return undefined;
+            },
+            (name) => {
+              if (name === 'good')
+                return {
+                  message: (
+                    <Box align="end">
+                      <StatusGood />
+                    </Box>
+                  ),
+                  status: 'info',
+                };
+              return undefined;
+            },
+          ]}
+          validateOn="change"
+        />
 
-          <Box direction="row" justify="between" margin={{ top: 'medium' }}>
-            <Button label="Cancel" />
-            <Button type="reset" label="Reset" />
-            <Button type="submit" label="Update" primary />
-          </Box>
-        </Form>
-      </Box>
+        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+          <Button label="Cancel" />
+          <Button type="reset" label="Reset" />
+          <Button type="submit" label="Update" primary />
+        </Box>
+      </Form>
     </Box>
-  </Grommet>
+  </Box>
 );
 
 TriggerValidationUsingFormField.storyName =
