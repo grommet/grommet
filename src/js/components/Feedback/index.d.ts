7ac1998713e2973d401960d03900f3d77d52162c
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-11-23 11:30:13 -0700

    fix ts file

--- src/js/components/Feedback/index.d.ts
+++ src/js/components/Feedback/index.d.ts
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { LayerProps } from '../Layer/index';
-export interface FeedbackProps {
+
+export interface FormExtendedEvent<R = Record<string, unknown>, T = Element>
+  extends React.FormEvent<T> {
+  value: R;
+  touched: Record<string, boolean>;
+}
+export interface FeedbackProps<T> {
   children?: React.ReactNode;
   layerProps?: LayerProps;
+  onSubmit?: (event: FormExtendedEvent<T>) => void;
   messages?: object;
   modal?: boolean;
   show?: boolean;
   title?: string;
 }
 
-declare const Feedback: React.FC<FeedbackProps>;
+type TypedFeedbackProps<T> = FeedbackProps<T> &
+  Omit<JSX.IntrinsicElements['form'], 'onChange' | 'onSubmit'>;
+
+declare const Feedback: <T = {}>(
+  p: TypedFeedbackProps<T>,
+) => React.ReactElement<TypedFeedbackProps<T>>;
 
 export { Feedback };
--- src/js/components/ThumbRating/propTypes.js
+++ src/js/components/ThumbRating/propTypes.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import { FormFieldPropTypes } from '../FormField/propTypes';
 
 let PropType = {};
 if (process.env.NODE_ENV !== 'production') {
   PropType = {
-    formProps: FormFieldPropTypes,
+    outlineColor: PropType.string,
+    fillColor: PropType.string,
     onChange: PropTypes.func,
     scale: PropTypes.number,
     value: PropTypes.number,
