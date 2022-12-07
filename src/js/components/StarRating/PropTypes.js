117cb0aa092d34fd2acda10e1ffc6011001c4562
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-12-07 09:43:19 -0700

    add scale to index ts

--- src/js/components/StarRating/PropTypes.js
+++ src/js/components/StarRating/PropTypes.js
@@ -3,13 +3,9 @@ import PropTypes from 'prop-types';
 let PropType = {};
 if (process.env.NODE_ENV !== 'production') {
   PropType = {
-    fillColor: PropTypes.string,
-    name: PropTypes.string,
-    label: PropType.string,
+    color: PropTypes.string,
     onChange: PropTypes.func,
-    outlineColor: PropType.string,
     scale: PropTypes.number,
-    value: PropTypes.number,
   };
 }
 export const FeedbackRatingTypes = PropType;
--- src/js/components/StarRating/index.d.ts
+++ src/js/components/StarRating/index.d.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 export interface StarRating {
   color?: string | object;
   value?: number;
+  scale?: number;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
