2b94e546d6b62e2105bee833ccf6ed86b6fd0a34
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-12-08 09:45:02 -0700

    clean up starrating

--- src/js/components/StarRating/StarRating.js
+++ src/js/components/StarRating/StarRating.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Star, StarOutline } from 'grommet-icons';
 import { RadioButtonGroup } from '../RadioButtonGroup';
 
-export const StarRating = ({ color, scale = 5, value, onChange, ...rest }) => {
-  const [rating, setRating] = useState(value);
+export const StarRating = ({ color, scale = 5, ...rest }) => {
+  const [rating, setRating] = useState();
 
   const options = [];
   for (let i = 0; i < scale; i += 1) {
@@ -14,15 +14,13 @@ export const StarRating = ({ color, scale = 5, value, onChange, ...rest }) => {
     <RadioButtonGroup
       direction="row"
       options={options}
-      value={rating}
       onChange={(event) => {
-        const adjustedRating = parseInt(event.target.value, 10) + 1;
-        setRating(adjustedRating);
+        setRating(event.target.value);
       }}
       {...rest}
     >
       {(option) =>
-        option < rating ? (
+        option <= rating ? (
           <Star color={typeof color === 'string' ? color : color?.fill} />
         ) : (
           <StarOutline
