09f90ba9ef8bed6abca648042af045090773a90f
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-12-12 15:39:48 -0700

    add axe test

--- src/js/components/ThumbsRating/__tests__/ThumbsRating.jsx
+++ src/js/components/ThumbsRating/__tests__/ThumbsRating.jsx
@@ -11,6 +11,18 @@ import { Grommet } from '../../Grommet';
 import { ThumbsRating } from '..';
 
 describe('ThumbsRating', () => {
+  test('StarRating should have no accessibility violations', async () => {
+    const { container } = render(
+      <Grommet>
+        <ThumbsRating name="test" />
+      </Grommet>,
+    );
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+    expect(container).toMatchSnapshot();
+  });
+
   test('ThumbsRating with string options', () => {
     const { container } = render(
       <Grommet>
