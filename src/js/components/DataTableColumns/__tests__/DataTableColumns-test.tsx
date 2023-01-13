fc4753e80892f8d06a289b932d240e64e6c146b3
Eric Soderberg <eric.soderberg@hpe.com>
2023-01-13 14:34:59 -0800

    Added test

--- src/js/components/DataTableColumns/__tests__/DataTableColumns-test.tsx
+++ src/js/components/DataTableColumns/__tests__/DataTableColumns-test.tsx
@@ -40,7 +40,49 @@ describe('DataTableColumns', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  test('search and select', () => {
+  test('remove column', () => {
+    jest.useFakeTimers();
+    const onView = jest.fn();
+    const { container, getByRole, getByText } = render(
+      <Grommet>
+        <Data id="test-data" data={data} updateOn="change" onView={onView}>
+          <DataFilters>
+            <DataTableColumns drop options={['name', 'size', 'age']} />
+          </DataFilters>
+          <DataTable
+            columns={[
+              { property: 'name', header: 'Name' },
+              { property: 'age', header: 'Age' },
+              { property: 'size', header: 'Size' },
+            ]}
+          />
+        </Data>
+      </Grommet>,
+    );
+    expect(getByRole('button', { name: 'Open column selector' })).toBeTruthy();
+
+    expect(container.firstChild).toMatchSnapshot();
+
+    fireEvent.click(getByRole('button', { name: 'Open column selector' }));
+
+    // advance timers so drop can open
+    act(() => jest.advanceTimersByTime(200));
+
+    // snapshot on drop
+    expectPortal('test-data--columns-control').toMatchSnapshot();
+
+    fireEvent.click(getByText('name'));
+    expect(onView).toBeCalledWith(
+      expect.objectContaining({ columns: ['size', 'age'] }),
+    );
+
+    fireEvent.click(getByText('name'));
+    expect(onView).toBeCalledWith(
+      expect.objectContaining({ columns: ['name', 'size', 'age'] }),
+    );
+  });
+
+  test('search', () => {
     jest.useFakeTimers();
     const onView = jest.fn();
     const { container, getByPlaceholderText, getByRole, getByText } = render(
