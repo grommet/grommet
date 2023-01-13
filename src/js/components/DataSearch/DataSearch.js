78bc40d22b952778ceab820f583b858a629f4ca2
Eric Soderberg <eric.soderberg@hpe.com>
2023-01-13 10:27:41 -0800

    Always put search icon on left

--- src/js/components/DataSearch/DataSearch.js
+++ src/js/components/DataSearch/DataSearch.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Search } from 'grommet-icons/icons/Search';
 import { DataForm } from '../Data/DataForm';
-import { DataContext } from '../../contexts/DataContext';
 import { FormContext } from '../Form/FormContext';
 import { useSkeleton } from '../Skeleton';
 import { TextInput } from '../TextInput';
@@ -11,9 +10,7 @@ import { DataSearchPropTypes } from './propTypes';
 export const DataSearch = ({ ...rest }) => {
   const skeleton = useSkeleton();
   const { format } = useContext(MessageContext);
-  const { updateOn } = useContext(DataContext);
   const { id: dataId, messages, noForm } = useContext(FormContext);
-  const addSubmit = noForm && updateOn === 'submit';
 
   let content = skeleton ? null : (
     <TextInput
@@ -24,15 +21,12 @@ export const DataSearch = ({ ...rest }) => {
       id={`${dataId}--search`}
       name="_search"
       icon={<Search />}
-      reverse={addSubmit}
       type="search"
       {...rest}
     />
   );
 
-  if (noForm) {
-    content = <DataForm footer={false}>{content}</DataForm>;
-  }
+  if (noForm) content = <DataForm footer={false}>{content}</DataForm>;
 
   return content;
 };
