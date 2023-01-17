f373422f0600b508b018de1db2b08fe31fbe7d46
Eric Soderberg <eric.soderberg@hpe.com>
2023-01-17 13:39:37 -0800

    Added named Data views (#6546)
    * Added named Data views
    
    * Increased bundlesize
    
    * Update src/js/components/DataView/README.md
    
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>
    
    * Refactored how view name and other filters interact
    
    * Try to preserve any selected view name as long as other changes aren't related
    
    * Preserve selected view even more
    
    * Fixed an issue with tracking whether range filters have been touched
    
    * Increase bundle size
    
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>

---
+++ src/js/components/DataView/DataView.js
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect } from 'react';
+import { DataForm } from '../Data/DataForm';
+import { DataContext } from '../../contexts/DataContext';
+import { FormContext } from '../Form/FormContext';
+import { RadioButtonGroup } from '../RadioButtonGroup';
+import { Select } from '../Select';
+import { DataViewPropTypes } from './propTypes';
+
+export const DataView = ({ ...rest }) => {
+  const { id: dataId, view, views, addToolbarKey } = useContext(DataContext);
+  const { noForm } = useContext(FormContext);
+
+  useEffect(() => {
+    if (noForm) addToolbarKey('_view');
+  }, [addToolbarKey, noForm]);
+
+  if (!views) return null;
+
+  const names = views.map((v) => v.name);
+
+  const id = `${dataId}-view`;
+  let content;
+
+  if (!noForm && names.length < 7) {
+    content = (
+      <RadioButtonGroup
+        id={id}
+        name="_view"
+        options={names}
+        value={view?.name}
+        {...rest}
+      />
+    );
+  } else {
+    content = (
+      <Select
+        id={id}
+        name="_view"
+        showSelectedInline
+        placeholder={noForm ? 'Select view ...' : undefined}
+        options={names}
+        value={view?.name}
+        {...rest}
+      />
+    );
+  }
+
+  if (noForm)
+    content = (
+      <DataForm footer={false} updateOn="change">
+        {content}
+      </DataForm>
+    );
+
+  return content;
+};
+
+DataView.propTypes = DataViewPropTypes;
