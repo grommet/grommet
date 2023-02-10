42c4eb0e0ddab17b221c64a4a9174967f7b60663
Eric Soderberg <eric.soderberg@hpe.com>
2023-02-10 09:18:57 -0800

    Changed DataSearch to add drop and responsive (#6633)
    * Changed DataSearch to add drop and responsive
    
    * Increase bundlesize
    
    * Fixed missing responsive prop type

--- src/js/components/DataSearch/__tests__/DataSearch-test.tsx
+++ src/js/components/DataSearch/__tests__/DataSearch-test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import 'jest-styled-components';
 
 import { Data } from '../../Data';
 import { DataFilters } from '../../DataFilters';
 import { Grommet } from '../../Grommet';
 import { DataSearch } from '..';
+import { createPortal, expectPortal } from '../../../utils/portal';
 
 const data = [{ name: 'a' }, { name: 'b' }];
 
 describe('DataSearch', () => {
+  window.scrollTo = jest.fn();
+  beforeEach(createPortal);
+
   test('renders', () => {
     const { container } = render(
       <Grommet>
@@ -22,5 +26,27 @@ describe('DataSearch', () => {
     );
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('drop', () => {
+    jest.useFakeTimers();
+
+    const { container, getByRole } = render(
+      <Grommet>
+        <Data id="test-data" data={data}>
+          <DataSearch drop />
+        </Data>
+      </Grommet>,
+    );
+
+    expect(getByRole('button', { name: 'Open search' })).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+
+    fireEvent.click(getByRole('button', { name: 'Open search' }));
+    // advance timers so drop can open
+    act(() => jest.advanceTimersByTime(200));
+
+    // snapshot on drop
+    expectPortal('test-data--search-control').toMatchSnapshot();
   });
 });
