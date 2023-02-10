b2daef02f5123b5e7abc7c481f6b93d5b0b82d3e
Eric Soderberg <eric.soderberg@hpe.com>
2023-02-10 08:18:05 -0800

    Fixed missing responsive prop type

--- src/js/components/DataSearch/propTypes.js
+++ src/js/components/DataSearch/propTypes.js
@@ -4,6 +4,7 @@ let PropType = {};
 if (process.env.NODE_ENV !== 'production') {
   PropType = {
     drop: PropTypes.bool,
+    responsive: PropTypes.bool,
   };
 }
 export const DataSearchPropTypes = PropType;
