059e5302fb9e9b3d469337dfc2e3ec6d48da7a9b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2023-02-13 12:50:55 -0700

    make test fail to see result

--- e2e/tests/heading-test.js
+++ e2e/tests/heading-test.js
@@ -5,5 +5,5 @@ fixture('TextInput place holder').page(`http://localhost:8080/`);
 
 test('check heading', async (t) => {
   const heading = screen.getByRole('heading', { level: 1 });
-  await t.expect(heading).exists;
+  await t.expect(heading).exists.notOk();
 });
