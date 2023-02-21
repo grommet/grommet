b4e7dc671d0c44dc1bd756039a7e3219f3e444aa
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-21 11:40:56 -0800

    Leave story untouched

--- src/js/components/Anchor/stories/Size.js
+++ src/js/components/Anchor/stories/Size.js
@@ -1,59 +1,17 @@
 import React from 'react';
 
-import { Anchor, Box, Grommet, Text } from 'grommet';
-import { deepMerge } from 'grommet/utils';
-import { hpe } from 'grommet-theme-hpe';
-import { LinkNext } from 'grommet-icons';
-
-const theme = deepMerge(hpe, {
-  anchor: {
-    color: 'text-strong',
-    textDecoration: 'underline',
-    fontWeight: 700,
-    size: {
-      large: {
-        color: 'green!',
-        textDecoration: 'none',
-      },
-      xlarge: {
-        color: 'green!',
-        textDecoration: 'none',
-      },
-      xxlarge: {
-        color: 'green!',
-        textDecoration: 'none',
-      },
-    },
-  },
-});
+import { Anchor, Box } from 'grommet';
 
 const SizeAnchor = () => (
-  <Grommet theme={theme}>
-    <Box align="center" pad="large">
-      {['xxlarge', 'xlarge', 'large', 'medium', 'small', 'xsmall', '10px'].map(
-        (size) => (
-          <Box key={size} margin="small">
-            <Text size={size}>
-              This anchor is <Anchor label={size} href="#" />
-            </Text>
-          </Box>
-        ),
-      )}
-      {['medium', 'small'].map((size) => (
+  <Box align="center" pad="large">
+    {['xxlarge', 'xlarge', 'large', 'medium', 'small', 'xsmall', '10px'].map(
+      (size) => (
         <Box key={size} margin="small">
-          <Anchor
-            size={size}
-            label={size || 'default'}
-            icon={<LinkNext color="green!" size="18px" />}
-            reverse
-            href="#"
-          />
+          <Anchor size={size} label={size} href="#" />
         </Box>
-      ))}
-
-      <Anchor label="Default" />
-    </Box>
-  </Grommet>
+      ),
+    )}
+  </Box>
 );
 
 export const Size = () => <SizeAnchor />;
