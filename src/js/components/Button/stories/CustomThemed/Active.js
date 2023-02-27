df50212c12f67f11edf93151879c7b0f6ef35dd8
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-27 15:02:53 -0800

    Leave story untouched

--- src/js/components/Button/stories/CustomThemed/Active.js
+++ src/js/components/Button/stories/CustomThemed/Active.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import {
-  Anchor,
-  Grommet,
-  Button,
-  Box,
-  Menu,
-  Pagination,
-  Select,
-  SelectMultiple,
-  MaskedInput,
-  TextInput,
-  DateInput,
-  Text,
-} from 'grommet';
-import { LinkNext, Down, Search } from 'grommet-icons';
+import { Grommet, Button, Box, Text } from 'grommet';
+import { Add } from 'grommet-icons';
 
 export const Active = () => (
   <Grommet
@@ -23,113 +10,17 @@ export const Active = () => (
           family: `-apple-system, BlinkMacSystemFont`,
         },
       },
-      icon: {
-        matchSize: true,
-        size: {
-          small: '12px',
-          medium: '18px',
-          large: '24px',
-        },
-      },
-      button: {
-        default: {
-          background: undefined,
-          border: {
-            radius: '2em',
-          },
-          pad: {
-            horizontal: '12px',
-            vertical: '12px',
-          },
-        },
-        primary: {
-          background: 'brand',
-          border: {
-            radius: '2em',
-          },
-          pad: {
-            horizontal: '12px',
-            vertical: '12px',
-          },
-        },
-        size: {
-          // small: {
-          //   pad: '9px',
-          // },
-          // large: {
-          //   pad: '9px',
-          // },
-        },
-      }, // enabling kind button functionality
-      menu: {
-        icons: {
-          down: Down,
-        },
-      },
-      select: {
-        icons: {
-          down: Down,
-        },
-      },
+      button: { default: {} }, // enabling kind button functionality
     }}
   >
-    <Box pad="large" align="center" gap="medium">
-      <Button icon={<LinkNext />} size="small" primary />
-      <Button icon={<LinkNext />} primary />
-      <Button icon={<LinkNext />} size="large" primary />
-      <Button
-        icon={<LinkNext size="12px" />}
-        label="Explicit size override on icon"
-        size="large"
-        primary
-        reverse
-      />
-    </Box>
-    <Box pad="large" align="center" gap="medium">
-      <Pagination numberItems={200} size="small" />
-      <Pagination numberItems={200} />
-      <Pagination numberItems={200} size="large" />
-    </Box>
-    <Box pad="large" align="center" gap="medium">
-      <Menu label="Menu" size="small" />
-      <Menu label="Menu" />
-      <Menu label="Menu" size="large" />
-    </Box>
-    <Box pad="large" align="center" gap="medium">
-      <Select placeholder="Select" size="small" options={[]} />
-      <Select placeholder="Select" options={[]} />
-      <Select placeholder="Select" size="large" options={[]} />
-    </Box>
-    <Box pad="large" align="center" gap="medium">
-      <SelectMultiple placeholder="SelectMultiple" size="small" options={[]} />
-      <SelectMultiple placeholder="SelectMultiple" options={[]} />
-      <SelectMultiple placeholder="SelectMultiple" size="large" options={[]} />
-    </Box>
-    <Box pad="large" align="center" gap="medium" width="medium">
-      <TextInput placeholder="TextInput" size="small" icon={<Search />} />
-      <TextInput placeholder="TextInput" icon={<Search />} />
-      <TextInput placeholder="TextInput" size="large" icon={<Search />} />
-    </Box>
-    <Box pad="large" align="center" gap="medium" width="medium">
-      <MaskedInput placeholder="MaskedInput" size="small" icon={<Search />} />
-      <MaskedInput placeholder="MaskedInput" icon={<Search />} />
-      <MaskedInput placeholder="MaskedInput" size="large" icon={<Search />} />
-    </Box>
-    <Box pad="large" align="center" gap="medium" width="medium">
-      <DateInput format="mm/dd/yyyy" size="small" />
-      <DateInput format="mm/dd/yyyy" />
-      <DateInput format="mm/dd/yyyy" size="large" />
-    </Box>
-    <Box pad="large" align="center" gap="medium" width="medium">
-      <Text size="small">
-        This is <Anchor label="small" icon={<LinkNext />} reverse />
-      </Text>
-      <Text>
-        This is <Anchor label="medium" icon={<LinkNext />} reverse />
-      </Text>
-      <Text size="large">
-        This is <Anchor label="large" icon={<LinkNext />} reverse />
-      </Text>
+    <Box pad="large" align="center">
+      <Button hoverIndicator="light-1" onClick={() => {}} active>
+        {/*  When kind Button include children, it is treated as plain */}
+        <Box pad="small" direction="row" align="center" gap="small">
+          <Add />
+          <Text>Kind</Text>
+        </Box>
+      </Button>
     </Box>
   </Grommet>
 );
