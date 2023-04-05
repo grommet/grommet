3ab20695b3a3e1407f756f9ecebacdeb00bb599d
Jessica Filben <jessica.filben@gmail.com>
2023-04-05 09:49:20 -0600

    adjust order of story loading

--- storybook/main.js
+++ storybook/main.js
@@ -12,6 +12,8 @@ module.exports = {
     '@storybook/addon-a11y',
   ],
   stories: [
+    '../src/js/all/**/stories/*.@(ts|tsx|js|jsx)',
+    '../src/js/all/stories/typescript/*.tsx',
     '../src/js/components/**/stories/typescript/*.tsx',
     '../src/js/components/**/stories/*.@(ts|tsx|js|jsx)',
     '../src/js/components/**/stories/CustomThemed/*.@(ts|tsx|js|jsx)',
@@ -19,8 +21,6 @@ module.exports = {
     '../src/js/contexts/**/*stories.js',
     '../src/js/contexts/**/stories/typescript/*.tsx',
     '../src/js/contexts/**/stories/*.@(ts|tsx|js|jsx)',
-    '../src/js/all/**/stories/*.@(ts|tsx|js|jsx)',
-    '../src/js/all/stories/typescript/*.tsx',
   ],
   features: {
     postcss: false,
