a39056e3adcd9ae9d54650b7c21fce109ea11943
Eric Soderberg <eric.soderberg@hpe.com>
2023-04-14 15:38:52 -0700

    Added propTypes

--- src/js/components/DataTableGroupBy/propTypes.js
+++ src/js/components/DataTableGroupBy/propTypes.js
@@ -1,5 +1,15 @@
+import PropTypes from 'prop-types';
+
 let PropType = {};
 if (process.env.NODE_ENV !== 'production') {
-  PropType = {};
+  PropType = {
+    options: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.string),
+      PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string,
+        property: PropTypes.string,
+      })),
+    ]),
+  };
 }
 export const DataTableGroupByPropTypes = PropType;
