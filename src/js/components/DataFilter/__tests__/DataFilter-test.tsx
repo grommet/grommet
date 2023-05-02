ea24cb4ff092893aa7ce3dab67f8122686d8522c
Jeffrey Chen <j.chen14641@gmail.com>
2023-05-02 09:49:22 -0700

    Update filter.js for Data Filtering (#6772)
    * Update filter logic to process filterValue as object
    
    * change logic order and add comments
    
    * add DataFilter test and update snapshot
    
    * Add DataFilter test for select options
    
    ---------
    
    Co-authored-by: Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>

--- src/js/components/DataFilter/__tests__/DataFilter-test.tsx
+++ src/js/components/DataFilter/__tests__/DataFilter-test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import 'jest-styled-components';
-
 import { Data } from '../../Data';
 import { DataFilters } from '../../DataFilters';
 import { Grommet } from '../../Grommet';
@@ -16,6 +15,7 @@ const data = [
 ];
 
 describe('DataFilter', () => {
+  window.scrollTo = jest.fn();
   test('renders', () => {
     const { container } = render(
       <Grommet>
@@ -65,6 +65,59 @@ describe('DataFilter', () => {
       </Grommet>,
     );
 
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('select multiple options', () => {
+    jest.useFakeTimers();
+
+    const { container, getByRole } = render(
+      <Grommet>
+        <Data
+          data={data}
+          properties={{
+            'type.name': {
+              label: 'Type',
+            },
+          }}
+        >
+          <DataFilters drop>
+            <DataFilter
+              property="type.name"
+              options={[
+                'ZZ', 'YY', 'aa', 'bb', 'cc', 'dd', 'ee', 'ff'
+              ]}
+            />
+          </DataFilters>
+        </Data>
+      </Grommet>,
+    );
+
+    expect(getByRole('button', { name: 'Open filters' })).toBeTruthy();
+    fireEvent.click(getByRole('button', { name: 'Open filters' }));
+    // advance timers so drop can open
+    act(() => jest.advanceTimersByTime(200));
+
+    // open SelectMultiple
+    fireEvent.click(getByRole('button', { name: /Open Drop/i }));
+    act(() => jest.advanceTimersByTime(200));
+
+    // click the first option 'ZZ'
+    fireEvent.click(getByRole('option', { name: /ZZ/i }));
+    act(() => jest.advanceTimersByTime(200));
+
+    // close SelectMultiple
+    fireEvent.click(getByRole('button', { name: /Close Select/i }));     
+    act(() => jest.advanceTimersByTime(200));
+  
+    // click Apply Filters button
+    expect(getByRole('button', { name: 'Apply filters' })).toBeTruthy();
+    fireEvent.click(getByRole('button', {name: 'Apply filters'}));
+    
+    // advance timers so filters can be applied
+    act(() => jest.advanceTimersByTime(200));
+
+    // snapshot on selected filter
     expect(container.firstChild).toMatchSnapshot();
   });
 
