e0c06ce382151a0523274f4c448a1c18d1f1284f
Jessica Filben <jessica.filben@gmail.com>
2023-05-25 15:34:38 -0600

    Adjust empty search message styling on Select

--- src/js/components/Select/EmptySearchOption.js
+++ src/js/components/Select/EmptySearchOption.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Box } from '../Box';
 import { Text } from '../Text';
-import { SelectOption } from './StyledSelect';
 
 export const EmptySearchOption = ({
   emptySearchMessage,
   selectOptionsStyle,
   theme,
 }) => (
-  <SelectOption
-    key="search_empty"
-    tabIndex="0"
-    role="menuitem"
-    hoverIndicator="background"
-    disabled
+  <Box
+    {...{
+      ...selectOptionsStyle,
+      pad: selectOptionsStyle?.pad || theme.button?.option?.pad,
+    }}
   >
-    <Box {...selectOptionsStyle}>
-      <Text aria-live="polite" role="alert" {...theme.select.container.text}>
-        {emptySearchMessage}
-      </Text>
-    </Box>
-  </SelectOption>
+    <Text aria-live="polite" role="alert" {...theme.select?.options?.text}>
+      {emptySearchMessage}
+    </Text>
+  </Box>
 );
