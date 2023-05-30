45844607ed70d2b11486b37dc21d82b68d29e734
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-05-30 14:22:12 -0300

    Add RTL support to progress-bar (#6398)
    * Add RTL support to progress-bar
    
    * Change rtl prop to reverse
    
    * Adds reverse and rtl tests to Meter
    
    Co-authored-by: gabrielcicero45 <gabrielcicero45@users.noreply.github.com>
    Co-authored-by: Júlia Albuquerque <julialbq@users.noreply.github.com>
    Co-authored-by: Leonardo Santiago <leossantiago@users.noreply.github.com>
    
    * update video snapshot
    
    * Apply review suggestions
    
    ---------
    
    Co-authored-by: gabrielcicero45 <gabrielcicero45@users.noreply.github.com>
    Co-authored-by: Júlia Albuquerque <julialbq@users.noreply.github.com>
    Co-authored-by: Leonardo Santiago <leossantiago@users.noreply.github.com>

--- src/js/components/Meter/StyledMeter.js
+++ src/js/components/Meter/StyledMeter.js
@@ -4,7 +4,7 @@ import { genericStyles } from '../../utils';
 import { defaultProps } from '../../default-props';
 
 const roundStyle = css`
-  border-radius: ${props => props.theme.global.edgeSize[props.round.size]};
+  border-radius: ${(props) => props.theme.global.edgeSize[props.round.size]};
 `;
 
 // overflow: hidden is needed for ie11
@@ -12,13 +12,19 @@ const StyledMeter = styled.svg`
   max-width: 100%;
   overflow: hidden;
 
-  ${genericStyles} ${props => props.round && roundStyle}
+  ${(props) =>
+    props.reverse &&
+    css`
+      transform: scale(-1, 1);
+    `}
+
+  ${genericStyles} ${(props) => props.round && roundStyle}
 
   path {
     transition: stroke 0.3s, stroke-width 0.3s;
   }
 
-  ${props => props.theme.meter && props.theme.meter.extend};
+  ${(props) => props.theme.meter && props.theme.meter.extend};
 `;
 
 StyledMeter.defaultProps = {};
