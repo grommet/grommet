59ee7d2cb30d5f6747797f5e12a386562df1110e
briancoburn <brian.coburn@hpe.com>
2023-07-26 09:41:43 -0700

    add truncation support and tip for long tags

--- src/js/components/Tag/Tag.js
+++ src/js/components/Tag/Tag.js
@@ -10,11 +10,11 @@ import { Box } from '../Box';
 import { Text } from '../Text';
 
 import { StyledRemoveButton, StyledTagButton } from './StyledTag';
+import {Tip} from "grommet";
 
 const Tag = forwardRef(
-  ({ name, value, size, onRemove, onClick, ...rest }, ref) => {
+  ({ name, value, size, truncate, truncateNumChars=16, onRemove, onClick, ...rest }, ref) => {
     const theme = useContext(ThemeContext) || defaultProps.theme;
-
     const containerProps = {
       ref,
       align: 'center',
@@ -23,27 +23,42 @@ const Tag = forwardRef(
       round: theme.tag.size?.[size]?.round || theme.tag.round,
       ...rest,
     };
-    const contents = (
+    const displayName = truncate && name.length > truncateNumChars? name.substring(0,truncateNumChars-3)+'...' : name;
+    const displayValue = truncate && value.length > truncateNumChars? value.substring(0,truncateNumChars-3)+'...' : value;
+    const tipValue1 = value.length < 128? value : value.substring(0,128);
+    const tipValue2 = value.length < 128? '' : value.substring(128);
+    const shouldDisplayTip  = truncate && (name.length > truncateNumChars || value.length > truncateNumChars);
+    const tipContent=(<Box direction='column'>
+      <Text size={size}>{name+' : '}</Text>
+      {tipValue1 && <Text weight='bold' size={size}>{tipValue1}</Text>}
+      {tipValue2 && <Text weight='bold' size={size}>{tipValue2}</Text>}
+    </Box>)
+
+    const textContent = (
       <Box
-        width={{ min: 'min-content' }}
-        pad={theme.tag.size?.[size]?.pad || theme.tag.pad}
-      >
-        <Text size={size}>
-          {name && (
-            <Text {...theme.tag.name} size={size}>
-              {' '}
-              {name}
-            </Text>
-          )}
-          {name && value ? <Text size={size}>{theme.tag.separator}</Text> : ''}
-          {value && (
-            <Text {...theme.tag.value} size={size}>
-              {value}
-            </Text>
-          )}
-        </Text>
-      </Box>
-    );
+      width={{ min: 'min-content', max: '100%' }}
+      pad={theme.tag.size?.[size]?.pad || theme.tag.pad}
+    >
+      <Text size={size}>
+        {name && (
+          <Text {...theme.tag.name} size={size}>
+            {' '}
+            {displayName}
+          </Text>
+        )}
+        {name && value ? <Text size={size}>{theme.tag.separator}</Text> : ''}
+        {value && (
+          <Text {...theme.tag.value} size={size}>
+            {displayValue}
+          </Text>
+        )}
+      </Text>
+    </Box>)
+    const contents = shouldDisplayTip? (
+      <Tip content={tipContent}>
+        {textContent}
+      </Tip>
+    ): textContent;
 
     if (onClick && onRemove) {
       console.warn('Tag cannot combine "onClick" and "onRemove".');
