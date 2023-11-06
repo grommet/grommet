f9f26a5180f7c5109d2c6bd9a048cbe11bc1457c
Taylor Seamans <taylor.seamans@yahoo.com>
2023-11-06 10:50:10 -0800

    Remove unneccessary .contains

--- src/js/components/Form/Form.js
+++ src/js/components/Form/Form.js
@@ -679,7 +679,7 @@ const Form = forwardRef(
           // bubbles up to the "outer" form even though in the DOM the portal
           // doesn't render as child of the "outer" form.
           // https://legacy.reactjs.org/docs/portals.html#event-bubbling-through-portals
-          if (formRef.current && event.target.contains(formRef.current)) {
+          if (formRef.current && event.target === formRef.current) {
             setPendingValidation(undefined);
             // adding validateOn: "submit" prop to the undefined validateOn
             // fields as we want to trigger "submit" validation once form
