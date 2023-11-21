4010bb2b74c340e6123e5b62aeec407534d32880
Taylor Seamans <taylor.seamans@yahoo.com>
2023-11-21 13:09:30 -0800

    Update prop types and typescript types

--- src/js/components/DataTableColumns/index.d.ts
+++ src/js/components/DataTableColumns/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export interface DataTableColumnsProps {
   drop: boolean;
-  options: (string | { property: string; label: string })[];
+  options: (string | { property: string; label: string; disabled?: boolean })[];
 }
 
 declare const DataTableColumns: React.FC<DataTableColumnsProps>;
