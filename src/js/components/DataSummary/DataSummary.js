18165ab952e2830a81c4405fb82592c753669944
Taylor Seamans <taylor.seamans@yahoo.com>
2023-11-27 10:54:00 -0800

    Update DataSummary messages object (#7022)
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>

--- src/js/components/DataSummary/DataSummary.js
+++ src/js/components/DataSummary/DataSummary.js
@@ -17,14 +17,25 @@ export const DataSummary = ({ messages, ...rest }) => {
   if (total !== filteredTotal) {
     if (filteredTotal === 1) messageId = 'dataSummary.filteredSingle';
     else messageId = 'dataSummary.filtered';
-  } else messageId = 'dataSummary.total';
+  } else if (total === 1) messageId = 'dataSummary.totalSingle';
+  else messageId = 'dataSummary.total';
+
+  // helps account for cases like 0 results of 1 item
+  const items = format({
+    id: total === 1 ? 'dataSummary.itemsSingle' : 'dataSummary.items',
+    messages: messages || dataMessages?.dataSummary,
+  });
 
   return (
     <Text margin={{ vertical: 'xsmall' }} {...rest}>
       {format({
         id: messageId,
         messages: messages || dataMessages?.dataSummary,
-        values: { filteredTotal, total },
+        values: {
+          filteredTotal,
+          total,
+          items,
+        },
       })}
     </Text>
   );
