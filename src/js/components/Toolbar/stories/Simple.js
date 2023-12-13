23765afaa25f1c694289ba76c0f97abc23449687
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2023-12-13 10:49:13 -0700

    fix toolbar story

--- src/js/components/Toolbar/stories/Simple.js
+++ src/js/components/Toolbar/stories/Simple.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-import { Box, Button, DropButton, Notification, TextInput } from 'grommet';
-import { Search } from 'grommet-icons/icons/Search';
-import { Filter } from 'grommet-icons/icons/Filter';
+import {
+  Box,
+  Button,
+  Data,
+  DataFilters,
+  DataSearch,
+  Notification,
+} from 'grommet';
 
 import { Toolbar } from '../Toolbar';
+import { DATA } from '../../DataTable/stories/data';
 
 export const Simple = () => (
   // Uncomment <Grommet> lines when using outside of storybook
@@ -15,14 +21,14 @@ export const Simple = () => (
       message="Toolbar is in 'beta'. The API surface is subject to change."
     />
     <Box width="large">
-      <Toolbar>
-        <Box width={{ max: 'small' }}>
-          <TextInput icon={<Search />} />
-        </Box>
-        <DropButton kind="toolbar" icon={<Filter />} />
-        <Box flex />
-        <Button label="Create" primary />
-      </Toolbar>
+      <Data data={DATA}>
+        <Toolbar>
+          <DataSearch />
+          <DataFilters />
+          <Box flex />
+          <Button label="Create" primary />
+        </Toolbar>
+      </Data>
     </Box>
   </Box>
   // </Grommet>
