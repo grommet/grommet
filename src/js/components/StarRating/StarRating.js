9dfe9dab658d868207512c9a5a4546c837a56082
abdulbasithqb <abdul.basith@qburst.com>
2023-12-20 18:26:39 +0530

    fix: starRating - form value not synched

--- src/js/components/StarRating/StarRating.js
+++ src/js/components/StarRating/StarRating.js
@@ -1,20 +1,26 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { Star } from 'grommet-icons/icons/Star';
 import { StarOutline } from 'grommet-icons/icons/StarOutline';
+import { FormContext } from '../Form/FormContext';
 import { RadioButtonGroup } from '../RadioButtonGroup';
 
-const StarRating = ({ ...rest }) => {
+const StarRating = ({name, defaultValue, value:valueProp, ...rest }) => {
+  const formContext = useContext(FormContext);
   const theme = useContext(ThemeContext);
-  const [value, setValue] = useState();
-
+  const [value, setValue] = formContext.useFormInput({
+    name,
+    value: valueProp,
+    initialValue: defaultValue ?? 0,
+  });
+  
   const options = [];
   for (let i = 1; i < 6; i += 1) {
     options.push(i);
   }
-
   return (
     <RadioButtonGroup
+      name={name}
       direction="row"
       options={options}
       onChange={(event) => {
