6cc02105e6115bc5b2f4194b4fe52fc97bb026a4
sthpe <sulaymon.tajudeen@hpe.com>
2024-01-26 22:26:18 +0000

    fix missing props and typings for tag component

--- src/js/components/Tag/propTypes.js
+++ src/js/components/Tag/propTypes.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
-import { genericProps } from '../../utils/general-prop-types';
+import {
+  backgroundPropType,
+  genericProps,
+} from '../../utils/general-prop-types';
 
 let PropType = {};
 if (process.env.NODE_ENV !== 'production') {
   PropType = {
     ...genericProps,
     as: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    background: backgroundPropType,
     name: PropTypes.string,
     value: PropTypes.string.isRequired,
     onClick: PropTypes.func,
