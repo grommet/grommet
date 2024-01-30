4df64616fccf1a2a8aea5eed86c6d051ef145533
Mike Kingdom <michael.kingdom@hpe.com>
2024-01-30 13:51:20 -0700

    Implement DataSearch updateOn property

--- src/js/components/DataSearch/index.d.ts
+++ src/js/components/DataSearch/index.d.ts
@@ -4,6 +4,7 @@ import { TextInputProps } from '../TextInput/index';
 export interface DataSearchProps {
   drop?: boolean;
   responsive?: boolean;
+  updateOn?: string;
 }
 
 export interface DataSearchExtendedProps
---
+++ src/js/components/DataSearch/stories/ChangeonEnter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Data, DataSummary, DataTable, Grid, Paragraph } from 'grommet';
+
+import { DataSearch } from '../DataSearch';
+import { columns, DATA } from '../../DataTable/stories/data';
+
+export const ChangeOnEnter = () => (
+  // Uncomment <Grommet> lines when using outside of storybook
+  // <Grommet theme={...}>
+  <Grid pad="large" columns={[['medium', 'large']]} justifyContent="center">
+    <Paragraph color="text-weak">
+      Note: Results are filtered once you hit enter.
+    </Paragraph>
+    <Data data={DATA}>
+      <DataSearch updateOn="submit" />
+      <DataSummary />
+      <DataTable columns={columns} />
+    </Data>
+  </Grid>
+  // </Grommet>
+);
+
+ChangeOnEnter.args = {
+  full: true,
+};
+
+export default {
+  title: 'Data/DataSearch/ChangeOnEnter',
+};
