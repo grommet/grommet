8a9779db93c0bdc9f877e26f1ccfc52b739314d0
Max Shepel <max@undeletable.name>
2024-05-09 22:31:36 +0300

    Add comment.
    DCO Remediation Commit for Max Shepel <max@undeletable.name>
    
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: cd9a5b7103b6129abd293f4f17b0e92411665fc1
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 14838be4fe009b7540254ecff43689d96749d95f
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 1766eedc7f3390b9795a8eb59714f7102b6179c1
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: b5e5d260111f87b5373b610594a3e6a97e6337aa
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: f005071a37da340e6d0bd775e8341c671ceb3aa8
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 9f56ecaf5473db2d12e98acb1a0900f3123b9e28
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 019d28bd80528218f3bd807149357fc06a4b51a4
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 45f81b21614daa8665b9489ad02749556679c516
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: b1ca758e536e38b4107564180b6d119fd7cff1fa
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: be3a510d827a3e08f4609d81bcddfcdb53413426
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: a8ed76ea4abb35a8b98cb9f0a2345a77e15a1177
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: e329a7a6c85189be7370732906f42be87a8fe17e
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: ccd1a6c4092c9f0272d76f1bf0216be94b151365
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 87adfd3c8b6c130cc6254bdc71883dcd96b1a08d
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 164ac90be9bcf8181645b36418528053d9e0b3fb
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 3926bc92c2fae4467ea5e5ee9dcaed455b03308a
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 1abbf1b8bd39a54f97d9040c05a1b3ce80978164
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: bc92022c57546860ea6d9ed9ce7362a1c510b430
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 50478dd2005fea4f1ed4c16db9ed1a2155ceb312
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 9e94bcee1714331fc9218899ed24e29603f08829
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 2911f3362755b5b5b23f2721c465c612662e9b7f
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 780ede88533cf61b5b9ae5a6d4c1e31925ec558c
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 74b3e2b0282856bf4392d355b6bdbb157f3d30e7
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 58770f02fe50c89be6a42b758b713668d0a17cf6
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 7d584c8e26d4624047b0b7ebf0e3725968d042e1
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: b36c664ea65e19270f1522c386e56eb462c91759
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: bd1a6065d9d059e2edc2286d870c480c3b1b7897
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 86687bfa127b518b8d9867a2fa744b06766b1d42
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 302564b5dc4862ad4cc437d3d5a1ff1784334a30
    I, Max Shepel <max@undeletable.name>, hereby add my Signed-off-by to this commit: 3772235766cd5134e7d64e79abab1bc6f00a1ee0
    
    Signed-off-by: Max Shepel <max@undeletable.name>

--- src/js/default-props.js
+++ src/js/default-props.js
@@ -9,6 +9,10 @@ export const extendDefaultTheme = theme => {
   defaultProps.theme = deepMerge(base, theme);
 };
 
+/*
+  Pass `theme` for component which can be loacted outside of theme context.
+  To be used as argument for `attrs` method from `styled-components`.
+ */
 export const ehnancePropsWithTheme = props => ({
   theme: deepMerge(defaultProps.theme, props.theme),
 });
