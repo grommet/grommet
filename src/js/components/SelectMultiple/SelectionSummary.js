8e949872a8422bee22660175d7aae814bf9414a0
Brittany Archibeque <brittanyarchibeque@Brittanys-MBP-2.home>
2024-06-04 13:59:41 -0600

    change logic

--- src/js/components/SelectMultiple/SelectionSummary.js
+++ src/js/components/SelectMultiple/SelectionSummary.js
@@ -80,18 +80,18 @@ const SelectionSummary = ({
   // or selected value is not disabled or value is
   // undefied or Checks if there are no selected values
   // after filtering based on a search query.
-
-  const showSelectAll = !!(
-    value?.length === 0 ||
-    selectedValuesDisabled() ||
-    !value ||
-    selectedInSearch().length === 0
-  );
+  const showSelectAll =
+    !limit &&
+    !onMore &&
+    (value?.length === 0 ||
+      selectedValuesDisabled() ||
+      !value ||
+      selectedInSearch().length === 0);
 
   // show clearAll button when no limit or no value or all values are disabled,
   // if there are selected values in search or if no limit
   const showClearAll =
-    options.length > 0 &&
+    !showSelectAll &&
     (!limit ||
       !(!value || (value?.length === 0 && selectedValuesDisabled()))) &&
     (selectedInSearch().length !== 0 || !limit) &&
@@ -141,6 +141,12 @@ const SelectionSummary = ({
     if (limit && !showSelectAll) setActiveIndex(0);
   };
 
+  const ButtonProps = {
+    onClick: (event) => summaryButtonClick(event),
+    onFocus: () => setActiveIndex(-1),
+    ref: clearRef,
+  };
+
   return (
     <Box
       pad={
@@ -155,35 +161,41 @@ const SelectionSummary = ({
       height={{ min: 'xxsmall' }}
     >
       <Text size="small">{summaryText}</Text>
-      {options.length > 0 && showClearAll && (
-        <Button
-          a11yTitle={
-            showSelectAll
-              ? format({
-                  id: 'selectMultiple.selectAllA11y',
-                  messages,
-                  values: {
-                    total: options.length,
-                  },
-                })
-              : format({
-                  id: 'selectMultiple.clearAllA11y',
-                  messages,
-                  values: {
-                    selected: value?.length || 0,
-                  },
-                })
-          }
-          label={format({
-            id: showSelectAll
-              ? 'selectMultiple.selectAll'
-              : 'selectMultiple.clearAll',
-            messages,
-          })}
-          onClick={(event) => summaryButtonClick(event)}
-          onFocus={() => setActiveIndex(-1)}
-          ref={clearRef}
-        />
+      {options.length > 0 && (
+        <>
+          {showSelectAll && (
+            <Button
+              a11yTitle={format({
+                id: 'selectMultiple.selectAllA11y',
+                messages,
+                values: {
+                  total: options.length,
+                },
+              })}
+              label={format({
+                id: 'selectMultiple.selectAll',
+                messages,
+              })}
+              {...ButtonProps}
+            />
+          )}
+          {showClearAll && (
+            <Button
+              a11yTitle={format({
+                id: 'selectMultiple.clearAllA11y',
+                messages,
+                values: {
+                  selected: value?.length || 0,
+                },
+              })}
+              label={format({
+                id: 'selectMultiple.clearAll',
+                messages,
+              })}
+              {...ButtonProps}
+            />
+          )}
+        </>
       )}
     </Box>
   );
