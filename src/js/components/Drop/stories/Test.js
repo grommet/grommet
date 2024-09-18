388d87bb1a7cc110971784fd62aadc11b1e7a7d9
britt6612 <brittany.archibeque@hpe.com>
2024-09-18 10:47:41 -0600

    push up modal in modal for testing

--- src/js/components/Drop/stories/Test.js
+++ src/js/components/Drop/stories/Test.js
@@ -1,18 +1,28 @@
 import React, { useRef, useState } from 'react';
-import { Box, Anchor, Button, Drop } from 'grommet';
+import { Box, Anchor, Button, Drop, Layer } from 'grommet';
 
 const TestDrop = () => {
   const [showDrop, setShowDrop] = useState(false);
-  const ref = useRef();
+  const [showLayer, setShowLayer] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const layerRef = useRef();
+  const dropRef = useRef();
+  const modalRef = useRef();
+
+  const onClose = () => setShowLayer(false);
 
   return (
     // Uncomment <Grommet> lines when using outside of storybook
     // <Grommet theme={...}>
     <>
-      <Button ref={ref} label="button" onClick={() => setShowDrop(true)} />
+      <Button
+        ref={dropRef}
+        label="single drop"
+        onClick={() => setShowDrop(true)}
+      />
       {showDrop && (
         <Drop
-          target={ref.current}
+          target={dropRef.current}
           onClickOutside={() => setShowDrop(false)}
           onEsc={() => setShowDrop(false)}
         >
@@ -22,6 +32,43 @@ const TestDrop = () => {
             <Anchor href="#" label="testing anchor" />
           </Box>
         </Drop>
+      )}
+      <Button
+        ref={layerRef}
+        label="open layer"
+        onClick={() => setShowLayer(true)}
+      />
+      {showLayer && (
+        <Layer
+          position="right"
+          full="vertical"
+          modal
+          onClickOutside={onClose}
+          onEsc={onClose}
+        >
+          <Box fill="vertical" overflow="auto" width="medium" pad="medium">
+            <Button label="testing button" />
+            <Anchor href="#" label="testing anchor" />
+            <Button
+              ref={modalRef}
+              label="open drop"
+              onClick={() => setShowDrop(true)}
+            />
+            {showModal && (
+              <Drop
+                target={modalRef.current}
+                onClickOutside={() => setShowModal(false)}
+                onEsc={() => setShowModal(false)}
+              >
+                <Box gap="medium" pad="large">
+                  Drop inside layer
+                  <Button label="testing button" />
+                  <Anchor href="#" label="testing anchor" />
+                </Box>
+              </Drop>
+            )}
+          </Box>
+        </Layer>
       )}
     </>
     // </Grommet>
