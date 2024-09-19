41b751194616d4e9678da2099e30ca3952c0c72f
britt6612 <brittany.archibeque@hpe.com>
2024-09-18 22:43:34 -0600

    added more logic to trapFocus

--- src/js/components/FocusedContainer.js
+++ src/js/components/FocusedContainer.js
@@ -22,6 +22,30 @@ export const FocusedContainer = ({
   const roots = useContext(RootsContext);
   const [nextRoots, setNextRoots] = useState(roots);
 
+  const isFocusable = (element) => {
+    if (element.tabIndex < 0) {
+      return false;
+    }
+
+    if (element.disabled) {
+      return false;
+    }
+
+    switch (element.nodeName) {
+      case 'A':
+        return !!element.href && element.rel != 'ignore';
+      case 'INPUT':
+        return element.type != 'hidden';
+      case 'BUTTON':
+      case 'SELECT':
+      case 'TEXTAREA':
+        return true;
+      default:
+        return false;
+    }
+  };
+  let IgnoreUtilFocusChanges = false;
+
   useEffect(() => {
     if (ref.current) {
       setNextRoots([...roots, ref.current]);
@@ -52,59 +76,103 @@ export const FocusedContainer = ({
     };
   }, [bodyOverflowStyle, hidden, trapFocus, restrictScroll]);
 
-  const trapFocusHandler = useCallback((event) => {
-    const container = ref.current;
-    if (!container) return;
-
-    const focusableElements = container.querySelectorAll(
-      `button, [href], input, select, textarea,
-       [tabindex]:not([tabindex="-1"])`,
-    );
-
-    if (focusableElements.length === 0) return;
-
-    const firstElement = focusableElements[0];
-    const lastElement = focusableElements[focusableElements.length - 1];
-
-    if (container.contains(event.target)) {
-      container.lastFocus = event.target;
-    } else if (container.lastFocus === firstElement) {
-      lastElement.focus();
-      event.preventDefault();
-    } else {
-      firstElement.focus();
-      event.preventDefault();
+  const focusLastDescendant = (element) => {
+    for (let i = element.childNodes.length - 1; i >= 0; i--) {
+      const child = element.childNodes[i];
+      if (attemptFocus(child) || focusLastDescendant(child)) {
+        return true;
+      }
     }
-  }, []);
+    return false;
+  };
+  const focusFirstDescendant = (element) => {
+    for (let i = 0; i < element.childNodes.length; i++) {
+      const child = element.childNodes[i];
+      if (attemptFocus(child) || focusFirstDescendant(child)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const attemptFocus = (element) => {
+    if (!isFocusable(element)) {
+      return false;
+    }
+
+    IgnoreUtilFocusChanges = true;
+    try {
+      element.focus();
+    } catch (e) {
+      // Continue regardless of error
+    }
+    IgnoreUtilFocusChanges = false;
+
+    return document.activeElement === element;
+  };
+
+  const trapFocusHandler = useCallback(
+    (event) => {
+      if (nextRoots && nextRoots.length > 0) {
+        if (IgnoreUtilFocusChanges) {
+          return;
+        }
+
+        // Get the last dialog in the array
+        const lastCurrentDialog = nextRoots[nextRoots.length - 1];
+        const currentDialog = {
+          dialogNode: lastCurrentDialog.querySelector('div'),
+          lastFocus: null,
+        };
+        console.log('?', currentDialog.dialogNode);
+        // currentDialog.lastFocus is sometimes null
+        // when the dialog is first opened also looks like
+        // its null when you try and get the last focusable element from the first focusable element
+        // backwards
+
+        if (currentDialog.dialogNode) {
+          if (currentDialog.dialogNode.contains(event.target)) {
+            currentDialog.lastFocus = event.target;
+            // console.log(currentDialog.lastFocus);
+          } else {
+            focusFirstDescendant(currentDialog.dialogNode);
+            if (currentDialog.lastFocus == document.activeElement) {
+              // console.log('second', currentDialog.lastFocus);
+              focusLastDescendant(currentDialog.dialogNode);
+            }
+            currentDialog.lastFocus = document.activeElement;
+          }
+        }
+      }
+    },
+    [nextRoots],
+  );
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!hidden && trapFocus && roots && roots[0]) {
-        document.addEventListener('focus', trapFocusHandler, true);
-        // Create and insert focusable nodes
-        const preDiv = document.createElement('div');
-        const postDiv = document.createElement('div');
+    if (!hidden && trapFocus && roots && roots[0]) {
+      document.addEventListener('focus', trapFocusHandler, true);
+      // Create and insert focusable nodes
+      const preDiv = document.createElement('div');
+      const postDiv = document.createElement('div');
 
-        preDiv.tabIndex = 0;
-        postDiv.tabIndex = 0;
+      preDiv.tabIndex = 0;
+      postDiv.tabIndex = 0;
 
-        preNodeRef.current = ref.current.parentNode.insertBefore(
-          preDiv,
-          ref.current,
-        );
-        postNodeRef.current = ref.current.parentNode.insertBefore(
-          postDiv,
-          ref.current.nextSibling,
-        );
+      preNodeRef.current = ref.current.parentNode.insertBefore(
+        preDiv,
+        ref.current,
+      );
+      postNodeRef.current = ref.current.parentNode.insertBefore(
+        postDiv,
+        ref.current.nextSibling,
+      );
 
-        // Make all nodes unfocusable except the last one in the list
-        roots.forEach(makeNodeUnfocusable);
-      }
-    }, 0);
+      // Make all nodes unfocusable except the last one in the list
+      roots.forEach(makeNodeUnfocusable);
+    }
 
     return () => {
       document.removeEventListener('focus', trapFocusHandler, true);
-      clearTimeout(timer);
       if (preNodeRef.current) preNodeRef.current.remove();
       if (postNodeRef.current) postNodeRef.current.remove();
 
