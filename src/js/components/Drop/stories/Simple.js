06911cb7bc49e57ecbe5024ef3bb991975cd73c9
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2024-10-15 13:07:30 -0600

    remove stories used for testing

--- src/js/components/Drop/stories/Simple.js
+++ src/js/components/Drop/stories/Simple.js
@@ -1,124 +1,40 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-import { Box, DropButton, Button, Anchor, Layer, Drop } from 'grommet';
-
-const InteractiveContent = () => (
-  <Box gap="medium" pad="large">
-    Drop Contents
-    <Button label="testing button" />
-    <Anchor href="#" label="testing anchor" />
-  </Box>
-);
+import { Box, Drop } from 'grommet';
 
 const align = { top: 'bottom', left: 'left' };
 
 const SimpleDrop = () => {
-  const [showDrop, setShowDrop] = useState(false);
-  const [showLayer, setShowLayer] = useState(false);
-  const [showDropInModal, setShowDropInModal] = useState(false);
-  const [showRestrictFocusDrop, setShowRestrictFocusDrop] = useState(false);
-  const layerRef = useRef();
-  const buttonRef = useRef();
-  const layerButtonRef = useRef();
-  const restrictFocusButtonRef = useRef();
+  const targetRef = useRef();
 
-  const onClose = () => setShowLayer(false);
-
+  const [, setShowDrop] = useState(false);
   useEffect(() => {
-    const timer = setTimeout(() => buttonRef.current.focus(), 5000);
-    return () => clearTimeout(timer);
+    setShowDrop(true);
   }, []);
 
   return (
     // Uncomment <Grommet> lines when using outside of storybook
     // <Grommet theme={...}>
-    <>
-      <Box direction="row" pad="large" gap="small">
-        <DropButton
-          label="Drop button"
-          dropContent={<InteractiveContent />}
-          dropAlign={align}
-        />
-        <Button
-          ref={buttonRef}
-          label="Regular button"
-          onClick={() => setShowDrop(true)}
-        />
-        <Button
-          ref={layerRef}
-          label="Open layer"
-          onClick={() => setShowLayer(true)}
-        />
-        {showLayer && (
-          <Layer
-            position="right"
-            full="vertical"
-            modal
-            onClickOutside={onClose}
-            onEsc={onClose}
-          >
-            <Box
-              fill="vertical"
-              overflow="auto"
-              width="medium"
-              pad="medium"
-              gap="small"
-            >
-              {/* <Anchor href="#" label="testing anchor" /> */}
-              <DropButton
-                label="Another drop button"
-                dropContent={<InteractiveContent />}
-                dropAlign={align}
-              />
-              <Button
-                ref={layerButtonRef}
-                label="Another regular button"
-                onClick={() => setShowDropInModal(true)}
-              />
-              {showDropInModal && (
-                <Drop
-                  target={layerButtonRef.current}
-                  align={align}
-                  onClickOutside={() => setShowDropInModal(false)}
-                  onEsc={() => setShowDropInModal(false)}
-                >
-                  <InteractiveContent />
-                </Drop>
-              )}
-              <Button
-                ref={restrictFocusButtonRef}
-                label="A regular button with restrict focus"
-                onClick={() => setShowRestrictFocusDrop(true)}
-              />
-              {showRestrictFocusDrop && (
-                <Drop
-                  target={restrictFocusButtonRef.current}
-                  align={{ top: 'bottom' }}
-                  onClickOutside={() => setShowRestrictFocusDrop(false)}
-                  onEsc={() => setShowRestrictFocusDrop(false)}
-                  restrictFocus
-                >
-                  <InteractiveContent />
-                </Drop>
-              )}
-            </Box>
-          </Layer>
-        )}
+    <Box fill align="center" justify="center">
+      <Box
+        background="dark-2"
+        pad="medium"
+        align="center"
+        justify="start"
+        ref={targetRef}
+      >
+        Target
       </Box>
-      {showDrop && (
-        <Drop
-          target={buttonRef.current}
-          align={align}
-          onClickOutside={() => setShowDrop(false)}
-          onEsc={() => setShowDrop(false)}
-        >
-          <InteractiveContent />
+      {targetRef.current && (
+        <Drop align={align} target={targetRef.current}>
+          <Box pad="large">Drop Contents</Box>
         </Drop>
       )}
-    </>
+    </Box>
     // </Grommet>
   );
 };
+
 export const Simple = () => <SimpleDrop />;
 Simple.parameters = {
   chromatic: { disable: true },
