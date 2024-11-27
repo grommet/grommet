b663b3892fd945e0b57bdc4ebf8791e769a64945
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2024-11-27 11:42:21 -0700

    fixed some storybook accessibility violations

--- src/js/components/Data/stories/Cards.js
+++ src/js/components/Data/stories/Cards.js
@@ -1,28 +1,39 @@
 import React from 'react';
 
-import { Box, Card, CardBody, CardFooter, Cards, Data, Heading } from 'grommet';
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  Cards,
+  Data,
+  Heading,
+  Main,
+} from 'grommet';
 
 import { DATA } from '../../DataTable/stories/data';
 
 export const Example = () => (
   // Uncomment <Grommet> lines when using outside of storybook
   // <Grommet theme={...}>
-  <Box pad="large">
-    <Data data={DATA} toolbar>
-      <Cards size="medium">
-        {(item) => (
-          <Card key={item.name} pad="small">
-            <CardBody>
-              <Heading level={2} margin="none">
-                {item.name}
-              </Heading>
-            </CardBody>
-            <CardFooter>{item.location || '--'}</CardFooter>
-          </Card>
-        )}
-      </Cards>
-    </Data>
-  </Box>
+  <Main>
+    <Box pad="large">
+      <Data data={DATA} toolbar>
+        <Cards size="medium">
+          {(item) => (
+            <Card as="li" key={item.name} pad="small">
+              <CardBody>
+                <Heading level={2} margin="none">
+                  {item.name}
+                </Heading>
+              </CardBody>
+              <CardFooter>{item.location || '--'}</CardFooter>
+            </Card>
+          )}
+        </Cards>
+      </Data>
+    </Box>
+  </Main>
   // </Grommet>
 );
 
--- src/js/components/Grommet/stories/Vars.js
+++ src/js/components/Grommet/stories/Vars.js
@@ -4,12 +4,12 @@ import { grommet, Grommet, Box } from 'grommet';
 
 export const Vars = () => (
   <Grommet theme={grommet} cssVars>
-    <Box pad="medium" background="var(--accent-2)" gap="medium">
+    <Box pad="medium" background="var(--light-6)" gap="medium">
       <Box>
         Checkout Grommet variables, you can find them in the StyledGrommet DOM.
       </Box>
       <Box with>
-        For example, the background color in this Box is using var(--accent-2)
+        For example, the background color in this Box is using var(--light-6)
       </Box>
     </Box>
   </Grommet>
