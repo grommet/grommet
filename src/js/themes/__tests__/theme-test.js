6dc20c8f647c431927de4c391d238dbf5946c478
taysea <taylor.seamans@yahoo.com>
2025-01-24 09:30:05 -0800

    Remove unused

--- src/js/themes/__tests__/theme-test.js
+++ src/js/themes/__tests__/theme-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import 'jest-styled-components';
 
 import { hpe } from 'grommet-theme-hpe';
