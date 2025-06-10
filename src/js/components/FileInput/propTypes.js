45c2c3558c811ed91630199a8e10f1f8d3980a8e
sulaymon333 <tjsulaymon@gmail.com>
2025-06-10 22:01:43 +0100

    add sr message for alert icon, prop-types and ts-types updates

--- src/js/components/FileInput/propTypes.js
+++ src/js/components/FileInput/propTypes.js
@@ -11,8 +11,18 @@ if (process.env.NODE_ENV !== 'production') {
       dropPrompt: PropTypes.string,
       dropPromptMultiple: PropTypes.string,
       files: PropTypes.string,
+      maxFile: PropTypes.string,
       remove: PropTypes.string,
       removeAll: PropTypes.string,
+      maxSizeSingle: PropTypes.string,
+      maxSizeMultiple: PropTypes.shape({
+        singular: PropTypes.string,
+        plural: PropTypes.string,
+      }),
+      alert: PropTypes.shape({
+        maxSize: PropTypes.string,
+        maxFile: PropTypes.string,
+      }),
     }),
     multiple: PropTypes.oneOfType([
       PropTypes.bool,
