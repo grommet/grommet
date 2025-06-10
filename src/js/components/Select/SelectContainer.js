73a4aa0cce739cfaea805ae5c32630cee2f0f87e
britt6612 <brittany.archibeque@hpe.com>
2025-06-10 15:00:32 -0600

    make focus indicator visuable

--- src/js/components/Select/SelectContainer.js
+++ src/js/components/Select/SelectContainer.js
@@ -477,7 +477,6 @@ const SelectContainer = forwardRef(
                       aria-setsize={options.length}
                       aria-posinset={index + 1}
                       aria-selected={optionSelected}
-                      focusIndicator={false}
                       aria-disabled={optionDisabled || undefined}
                       plain={!child ? undefined : true}
                       align="start"
