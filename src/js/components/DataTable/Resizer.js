f4e06300a365ade54713ca7a2c23db7614e64123
taysea <taylor.seamans@yahoo.com>
2025-07-16 15:47:13 -0700

    Add touch events and drag alternative to resize

--- src/js/components/DataTable/Resizer.js
+++ src/js/components/DataTable/Resizer.js
@@ -6,18 +6,24 @@ import React, {
   useState,
 } from 'react';
 import styled from 'styled-components';
+import { Add, Subtract } from 'grommet-icons';
 
 import { Box } from '../Box';
 import { Button } from '../Button';
+import { DropButton } from '../DropButton';
 import { Keyboard } from '../Keyboard';
 import { Stack } from '../Stack';
 import { useThemeValue } from '../../utils/useThemeValue';
 import { MessageContext } from '../../contexts/MessageContext';
 
+// We determined 12 empirically as being wide enough to hit but
+// not too wide to cause false hits.
+const STEP = 12; // Used to determine the width change on resize
+
 // Added a temporary min-width of 2px here so that the element doesn't
 // end up with a width of 0px. This is a placeholder solution until we
 // revisit this in https://github.com/grommet/grommet/issues/7273
-const InteractionBox = styled(Button)`
+const InteractionBox = styled(DropButton)`
   min-width: 2px;
   cursor: col-resize;
   > * {
@@ -40,14 +46,24 @@ const Resizer = ({ onResize, property, headerText, messages }) => {
   const [start, setStart] = useState();
   const [width, setWidth] = useState();
   const ref = useRef();
+  const thRef = useRef();
   const { format } = useContext(MessageContext);
 
-  const onResizeStart = useCallback((event) => {
-    const clientX = event.touches ? event.touches[0].clientX : event.clientX;
+  useEffect(() => {
     if (ref.current) {
       let element = ref.current;
       // find TH parent
-      while (element && element.nodeName !== 'TH') element = element.parentNode;
+      while (element && element.nodeName !== 'TH') {
+        element = element.parentNode;
+      }
+      thRef.current = element;
+    }
+  }, []);
+
+  const onResizeStart = useCallback((event) => {
+    const clientX = event.touches ? event.touches[0].clientX : event.clientX;
+    if (thRef.current) {
+      const element = thRef.current;
       const rect = element.getBoundingClientRect();
       setStart(clientX);
       setWidth(rect.width);
@@ -58,9 +74,7 @@ const Resizer = ({ onResize, property, headerText, messages }) => {
   const onResizeMove = useCallback(
     (event) => {
       const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-      // We determined 12 empirically as being wide enough to hit but
-      // not too wide to cause false hits.
-      const nextWidth = Math.max(12, width + (clientX - start));
+      const nextWidth = Math.max(STEP, width + (clientX - start));
       onResize(property, nextWidth);
     },
     [onResize, property, start, width],
@@ -105,8 +119,8 @@ const Resizer = ({ onResize, property, headerText, messages }) => {
       let element = ref.current;
       while (element && element.nodeName !== 'TH') element = element.parentNode;
       const currentWidth = element.getBoundingClientRect().width;
-      // Used 12 here to align with the value set in onMouseMove
-      const delta = event.key === 'ArrowLeft' ? -12 : 12;
+      // Used STEP here to align with the value set in onMouseMove
+      const delta = event.key === 'ArrowLeft' ? -STEP : STEP;
       onResize(property, currentWidth + delta);
     },
     [onResize, property],
@@ -140,6 +154,36 @@ const Resizer = ({ onResize, property, headerText, messages }) => {
           onTouchStart={onResizeStart}
           onTouchMove={start !== undefined ? onResizeMove : undefined}
           onTouchEnd={start !== undefined ? onResizeEnd : undefined}
+          dropContent={
+            <Box direction="row" pad="xsmall">
+              <Button
+                icon={<Subtract />}
+                onClick={() => {
+                  if (thRef.current) {
+                    const element = thRef.current;
+                    const rect = element.getBoundingClientRect();
+                    const currentWidth = rect.width;
+                    const nextWidth = Math.max(STEP, currentWidth - STEP);
+                    onResize(property, nextWidth);
+                  }
+                }}
+                autoFocus
+              />
+              <Button
+                icon={<Add />}
+                onClick={() => {
+                  if (thRef.current) {
+                    const element = thRef.current;
+                    const rect = element.getBoundingClientRect();
+                    const currentWidth = rect.width;
+                    const nextWidth = Math.max(STEP, currentWidth + STEP);
+                    onResize(property, nextWidth);
+                  }
+                }}
+              />
+            </Box>
+          }
+          dropAlign={{ top: 'bottom' }}
         >
           <Box pad={{ vertical: 'small' }} border={border} />
         </InteractionBox>
