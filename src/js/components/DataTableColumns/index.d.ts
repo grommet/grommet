fb2c0a038bc46af3b0258d632f1ce6d29f473eaa
Siarhei Miksiuk <81712383+smiksiukhpe@users.noreply.github.com>
2025-08-21 10:49:42 -0700

    Update src/js/components/DataTableColumns/index.d.ts
    Co-authored-by: Brittany <42451602+britt6612@users.noreply.github.com>

--- src/js/components/DataTableColumns/index.d.ts
+++ src/js/components/DataTableColumns/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export interface DataTableColumnsProps {
   drop: boolean;
-  activePanel?: string;
+  activePanel?: 'orderColumns' | 'selectColumns';
   options: (
     | string
     | {
