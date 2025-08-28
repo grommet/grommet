5d5726c19c10aec2a5ed9ab23732636ac6242c2b
taysea <taylor.seamans@yahoo.com>
2025-08-28 10:21:01 -0700

    Add theme.tag.hover support

--- src/js/components/Tag/stories/OnClick.stories.js
+++ src/js/components/Tag/stories/OnClick.stories.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
-import { Box, Tag } from 'grommet';
+import { Box, Tag, Grommet } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
+import { deepMerge } from '../../../utils';
+
+const customTheme = deepMerge(hpe, {
+  tag: {
+    background: 'background-contrast',
+    border: {
+      color: 'transparent',
+    },
+    hover: {
+      background: 'background-contrast-hover',
+    },
+    round: 'small',
+  },
+});
 
 export const OnClick = () => {
   const onClick = () => {};
   return (
-    <Box pad="large" gap="medium" align="start">
-      <Tag name="name" value="value" onClick={onClick} />
-      <Tag value="value" onClick={onClick} />
-    </Box>
+    <Grommet theme={customTheme}>
+      <Box pad="large" gap="medium" align="start">
+        <Tag name="name" value="value" onClick={onClick} />
+        <Tag value="value" onRemove={() => {}} />
+        <Tag value="value" />
+      </Box>
+    </Grommet>
   );
 };
 
