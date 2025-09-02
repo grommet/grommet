695cd4e77adfcddc84de10490328394c53ac4eee
Brittany <42451602+britt6612@users.noreply.github.com>
2025-09-02 09:04:31 -0600

    Update OnClick.stories.js

--- src/js/components/Tag/stories/OnClick.stories.js
+++ src/js/components/Tag/stories/OnClick.stories.js
@@ -1,32 +1,14 @@
 import React from 'react';
 
-import { Box, Tag, Grommet } from 'grommet';
-import { hpe } from 'grommet-theme-hpe';
-import { deepMerge } from '../../../utils';
-
-const customTheme = deepMerge(hpe, {
-  tag: {
-    background: 'background-contrast',
-    border: {
-      color: 'transparent',
-    },
-    hover: {
-      background: 'background-contrast-hover',
-    },
-    round: 'small',
-  },
-});
+import { Box, Tag } from 'grommet';
 
 export const OnClick = () => {
   const onClick = () => {};
   return (
-    <Grommet theme={customTheme}>
-      <Box pad="large" gap="medium" align="start">
-        <Tag name="name" value="value" onClick={onClick} />
-        <Tag value="value" onRemove={() => {}} />
-        <Tag value="value" />
-      </Box>
-    </Grommet>
+    <Box pad="large" gap="medium" align="start">
+      <Tag name="name" value="value" onClick={onClick} />
+      <Tag value="value" onClick={onClick} />
+    </Box>
   );
 };
 
