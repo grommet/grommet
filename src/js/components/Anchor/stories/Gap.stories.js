4d44aa8e01716b99b170067e3e574c8654541ac6
britt6612 <brittany.archibeque@hpe.com>
2025-12-05 09:20:02 -0700

    add extra large gap

--- src/js/components/Anchor/stories/Gap.stories.js
+++ src/js/components/Anchor/stories/Gap.stories.js
@@ -9,6 +9,7 @@ const GapAnchor = () => (
     <Anchor icon={<Upload />} label="Small Gap" href="#" gap="small" />
     <Anchor icon={<Upload />} label="Medium Gap" href="#" gap="medium" />
     <Anchor icon={<Upload />} label="Large Gap" href="#" gap="large" />
+    <Anchor icon={<Upload />} label="xLarge Gap" href="#" gap="xlarge" />
     <Anchor icon={<Upload />} label="5px Gap" href="#" gap="5px" />
   </Box>
 );
