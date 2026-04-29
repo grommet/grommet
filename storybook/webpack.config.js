9f0180664bc7a519e0cceec08dcc33943e218dbe
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2026-04-29 16:17:31 -0600

    storybook upgrade phase 1

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -2,23 +2,8 @@ const path = require('path');
 
 module.exports = async ({ config }) => {
   config.module.rules.push({
-    test: /\.stories\.js$|(\/|\\)stories(\/|\\).*\.js$/,
-    use: [
-      {
-        loader: require.resolve('@storybook/source-loader'),
-        options: {
-          prettierConfig: {
-            parser: 'babel',
-          },
-        },
-      },
-    ],
-    enforce: 'pre',
-  });
-
-  config.module.rules.push({
     test: /stories(\\|\/).*\.(ts|tsx)$/,
-    use: ['ts-loader', '@storybook/source-loader'],
+    use: ['ts-loader'],
   });
 
   // eslint-disable-next-line no-param-reassign
