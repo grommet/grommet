6e3d6cf5508ab3c85ea47d04ace70f40fdfbdee7
sulaymon333 <tjsulaymon@gmail.com>
2026-08-06 22:30:33 +0100

    chore: add SPDX license headers

--- src/js/components/TimeInput/useSectionedTimeField.js
+++ src/js/components/TimeInput/useSectionedTimeField.js
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: © Hewlett Packard Enterprise Development LP
+// SPDX-License-Identifier: Apache-2.0
 /* eslint-disable no-nested-ternary */
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
