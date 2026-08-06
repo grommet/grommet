2075778b57588d4e291ff0fa8a13921282a7e897
sulaymon333 <tjsulaymon@gmail.com>
2026-08-06 16:36:04 +0100

    chore: add SPDX license headers

--- src/js/components/DateTimeInput/stories/CustomThemed/Custom.stories.tsx
+++ src/js/components/DateTimeInput/stories/CustomThemed/Custom.stories.tsx
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: © Hewlett Packard Enterprise Development LP
+// SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
