2b555f01c011f038b91819f4fc188884e9334133
sulaymon333 <tjsulaymon@gmail.com>
2026-08-17 13:41:16 +0100

    New chore(license): add SPDX headers (4/4)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: © Hewlett Packard Enterprise Development LP
+// SPDX-License-Identifier: Apache-2.0
 global.console.warn = (message) => {
   throw message;
 };
--- tools/grommet-babel-preset-es2015.js
+++ tools/grommet-babel-preset-es2015.js
@@ -1,12 +1,17 @@
+// SPDX-FileCopyrightText: © Hewlett Packard Enterprise Development LP
+// SPDX-License-Identifier: Apache-2.0
 const ENV = process.env.BABEL_ENV;
 
 module.exports = function () {
   return {
     presets: [
-      ['@babel/preset-env', {
-        loose: true,
-        modules: ENV === 'es6' ? false : 'commonjs',
-      }],
+      [
+        '@babel/preset-env',
+        {
+          loose: true,
+          modules: ENV === 'es6' ? false : 'commonjs',
+        },
+      ],
     ],
   };
 };
