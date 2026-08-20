c79155359f257d325f2087389961c83a03a39a4a
Sean Hoang Hill <seanhillweb@gmail.com>
2026-08-20 15:04:09 -0500

    feat(TextInput): add copy affordance

---
+++ src/js/components/TextInput/stories/Copy.stories.js
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: © Hewlett Packard Enterprise Development LP
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import { Box, Text, TextInput } from 'grommet';
+
+export const Copy = () => {
+  const [value, setValue] = React.useState('Editable value');
+  const [status, setStatus] = React.useState('');
+
+  return (
+    // Uncomment <Grommet> lines when using outside of storybook
+    // <Grommet theme={...}>
+    <Box fill align="center" justify="start" pad="large" gap="medium">
+      <Box width="medium" gap="small">
+        <TextInput
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          copy
+          aria-label="Editable value"
+        />
+        <TextInput
+          value="Custom copy handler"
+          copy
+          onClickCopy={() => setStatus('Custom copy handler called')}
+          aria-label="Custom copy handler"
+        />
+        {status && <Text>{status}</Text>}
+      </Box>
+    </Box>
+    // </Grommet>
+  );
+};
+
+export default {
+  title: 'Input/TextInput/Copy',
+};
