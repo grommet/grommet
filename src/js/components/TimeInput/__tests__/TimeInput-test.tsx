2c27fdfcffc7d9ee8a44a42f6cbf759913a28f28
Brittany <42451602+britt6612@users.noreply.github.com>
2026-09-08 14:05:21 -0600

    TimeInput - wire up the text weight (#8100)
    * wire up the text weight
    
    * make adjustments
    
    ---------
    
    Co-authored-by: Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>

--- src/js/components/TimeInput/__tests__/TimeInput-test.tsx
+++ src/js/components/TimeInput/__tests__/TimeInput-test.tsx
@@ -1971,6 +1971,9 @@ describe('TimeInput', () => {
             selected: {
               background: '#0000FF',
               color: '#FFFFFF',
+              text: {
+                weight: 'bold',
+              },
               hover: {
                 background: '#00FF00',
               },
--- src/js/themes/base.js
+++ src/js/themes/base.js
@@ -2700,7 +2700,7 @@ export const generate = (baseSpacing = 24, scale = 6) => {
             background: 'selected',
             color: 'white',
             text: {
-              weight: 'medium',
+              weight: 500,
             },
             hover: {
               // background: undefined,
